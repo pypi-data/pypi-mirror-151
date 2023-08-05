9fc5a0d113ea950eaa00ad95387f2b1ddc9032c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:37 +0000

    Add package flask-gatekeeper/1.0.0/flask_gatekeeper-1.0.0.tar.gz

---
+++ packages/flask-gatekeeper/flask_gatekeeper-1.0.0.tar.gz/flask_gatekeeper-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from flask_gatekeeper import __version__
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask_gatekeeper",
+    version=__version__,
+    author="k0rventen",
+    python_requires='>=3.7',
+    description="A (very) simple banning & rate limiting extension for Flask.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/k0rventen/flask-gatekeeper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'Flask>=1.1.*',
+    ],
+)
