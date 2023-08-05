fa99feb694e17852a167c0f9bf4e047f864d30a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:24 +0000

    Add package All-In-1-sathishbilli/0.0.2/All_In_1-sathishbilli-0.0.2.tar.gz

---
+++ packages/All-In-1-sathishbilli/All_In_1-sathishbilli-0.0.2.tar.gz/All_In_1-sathishbilli-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+USERNAME="sathishbilli"
+PROJECT_NAME="All_In_1"
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USERNAME}",
+    version="0.0.2",
+    author=f"{USERNAME}",
+    author_email="sathishmahi398@gmail.com",
+    description="this is a basic function to machine learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USERNAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USERNAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "matplotlib",
+        "seaborn",
+        "numpy",
+        "pandas", 
+        "statsmodels",
+        "sklearn"
+
+    ]
+)
