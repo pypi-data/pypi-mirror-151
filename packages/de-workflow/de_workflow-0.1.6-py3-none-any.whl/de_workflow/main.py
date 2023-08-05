297bc36ffc022d45568f44383e1b3b043ac2024f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:58 +0000

    Add package de-workflow/0.1.6/de_workflow-0.1.6-py3-none-any.whl

---
+++ packages/de-workflow/de_workflow-0.1.6-py3-none-any.whl/de_workflow/main.py
@@ -0,0 +1,63 @@
+from typing import Optional
+from rich import print
+import click
+
+import pathlib
+from . import utils
+
+app = click.Group()
+
+
+@app.command()
+@click.argument(
+    "file_name",
+    type=click.Path(exists=True),
+    required=True,
+)
+@click.argument("id-column", type=click.STRING, required=True)
+@click.argument(
+    "target-columns",
+    type=click.STRING,
+    required=True,
+    nargs=2,
+)
+@click.option(
+    "--report/--no-report",
+    default=True,
+    is_flag=True,
+    help="Analyze the data and generate a report",
+)
+@click.option(
+    "--live/--no-live",
+    default=True,
+    is_flag=True,
+    help="Live mode, use our live template.",
+)
+@click.option(
+    "--search-file",
+    default=None,
+    type=click.Path(exists=True),
+    required=False,
+    help="Custom search file. Must provide if live is false.",
+)
+@click.option(
+    "--algorithm",
+    default="levenshtein",
+    type=click.Choice(["levenshtein", "osa"]),
+    required=False,
+    help="Algorithm to use for matching.",
+)
+def execute(
+    file_name: pathlib.Path,
+    id_column: str,
+    target_columns: tuple[str, str],
+    report: bool,
+    live: bool,
+    search_file: Optional[str],
+    algorithm: str,
+):
+    print("[cyan]Running wrapper program...")
+    utils.run(
+        file_name, id_column, target_columns, report, live, search_file, algorithm
+    )
+    print("[green]Finished wrapper program!")
