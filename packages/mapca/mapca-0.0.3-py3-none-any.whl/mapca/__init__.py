e1b8e99439ada60b08941cf05a116d4df096ee75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:10 +0000

    Add package mapca/0.0.3/mapca-0.0.3-py3-none-any.whl

---
+++ packages/mapca/mapca-0.0.3-py3-none-any.whl/mapca/__init__.py
@@ -0,0 +1,23 @@
+"""
+mapca: A Python implementation of the moving average principal components analysis methods from
+GIFT.
+"""
+
+from ._version import get_versions
+from .due import Doi, due
+from .mapca import MovingAveragePCA, ma_pca
+
+__version__ = get_versions()["version"]
+
+import warnings
+
+# cmp is not used, so ignore nipype-generated warnings
+warnings.filterwarnings("ignore", r"cmp not installed")
+
+__all__ = [
+    "ma_pca",
+    "MovingAveragePCA",
+    "__version__",
+]
+
+del get_versions
