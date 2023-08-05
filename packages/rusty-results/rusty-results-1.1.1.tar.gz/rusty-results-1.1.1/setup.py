fb822e7c302d90f5d8cbb7ff3f16639640a08970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:02 +0000

    Add package rusty-results/1.1.1/rusty-results-1.1.1.tar.gz

---
+++ packages/rusty-results/rusty-results-1.1.1.tar.gz/rusty-results-1.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rusty-results",
+    version="1.1.1",
+    author="Daniel Sanchez Quiros, Antonio Jose Checa Bustos",
+    author_email="sanchez.quiros.daniel@gmail.com, antonio.checa.bustos@gmail.com",
+    description="Rust inspired Result and Option types",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danielSanchezQ/rusty_results",
+    package_data={"rusty_results": ["py.typed"]},
+    packages=setuptools.find_packages(exclude=("*tests*",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        "pydantic": ["pydantic"]
+    },
+    python_requires='>=3.7'
+)
