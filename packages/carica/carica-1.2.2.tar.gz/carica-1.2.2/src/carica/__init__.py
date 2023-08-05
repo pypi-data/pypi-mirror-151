d0ff9b92a1b34715b1fec93dd1d2be408c837cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:51 +0000

    Add package carica/1.2.2/carica-1.2.2.tar.gz

---
+++ packages/carica/carica-1.2.2.tar.gz/carica-1.2.2/src/carica/__init__.py
@@ -0,0 +1,6 @@
+from .carica import makeDefaultCfg, loadCfg, BadTypeBehaviour, BadTypeHandling, ErrorHandling
+from .models import *
+from .interface import *
+from . import exceptions
+
+__version__ = '1.2.2'
