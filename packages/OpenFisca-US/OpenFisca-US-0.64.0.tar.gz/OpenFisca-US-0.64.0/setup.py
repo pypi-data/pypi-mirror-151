7f91db4b0d0c4c4197f6a361ad1d0b07f6a7878b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:50 +0000

    Add package OpenFisca-US/0.64.0/OpenFisca-US-0.64.0.tar.gz

---
+++ packages/OpenFisca-US/OpenFisca-US-0.64.0.tar.gz/OpenFisca-US-0.64.0/setup.py
@@ -0,0 +1,66 @@
+"""This file contains your country package's metadata and dependencies."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="OpenFisca-US",
+    version="0.64.0",
+    author="Nikhil Woodruff",
+    author_email="nikhil@policyengine.org",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+    description="OpenFisca tax and benefit system for the US",
+    keywords="benefit microsimulation social tax",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    url="https://github.com/PolicyEngine/openfisca-us",
+    include_package_data=True,  # Will read MANIFEST.in
+    data_files=[
+        (
+            "share/openfisca/openfisca-country-template",
+            ["CHANGELOG.md", "LICENSE", "README.md"],
+        ),
+    ],
+    install_requires=[
+        "OpenFisca-Core[web-api] >= 35.0.0",
+        "microdf_python",
+        "pandas",
+        "tqdm",
+        "requests",
+        "OpenFisca-Tools>=0.8.0,<1.0.0",
+        "pyyaml",
+        "pandas",
+        "pathlib",
+        "tables",
+        "h5py",
+        "synthimpute",
+        "pytest",
+        "pytest-dependency",
+    ],
+    extras_require={
+        "dev": [
+            "autopep8 >= 1.5.4, < 2.0.0",
+            "black",
+            "wheel",
+            "pytest",
+            "setuptools",
+            "jupyter-book",
+            "coverage",
+            "plotly",
+            "yaml-changelog>=0.1.6",
+        ],
+    },
+    python_requires=">=3.7,<3.8",
+    entry_points={
+        "console_scripts": [
+            "openfisca-us = openfisca_us.tools.cli:main",
+            "openfisca-us-data = openfisca_us.data.cli:cli",
+        ],
+    },
+    packages=find_packages(),
+)
