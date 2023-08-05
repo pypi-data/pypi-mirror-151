3a07593d28ba5728ed5653838f76f293f91a9320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:06 +0000

    Add package torchx-nightly/2022.5.16/torchx_nightly-2022.5.16-py3-none-any.whl

---
+++ packages/torchx-nightly/torchx_nightly-2022.5.16-py3-none-any.whl/torchx/schedulers/ray/ray_driver.py
@@ -0,0 +1,167 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import json
+import logging
+import os
+import subprocess
+import sys
+from typing import Dict, List, Optional, Tuple
+
+import ray
+from ray.train.utils import get_address_and_port
+from ray.util.placement_group import PlacementGroup
+
+# Hack to make code work for tests as well as running ray job.
+# For tests the `torchx.schedulers.ray.ray_common` import must be used
+# For running ray jobs `ray_common` import must be used
+try:
+    # pyre-ignore[21]: Could not find a module corresponding to import `ray_common`
+    from ray_common import RayActor
+except ModuleNotFoundError:
+    from torchx.schedulers.ray.ray_common import RayActor
+
+_logger: logging.Logger = logging.getLogger(__name__)
+_logger.setLevel(logging.getLevelName(os.environ.get("LOGLEVEL", "INFO")))
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+@ray.remote
+class CommandActor:  # pragma: no cover
+    def __init__(self, command: str, env: Dict[str, str]) -> None:
+        self.cmd: List[str] = command.split(" ")
+        self.env = env
+        self.master_addr: Optional[str] = None
+        self.master_port: Optional[int] = None
+
+    def exec_module(self) -> None:
+        if self.master_addr is None or self.master_port is None:
+            raise RuntimeError(
+                "Either MASTER_ADDR or MASTER_PORT are not set. This is most likely bug in torchx"
+                "Open issue at https://github.com/pytorch/torchx"
+            )
+        worker_evn = {}
+        worker_evn.update(os.environ)
+        worker_evn.update(self.env)
+        worker_evn["MASTER_ADDR"] = self.master_addr
+        worker_evn["MASTER_PORT"] = str(self.master_port)
+        popen = subprocess.Popen(self.cmd, env=worker_evn)
+
+        returncode = popen.wait()
+        _logger.info(f"Finished with code {returncode}")
+
+        if returncode != 0:
+            raise RuntimeError(f"exec_module failed with return code {returncode}")
+
+    def get_actor_address_and_port(self) -> Tuple[str, int]:
+        return get_address_and_port()
+
+    def set_address_and_port(self, address: str, port: int) -> None:
+        self.master_addr = address
+        self.master_port = port
+
+
+# pyre-ignore[11]
+def load_actor_json(filename: str) -> List["RayActor"]:
+    with open(filename) as f:
+        actors: List[RayActor] = []
+        # Yes this is gross but it works
+        actor_dict = json.load(f)
+        actor_dict = json.loads(actor_dict)
+        for actor in actor_dict:
+            actors.append(RayActor(**actor))
+        return actors
+
+
+def create_placement_groups(actors: List[RayActor]) -> List[PlacementGroup]:
+    pgs: List[PlacementGroup] = []
+    for actor in actors:
+        bundle = {"CPU": actor.num_cpus, "GPU": actor.num_gpus}
+        bundles = [bundle] * actor.num_replicas
+
+        # To change the strategy type
+        # refer to available options here https://docs.ray.io/en/latest/placement-group.html#pgroup-strategy
+        pg = ray.util.placement_group(bundles, strategy="SPREAD")
+        pgs.append(pg)
+
+        _logger.info("Waiting for placement group to start.")
+        ready = pg.wait(timeout_seconds=100)
+
+        if not ready:  # pragma: no cover
+            raise TimeoutError(
+                "Placement group creation timed out. Make sure "
+                "your cluster either has enough resources or use "
+                "an autoscaling cluster. Current resources "
+                "available: {}, resources requested by the "
+                "placement group: {}".format(ray.available_resources(), pg.bundle_specs)
+            )
+    return pgs
+
+
+def create_command_actors(
+    actors: List[RayActor], pgs: List[PlacementGroup]
+) -> List[CommandActor]:
+    cmd_actors: List[CommandActor] = []
+    for i in range(len(actors)):
+        world_size = actors[i].num_replicas
+        actor_group = []
+
+        for rank in range(world_size):
+
+            # Environment variables for distributed training
+            rank_env = {
+                "WORLD_SIZE": str(world_size),
+                "RANK": str(rank),
+            }
+
+            actor_and_rank_env = {**actors[i].env, **rank_env}
+
+            actor_group.append(
+                CommandActor.options(  # pyre-ignore[16]
+                    placement_group=pgs[i],
+                    num_cpus=actors[i].num_cpus,
+                    num_gpus=actors[i].num_gpus,
+                ).remote(actors[i].command, actor_and_rank_env)
+            )
+
+            rank_0_address, rank_0_port = ray.get(
+                actor_group[0].get_actor_address_and_port.remote()
+            )
+
+            for actor in actor_group:
+                ray.get(actor.set_address_and_port.remote(rank_0_address, rank_0_port))
+
+            cmd_actors.extend(actor_group)
+
+    return cmd_actors
+
+
+def main() -> None:  # pragma: no cover
+    actors: List[RayActor] = load_actor_json("actors.json")
+    # pyre-fixme[16]: Module `worker` has no attribute `init`.
+    ray.init(address="auto", namespace="torchx-ray")
+    pgs: List[PlacementGroup] = create_placement_groups(actors)
+    command_actors: List[CommandActor] = create_command_actors(actors, pgs)
+
+    active_workers = [
+        command_actor.exec_module.remote()  # pyre-ignore
+        for command_actor in command_actors
+    ]
+
+    # Await return result of remote ray function
+    while len(active_workers) > 0:
+        _logger.info(f"running ray.wait on {active_workers}")
+
+        # pyre-fixme[16]: Module `worker` has no attribute `wait`.
+        completed_workers, active_workers = ray.wait(active_workers)
+        # If a failure occurs the ObjectRef will be marked as completed.
+        # Calling ray.get will expose the failure as a RayActorError.
+        for object_ref in completed_workers:
+            ray.get(object_ref)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/torchx-nightly/torchx_nightly-2022.5.16-py3-none-any.whl/torchx/schedulers/ray_scheduler.py
@@ -0,0 +1,374 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import dataclasses
+import json
+import logging
+import os
+import time
+from dataclasses import dataclass, field
+from datetime import datetime
+from shutil import copy2, copytree, rmtree
+from tempfile import mkdtemp
+from typing import Any, cast, Dict, List, Mapping, Optional, Set, Type  # noqa
+
+from torchx.schedulers.api import (
+    AppDryRunInfo,
+    AppState,
+    DescribeAppResponse,
+    Scheduler,
+    split_lines,
+    Stream,
+)
+from torchx.schedulers.ids import make_unique
+from torchx.schedulers.ray.ray_common import RayActor
+from torchx.specs import (
+    AppDef,
+    CfgVal,
+    macros,
+    NONE,
+    ReplicaStatus,
+    Role,
+    RoleStatus,
+    runopts,
+)
+
+
+try:
+    from ray.autoscaler import sdk as ray_autoscaler_sdk
+    from ray.dashboard.modules.job.common import JobStatus
+    from ray.dashboard.modules.job.sdk import JobSubmissionClient
+
+    _has_ray = True
+
+except ImportError:
+    _has_ray = False
+
+
+def has_ray() -> bool:
+    """Indicates whether Ray is installed in the current Python environment."""
+    return _has_ray
+
+
+if _has_ray:
+
+    _logger: logging.Logger = logging.getLogger(__name__)
+
+    _ray_status_to_torchx_appstate: Dict[JobStatus, AppState] = {
+        JobStatus.PENDING: AppState.PENDING,
+        JobStatus.RUNNING: AppState.RUNNING,
+        JobStatus.SUCCEEDED: AppState.SUCCEEDED,
+        JobStatus.FAILED: AppState.FAILED,
+        JobStatus.STOPPED: AppState.CANCELLED,
+    }
+
+    class _EnhancedJSONEncoder(json.JSONEncoder):
+        def default(self, o: RayActor):  # pyre-ignore[3]
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            return super().default(o)
+
+    def serialize(
+        actors: List[RayActor], dirpath: str, output_filename: str = "actors.json"
+    ) -> None:
+        actors_json = json.dumps(actors, cls=_EnhancedJSONEncoder)
+        with open(os.path.join(dirpath, output_filename), "w") as tmp:
+            json.dump(actors_json, tmp)
+
+    @dataclass
+    class RayJob:
+        """Represents a job that should be run on a Ray cluster.
+
+        Attributes:
+            app_id:
+                The unique ID of the application (a.k.a. job).
+            cluster_config_file:
+                The Ray cluster configuration file.
+            cluster_name:
+                The cluster name to use.
+            dashboard_address:
+                The existing dashboard IP address to connect to
+            working_dir:
+            The working directory to copy to the cluster
+            requirements:
+                The libraries to install on the cluster per requirements.txt
+            actors:
+                The Ray actors which represent the job to be run. This attribute is
+                dumped to a JSON file and copied to the cluster where `ray_main.py`
+                uses it to initiate the job.
+        """
+
+        app_id: str
+        cluster_config_file: Optional[str] = None
+        cluster_name: Optional[str] = None
+        dashboard_address: Optional[str] = None
+        working_dir: Optional[str] = None
+        requirements: Optional[str] = None
+        actors: List[RayActor] = field(default_factory=list)
+
+    class RayScheduler(Scheduler):
+        """
+        **Config Options**
+
+        .. runopts::
+            class: torchx.schedulers.ray_scheduler.RayScheduler
+
+        **Compatibility**
+
+        .. compatibility::
+            type: scheduler
+            features:
+                cancel: true
+                logs: true
+                distributed: true
+                describe: |
+                    Partial support. RayScheduler will return job status but
+                    does not provide the complete original AppSpec.
+                workspaces: false
+                mounts: false
+
+        """
+
+        def __init__(self, session_name: str) -> None:
+            super().__init__("ray", session_name)
+
+        # TODO: Add address as a potential CLI argument after writing ray.status() or passing in config file
+        def run_opts(self) -> runopts:
+            opts = runopts()
+            opts.add(
+                "cluster_config_file",
+                type_=str,
+                required=False,
+                help="Use CLUSTER_CONFIG_FILE to access or create the Ray cluster.",
+            )
+            opts.add(
+                "cluster_name",
+                type_=str,
+                help="Override the configured cluster name.",
+            )
+            opts.add(
+                "dashboard_address",
+                type_=str,
+                required=False,
+                default="127.0.0.1:8265",
+                help="Use ray status to get the dashboard address you will submit jobs against",
+            )
+            opts.add(
+                "working_dir",
+                type_=str,
+                help="Copy the the working directory containing the Python scripts to the cluster.",
+            )
+            opts.add("requirements", type_=str, help="Path to requirements.txt")
+            return opts
+
+        def schedule(self, dryrun_info: AppDryRunInfo[RayJob]) -> str:
+            cfg: RayJob = dryrun_info.request
+
+            # Create serialized actors for ray_driver.py
+            actors = cfg.actors
+            dirpath = mkdtemp()
+            serialize(actors, dirpath)
+
+            job_submission_addr: str = ""
+            if cfg.cluster_config_file:
+                job_submission_addr = ray_autoscaler_sdk.get_head_node_ip(
+                    cfg.cluster_config_file
+                )  # pragma: no cover
+            elif cfg.dashboard_address:
+                job_submission_addr = cfg.dashboard_address
+            else:
+                raise RuntimeError(
+                    "Either `dashboard_address` or `cluster_config_file` must be specified"
+                )
+
+            # 0. Create Job Client
+            client: JobSubmissionClient = JobSubmissionClient(
+                f"http://{job_submission_addr}"
+            )
+
+            # 1. Copy working directory
+            if cfg.working_dir:
+                copytree(cfg.working_dir, dirpath, dirs_exist_ok=True)
+
+            # 2. Copy Ray driver utilities
+            current_directory = os.path.dirname(os.path.abspath(__file__))
+            copy2(os.path.join(current_directory, "ray", "ray_driver.py"), dirpath)
+            copy2(os.path.join(current_directory, "ray", "ray_common.py"), dirpath)
+
+            # 3. Parse requirements.txt
+            reqs: List[str] = []
+            if cfg.requirements:  # pragma: no cover
+                with open(cfg.requirements) as f:
+                    for line in f:
+                        reqs.append(line.strip())
+
+            # 4. Submit Job via the Ray Job Submission API
+            try:
+                job_id: str = client.submit_job(
+                    # we will pack, hash, zip, upload, register working_dir in GCS of ray cluster
+                    # and use it to configure your job execution.
+                    entrypoint="python ray_driver.py",
+                    runtime_env={"working_dir": dirpath, "pip": reqs},
+                )
+
+            except Exception:
+                raise
+
+            finally:
+                rmtree(dirpath)
+
+            # Encode job submission client in job_id
+            return f"{job_submission_addr}-{job_id}"
+
+        def _submit_dryrun(
+            self, app: AppDef, cfg: Mapping[str, CfgVal]
+        ) -> AppDryRunInfo[RayJob]:
+            app_id = make_unique(app.name)
+            requirements: Optional[str] = cast(Optional[str], cfg.get("requirements"))
+
+            cluster_cfg = cfg.get("cluster_config_file")
+            if cluster_cfg:
+                if not isinstance(cluster_cfg, str) or not os.path.isfile(cluster_cfg):
+                    raise ValueError(
+                        "The cluster configuration file must be a YAML file."
+                    )
+
+                job: RayJob = RayJob(
+                    app_id,
+                    cluster_cfg,
+                    requirements=requirements,
+                )
+
+            else:  # pragma: no cover
+                dashboard_address = cast(Optional[str], cfg.get("dashboard_address"))
+                job: RayJob = RayJob(
+                    app_id=app_id,
+                    dashboard_address=dashboard_address,
+                    requirements=requirements,
+                )
+            job.cluster_name = cast(Optional[str], cfg.get("cluster_name"))
+            job.working_dir = cast(Optional[str], cfg.get("working_dir"))
+
+            for role in app.roles:
+                # Replace the ${img_root}, ${app_id}, and ${replica_id} placeholders
+                # in arguments and environment variables.
+                role = macros.Values(
+                    img_root=role.image,
+                    app_id=app_id,
+                    replica_id="${rank}",
+                    rank0_env="MASTER_ADDR",
+                ).apply(role)
+
+                actor = RayActor(
+                    name=role.name,
+                    command=" ".join([role.entrypoint] + role.args),
+                    env=role.env,
+                    num_replicas=max(1, role.num_replicas),
+                    num_cpus=max(1, role.resource.cpu),
+                    num_gpus=max(0, role.resource.gpu),
+                )
+
+                job.actors.append(actor)
+
+            return AppDryRunInfo(job, repr)
+
+        def _validate(self, app: AppDef, scheduler: str) -> None:
+            if scheduler != "ray":
+                raise ValueError(
+                    f"An unknown scheduler backend '{scheduler}' has been passed to the Ray scheduler."
+                )
+
+            if app.metadata:
+                _logger.warning("The Ray scheduler does not use metadata information.")
+
+            for role in app.roles:
+                if role.resource.capabilities:
+                    _logger.warning(
+                        "The Ray scheduler does not support custom resource capabilities."
+                    )
+                    break
+
+            for role in app.roles:
+                if role.port_map:
+                    _logger.warning("The Ray scheduler does not support port mapping.")
+                    break
+
+        def wait_until_finish(self, app_id: str, timeout: int = 30) -> None:
+            """
+            ``wait_until_finish`` waits until the specified job has finished
+            with a given timeout. This is intended for testing. Programmatic
+            usage should use the runner wait method instead.
+            """
+            addr, app_id = app_id.split("-")
+
+            client = JobSubmissionClient(f"http://{addr}")
+            start = time.time()
+            while time.time() - start <= timeout:
+                status_info = client.get_job_status(app_id)
+                status = status_info
+                if status in {JobStatus.SUCCEEDED, JobStatus.STOPPED, JobStatus.FAILED}:
+                    break
+                time.sleep(1)
+
+        def _cancel_existing(self, app_id: str) -> None:  # pragma: no cover
+            addr, app_id = app_id.split("-")
+            client = JobSubmissionClient(f"http://{addr}")
+            client.stop_job(app_id)
+
+        def describe(self, app_id: str) -> Optional[DescribeAppResponse]:
+            addr, app_id = app_id.split("-")
+            client = JobSubmissionClient(f"http://{addr}")
+            job_status_info = client.get_job_status(app_id)
+            state = _ray_status_to_torchx_appstate[job_status_info]
+            roles = [Role(name="ray", num_replicas=-1, image="<N/A>")]
+
+            # get ip_address and put it in hostname
+
+            roles_statuses = [
+                RoleStatus(
+                    role="ray",
+                    replicas=[
+                        ReplicaStatus(
+                            id=0,
+                            role="ray",
+                            hostname=NONE,
+                            state=state,
+                        )
+                    ],
+                )
+            ]
+            return DescribeAppResponse(
+                app_id=app_id,
+                state=state,
+                msg=job_status_info,
+                roles_statuses=roles_statuses,
+                roles=roles,
+            )
+
+        def log_iter(
+            self,
+            app_id: str,
+            role_name: Optional[str] = None,
+            k: int = 0,
+            regex: Optional[str] = None,
+            since: Optional[datetime] = None,
+            until: Optional[datetime] = None,
+            should_tail: bool = False,
+            streams: Optional[Stream] = None,
+        ) -> List[str]:
+            # TODO: support regex, tailing, streams etc..
+            addr, app_id = app_id.split("-")
+            client: JobSubmissionClient = JobSubmissionClient(f"http://{addr}")
+            logs: str = client.get_job_logs(app_id)
+            return split_lines(logs)
+
+    def create_scheduler(session_name: str, **kwargs: Any) -> RayScheduler:
+        if not has_ray():  # pragma: no cover
+            raise RuntimeError(
+                "Ray is not installed in the current Python environment."
+            )
+
+        return RayScheduler(session_name=session_name)
