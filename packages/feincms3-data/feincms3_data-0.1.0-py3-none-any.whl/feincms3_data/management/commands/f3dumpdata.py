9ecbe8a73c53e429d497894ffecd366d5f57c7d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:54 +0000

    Add package feincms3-data/0.1.0/feincms3_data-0.1.0-py3-none-any.whl

---
+++ packages/feincms3-data/feincms3_data-0.1.0-py3-none-any.whl/feincms3_data/management/commands/f3dumpdata.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from feincms3_data.data import dump_specs, specs
+
+
+SPECS = specs()
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "args",
+            nargs="*",
+            help=f"Model specs which should be dumped ({', '.join(sorted(SPECS))}).",
+        )
+        parser.add_argument(
+            "--all",
+            action="store_true",
+            dest="all_specs",
+            help="Dump all model specs.",
+        )
+
+    def handle(self, *args, **options):
+        if options["all_specs"]:
+            args = SPECS.keys()
+        specs = []
+        for arg in args:
+            try:
+                model, sep, args = arg.partition(":")
+            except KeyError:
+                raise CommandError(f'Invalid spec "{arg}"')
+            else:
+                specs.extend(SPECS[model](args))
+        self.stdout.write(dump_specs(specs))
