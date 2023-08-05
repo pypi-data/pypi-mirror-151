34ff76dafc10912a96363814bbf0232e483229fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:10 +0000

    Add package images-utils-978/1.0.3/images-utils-978-1.0.3.tar.gz

---
+++ packages/images-utils-978/images-utils-978-1.0.3.tar.gz/images-utils-978-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from images_utils_978 import __version__
+
+setup(
+    name='images-utils-978',
+    version=__version__,
+
+    url='https://github.com/seppia978/images_utils',
+    author='seppia978',
+    author_email='samuele.poppi@uninmore.it',
+
+    py_modules=find_packages(),
+    packages=find_packages(),
+)
