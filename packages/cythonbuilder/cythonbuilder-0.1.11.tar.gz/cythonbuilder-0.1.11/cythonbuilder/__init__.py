295d6de886395cd44a9ed26668765492e690d590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:17 +0000

    Add package cythonbuilder/0.1.11/cythonbuilder-0.1.11.tar.gz

---
+++ packages/cythonbuilder/cythonbuilder-0.1.11.tar.gz/cythonbuilder-0.1.11/cythonbuilder/__init__.py
@@ -0,0 +1,9 @@
+# import code from your package here to make them available outside the module like:
+from cythonbuilder.cython_builder import cy_init, cy_clean, cy_build, cy_list, cy_interface
+from cythonbuilder.logs import LoggerSettings, create_logger, set_logger_debug_mode
+
+__version__ = "0.1.11"
+__last_build_datetime__ ="2022-05-17 11:20:16"
+__last_publish_version__ ="0.1.10"
+__last_publish_datetime__ ="2022-05-16 17:00:17"
+
