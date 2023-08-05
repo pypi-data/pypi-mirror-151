73bba635597af63c643e9578c3e38421921bf591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:46 +0000

    Add package kornet/0.0.17/kornet-0.0.17.tar.gz

---
+++ packages/kornet/kornet-0.0.17.tar.gz/kornet-0.0.17/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+all_packages = find_packages()
+
+setup(
+    name="kornet",
+    packages=all_packages,
+    version="0.0.17",
+    license="MIT",
+    description="A library for mass execution of ssh commands on remote machines fleet.",
+    author="Vladimir Semenov",
+    author_email="subatiq@gmail.com",
+    url="https://github.com/subatiq/kornet",
+    download_url="https://github.com/subatiq/kornet/archive/refs/tags/0.0.17.tar.gz",
+    keywords=["ssh", "automation", "orchestration"],
+    install_requires=[
+        "asyncssh",
+        "pydantic",
+        "pyyaml",
+        "loguru",
+        "typer",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: System Administrators",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
