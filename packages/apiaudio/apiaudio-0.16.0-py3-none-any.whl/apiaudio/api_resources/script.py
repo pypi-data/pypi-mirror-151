d344728caa57e47e516694499414d9bdbea05f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:04 +0000

    Add package apiaudio/0.16.0/apiaudio-0.16.0-py3-none-any.whl

---
+++ packages/apiaudio/apiaudio-0.16.0-py3-none-any.whl/apiaudio/api_resources/script.py
@@ -0,0 +1,31 @@
+from apiaudio.helper_classes import (
+    CreatableResource,
+    DeletableResource,
+    ListableResource,
+    RetrievableResource,
+)
+
+
+class Script(
+    ListableResource, CreatableResource, RetrievableResource, DeletableResource
+):
+    OBJECT_NAME = "script"
+    resource_path = "/script"
+    random_url = "/script/random"
+
+    @classmethod
+    def get_random_text(cls, category=None):
+        return cls._get_request(
+            path_param=cls.random_url, request_params={"category": category}
+        )
+
+    @classmethod
+    def preview(cls, scriptId, language):
+        params = {"preview": True, "lang": language}
+        r = cls._get_request(
+            path_param=cls.resource_path + f"/{scriptId}", request_params=params
+        )
+        if "scriptText" in r:
+            return "Script Text Preview: " + r["scriptText"]
+        else:  # in practice this won't happen as _get_request raises an exception
+            return "PREVIEW FAILED"
