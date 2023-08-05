5b7c9b0014df30ca940ddec2fe4082684533ac91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:17 +0000

    Add package Mits-Engine/1.0.5/Mits%20Engine-1.0.5.tar.gz

---
+++ packages/Mits-Engine/Mits%20Engine-1.0.5.tar.gz/Mits Engine-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setuptools
+
+setuptools.setup(
+	name="Mits Engine",
+	version="1.0.5",
+	license="MB INC",
+	authors= ["MB Craft"],
+	author_email="mbcraft@orange.fr",
+	description="MITS is a basic devlopement system (based on Python).",
+	long_description="Don't install Mits alone, it needs Mits Reader to be used.",
+	download_url = "https://github.com/mbcraft-exe/MitsEngine/blob/main/Mits%20Engine-1.0.2.tar.gz",
+	url = 'https://github.com/mbcraft-exe/MitsEngine/',
+	keywords=["Mits", "MITS"],
+	packages=["Engine"]
+	)
