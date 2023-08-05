174e05949004f85032a8ff84bb7ea74a5b16fbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:50 +0000

    Add package invenio-requests/0.3.21/invenio-requests-0.3.21.tar.gz

---
+++ packages/invenio-requests/invenio-requests-0.3.21.tar.gz/invenio-requests-0.3.21/invenio_requests/assets/semantic-ui/js/invenio_requests/components/Buttons.js
@@ -0,0 +1,17 @@
+// This file is part of InvenioRequests
+// Copyright (C) 2022 CERN.
+//
+// Invenio RDM Records is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+import { i18next } from "@translations/invenio_requests/i18next";
+
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+export const SaveButton = (props) => (
+  <Button icon="save" positive size="mini" content={i18next.t("Save")} {...props} />
+);
+
+export const CancelButton = (props) => (
+  <Button icon="cancel" content={i18next.t("Cancel")} size="mini" {...props} />
+);
