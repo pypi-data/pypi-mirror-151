f1bd9c66fb528dcef4a6786102e5f019816477d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:24:16 +0000

    Add package tokyo-annotation/0.1a0/tokyo_annotation-0.1a0-py3-none-any.whl

---
+++ packages/tokyo-annotation/tokyo_annotation-0.1a0-py3-none-any.whl/tokyo_annotation/models/lineage.py
@@ -0,0 +1,19 @@
+import attr
+
+@attr.s
+class Node:
+    id: str = attr.ib(default=None)
+    type: str = attr.ib(default=None)
+
+    def __eq__(self, __o: object) -> bool:
+        return self.id == __o.id
+
+
+@attr.s
+class DataNode(Node):
+    pass
+
+
+@attr.s
+class JobNode(Node):
+    pass
