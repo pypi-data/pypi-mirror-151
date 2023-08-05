ed954e89b22dd5bdbdfa30a7c26ee7a18f9bfa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:27 +0000

    Add package alaric/0.0.2/alaric-0.0.2-py3-none-any.whl

---
+++ packages/alaric/alaric-0.0.2-py3-none-any.whl/alaric/meta/negate.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict
+
+from alaric.comparison import IN
+from alaric.comparison.comparison_exists import EXISTS
+
+if TYPE_CHECKING:
+    from alaric.abc import ComparisonT
+
+
+class NEGATE:
+    """
+    Negate a given option, I.e. Do the opposite.
+
+    Supported operands:
+
+    * EXISTS
+    * IN
+    """
+
+    def __init__(self, comparison: ComparisonT):
+        self.comparison: ComparisonT = comparison
+
+    def __repr__(self):
+        return f"NEGATE({self.comparison})"
+
+    def build(self) -> Dict:
+        if isinstance(self.comparison, EXISTS):
+            self.comparison._val = False
+            return self.comparison.build()
+
+        elif isinstance(self.comparison, IN):
+            self.comparison._operator = "$nin"
+            return self.comparison.build()
+
+        raise RuntimeError("Invalid wrapped comparison.")
