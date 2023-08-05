c42338cb7d0aab883a417d741f2fda802febdc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:45 +0000

    Add package fenics-basix/0.4.2.post1/fenics_basix-0.4.2.post1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/fenics-basix/fenics_basix-0.4.2.post1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/basix/include/basix/e-lagrange.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2020 Chris Richardson
+// FEniCS Project
+// SPDX-License-Identifier:    MIT
+
+#pragma once
+
+#include "cell.h"
+#include "element-families.h"
+#include "finite-element.h"
+
+namespace basix::element
+{
+/// Create a Lagrange element on cell with given degree
+/// @param[in] celltype The cell type
+/// @param[in] degree The degree of the element
+/// @param[in] variant The variant of the element to be created
+/// @param[in] discontinuous Controls whether the element is continuous or
+/// discontinuous
+/// @return A finite element
+FiniteElement create_lagrange(cell::type celltype, int degree,
+                              element::lagrange_variant variant,
+                              bool discontinuous);
+} // namespace basix::element
