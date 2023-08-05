58646b5e06c41583a6072ab9a8ffede47b28e5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:03 +0000

    Add package PyScoreSaber/1.0.6/PyScoreSaber-1.0.6.tar.gz

---
+++ packages/PyScoreSaber/PyScoreSaber-1.0.6.tar.gz/PyScoreSaber-1.0.6/README.md
@@ -0,0 +1,41 @@
+[![PyPI version](https://badge.fury.io/py/PyScoreSaber.svg)](https://pypi.org/project/PyScoreSaber)
+# PyScoreSaber
+Score Saber API wrapper
+
+Comes with caching and rate limiting out of the box.
+
+There is also a test mode which can be enabled like this ```scoresaber = ScoreSaber(test_mode=True)```.
+This will return random data instead of making API requests to Score Saber.
+
+### Usage:
+```python
+import asyncio
+from pyscoresaber import ScoreSaberAPI
+
+
+async def main():
+    async with ScoreSaberAPI() as scoresaber:
+        player = await scoresaber.player_full("76561198029447509")
+        print(player)
+
+# Get fake data instead
+async def main_fake():
+    async with ScoreSaberAPI(test_mode=True) as scoresaber:
+        player = await scoresaber.player_basic("76561198029447509")
+        print(player)
+
+asyncio.run(main())
+asyncio.run(main_fake())
+```
+
+### Faker provider:
+```python
+from faker import Faker
+from pyscoresaber import ScoreSaberProvider
+
+faker = Faker()
+faker.add_provider(ScoreSaberProvider)
+
+player = faker.player_basic("76561198029447509")
+print(player)
+```
---
+++ packages/PyScoreSaber/PyScoreSaber-1.0.6.tar.gz/PyScoreSaber-1.0.6/setup.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+import setuptools
+
+# Get version info
+__version__: Optional[str] = None
+exec(open('src/pyscoresaber/version.py').read())
+
+if __version__ is None:
+    raise RuntimeError("Failed to get version!")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Requirements
+requirements = []
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements += f.read().splitlines()
+
+setuptools.setup(
+    name="PyScoreSaber",
+    version=__version__,
+    author="LuCkEr-",
+    author_email="lucker@lucker.xyz",
+    description="Score Saber API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kiyomi-Parents/PyScoreSaber",
+    project_urls={
+        "Bug Tracker": "https://github.com/Kiyomi-Parents/PyScoreSaber/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=requirements
+)
