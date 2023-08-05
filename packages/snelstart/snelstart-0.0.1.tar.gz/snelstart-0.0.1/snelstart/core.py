b3036681d1f842c61794827b4dfe678258259560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package snelstart/0.0.1/snelstart-0.0.1.tar.gz

---
+++ packages/snelstart/snelstart-0.0.1.tar.gz/snelstart-0.0.1/snelstart/core.py
@@ -0,0 +1,48 @@
+import json
+import logging
+import os
+
+import requests
+
+
+class Base:
+    def __init__(self):
+        self.api_version = "v2"
+        self.base_url = f"https://b2bapi.snelstart.nl/{self.api_version}/"
+        self.client_key = os.environ.get("SNELSTART_CLIENT_KEY")
+        self.subscription_key = os.environ.get("SNELSTART_SUBSCRIPTION_KEY")
+        self.access_token = self.set_access_token()
+
+    def set_access_token(self):
+
+        service_url = "https://auth.snelstart.nl/b2b/token"
+        response = requests.post(service_url, data={"grant_type": "clientkey", "clientkey": self.client_key})
+        if response:
+            json_data = json.loads(response.text)
+            access_token = json_data.get("access_token")
+            logging.info("obtained access token.")
+        else:
+            access_token = None
+            logging.info(response.text)
+
+        return access_token
+
+    def request_data(self, endpoint: str):
+
+        url = self.base_url + endpoint
+
+        response = requests.get(
+            url,
+            headers={
+                "Authorization": f"Bearer {self.access_token}",
+                "Ocp-Apim-Subscription-Key": self.subscription_key,
+            },
+        )
+        if response:
+            json_data = response.json()
+        else:
+            json_data = {}
+            logging.info(response.text)
+        logging.info(f"requesting data for endpoint {url}")
+
+        return json_data
