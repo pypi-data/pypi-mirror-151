89d09833069f06c451e3d8bc4fb0831e7f76aebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:38 +0000

    Add package kabbes-smart-documentation/0.6.0/kabbes_smart_documentation-0.6.0.tar.gz

---
+++ packages/kabbes-smart-documentation/kabbes_smart_documentation-0.6.0.tar.gz/kabbes_smart_documentation-0.6.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        package_data={'smart_documentation': 
+        [ 
+            'Templates/default/Template/docs/_static/*.css',
+            'Templates/default/Template/docs/_templates/*.rst',
+            'Templates/default/Template/docs/*.py',
+            'Templates/default/Template/docs/*.rst',
+            'Templates/github_workflows_yml/Template/.github/workflows/*.yml',
+        ]
+        }
+    )
