65a3d1609e06e5944f48ec4690453b94b132fc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package pytorchmetrics/0.0.1/pytorchmetrics-0.0.1.tar.gz

---
+++ packages/pytorchmetrics/pytorchmetrics-0.0.1.tar.gz/pytorchmetrics-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup
+
+setup(
+    name="pytorchmetrics",
+    version="0.0.1",
+    description="Placeholder for PyTorch Metrics",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/facebookresearch/torchtlc",
+    author="Edward Wang",
+    author_email="edward.auc@gmail.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=[],
+    include_package_data=True,
+    entry_points={},
+)
