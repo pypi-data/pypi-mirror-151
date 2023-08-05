d7ba74800199682880fc6ecf491fb74c7cf36d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:29:58 +0000

    Add package social-apis/0.5/social-apis-0.5.tar.gz

---
+++ packages/social-apis/social-apis-0.5.tar.gz/social-apis-0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from src.social_apis import __version__
+
+setup(
+    name='social-apis',
+    version=f'{__version__}',
+    url='https://github.com/azavodov/social-apis',
+    license='MIT',
+    author='Andrey Zavodov',
+    author_email='a.p.zavodov@gmail.com',
+    description='Make requests to different APIs with one library',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    keywords='social networks API',
+    zip_safe=False
+)
