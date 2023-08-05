b6e8f7a62a0fad841cf0d1d626464ef372111580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:51 +0000

    Add package txtai/4.5.0/txtai-4.5.0.tar.gz

---
+++ packages/txtai/txtai-4.5.0.tar.gz/txtai-4.5.0/src/python/txtai/__init__.py
@@ -0,0 +1,11 @@
+"""
+Version string
+"""
+
+import logging
+
+# Set default logging format
+logging.basicConfig(format="%(asctime)s [%(levelname)s] %(funcName)s: %(message)s")
+
+# Current version tag
+__version__ = "4.5.0"
