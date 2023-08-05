6897a42c8ca8f72bcff8658ff0a0aacb4c46fb43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:23:10 +0000

    Add package giveagame/0.0.1/giveagame-0.0.1-py3-none-any.whl

---
+++ packages/giveagame/giveagame-0.0.1-py3-none-any.whl/giveagame/sprites/PlatformSprite.py
@@ -0,0 +1,10 @@
+from libs.sprites.Sprite import *
+from libs.Coords import *
+
+class PlatformSprite(Sprite):
+    def __init__(self, game, photo_image, x, y, width, height):
+        Sprite.__init__(self, game)
+        self.photo_image = photo_image
+        self.image = game.canvas.create_image(x, y, \
+                                              image=self.photo_image, anchor='nw')
+        self.coordinates = Coords(x, y, x + width, y + height)
