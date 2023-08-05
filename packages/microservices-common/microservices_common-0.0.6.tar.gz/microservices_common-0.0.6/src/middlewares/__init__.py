0658b4b39bfdec457dc4a2706ce38cfefe6dd6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:15 +0000

    Add package microservices-common/0.0.6/microservices_common-0.0.6.tar.gz

---
+++ packages/microservices-common/microservices_common-0.0.6.tar.gz/microservices_common-0.0.6/src/middlewares/__init__.py
@@ -0,0 +1,5 @@
+from middlewares.admin import *
+from middlewares.authorized import *
+from middlewares.own_optimization_record import *
+from middlewares.process import *
+from middlewares.exception_handler import *
