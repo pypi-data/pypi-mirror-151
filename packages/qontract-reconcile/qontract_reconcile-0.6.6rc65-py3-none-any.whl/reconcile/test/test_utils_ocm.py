1b93524191aae83eaaddc7d97349ba385cb441dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:47 +0000

    Add package qontract-reconcile/0.6.6rc65/qontract_reconcile-0.6.6rc65-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.6.6rc65-py3-none-any.whl/reconcile/test/test_utils_ocm.py
@@ -0,0 +1,63 @@
+import pytest
+from reconcile.utils.ocm import OCM
+
+
+@pytest.fixture
+def ocm(mocker):
+    mocker.patch("reconcile.utils.ocm.OCM._init_access_token")
+    mocker.patch("reconcile.utils.ocm.OCM._init_request_headers")
+    mocker.patch("reconcile.utils.ocm.OCM._init_clusters")
+    mocker.patch("reconcile.utils.ocm.OCM._init_blocked_versions")
+    mocker.patch("reconcile.utils.ocm.OCM._init_version_gates")
+    return OCM("name", "url", "tid", "turl", "ot")
+
+
+def test_get_cluster_aws_account_id_none(mocker, ocm):
+    role_grants_mock = mocker.patch.object(
+        ocm, "get_aws_infrastructure_access_role_grants"
+    )
+    role_grants_mock.return_value = []
+    result = ocm.get_cluster_aws_account_id("cluster")
+    assert result is None
+
+
+def test_get_cluster_aws_account_id_ok(mocker, ocm):
+    console_url = (
+        "https://signin.aws.amazon.com/switchrole?account=12345&roleName=role-1"
+    )
+    expected = "12345"
+    role_grants_mock = mocker.patch.object(
+        ocm, "get_aws_infrastructure_access_role_grants"
+    )
+    role_grants_mock.return_value = [(None, None, None, console_url)]
+    result = ocm.get_cluster_aws_account_id("cluster")
+    assert result == expected
+
+
+@pytest.fixture
+def clusters_by_readiness():
+    return [
+        ({"managed": False, "state": "ready", "storage_quota": 42}, False),
+        ({"managed": True, "state": "ready", "storage_quota": 42}, True),
+        ({"managed": True, "state": "not ready", "storage_quota": 42}, False),
+        # ROSA-like cluster
+        ({"managed": True, "state": "ready"}, False),
+    ]
+
+
+def test__ready_for_app_interface(clusters_by_readiness, ocm):
+    for cluster, readiness in clusters_by_readiness:
+        assert ocm._ready_for_app_interface(cluster) == readiness
+
+
+def test_get_version_gate(ocm):
+    ocm.version_gates = [
+        {"version_raw_id_prefix": "4.9", "sts_only": True},
+        {"version_raw_id_prefix": "4.9", "sts_only": False},
+        {"version_raw_id_prefix": "4.10", "sts_only": False},
+    ]
+    gates = ocm.get_version_gates("4.9")
+    assert gates == [{"version_raw_id_prefix": "4.9", "sts_only": False}]
+    gates = ocm.get_version_gates("4.9", sts_only=True)
+    assert gates == [{"version_raw_id_prefix": "4.9", "sts_only": True}]
+    assert len(ocm.get_version_gates("4.8")) == 0
