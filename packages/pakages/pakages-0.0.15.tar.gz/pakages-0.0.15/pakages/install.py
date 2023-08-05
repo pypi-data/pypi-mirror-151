52fe1e28b8c73569f70dae7071a9113341122a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:32:13 +0000

    Add package pakages/0.0.15/pakages-0.0.15.tar.gz

---
+++ packages/pakages/pakages-0.0.15.tar.gz/pakages-0.0.15/pakages/install.py
@@ -0,0 +1,275 @@
+__author__ = "Vanessa Sochat, Alec Scott"
+__copyright__ = "Copyright 2021-2022, Vanessa Sochat and Alec Scott"
+__license__ = "Apache-2.0"
+
+import os
+import shutil
+import tarfile
+from collections import defaultdict
+
+import pakages.oras
+import pakages.sbom
+import pakages.utils
+import pakages.defaults
+from pakages.logger import logger
+
+import spack.binary_distribution as bd
+import spack.hooks
+import spack.store
+import spack.util.crypto
+
+
+def do_install(self, **kwargs):
+    """
+    Refactored install process to add proper cache handling.
+    """
+    from spack.installer import PackageInstaller
+
+    dev_path_var = self.spec.variants.get("dev_path", None)
+    registries = kwargs.get("registries")
+    tag = kwargs.get("tag")
+
+    if dev_path_var:
+        kwargs["keep_stage"] = True
+
+    class PakInstaller(PackageInstaller):
+        """
+        The PakInstaller wraps the PackageInstaller.
+
+        We take this inline approach because we are not able to import it.
+        """
+
+        def _get_task(self, pkg, request, is_compiler, all_deps):
+            """
+            Get a build task for the package.
+            """
+            task = spack.installer.BuildTask(
+                pkg,
+                request,
+                is_compiler,
+                0,
+                0,
+                spack.installer.STATUS_ADDED,
+                self.installed,
+            )
+            pkg_id = spack.installer.package_id(pkg)
+
+            for dep_id in task.dependencies:
+                all_deps[dep_id].add(pkg_id)
+            return task
+
+        def prepopulate_tasks(self):
+            """
+            Pre-populate a lookup of tasks for the cache.
+            """
+            # Prepare the complete list of dependencies to look for
+            # This mimicks init_queue
+            self._pakages_tasks = {}
+            all_deps = defaultdict(set)
+            logger.debug("Initializing the build queue from the build requests")
+            for request in self.build_requests:
+                for dep in request.traverse_dependencies():
+                    dep_id = spack.installer.package_id(dep.package)
+                    if dep_id not in self.build_tasks:
+                        task = self._get_task(dep.package, request, False, all_deps)
+                        self._pakages_tasks[task.pkg_id] = task
+                    spack.store.db.clear_failure(dep, force=False)
+
+                if request.pkg_id not in self._pakages_tasks:
+                    spack.store.db.clear_failure(request.spec, force=True)
+                    task = self._get_task(request.pkg, request, False, all_deps)
+                    self._pakages_tasks[task.pkg_id] = task
+
+            # Add missing dependents to ensure proper uninstalled dependency
+            # tracking when installing multiple specs
+            for dep_id, dependents in all_deps.items():
+                if dep_id in self._pakages_tasks:
+                    task = self._pakages_tasks[dep_id]
+                    for dependent_id in dependents.difference(task.dependents):
+                        task.add_dependent(dependent_id)
+
+        def prepare_cache(self, registries=None, tag=None):
+            """
+            Given that we have a build cache for a package, install it.
+
+            Since the GitHub packages API requires a token, we take an approach
+            that attempts a pull for an artifact, and just continue if we don't
+            have one.
+            """
+            # If no registries in user settings or command line, use default
+            if not registries:
+                registries = [pakages.defaults.trusted_packages_registry]
+            tag = tag or pakages.defaults.default_tag
+
+            # prepare oras client
+            oras = pakages.oras.Oras()
+
+            if not self.build_requests:
+                return
+
+            # We will update build requests with specs we find
+            # This is a loose matching, we just care about name and version for now
+            requests = {
+                "%s:%s" % (x.pkg.spec.name, x.pkg.spec.version): x
+                for x in self.build_requests
+            }
+
+            # If we want to use Github packages API, it requires token with package;read scope
+            # https://docs.github.com/en/rest/reference/packages#list-packages-for-an-organization
+            for pkg_id, request in self._pakages_tasks.items():
+
+                # Don't continue if installed!
+                if request.pkg.spec.install_status() == True:
+                    continue
+
+                # The name of the expected package, and directory to put it
+                tmpdir = pakages.utils.get_tmpdir()
+
+                # Try until we get a cache hit
+                artifact = None
+                for registry in registries:
+                    name = bd.tarball_name(request.pkg.spec, ".spack")
+                    generic_name = pakages.utils.generalize_spack_archive(name)
+                    uri = "%s/%s:%s" % (registry, generic_name, tag)
+                    artifact = oras.fetch(uri, os.path.join(tmpdir, name))
+                    if artifact:
+                        break
+
+                # Don't continue if not found
+                if not artifact:
+                    shutil.rmtree(tmpdir)
+                    continue
+
+                # Checksum check (removes sha256 prefix)
+                sha256 = oras.get_manifest_digest(uri)
+                if sha256:
+                    checker = spack.util.crypto.Checker(sha256)
+                    if not checker.check(artifact):
+                        logger.exit("Checksum of %s is not correct." % artifact)
+
+                # If we have an artifact, extract where needed and tell spack it's installed!
+                if artifact:
+                    # Note - for now not signing, since we don't have a consistent key strategy
+                    # The spack function is a hairball that doesn't respect the provided filename
+                    spec = extract_tarball(request.pkg.spec, artifact)
+                    if not spec:
+                        continue
+
+                    # Remove the build request if we hit it. Note that this
+                    # might fail if dependencies are still needed (and not hit)
+                    # I haven't tested it yet
+                    spec_id = f"{spec.name}:{spec.version}"
+                    if spec_id in requests:
+
+                        # update the build request
+                        requests[spec_id].pkg.spec = spec
+                        requests[spec_id].pkg_id = spack.installer.package_id(
+                            spec.package
+                        )
+                        requests[spec_id].dependencies = {
+                            f"{x.name}-{x.version}-{x._hash}"
+                            for x in spec.dependencies()
+                        }
+
+                        # And flag the task as installed
+                        task = requests[spec_id]
+                        requests[spec_id].task = spack.installer.STATUS_INSTALLED
+                        self._flag_installed(task.pkg, spec.dependents())
+
+                    # And finish this piece of the install
+                    spec.package.installed_from_binary_cache = True
+                    spack.hooks.post_install(spec)
+                    spack.store.db.add(spec, spack.store.layout)
+
+            # Update build requests
+            self.build_requests = list(requests.values())
+
+    builder = PakInstaller([(self, kwargs)])
+
+    # Download what we can find from the GitHub cache
+    builder.prepopulate_tasks()
+    builder.prepare_cache(registries, tag)
+    builder.install()
+
+    # If successful, generate an sbom
+    meta_dir = os.path.join(self.prefix, ".spack")
+    if os.path.exists(meta_dir):
+        sbom = pakages.sbom.generate_sbom(self.spec)
+        sbom_file = os.path.join(meta_dir, "sbom.json")
+        pakages.utils.write_json(sbom, sbom_file)
+
+
+def extract_tarball(spec, filename):
+    """
+    extract binary tarball for given package into install area
+    """
+    from_dir = os.path.dirname(filename)
+    name = os.path.basename(filename)
+    dag_hash = name.split("-")[-1].replace(".spack", "")
+
+    # Anything installed from system
+    if spack.store.layout.root not in spec.prefix:
+        return
+    else:
+        extract_dir = os.path.dirname(spec.prefix)
+        prefix = os.path.join(extract_dir, f"{spec.name}-{spec.version}-{dag_hash}")
+
+    # The spec prefix is an easy way to get the directory name
+    if os.path.exists(prefix):
+        shutil.rmtree(prefix)
+
+    tmpdir = pakages.utils.get_tmpdir()
+    with tarfile.open(filename, "r") as tar:
+        tar.extractall(tmpdir)
+
+    # Find the json file and .tar.gz
+    spec_file = None
+    targz = None
+    for path in os.listdir(tmpdir):
+        if path.endswith(".json"):
+            spec_file = os.path.join(tmpdir, path)
+        elif path.endswith("gz"):
+            targz = os.path.join(tmpdir, path)
+
+    if not spec_file:
+        shutil.rmtree(tmpdir)
+        shutil.rmtree(from_dir)
+        logger.exit(f"{spec} did not come with a spec json.")
+
+    new_prefix = str(os.path.relpath(prefix, spack.store.layout.root))
+    content = pakages.utils.read_json(spec_file)
+
+    # if the original relative prefix is in the spec file use it
+    buildinfo = content.get("buildinfo", {})
+    relative_prefix = buildinfo.get("relative_prefix", new_prefix)
+
+    extract_tmp = os.path.join(spack.store.layout.root, ".tmp")
+    pakages.utils.mkdirp(extract_tmp)
+    extracted_dir = os.path.join(extract_tmp, relative_prefix.split(os.path.sep)[-1])
+
+    with tarfile.open(targz, "r") as tar:
+        tar.extractall(path=extract_tmp)
+    try:
+        shutil.move(extracted_dir, prefix)
+    except Exception as e:
+        shutil.rmtree(extracted_dir)
+        raise e
+
+    # Clean up
+    os.remove(targz)
+    os.remove(spec_file)
+
+    # Create a dummy spec to do the relocation
+    new_spec = spack.spec.Spec.from_dict({"spec": content["spec"]})
+    new_spec._prefix = prefix
+
+    try:
+        bd.relocate_package(new_spec, False)
+    # This probably shouldn't fail, will let it slide for now
+    except Exception as e:
+        raise e
+    finally:
+        shutil.rmtree(tmpdir)
+        if os.path.exists(filename):
+            os.remove(filename)
+    return new_spec
---
+++ packages/pakages/pakages-0.0.15.tar.gz/pakages-0.0.15/pakages/oras.py
@@ -0,0 +1,111 @@
+__author__ = "Vanessa Sochat, Alec Scott"
+__copyright__ = "Copyright 2021-2022, Vanessa Sochat and Alec Scott"
+__license__ = "Apache-2.0"
+
+import spack.bootstrap
+import spack.spec
+import pakages.utils as utils
+import pakages.defaults
+import spack.util.executable
+import spack.util.crypto
+
+from pakages.logger import logger
+
+import os
+import random
+import requests
+import time
+
+
+class Oras:
+    def __init__(self):
+        self._oras = None
+
+    @property
+    def oras(self):
+        """
+        Get the oras executable (easier to install on your computer over bootstrap)
+        """
+        if not self._oras:
+            with spack.bootstrap.ensure_bootstrap_configuration():
+                spec = spack.spec.Spec("oras")
+                spack.bootstrap.ensure_executables_in_path_or_raise(
+                    ["oras"], abstract_spec=spec
+                )
+                self._oras = spack.util.executable.which("oras")
+        return self._oras
+
+    def get_manifest(self, uri):
+        """
+        Use crane to get the image manifest
+        """
+        response = requests.get("https://crane.ggcr.dev/manifest/" + uri)
+        if response.status_code == 200:
+            return response.json()
+
+    def get_manifest_digest(self, uri):
+        """
+        Get the first layer digest (the spack package archive)
+        """
+        response = self.get_manifest(uri)
+        if not response:
+            return
+
+        layers = response.get("layers")
+        if layers:
+            digest = layers[0].get("digest")
+            if digest:
+                return digest.replace("sha256:", "")
+
+    def push(self, uri, push_file, content_type=None, retry=3, sleep=1):
+        """
+        Push an oras artifact to an OCI registry
+        """
+        tries = 0
+        content_type = content_type or pakages.defaults.content_type
+        logger.info("Pushing oras {0}".format(uri))
+        with utils.workdir(os.path.dirname(push_file)):
+            while tries < retry:
+                try:
+                    return self._push(uri, push_file, content_type)
+                except:
+                    time.sleep(sleep)
+                    sleep = sleep * 2**tries + random.uniform(0, 1)
+                    tries += 1
+
+    def _push(self, uri, push_file, content_type):
+        """
+        Helper to push that provides consistent metadata
+        """
+        self.oras(
+            "push",
+            uri,
+            "--manifest-config",
+            "/dev/null:application/vnd.unknown.config.v1+json",
+            # GitHub does not honor this content type - it will return an empty artifact
+            os.path.basename(push_file) + ":" + content_type,
+        )
+
+    def fetch(self, url, save_file):
+        """
+        Fetch an oras artifact from an OCI registry
+        """
+        # We don't have programmatic access to list, so we just try to pull
+        save_dir = os.path.dirname(save_file)
+        try:
+            logger.debug("Trying fetch for oras {0}".format(url))
+            self.oras("pull", url, "-a", "--output", save_dir, output=str, error=str)
+        except:
+            logger.debug("{0} is not available for pull.".format(url))
+            return
+
+        # Just print those that are successful
+        logger.info("Fetched oras {0}".format(url))
+        # Files are technically saved with the hash, we just hope spack will use
+        files = os.listdir(save_dir)
+        if len(files) > 0:
+            save_file = os.path.join(save_dir, files[0])
+
+        # Return the file if exists
+        if os.path.exists(save_file):
+            return save_file
