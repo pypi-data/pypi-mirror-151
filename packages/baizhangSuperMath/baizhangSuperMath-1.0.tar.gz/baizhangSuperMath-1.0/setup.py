760a51a7bd2968f3b50bf095ac8144b1b094893a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:22 +0000

    Add package baizhangSuperMath/1.0/baizhangSuperMath-1.0.tar.gz

---
+++ packages/baizhangSuperMath/baizhangSuperMath-1.0.tar.gz/baizhangSuperMath-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='baizhangSuperMath',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='王志远',
+      author_email='gward@python.net',
+      py_modules=['baizhangSuperMath.demo1', 'baizhangSuperMath.demo2'],
+     )
