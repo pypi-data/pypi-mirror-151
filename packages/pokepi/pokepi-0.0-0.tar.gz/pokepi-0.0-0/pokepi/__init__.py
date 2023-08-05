744acca2f6a575528ee2a1d05a88809cfe3fcc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:54 +0000

    Add package pokepi/0.0.post0/pokepi-0.0-0.tar.gz

---
+++ packages/pokepi/pokepi-0.0-0.tar.gz/pokepi-0.0-0/pokepi/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.0.0'
+__author__ = 'VarMonke'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 VarMonke'
---
+++ packages/pokepi/pokepi-0.0-0.tar.gz/pokepi-0.0-0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='pokepi',
+    version='0.0,0',
+    description='Pokemon API',
+    author='VarMonke',
+    license='MIT',
+    packages=['pokepi'],
+    install_requires=[
+        'aiohttp',
+    ]
+)
