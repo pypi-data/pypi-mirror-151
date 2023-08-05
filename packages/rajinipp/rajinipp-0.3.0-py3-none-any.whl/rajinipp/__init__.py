9da50e002abcb55864302c489d88bd11636da539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:20 +0000

    Add package rajinipp/0.3.0/rajinipp-0.3.0-py3-none-any.whl

---
+++ packages/rajinipp/rajinipp-0.3.0-py3-none-any.whl/rajinipp/__init__.py
@@ -0,0 +1,8 @@
+"""rajini++"""
+
+__version__ = "0.3.0"
+__version_str__ = (
+    f"rajini++ v{__version__}."
+    + "\nrajini++ is a programming language based on the iconic dialogues of super star Rajinikanth."
+    + "\nCreated by Aadhithya Sankar(a.sankar@tum.de)."
+)
