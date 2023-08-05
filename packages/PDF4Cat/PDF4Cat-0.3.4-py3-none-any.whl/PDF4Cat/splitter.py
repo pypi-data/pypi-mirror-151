3caf389e4ddd8be2fc7fb144e24f7409e6258ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:51 +0000

    Add package PDF4Cat/0.3.4/PDF4Cat-0.3.4-py3-none-any.whl

---
+++ packages/PDF4Cat/PDF4Cat-0.3.4-py3-none-any.whl/PDF4Cat/splitter.py
@@ -0,0 +1,45 @@
+import os
+import io
+import zipfile
+
+from .cat import PDF4Cat
+
+class Splitter(PDF4Cat):
+	def __init__(self, *args, **kwargs):
+		super(Splitter, self).__init__(*args, **kwargs)
+
+	# Generate name with BytesIO object (it is faster)
+	def gen_split(self, fpages, start_from) -> tuple:
+		pdf = self.pdf_open(self.doc_file, passwd=self.passwd)
+		for num in range(pdf.page_count): ###
+			dst = self.pdf_open()
+			dst.insert_pdf(pdf, from_page=num, to_page=num)
+			io_data = io.BytesIO()
+			dst.save(io_data)
+			dst.close()
+			del dst
+
+			pdfn = fpages.format(name=self.doc_filename, num=num+start_from)
+			pdfp = io_data
+			yield pdfn, pdfp
+
+	@PDF4Cat.run_in_subprocess # need add range
+	def split_pages2zip(
+		self,
+		out_zip_file: str, 
+		fpages: str = '{name}_{num}.pdf',
+		start_from: int = 0) -> None:
+		pdf = self.pdf_open(self.doc_file, passwd=self.passwd)
+
+		# Compression level: zipfile.ZIP_DEFLATED (8) and disable ZIP64 ext.
+		with zipfile.ZipFile(out_zip_file, 'w', zipfile.ZIP_DEFLATED, False) as zf:
+
+			for file_name, io_data in self.gen_split(fpages, start_from):
+				zf.writestr(file_name, io_data.getvalue())
+				self.counter += 1 #need enumerate
+				self.progress_callback(self.counter, pdf.page_count)
+
+				del io_data
+
+		self.counter = 0
+	
