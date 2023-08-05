8e6e55295267126c379a40ef10e9472501c5b4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:12 +0000

    Add package portalcomponentscg/1.0.2/portalcomponentscg-1.0.2.tar.gz

---
+++ packages/portalcomponentscg/portalcomponentscg-1.0.2.tar.gz/portalcomponentscg-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="portalcomponentscg",
+    version="1.0.2",
+    description="Components para o portal de clientes CG.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nikhilkumarsingh/weather-reporter",
+    author="Gabriel Dalacorte",
+    author_email="gnunes.servico@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["portalcomponentscg"],
+    include_package_data=True,
+    install_requires=["requests"],
+)
