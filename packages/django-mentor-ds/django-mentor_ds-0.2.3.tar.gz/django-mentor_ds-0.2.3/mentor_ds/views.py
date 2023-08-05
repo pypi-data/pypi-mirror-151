0ff4c4676260d5da126715fbed74c0c19613cd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:40 +0000

    Add package django-mentor-ds/0.2.3/django-mentor_ds-0.2.3.tar.gz

---
+++ packages/django-mentor-ds/django-mentor_ds-0.2.3.tar.gz/django-mentor_ds-0.2.3/mentor_ds/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from appointment.templatetags.appointment_tags import make_post_context
+from _data import context
+
+import logging
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(levelname)s: [%(name)s] %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel(logging.INFO)
+
+
+def buildup(request):
+    c = context.context
+    # django-base-ds에 아래 코드가 이미 포함되어 있음. 링크만 추가하면됨
+    # <link rel="preconnect" href="https://fonts.googleapis.com">
+    # <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    c['font_link'] = "https://fonts.googleapis.com/css2?" \
+                     "family=Dongle:wght@300;400;700&" \
+                     "family=Hahmlet:wght@100;200;300;400;500;600;700;800;900&" \
+                     "family=Noto+Sans+KR:wght@100;300;400;500;700;900&" \
+                     "family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap"
+    logger.debug(c)
+    if request.method == 'GET':
+        return render(request, f"mentor_ds/base.html", c)
+    elif request.method == "POST":
+        # appointment가 contact에 포함된 경우는 anchor를 contact으로 설정한다.
+        c.update(make_post_context(request.POST, c['basic_info']['consult_email'], anchor='contact'))
+        return render(request, f"mentor_ds/base.html", c)
