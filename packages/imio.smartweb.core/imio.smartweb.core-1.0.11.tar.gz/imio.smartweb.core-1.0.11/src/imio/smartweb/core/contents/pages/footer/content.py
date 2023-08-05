11e68762a429a271ed9a0b238bde0843dd65f99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:20 +0000

    Add package imio.smartweb.core/1.0.11/imio.smartweb.core-1.0.11.tar.gz

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.0.11.tar.gz/imio.smartweb.core-1.0.11/src/imio/smartweb/core/contents/pages/footer/content.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core.contents import IPages
+from imio.smartweb.core.contents import Pages
+from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone.autoform.directives import write_permission
+from plone.namedfile.field import NamedBlobImage
+from plone.supermodel import model
+from zope import schema
+from zope.interface import implementer
+
+
+class IFooter(IPages):
+    """Marker interface and Dexterity Python Schema for Footer"""
+
+    model.fieldset(
+        "layout", label=_("Layout"), fields=["background_image", "css_class"]
+    )
+
+    write_permission(background_image="cmf.ManagePortal")
+    background_image = NamedBlobImage(
+        title=_("Background image"),
+        required=False,
+    )
+
+    write_permission(css_class="cmf.ManagePortal")
+    css_class = schema.TextLine(title=_("CSS class"), default="", required=False)
+
+
+@implementer(IFooter)
+class Footer(Pages):
+    """Footer class"""
