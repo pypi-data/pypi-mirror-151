ae70daacd91b23a73b43b92f587a55aaaef1bdd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:22:43 +0000

    Add package sadie-antibody/0.4.12/sadie_antibody-0.4.12-cp37-cp37m-macosx_10_7_x86_64.whl

---
+++ packages/sadie-antibody/sadie_antibody-0.4.12-cp37-cp37m-macosx_10_7_x86_64.whl/sadie_antibody-0.4.12.data/purelib/sadie/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.4.12"
+from . import hmmer, airr, reference, antibody, numbering
+
+
+__all__ = ["hmmer", "airr", "reference", "antibody", "numbering"]
---
+++ packages/sadie-antibody/sadie_antibody-0.4.12-cp37-cp37m-macosx_10_7_x86_64.whl/sadie_antibody-0.4.12.data/purelib/sadie/anarci/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.4.5"
+from .anarci import Anarci, AnarciDuplicateIdError
+from .result import AnarciResults
+
+__all__ = ["Anarci", "AnarciResults", "AnarciDuplicateIdError"]
