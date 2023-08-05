7fe186e665785b3e0d084622faff9d409a706036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:01 +0000

    Add package allianceauth-graphql/0.8.0/allianceauth_graphql-0.8.0-py2.py3-none-any.whl

---
+++ packages/allianceauth-graphql/allianceauth_graphql-0.8.0-py2.py3-none-any.whl/allianceauth_graphql/corputils/types.py
@@ -0,0 +1,45 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+from allianceauth.corputils.models import CorpStats, CorpMember
+from allianceauth.eveonline.models import EveCharacter
+
+
+User = get_user_model()
+
+
+class CorpMemberType(DjangoObjectType):
+    character = graphene.Field('allianceauth_graphql.eveonline.types.EveCharacterType')
+
+    class Meta:
+        model = CorpMember
+        fields = ('corpstats',)
+
+    def resolve_character(self, info):
+        return EveCharacter.objects.get(character_id=self.character_id)
+
+
+class CorpStatsType(DjangoObjectType):
+    registered = graphene.List('allianceauth_graphql.eveonline.types.EveCharacterType')
+    unregistered = graphene.List('allianceauth_graphql.eveonline.types.EveCharacterType')
+    mains = graphene.List('allianceauth_graphql.eveonline.types.EveCharacterType')
+
+    class Meta:
+        model = CorpStats
+        fields = ('corp', 'last_update', 'members',)
+
+    def resolve_registered(self, info):
+        return EveCharacter.objects.filter(character_id__in=self.members.values('character_id'), character_ownership__isnull=False)\
+            .select_related('character_ownership', 'character_ownership__user__profile__main_character')\
+            .prefetch_related('character_ownership__user__character_ownerships')\
+            .prefetch_related('character_ownership__user__character_ownerships__character')
+
+    def resolve_unregistered(self, info):
+        return EveCharacter.objects.filter(character_id__in=self.members.values('character_id'), character_ownership__isnull=True)
+
+    def resolve_mains(self, info):
+        main_ids = User.objects.values('profile__main_character__character_id')
+        return EveCharacter.objects.filter(Q(character_id__in=self.members.values('character_id')) & Q(character_id__in=main_ids))
