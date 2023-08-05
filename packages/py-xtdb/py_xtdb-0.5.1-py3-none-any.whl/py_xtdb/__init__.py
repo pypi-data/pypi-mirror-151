389cec276d440691b1f122a7e5d5e1aac488ea61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package py-xtdb/0.5.1/py_xtdb-0.5.1-py3-none-any.whl

---
+++ packages/py-xtdb/py_xtdb-0.5.1-py3-none-any.whl/py_xtdb/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '0.5.1'
+
+import py_xtdb.xt as xt
+
+swagger_json        = xt.swagger_json
+status              = xt.status
+attribute_stats     = xt.attribute_stats
+latest_completed_tx = xt.latest_completed_tx
+latest_submitted_tx = xt.latest_submitted_tx
+active_queries      = xt.active_queries
+recent_queries      = xt.recent_queries
+slowest_queries     = xt.slowest_queries
+entity_json         = xt.entity_json
+
+query_edn           = xt.query_edn
+
+submit_tx           = xt.submit_tx
+
+if __name__ == "__main__":
+    pass
