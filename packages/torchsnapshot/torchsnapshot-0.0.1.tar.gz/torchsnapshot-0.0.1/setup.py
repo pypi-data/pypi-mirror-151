cdf97d5a9998143fe29eb23edec8efff79987a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:30 +0000

    Add package torchsnapshot/0.0.1/torchsnapshot-0.0.1.tar.gz

---
+++ packages/torchsnapshot/torchsnapshot-0.0.1.tar.gz/torchsnapshot-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import pathlib
+from setuptools import setup
+
+
+setup(
+    name="torchsnapshot",
+    version="0.0.1",
+    description="Placeholder for torchsnapshot",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/facebookresearch/torchtlc",
+    author="Edward Wang",
+    author_email="edward.auc@gmail.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=[],
+    include_package_data=True,
+    entry_points={},
+)
