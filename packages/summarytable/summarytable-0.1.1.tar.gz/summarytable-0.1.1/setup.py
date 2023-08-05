c4308e6eea553ccc5fc435dc55b01d1643525b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:32:35 +0000

    Add package summarytable/0.1.1/summarytable-0.1.1.tar.gz

---
+++ packages/summarytable/summarytable-0.1.1.tar.gz/summarytable-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='summarytable',
+    version='0.1.1',
+    author='Dale Kreitler',
+    author_email='dkreitler@bnl.gov',
+    packages=['summarytable',],
+    scripts=['bin/summarytable','bin/summarytable_ap',],
+    url='https://www.github.com/dalekreitler-bnl/summarytable',
+    license='LICENSE.txt',
+    description='Gathering MX processing summaries',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "xmltodict",
+    ],
+    python_requires='>=3.8',
+)
