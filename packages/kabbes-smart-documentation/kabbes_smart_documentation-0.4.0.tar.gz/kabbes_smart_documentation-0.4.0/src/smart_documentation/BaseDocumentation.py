ed441086e00d1f3db204be062e5837c1baccfff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:03 +0000

    Add package kabbes-smart-documentation/0.4.0/kabbes_smart_documentation-0.4.0.tar.gz

---
+++ packages/kabbes-smart-documentation/kabbes_smart_documentation-0.4.0.tar.gz/kabbes_smart_documentation-0.4.0/src/smart_documentation/BaseDocumentation.py
@@ -0,0 +1,25 @@
+from pypi_builder import BasePackage
+import py_starter as ps
+import pandoc
+
+class BaseDocumentation( BasePackage ):
+
+    CONFIG_ATTS_NEEDED = ['author','version','name']
+
+    DEFAULT_KWARGS = {
+        'readme_rst': '',
+    }
+
+    def __init__( self, *args, **kwargs ):
+
+        joined_kwargs = ps.merge_dicts( BaseDocumentation.DEFAULT_KWARGS, kwargs )
+        BasePackage.__init__( self, *args, **joined_kwargs )
+        self.convert_readme()
+
+    def convert_readme( self ):
+
+        if self.readme_Path.exists():
+            md_string = self.readme_Path.read()
+            Doc = pandoc.read( source = md_string, format = 'markdown' )
+            self.readme_rst = pandoc.write( Doc, format = 'rst' )
+            
