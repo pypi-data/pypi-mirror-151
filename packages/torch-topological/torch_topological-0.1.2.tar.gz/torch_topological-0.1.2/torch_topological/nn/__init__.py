10c0bc9ac2d936a66aea8f0d639e39fdcb6aad3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:49 +0000

    Add package torch-topological/0.1.2/torch_topological-0.1.2.tar.gz

---
+++ packages/torch-topological/torch_topological-0.1.2.tar.gz/torch_topological-0.1.2/torch_topological/nn/__init__.py
@@ -0,0 +1,21 @@
+"""Layers and loss terms for persistence-based optimisation."""
+
+from .data import PersistenceInformation
+
+from .distances import WassersteinDistance
+
+from .loss import SignatureLoss
+from .loss import SummaryStatisticLoss
+
+from .cubical_complex import CubicalComplex
+from .vietoris_rips_complex import VietorisRipsComplex
+
+
+__all__ = [
+    'CubicalComplex',
+    'PersistenceInformation',
+    'SignatureLoss',
+    'SummaryStatisticLoss',
+    'VietorisRipsComplex',
+    'WassersteinDistance',
+]
