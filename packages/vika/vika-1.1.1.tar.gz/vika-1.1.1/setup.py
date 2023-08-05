eb83910f4ee1a9e6b1260ecc686e36bb44673a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:33 +0000

    Add package vika/1.1.1/vika-1.1.1.tar.gz

---
+++ packages/vika/vika-1.1.1.tar.gz/vika-1.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vika",
+    version="1.1.1",
+    author="vikadata",
+    author_email="dev@vikadata.com",
+    description="维格表官方 Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vikadata/vika.py",
+    packages=[
+        "vika", "vika.datasheet", "vika.types", "vika.node", "vika.space"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["requests", "pydantic", "environs"],
+)
