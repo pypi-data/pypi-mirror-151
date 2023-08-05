70ffa6087daad8a17f5ca8a37f0e4e663b972f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:14 +0000

    Add package selenium-smart-locator/0.3.0/selenium-smart-locator-0.3.0.tar.gz

---
+++ packages/selenium-smart-locator/selenium-smart-locator-0.3.0.tar.gz/selenium-smart-locator-0.3.0/setup.py
@@ -0,0 +1,8 @@
+#   Author(s): Milan Falesnik   <milan@falesnik.net>
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from setuptools import setup
+
+
+setup(
+    use_scm_version=True,
+)
