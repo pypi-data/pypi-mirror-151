a19d52f26ed2d6919ab096d7f41cde59998197ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:22 +0000

    Add package hl7-conversion/0.1.0/hl7-conversion-0.1.0.tar.gz

---
+++ packages/hl7-conversion/hl7-conversion-0.1.0.tar.gz/hl7-conversion-0.1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+import pathlib
+
+HERE = pathlib.Path(__file__).resolve().parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="hl7-conversion",
+    version="0.1.0",
+    author="Imran Hasan",
+    author_email="imran221b@gmail.com",
+    description="Package to hl7_convert HL7 messages to JSON format",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/imran221b/hl7-conversion",
+    packages=find_packages(exclude=["docs", "tests"]),
+    platforms=["posix"],
+    entry_points={
+        "console_scripts": [
+            "hl7 = hl7_convert.main:cli",
+        ],
+    },
+    install_requires=[
+        "Click",
+        "hl7apy"
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+        "dev": [
+            "black",
+        ],
+    },
+)
