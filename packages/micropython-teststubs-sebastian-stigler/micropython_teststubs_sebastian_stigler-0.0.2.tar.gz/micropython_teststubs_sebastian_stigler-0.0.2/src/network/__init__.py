66e8af9467192718ec9ee518529ecd1be1911c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:55 +0000

    Add package micropython-teststubs-sebastian-stigler/0.0.2/micropython_teststubs_sebastian_stigler-0.0.2.tar.gz

---
+++ packages/micropython-teststubs-sebastian-stigler/micropython_teststubs_sebastian_stigler-0.0.2.tar.gz/micropython_teststubs_sebastian_stigler-0.0.2/src/network/__init__.py
@@ -0,0 +1,41 @@
+from unittest.mock import Mock
+
+__author__ = "Sebastian Stigler"
+__copyright__ = "Copyright 2022, Sebastian Stigler"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "0.0.2"
+__maintainer__ = "Sebastian Stigler"
+__email__ = "sebastian.stigler@hs-aalen.de"
+__status__ = "Production"
+
+AP_IF = 0
+STA_IF = 1
+
+
+class WLAN(Mock):
+    def __init__(self, inteface_id):
+        super().__init__()
+        self._interface_id = inteface_id
+        self._active = False
+        self._ssid = ""
+        self._password = ""
+        self._bssid = ""
+        self._isconnected = False
+
+    def active(self, state):
+        self._active = state
+
+    def connect(self, ssid=None, password=None, *, bssid=None):
+        self._ssid = ssid
+        self._password = password
+        self._bssid = bssid
+
+    def isconnected(self):
+        if not self._isconnected:
+            self._isconnected = True
+            return False
+        return self._isconnected
+
+    def ifconfig(isgd=None):
+        return ("192.168.0.4", "255.255.255.0", "192.168.0.1", "8.8.8.8")
