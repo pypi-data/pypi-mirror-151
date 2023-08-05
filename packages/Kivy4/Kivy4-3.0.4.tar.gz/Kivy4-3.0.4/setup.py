357c0a33bd00e68f5403d054440c5b90e9bf3779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:16 +0000

    Add package Kivy4/3.0.4/Kivy4-3.0.4.tar.gz

---
+++ packages/Kivy4/Kivy4-3.0.4.tar.gz/Kivy4-3.0.4/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="Kivy4",
+    version="3.0.4",
+    author="Eshqol Development",
+    author_email="support@eshqol.com",
+    packages=["kivy4"],
+    description="This package allows you to create Kivy projects faster with a pre-made kivy template.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    python_requires='>=3.7',
+    install_requires=['kivy==2.0.0', 'kivymd', 'darkdetect', 'screeninfo', 'pyperclip', 'winshell', 'pywin32']
+)
+
+#Eshqol_Development
