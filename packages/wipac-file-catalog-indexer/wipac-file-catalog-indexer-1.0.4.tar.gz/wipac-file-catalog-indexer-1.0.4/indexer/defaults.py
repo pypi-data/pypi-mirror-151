d3674045a78a50bae163ad1304b5a16ace6605db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:30:34 +0000

    Add package wipac-file-catalog-indexer/1.0.4/wipac-file-catalog-indexer-1.0.4.tar.gz

---
+++ packages/wipac-file-catalog-indexer/wipac-file-catalog-indexer-1.0.4.tar.gz/wipac-file-catalog-indexer-1.0.4/indexer/defaults.py
@@ -0,0 +1,23 @@
+"""Default values for starting the indexer."""
+
+try:
+    from typing import Final
+except ImportError:
+    from typing_extensions import Final  # type: ignore[misc]
+
+_AGGREGATE_LATENCY_MINUTES: Final[int] = 30  # minutes
+
+PATHS = None
+PATHS_FILE = ""
+BLACKLIST = None
+BLACKLIST_FILE = ""
+URL = "https://file-catalog.icecube.wisc.edu/"
+TIMEOUT = 30  # seconds
+RETRIES = int((60 / TIMEOUT) * _AGGREGATE_LATENCY_MINUTES)
+BASIC_ONLY = False
+PATCH = False
+ICEPRODV2_RC_TOKEN = ""
+ICEPRODV1_DB_PASS = ""
+DRYRUN = False
+NON_RECURSIVE = False
+N_PROCESSES = 1
