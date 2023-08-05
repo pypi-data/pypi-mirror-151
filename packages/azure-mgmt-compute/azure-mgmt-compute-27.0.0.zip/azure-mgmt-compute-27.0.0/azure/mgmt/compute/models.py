d64d4db48fcc8f2cfeb6009eea414d8b2bfaf452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:05 +0000

    Add package azure-mgmt-compute/27.0.0/azure-mgmt-compute-27.0.0.zip

---
+++ packages/azure-mgmt-compute/azure-mgmt-compute-27.0.0.zip/azure-mgmt-compute-27.0.0/azure/mgmt/compute/models.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+from .v2021_03_01.models import *
+from .v2021_07_01.models import *
+from .v2021_10_01.models import *
+from .v2021_12_01.models import *
+from .v2022_03_01.models import *
