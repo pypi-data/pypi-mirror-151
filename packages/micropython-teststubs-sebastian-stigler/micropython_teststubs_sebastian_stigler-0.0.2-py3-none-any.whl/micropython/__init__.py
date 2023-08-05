d41d3002f37273771a978fe4a6203134302820aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:55 +0000

    Add package micropython-teststubs-sebastian-stigler/0.0.2/micropython_teststubs_sebastian_stigler-0.0.2-py3-none-any.whl

---
+++ packages/micropython-teststubs-sebastian-stigler/micropython_teststubs_sebastian_stigler-0.0.2-py3-none-any.whl/micropython/__init__.py
@@ -0,0 +1,12 @@
+from unittest.mock import Mock
+
+__author__ = "Sebastian Stigler"
+__copyright__ = "Copyright 2022, Sebastian Stigler"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "0.0.2"
+__maintainer__ = "Sebastian Stigler"
+__email__ = "sebastian.stigler@hs-aalen.de"
+__status__ = "Production"
+
+alloc_emergency_exception_buf = Mock()
