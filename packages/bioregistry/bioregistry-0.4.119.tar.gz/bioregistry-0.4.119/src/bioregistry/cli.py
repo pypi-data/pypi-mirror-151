b7f58b95bca8e00a3b9ee48f72793f55d61f6943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:23:29 +0000

    Add package bioregistry/0.4.119/bioregistry-0.4.119.tar.gz

---
+++ packages/bioregistry/bioregistry-0.4.119.tar.gz/bioregistry-0.4.119/src/bioregistry/cli.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""Command line interface for the bioregistry."""
+
+import click
+from more_click import make_web_command
+
+from .align.cli import align
+from .compare import compare
+from .export.cli import export
+from .external.cli import download
+from .lint import lint
+from .version import VERSION
+
+
+@click.group()
+@click.version_option(version=VERSION)
+def main():
+    """Run the Bioregistry CLI."""
+
+
+main.add_command(lint)
+main.add_command(compare)
+main.add_command(export)
+main.add_command(download)
+main.add_command(align)
+main.add_command(make_web_command("bioregistry.app.wsgi:app"))
+
+
+@main.command()
+@click.pass_context
+def update(ctx: click.Context):
+    """Update the Bioregistry."""
+    ctx.invoke(align)
+    ctx.invoke(lint)
+    ctx.invoke(export)
+    ctx.invoke(compare)
+
+    try:
+        from . import upload_ndex
+    except ImportError:
+        click.secho("Could not import ndex")
+    else:
+        ctx.invoke(upload_ndex.main)
+
+
+if __name__ == "__main__":
+    main()
