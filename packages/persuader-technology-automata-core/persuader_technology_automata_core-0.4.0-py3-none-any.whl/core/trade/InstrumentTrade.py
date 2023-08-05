8d49a1ab66b6988fcd4afdc8ba927d6128100c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:24:35 +0000

    Add package persuader-technology-automata-core/0.4.0/persuader_technology_automata_core-0.4.0-py3-none-any.whl

---
+++ packages/persuader-technology-automata-core/persuader_technology_automata_core-0.4.0-py3-none-any.whl/core/trade/InstrumentTrade.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+from enum import Enum
+
+from core.number.BigFloat import BigFloat
+
+
+class Status(Enum):
+    NEW = 'new'
+    SUBMITTED = 'submitted'
+    CANCELLED = 'cancelled'
+    EXECUTED = 'executed'
+    ERROR = 'error'
+
+    @staticmethod
+    def parse(value):
+        result = [member for name, member in Status.__members__.items() if member.value.lower() == value.lower()]
+        return result[0]
+
+
+@dataclass
+class InstrumentTrade:
+    instrument_from: str
+    instrument_to: str
+    quantity: BigFloat
+    price: BigFloat = field(default=None)
+    value: BigFloat = field(default=None)
+    status: Status = field(default=Status.NEW)
+    description: str = field(default=None)
+    order_id: str = field(default=None)
+    instant: int = field(default=None)
