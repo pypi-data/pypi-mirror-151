3a850c4078d8808645bf5866d2d8093dd79be6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:26 +0000

    Add package giant-social-links/0.1.0/giant_social_links-0.1.0-py3-none-any.whl

---
+++ packages/giant-social-links/giant_social_links-0.1.0-py3-none-any.whl/giant_social_links/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from giant_social_links import models
+
+
+@admin.register(models.SocialLink)
+class SocialLinkAdmin(admin.ModelAdmin):
+    list_display = ["name", "url", "order", "is_enabled"]
+    list_editable = ["order", "is_enabled"]
+    list_filter = ["is_enabled"]
+    search_fields = ["name"]
+    fieldsets = [
+        (
+            None,
+            {"fields": ["name", "url", "icon", "fontawesome_icon_name", "order", "is_enabled"]},
+        ),
+        ("Meta Data", {"classes": ("collapse",), "fields": ["created_at", "updated_at"]}),
+    ]
+    readonly_fields = ["created_at", "updated_at"]
