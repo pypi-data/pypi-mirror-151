b6124257eca01802d56df3f9b2b2cab700eff2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:34 +0000

    Add package scale-lidar-io-debug/0.1.53/scale_lidar_io_debug-0.1.53.tar.gz

---
+++ packages/scale-lidar-io-debug/scale_lidar_io_debug-0.1.53.tar.gz/scale_lidar_io_debug-0.1.53/scale_lidar_io_debug/__init__.py
@@ -0,0 +1,7 @@
+from scale_lidar_io import *
+from .scene import DebugLidarScene as LidarScene
+from .lidarLiteHelper import get_points, get_cameras, combine_scenes
+
+
+def import_test():
+    print('Import OK')
