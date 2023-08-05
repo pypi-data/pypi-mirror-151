90296ae36fd1b5e9443f9c98d4f27184986439c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:22:19 +0000

    Add package cdktf-cdktf-provider-google/0.8.41/cdktf_cdktf_provider_google-0.8.41-py3-none-any.whl

---
+++ packages/cdktf-cdktf-provider-google/cdktf_cdktf_provider_google-0.8.41-py3-none-any.whl/cdktf_cdktf_provider_google/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdktf._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdktf/provider-google",
+    "0.8.41",
+    __name__[0:-6],
+    "provider-google@0.8.41.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
