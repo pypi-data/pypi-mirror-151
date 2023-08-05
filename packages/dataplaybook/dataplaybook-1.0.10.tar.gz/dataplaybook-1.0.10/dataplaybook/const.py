899b9716ad0f68ca48515ba6aab7b06b63dfe65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:22 +0000

    Add package dataplaybook/1.0.10/dataplaybook-1.0.10.tar.gz

---
+++ packages/dataplaybook/dataplaybook-1.0.10.tar.gz/dataplaybook-1.0.10/dataplaybook/const.py
@@ -0,0 +1,23 @@
+"""Constants."""
+from typing import Any, Dict, List, Union
+
+from dataplaybook.utils import (  # noqa, pylint: disable=unused-import
+    DataEnvironment,
+    Table,
+)
+
+VERSION = "1.0.10"
+
+
+Columns = List[str]
+Column = str
+# Defined in utils
+# Table = List[Dict[str, Any]]
+Tables = Union[Dict[str, List[Dict[str, Any]]], DataEnvironment]
+
+
+class ATable(list):
+    """A table/list with some metadata."""
+
+    name: str = ""
+    headers: int = 0
