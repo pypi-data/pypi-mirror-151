0bef445bf90ba428d5d69eb6bae6b1cd1690a7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:57 +0000

    Add package dagster/0.14.16rc2/dagster-0.14.16rc2-py3-none-any.whl

---
+++ packages/dagster/dagster-0.14.16rc2-py3-none-any.whl/dagster/api/snapshot_pipeline.py
@@ -0,0 +1,37 @@
+from typing import TYPE_CHECKING, List, Optional
+
+import dagster._check as check
+from dagster.core.errors import DagsterUserCodeProcessError
+from dagster.core.host_representation.external_data import ExternalPipelineSubsetResult
+from dagster.core.host_representation.origin import ExternalPipelineOrigin
+from dagster.grpc.types import PipelineSubsetSnapshotArgs
+from dagster.serdes import deserialize_as
+
+if TYPE_CHECKING:
+    from dagster.grpc.client import DagsterGrpcClient
+
+
+def sync_get_external_pipeline_subset_grpc(
+    api_client: "DagsterGrpcClient",
+    pipeline_origin: ExternalPipelineOrigin,
+    solid_selection: Optional[List[str]] = None,
+) -> ExternalPipelineSubsetResult:
+    from dagster.grpc.client import DagsterGrpcClient
+
+    check.inst_param(api_client, "api_client", DagsterGrpcClient)
+    pipeline_origin = check.inst_param(pipeline_origin, "pipeline_origin", ExternalPipelineOrigin)
+    solid_selection = check.opt_list_param(solid_selection, "solid_selection", of_type=str)
+
+    result = deserialize_as(
+        api_client.external_pipeline_subset(
+            pipeline_subset_snapshot_args=PipelineSubsetSnapshotArgs(
+                pipeline_origin=pipeline_origin, solid_selection=solid_selection
+            ),
+        ),
+        ExternalPipelineSubsetResult,
+    )
+
+    if result.error:
+        raise DagsterUserCodeProcessError.from_error_info(result.error)
+
+    return result
