f3c9c1139383963431d5559c698f719ce70a009a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:22:54 +0000

    Add package nonebot-plugin-alipayvoice/0.3.0/nonebot_plugin_alipayvoice-0.3.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-alipayvoice/nonebot_plugin_alipayvoice-0.3.0-py3-none-any.whl/nonebot_plugin_alipayvoice/__init__.py
@@ -0,0 +1,22 @@
+import cn2an
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg
+
+alipay_voice = on_command("支付宝到账", aliases={"支付宝语音"})
+
+@alipay_voice.handle()
+async def alipay(matcher: Matcher, args: Message = CommandArg()):
+    amount = args.extract_plain_text().replace("元", "")
+    if not amount:
+        return
+    try:
+        amount = cn2an.cn2an(amount, "smart")
+    except Exception:
+        await matcher.send("错误的，请输入正确的数字")
+    if 0.01 <= amount <= 999999999999.99:
+        url = f"https://mm.cqu.cc/share/zhifubaodaozhang/?money={amount}"
+        await matcher.send(MessageSegment.record(url))
+    else:
+        await matcher.send("数字大小超出限制，支持范围为0.01~999999999999.99")
