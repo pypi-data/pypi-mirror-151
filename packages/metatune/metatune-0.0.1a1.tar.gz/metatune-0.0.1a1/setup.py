d165f9cf24133d68ab74b003ae1cddde2dbfcda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:47 +0000

    Add package metatune/0.0.1a1/metatune-0.0.1a1.tar.gz

---
+++ packages/metatune/metatune-0.0.1a1.tar.gz/metatune-0.0.1a1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='metatune',
+    version='0.0.1a1',
+    license='MIT',
+    author="Chris Cummins",
+    author_email='cummins@fb.com',
+    packages=[],
+    url='',
+    keywords='',
+)
