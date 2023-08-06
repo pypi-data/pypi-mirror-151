f9b625e667b389ed1f6456e6f2def4c58d929d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:49 +0000

    Add package ani-donut/0.1.1/ani_donut-0.1.1.tar.gz

---
+++ packages/ani-donut/ani_donut-0.1.1.tar.gz/ani_donut-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ani_donut',
+    version='0.1.1',
+    description='Python package to print donut.',
+    url='',
+    author='Ani Marutyan',
+    author_email='ani_marutyan@edu.aua.am',
+    license='MIT',
+    packages=['ani_donut'],
+    install_requires=['pandas',
+                      'scipy',
+                      'matplotlib'
+                      ],
+)
