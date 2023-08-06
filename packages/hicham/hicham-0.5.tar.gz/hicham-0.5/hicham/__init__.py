3e5abdb493d6bbbc7890c059a43f8e9f34df75e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:17 +0000

    Add package hicham/0.5/hicham-0.5.tar.gz

---
+++ packages/hicham/hicham-0.5.tar.gz/hicham-0.5/hicham/__init__.py
@@ -0,0 +1,13 @@
+from hicham.hicham import Hicham
+
+print("salut tout le monde")
+Hicham().nom()
+Hicham().prenom()
+Hicham().age()
+Hicham().fonction()
+Hicham().contact()
+print("----------------------------------------------------------------------")
+print("----------------------------Mon CV------------------------------------")
+print("----------------------------------------------------------------------")
+
+Hicham().cv()
