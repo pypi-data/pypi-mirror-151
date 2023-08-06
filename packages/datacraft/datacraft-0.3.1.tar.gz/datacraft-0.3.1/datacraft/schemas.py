2f11fcdec583dbb37f687527daa7adfb31ad9927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:59 +0000

    Add package datacraft/0.3.1/datacraft-0.3.1.tar.gz

---
+++ packages/datacraft/datacraft-0.3.1.tar.gz/datacraft-0.3.1/datacraft/schemas.py
@@ -0,0 +1,62 @@
+"""
+Module for loading and validating schemas for various types
+"""
+import json
+import logging
+import importlib_resources as pkg_resources
+from jsonschema import Draft7Validator  # type: ignore
+from . import schema
+from .exceptions import ResourceError, SpecException
+
+_log = logging.getLogger(__name__)
+
+
+def load(key: str) -> dict:
+    """
+    load the internal schema file for the provided key
+
+    Args:
+        key: type name for schema
+
+    Returns:
+        Schema as dictionary for given key
+
+    Raises:
+        ResourceError if schema for key not found
+    """
+    naming_convention = f'{key}.schema.json'
+    return _load_resource_as_json(naming_convention)
+
+
+def _load_resource_as_json(resource_name: str) -> dict:
+    """ loads the internal json resource with the given resource_name """
+    try:
+        with pkg_resources.open_text(schema, resource_name) as schema_resource:
+            return json.load(schema_resource)
+    except FileNotFoundError as err:
+        raise ResourceError(f'No resource with name {resource_name} was found') from err
+
+
+def validate_schema_for_spec(spec_type, field_spec, type_schema):
+    """
+    performs schema validation for the provided field_spec
+
+    Args:
+        spec_type: type name for spec
+        field_spec: spec to apply validation to
+        type_schema: schema to use for validation
+
+    Raises:
+        SpecException if validation fails
+    """
+    definitions = _load_resource_as_json('definitions.json')
+    if 'definitions' in type_schema:
+        type_schema['definitions'].update(definitions['definitions'])
+    else:
+        type_schema['definitions'] = definitions['definitions']
+    validator = Draft7Validator(type_schema)
+    errors = sorted(validator.iter_errors(field_spec), key=lambda e: e.path)
+    if len(errors) > 0:
+        for error in errors:
+            _log.warning(error.message)
+        raise SpecException(f'Failed to validate spec type: {spec_type} with spec: {field_spec}')
---
+++ packages/datacraft/datacraft-0.3.1.tar.gz/datacraft-0.3.1/tests/test_registry.py
@@ -0,0 +1,113 @@
+import os
+import pytest
+import datacraft
+from datacraft.loader import field_loader
+import datacraft.registries as registries
+from datacraft.utils import load_custom_code
+from datacraft import casters
+
+spec = {
+    'foo': {
+        'type': 'reverse_string',
+        'ref': 'ONE'
+    },
+    'refs': {
+        'ONE': {'type': 'values', 'data': ['raed a eod']}
+    }
+}
+
+test_dir = f'{os.path.dirname(os.path.realpath(__file__))}/data'
+
+
+class ReverseStringSupplier:
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def next(self, iteration):
+        value = str(self.wrapped.next(iteration))
+        return value[::-1]
+
+
+@datacraft.registry.types('reverse_string')
+def configure_supplier(field_spec, loader):
+    key = field_spec.get('ref')
+    wrapped = loader.get(key)
+    return ReverseStringSupplier(wrapped)
+
+
+def test_registry_from_local():
+    loader = field_loader(spec)
+
+    reg = registries.Registry
+    all_types = reg.types.get_all()
+    handler = all_types.get('reverse_string')
+
+    supplier = handler(spec.get('foo'), loader)
+
+    assert supplier.next(0) == 'doe a dear'
+
+
+def test_registry_from_file():
+    # string_reverser, same as above just different key
+    load_custom_code(os.path.join(test_dir, 'custom.py'))
+
+    loader = field_loader(spec)
+
+    reg = registries.Registry
+    all_types = reg.types.get_all()
+    handler = all_types.get('string_reverser')
+
+    supplier = handler(spec.get('foo'), loader)
+
+    assert supplier.next(0) == 'doe a dear'
+
+
+def test_registry_error_case():
+    with pytest.raises(FileNotFoundError):
+        # string_reverser, same as above just different key
+        load_custom_code(f'{test_dir}/custom_does_not_exist.py')
+
+
+type_schema_key_lookup_tests = [
+    ("uuid", True),
+    ("values", True),
+    ("spam", False),
+    ("seaweed", False),
+]
+
+
+@pytest.mark.parametrize("key, should_exist", type_schema_key_lookup_tests)
+def test_lookup_schema(key, should_exist):
+    schema = registries.lookup_schema(key)
+    if should_exist:
+        assert schema is not None
+    else:
+        assert schema is None
+
+
+@pytest.mark.parametrize("key, should_exist", type_schema_key_lookup_tests)
+def test_lookup_type(key, should_exist):
+    type_load_func = registries.lookup_type(key)
+    if should_exist:
+        assert type_load_func is not None
+    else:
+        assert type_load_func is None
+
+
+class TestCaster(datacraft.CasterInterface):
+    def cast(self, value):
+        return value
+
+
+@datacraft.registry.casters('test')
+def _test_registered_caster():
+    return TestCaster()
+
+
+def test_lookup_caster():
+    caster = registries.lookup_caster('test')
+    assert caster is not None
+    caster = registries.lookup_caster('not_registered')
+    assert caster is None
+    all_registered = registries.registered_casters()
+    assert all_registered == ['test']
