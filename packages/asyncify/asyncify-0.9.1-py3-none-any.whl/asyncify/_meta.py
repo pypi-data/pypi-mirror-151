d751bace3382e4ef8bbb6aacc77c284b47ca17f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:21 +0000

    Add package asyncify/0.9.1/asyncify-0.9.1-py3-none-any.whl

---
+++ packages/asyncify/asyncify-0.9.1-py3-none-any.whl/asyncify/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "asyncify"
+__description__ = "sync 2 async"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.9.1"
