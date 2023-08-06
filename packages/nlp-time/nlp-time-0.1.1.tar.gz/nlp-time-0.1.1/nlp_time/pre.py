6a7eeef9e459e59d70d018f86002a01eda4b8d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:55 +0000

    Add package nlp-time/0.1.1/nlp-time-0.1.1.tar.gz

---
+++ packages/nlp-time/nlp-time-0.1.1.tar.gz/nlp-time-0.1.1/nlp_time/pre.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+# -*-coding:utf-8-*-
+# Author: Eason.Deng
+# Github: https://github.com/holbos-deng
+# Email: 2292861292@qq.com
+# CreateDate: 2022/5/23 14:49
+# Description: 
+
+def text_pre(text: str):
+    text = text.replace("这个月", "本月")
+    return text
