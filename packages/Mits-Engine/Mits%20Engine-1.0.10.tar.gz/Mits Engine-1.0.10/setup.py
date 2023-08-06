4c3ab18538531317f9a1aff846b5b336464f1755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:51 +0000

    Add package Mits-Engine/1.0.10/Mits%20Engine-1.0.10.tar.gz

---
+++ packages/Mits-Engine/Mits%20Engine-1.0.10.tar.gz/Mits Engine-1.0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="Mits Engine",
+    version="1.0.10",
+    description="A basic sub-python dev langage.",
+    long_description="https://github.com/mbcraft-exe/MitsEngine/blob/main/Mits%20Engine-1.0.2.tar.gz",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mbcraft-exe/MitsEngine",
+    author="MB Craft",
+    author_email="mbcraft@orange.fr",
+    license="MB INC",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["engine"],
+    install_requires=["colorama"],
+    entry_points={
+        "console_scripts": [
+            "mits=engine.__main__:code",
+        ]
+    },
+)
