013c62a16297f1325fdfe1b06be5f4a94ff663c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:06 +0000

    Add package llamirhosseinll/1.0.0/llamirhosseinll-1.0.0.tar.gz

---
+++ packages/llamirhosseinll/llamirhosseinll-1.0.0.tar.gz/llamirhosseinll-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='llamirhosseinll',
+    version='1.0.0',
+    packages=['llamirhosseinll'],
+    url='https://github.com/llamirhosseinll?tab=repositories',
+    license='Apache 2.0',
+    author='amirhossein rezaei',
+    author_email='awmirhossein5@gmail.com',
+    description='this is a test'
+)
