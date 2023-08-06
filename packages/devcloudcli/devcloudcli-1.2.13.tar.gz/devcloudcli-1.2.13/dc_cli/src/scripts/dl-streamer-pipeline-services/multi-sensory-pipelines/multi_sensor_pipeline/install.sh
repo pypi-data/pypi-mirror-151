a079b1c49771ee415a26cefba2770815fe27acb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:02 +0000

    Add package devcloudcli/1.2.13/devcloudcli-1.2.13.tar.gz

---
+++ packages/devcloudcli/devcloudcli-1.2.13.tar.gz/devcloudcli-1.2.13/dc_cli/src/scripts/dl-streamer-pipeline-services/multi-sensory-pipelines/multi_sensor_pipeline/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~
+DIR=$PWD/multi_sensor_sample
+if [ -d "$DIR" ]; then
+        echo "Success"
+else
+	sudo pip3 install gdown
+	gdown --folder https://drive.google.com/drive/folders/1iSEjiRlYlTbuEgTb10T76f9y6aLbtavz --output $DIR
+	echo "Successfully installed"
+fi
+
