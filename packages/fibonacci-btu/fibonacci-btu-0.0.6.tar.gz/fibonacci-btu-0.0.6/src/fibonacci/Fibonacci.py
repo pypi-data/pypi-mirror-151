47c07659a92fcf2d03b9b9751d79d77f8dc08ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:58 +0000

    Add package fibonacci-btu/0.0.6/fibonacci-btu-0.0.6.tar.gz

---
+++ packages/fibonacci-btu/fibonacci-btu-0.0.6.tar.gz/fibonacci-btu-0.0.6/src/fibonacci/Fibonacci.py
@@ -0,0 +1,15 @@
+def Fibonacci(n):
+    sequenceList = [0]
+    n1 = 0
+    n2 = 1
+    count = 0
+    while count < 100:
+        n3 = n1 + n2
+        sequenceList.append(n3)
+        n1 = n2
+        n2 = n3
+        count += 1
+    return sequenceList[n-1]
+
+
+Fibonacci(n)
