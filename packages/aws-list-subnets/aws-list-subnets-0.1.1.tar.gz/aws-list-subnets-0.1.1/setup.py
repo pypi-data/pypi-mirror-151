86348254f6af10e2461488684692e228097ac726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:45 +0000

    Add package aws-list-subnets/0.1.1/aws-list-subnets-0.1.1.tar.gz

---
+++ packages/aws-list-subnets/aws-list-subnets-0.1.1.tar.gz/aws-list-subnets-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name        = 'aws-list-subnets',
+    version     = '0.1.1',
+    description = "list aws subnets",
+    author      = "suzuki-navi",
+    packages    = find_packages(),
+    install_requires = install_requirements,
+    include_package_data = True,
+    entry_points = {
+        "console_scripts": [
+            "aws-list-subnets = aws_list_subnets.main:main",
+        ]
+    },
+)
