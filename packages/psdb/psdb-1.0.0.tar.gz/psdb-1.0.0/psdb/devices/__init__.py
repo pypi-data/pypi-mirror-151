8cdd9840cc778cd05d1956aa7f8dc6ba3424ecae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:24:23 +0000

    Add package psdb/1.0.0/psdb-1.0.0.tar.gz

---
+++ packages/psdb/psdb-1.0.0.tar.gz/psdb-1.0.0/psdb/devices/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2020 Phase Advanced Sensor Systems, Inc.
+from .device import (Device, ReadCommand, Reg, Reg32, Reg32R, Reg32W, Reg32RS,
+                     AReg32, AReg32R, AReg32W, AReg32RS, RegDiv, MemDevice,
+                     RAMDevice)
+from .flash import Flash
+from . import core
+
+
+__all__ = ['AReg32',
+           'AReg32R',
+           'AReg32W',
+           'AReg32RS',
+           'Device',
+           'Flash',
+           'MemDevice',
+           'RAMDevice',
+           'ReadCommand',
+           'Reg',
+           'Reg32',
+           'Reg32R',
+           'Reg32W',
+           'Reg32RS',
+           'RegDiv',
+           'core',
+           ]
---
+++ packages/psdb/psdb-1.0.0.tar.gz/psdb-1.0.0/psdb/probes/usb_probe.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2018-2019 Phase Advanced Sensor Systems, Inc.
+import usb.core
+
+from . import probe
+import psdb
+
+
+class Probe(probe.Probe):
+    def __init__(self, usb_dev, name, usb_reset=False):
+        super().__init__(name)
+        self.usb_dev = usb_dev
+        try:
+            self.serial_num = usb_dev.serial_number
+        except ValueError as e:
+            if str(e) == 'The device has no langid':
+                raise psdb.ProbeException('Device has no langid; ensure '
+                                          'running as root!')
+
+        self.usb_path = '%s:%s' % (
+                self.usb_dev.bus,
+                '.'.join('%u' % n for n in self.usb_dev.port_numbers))
+        configurations = usb_dev.configurations()
+        assert len(configurations) == 1
+
+        if usb_reset:
+            usb_dev.reset()
+
+        self._set_configuration(configurations[0].bConfigurationValue)
+
+    def __str__(self):
+        return '%s Debug Probe at %s' % (self.name, self.usb_path)
+
+    def _get_active_configuration(self):
+        try:
+            return self.usb_dev.get_active_configuration()
+        except usb.core.USBError as e:
+            if e.strerror != 'Configuration not set':
+                raise
+        return None
+
+    def _set_configuration(self, bConfigurationValue, force=False):
+        cfg = self._get_active_configuration()
+        if (cfg is None or cfg.bConfigurationValue != bConfigurationValue or
+                force):
+            usb.util.dispose_resources(self.usb_dev)
+            self.usb_dev.set_configuration(bConfigurationValue)
+
+    def show_info(self):
+        print(('============= %s Bus 0x%02X Address 0x%02X %04X:%04X '
+               '=============' % (self.name, self.usb_dev.bus,
+                                  self.usb_dev.address, self.usb_dev.idVendor,
+                                  self.usb_dev.idProduct)))
+        print(' Manufacturer: %s' % self.usb_dev.manufacturer)
+        print('      Product: %s' % self.usb_dev.product)
+        print('Serial Number: %s' % self.usb_dev.serial_number)
+        print('     USB Path: %s' % self.usb_path)
