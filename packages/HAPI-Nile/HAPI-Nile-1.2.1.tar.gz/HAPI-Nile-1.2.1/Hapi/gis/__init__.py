e635a2e4fab8871e970c80b809897dbdda472039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:18 +0000

    Add package HAPI-Nile/1.2.1/HAPI-Nile-1.2.1.tar.gz

---
+++ packages/HAPI-Nile/HAPI-Nile-1.2.1.tar.gz/HAPI-Nile-1.2.1/Hapi/gis/__init__.py
@@ -0,0 +1,14 @@
+"""
+Created on Sat Apr 24 20:57:45 2021
+
+@author: mofarrag
+"""
+
+
+# from .giscatchment import *
+# from .raster import *
+# from .vector import *
+
+import Hapi.gis.giscatchment as giscatchment
+import Hapi.gis.raster as raster
+import Hapi.gis.vector as vector
