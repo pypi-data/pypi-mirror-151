ffff6cc7753b2b238748c8ca450faeaf17f1532a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:17 +0000

    Add package pydflow/1.0.9/pydflow-1.0.9-py3-none-any.whl

---
+++ packages/pydflow/pydflow-1.0.9-py3-none-any.whl/dflow/__init__.py
@@ -0,0 +1,18 @@
+from .workflow import Workflow
+from .step import Step, argo_range, argo_sequence, argo_len, if_expression
+from .io import (
+    AutonamedDict,
+    Inputs,
+    InputArtifact,
+    InputParameter,
+    Outputs,
+    OutputArtifact,
+    OutputParameter,
+    S3Artifact
+)
+from .steps import Steps
+from .op_template import OPTemplate, ShellOPTemplate, PythonScriptOPTemplate
+from .utils import upload_s3, download_s3, copy_s3, upload_artifact, download_artifact, copy_artifact
+from .executor import Executor, RemoteExecutor, SlurmRemoteExecutor
+from .resource import Resource, SlurmJob
+from .custom_template import SlurmJobTemplate
