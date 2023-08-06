b35c79d1a0ede3992c04369567effd9a7a65cc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:58 +0000

    Add package ucalcs/0.1.2/ucalcs-0.1.2.tar.gz

---
+++ packages/ucalcs/ucalcs-0.1.2.tar.gz/ucalcs-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ucalcs']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['ucalcs = ucalcs.basics:main']}
+
+setup_kwargs = {
+    'name': 'ucalcs',
+    'version': '0.1.2',
+    'description': 'This is a library of simple math operations.',
+    'long_description': '# uCALC FOR SIMPLE CALCULATIONS OF NUMBERS LIST\n---\n\n### This is a library of simple math operations.\n--- \n\n\n\n- You can pass a list of values \u200b\u200bso that it returns a result.\n\n- Use the methods (*some_all, subtract_all, multiply_all, divide_all*).\n\n- The calculation is done in the order sent.',
+    'author': 'onezer00',
+    'author_email': 'caimbe2@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/ucalcs/ucalcs-0.1.2.tar.gz/ucalcs-0.1.2/ucalcs/basics.py
@@ -0,0 +1,34 @@
+class Basics:
+    def __init__(self):
+        self.value = int()
+
+    def some_all(self, *args):
+        for value in args:
+            self.value += value
+        return self.value
+    
+    def subtract_all(self, *args):
+        for value in args:
+            if self.value == 0:
+                self.value = value
+            else:
+                self.value -= value
+        return self.value
+    
+    def multiply_all(self, *args):
+        for value in args:
+            self.value *= value
+            if self.value == 0:
+                self.value = value
+        return self.value
+    
+    def divide_all(self, *args):
+        for value in args:
+            if self.value == 0:
+                self.value = value
+            else:
+                self.value /= value
+        return self.value
+
+def main():
+    return Basics
