21a69171f40a675a6ddd285a10989dcfc826e4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:35 +0000

    Add package fastpf/0.0.7/fastpf-0.0.7-py2.py3-none-any.whl

---
+++ packages/fastpf/fastpf-0.0.7-py2.py3-none-any.whl/fastpf/__init__.py
@@ -0,0 +1,36 @@
+"""
+Performance-first Power Flow implemented in Python with AOT Compilation
+"""
+
+__version__ = "0.0.7"
+
+from .log import logger
+from .log import set_loglevel
+
+from . import powerflow_methods
+
+try:
+    from . import powerflow_methods_cc
+except ImportError:
+    logger.info("Attempting to compile powerflow_methods_cc...")
+    powerflow_methods.compile()
+    from . import powerflow_methods_cc
+
+from .data.parse_matpower_casefile import parse_matpower_casefile
+from .data import testgrids
+from .data import testloads
+
+from .validation import validate_grid
+from .process_grid import process_grid
+from .process_grid import integrate_slacks_for_Yident
+
+from .powerflow import ybusjacobi
+from .powerflow import ybusgaussseidel
+from .powerflow import ybusnewton
+from .powerflow import zbusjacobi
+from .powerflow import compare_methods
+
+from .plotting import *
+
+
+set_loglevel("INFO")
