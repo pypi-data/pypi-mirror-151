7f0e3efa337a9a60b238e0b05985e57b65c74a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package pykawaii/2.0.0/pykawaii-2.0.0.tar.gz

---
+++ packages/pykawaii/pykawaii-2.0.0.tar.gz/pykawaii-2.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='pykawaii',
+    version='2.0.0',
+    license='MIT',
+    author='Okimii',
+    packages=find_packages('pykawaii'),
+    package_dir={'': 'pykawaii'},
+    url='https://github.com/Okimii/pykawaii',
+    keywords='waifu.pics python api wrapper ',
+    install_requires=[
+        'aiohttp'
+    ],
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown'
+)
