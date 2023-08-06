69591f3cdfc67105debf88cb0e4de42d10580522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:00 +0000

    Add package discordCommunity/0.0.6/discordCommunity-0.0.6.tar.gz

---
+++ packages/discordCommunity/discordCommunity-0.0.6.tar.gz/discordCommunity-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+]
+
+setup(
+    name='discordCommunity',
+    version='0.0.6',
+    description='A Community Nuker library for discord.py',
+    long_description=open('README.txt').read(),
+    url='',
+    author='DestructiveLone',
+    author_email='lambo.blac123@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='discord',
+    packages=find_packages(),
+    install_requires=['requests']
+)
