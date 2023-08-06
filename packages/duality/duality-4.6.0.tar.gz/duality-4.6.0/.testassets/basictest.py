00e8e5898c86872bff334b10cbb90c944202a7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:07 +0000

    Add package duality/4.6.0/duality-4.6.0.tar.gz

---
+++ packages/duality/duality-4.6.0.tar.gz/duality-4.6.0/.testassets/basictest.py
@@ -0,0 +1,31 @@
+
+# EXAMPLE OF USE IN SIMPLE APP CREATION.
+
+from duality import Record
+
+t = Record()
+
+class App:
+
+    @t.entry(option_name='init', autoinit=True)
+    def __init__(self, dictionary = {}):
+        self.dictionary = dictionary
+        return
+
+    @t.entry(option_name='zbrajanje')
+    def zbrajanje(self, x = t.store('x', 'float',), y = t.store('y', 'float')):
+        rez = print('Zbroj je: ', x + y)
+        return rez
+
+    @t.entry(option_name='oduzimanje')
+    def oduzimanje(self, x = t.store('x', 'int') , y = t.store('y', 'int')):
+        rez = print('Razlika je: ', x - y)
+        return rez
+
+    def initconfig(self):
+        t.config(type = 'dynamic', queue=True)
+
+a = App()
+
+if __name__ == '__main__':
+    a.initconfig()
