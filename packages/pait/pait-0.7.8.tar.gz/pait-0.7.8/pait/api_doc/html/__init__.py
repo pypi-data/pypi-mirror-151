a9ca73bd925244eb550e73dc9766c2f81c2c4c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:32 +0000

    Add package pait/0.7.8/pait-0.7.8.tar.gz

---
+++ packages/pait/pait-0.7.8.tar.gz/pait-0.7.8/pait/api_doc/html/__init__.py
@@ -0,0 +1,68 @@
+def get_redoc_html(
+    open_api_json_url: str,
+    title: str = "ReDoc",
+) -> str:
+    """copy from https://github.com/Redocly/redoc#tldr"""
+    return f"""
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>{title}</title>
+        <!-- needed for adaptive design -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+
+        <!--
+        ReDoc doesn't change outer page styles
+        -->
+        <style>
+          body {{
+            margin: 0;
+            padding: 0;
+          }}
+        </style>
+      </head>
+      <body>
+        <redoc spec-url='{open_api_json_url}'></redoc>
+        <script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"> </script>
+      </body>
+    </html>
+    """
+
+
+def get_swagger_ui_html(
+    open_api_json_url: str,
+    title: str = "Swagger",
+) -> str:
+
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css">
+    <link rel="shortcut icon" href="https://static1.smartbear.co/swagger/media/assets/swagger_fav.png">
+    <title>{title}</title>
+    </head>
+    <body>
+    <div id="swagger-ui">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
+    <!-- `SwaggerUIBundle` is now available on the page -->
+    <script>
+    const ui = SwaggerUIBundle({{
+        url: '{open_api_json_url}',
+        dom_id: '#swagger-ui',
+        presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+        ],
+        layout: "BaseLayout",
+        deepLinking: true,
+        showExtensions: true,
+        showCommonExtensions: true
+    }})
+    </script>
+    </body>
+    </html>
+    """
