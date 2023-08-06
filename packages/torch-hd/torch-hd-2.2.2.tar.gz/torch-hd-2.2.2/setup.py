fe9d955a0ee5c6f3ac1c79e1ee9d9a8735a0266f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:44 +0000

    Add package torch-hd/2.2.2/torch-hd-2.2.2.tar.gz

---
+++ packages/torch-hd/torch-hd-2.2.2.tar.gz/torch-hd-2.2.2/setup.py
@@ -0,0 +1,28 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="torch-hd",  # use torch-hd on PyPi to install torchhd, torchhd is too similar according to PyPi
+    version="2.2.2",
+    description="Torchhd is a Python library for Hyperdimensional Computing",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hyperdimensional-computing/torchhd",
+    license="MIT",
+    install_requires=[
+        "torch",
+        "pandas",
+        "numpy",
+        "requests",
+    ],
+    packages=find_packages(exclude=["docs", "torchhd.tests", "examples"]),
+    python_requires=">=3.6, <4",
+    project_urls={
+        "Source": "https://github.com/hyperdimensional-computing/torchhd",
+        "Documentation": "https://torchhd.readthedocs.io",
+    },
+)
