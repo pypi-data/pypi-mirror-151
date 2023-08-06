d7cbe986e1b679694c56896695d68a8593cacfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:36 +0000

    Add package mgbasiccalculator/0.0.4/mgbasiccalculator-0.0.4.tar.gz

---
+++ packages/mgbasiccalculator/mgbasiccalculator-0.0.4.tar.gz/mgbasiccalculator-0.0.4/mukulbasiccauculator/__init__.py
@@ -0,0 +1,21 @@
+def add_num(x,y):
+    print("adding numbers")
+    return(x+y)
+
+def sub_num(x,y):
+    print("subtracting numbers")
+    return(x-y)
+
+def mult_num(x,y):
+    print("multiplying numbers")
+    return(x*y)
+
+def div_num(x,y):
+    return(x/y)
+
+def exp_num(x,y):
+    return(x**y)
+
+def try_num(x,y):
+    return(x**y+y**x)
+
