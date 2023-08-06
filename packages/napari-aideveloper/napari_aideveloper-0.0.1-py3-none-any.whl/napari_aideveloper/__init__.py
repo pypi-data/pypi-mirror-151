26bb05725b201d6eda54d4ea706c81aec90a4406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:27 +0000

    Add package napari-aideveloper/0.0.1/napari_aideveloper-0.0.1-py3-none-any.whl

---
+++ packages/napari-aideveloper/napari_aideveloper-0.0.1-py3-none-any.whl/napari_aideveloper/__init__.py
@@ -0,0 +1,12 @@
+
+__version__ = "0.0.1"
+
+
+from ._dock_widget import napari_experimental_provide_dock_widget
+from .aid_bin import*
+from .aid_dl import*
+from . import aid_img
+from .aid_start import*
+from . import model_zoo
+
+
