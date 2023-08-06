1b41554154c99f5be5a48430ef2cbd3f346d0af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:13 +0000

    Add package swbootstrap/0.0.19/swbootstrap-0.0.19.tar.gz

---
+++ packages/swbootstrap/swbootstrap-0.0.19.tar.gz/swbootstrap-0.0.19/src/main/__init__.py
@@ -0,0 +1,26 @@
+import os
+import random
+import sys
+import time
+
+import click
+
+from .cmd import bootstrap_cmd
+
+ABSPATH = os.path.abspath(sys.argv[0])
+ABSPATH = os.path.dirname(ABSPATH)
+
+def create_sw_bootstrap() -> click.core.Group:
+    @click.group()
+    @click.version_option(version='0.0.1')
+    @click.option("-v", "--verbose", count=True, help="verbose for log")
+    def bs(verbose: bool) -> None:
+        print('hello')
+    random.seed(time.time_ns)
+    bs.add_command(bootstrap_cmd)
+    return bs
+
+
+bootstrap = create_sw_bootstrap()
+if __name__ == "__main__":
+    bootstrap()
