65eed6a2ceb9caad795c955157b4ab51e74fbdeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:17 +0000

    Add package podcast-recommendation/0.1.4/podcast_recommendation-0.1.4.tar.gz

---
+++ packages/podcast-recommendation/podcast_recommendation-0.1.4.tar.gz/podcast_recommendation-0.1.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="podcast_recommendation",
+    version="0.1.4",
+    author="Manuel Martinez",
+    author_email="manuelalejandromartinezf@gmail.com",
+    description="Podcast recommendation algorithm in Neo4j",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ManuelAlejandroMartinezFlores/PodcastRecommendation",
+    project_urls={
+        "Bug Tracker": "https://github.com/ManuelAlejandroMartinezFlores/PodcastRecommendation/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    package_data={
+        '': ['data/*.csv']
+    },
+    include_package_data = True,
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
