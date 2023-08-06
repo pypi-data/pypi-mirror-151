49d6b37817bec7829fe88edc289d8e9509938a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:34 +0000

    Add package v3wrapper/0.0.7/v3wrapper-0.0.7.tar.gz

---
+++ packages/v3wrapper/v3wrapper-0.0.7.tar.gz/v3wrapper-0.0.7/v3wrapper/constants.py
@@ -0,0 +1,9 @@
+parameter_to_return_value = {
+    'TEMPERATURE': 'Temperature',
+    'CO2': 'CO2',
+    'HUMIDITY': 'Humidity',
+    'VOC': 'VOC',
+    'DUST_PM_2_5': 'Dust PM2.5',
+    'NOISE': 'Noise',
+    'CAPACITY_COUNT': 'Capacity Count',
+    'AREA_COUNT': 'Area Count'}
