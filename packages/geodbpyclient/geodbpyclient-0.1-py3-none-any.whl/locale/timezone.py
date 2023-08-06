170c08b3b716a9b496035c1948014d3a2605bdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:50 +0000

    Add package geodbpyclient/0.1/geodbpyclient-0.1-py3-none-any.whl

---
+++ packages/geodbpyclient/geodbpyclient-0.1-py3-none-any.whl/locale/timezone.py
@@ -0,0 +1,37 @@
+import requests
+
+from utils.config import serverUrl, headers
+
+
+class TimeZones:
+    def __init__(self, apiToken):
+        self.token = apiToken
+
+    path = "/v1/locale/timezones"
+
+    def get_all(self, limit=5, offset=0):
+        params = {
+            "limit": str(limit),
+            "offset": str(offset)
+        }
+        response = requests.request("GET",
+                                    serverUrl() + self.path,
+                                    headers=headers(self.token),
+                                    params=params)
+        return response.json()
+
+    def get_by_id(self, zoneId):
+        response = requests.request("GET",
+                                    serverUrl() + self.path + "/" + zoneId,
+                                    headers=headers(self.token),)
+        return response.json()
+
+    def time_in_zone(self, zoneId):
+        response = requests.request("GET",
+                                    serverUrl() + self.path + "/" + zoneId + "/" + "time",
+                                    headers=headers(self.token), )
+        return response.json()
+
+
+if __name__ == "__main__":
+    print(TimeZones("402acade62msh388e91d32480224p1e4fd2jsn998d47344407").time_in_zone('America__Cuiaba'))
