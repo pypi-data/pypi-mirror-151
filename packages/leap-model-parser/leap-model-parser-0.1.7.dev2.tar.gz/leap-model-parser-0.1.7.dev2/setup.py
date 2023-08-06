fd04bf52a242cfa583ef6bf8ddca58defdb139b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:00 +0000

    Add package leap-model-parser/0.1.7.dev2/leap-model-parser-0.1.7.dev2.tar.gz

---
+++ packages/leap-model-parser/leap-model-parser-0.1.7.dev2.tar.gz/leap-model-parser-0.1.7.dev2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['leap_model_parser', 'leap_model_parser.contract']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['keras-data-format-converter==0.0.8',
+ 'numpy>=1.22.3,<2.0.0',
+ 'onnx2kerastl==0.0.33.dev1',
+ 'onnx>=1.11.0,<2.0.0',
+ 'tensorflow>=2.8,<3.0']
+
+setup_kwargs = {
+    'name': 'leap-model-parser',
+    'version': '0.1.7.dev2',
+    'description': '',
+    'long_description': '# Tensorleap model parser\nUsed to parse model to the import format \n',
+    'author': 'idan',
+    'author_email': 'idan.yogev@tensorleap.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tensorleap/leap-model-parser',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
