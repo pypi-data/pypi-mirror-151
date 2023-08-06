7ec6df5e6c2964def2fba7d877629e58925b5710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:42 +0000

    Add package iuguclient/0.1.5/iuguclient-0.1.5-py3-none-any.whl

---
+++ packages/iuguclient/iuguclient-0.1.5-py3-none-any.whl/iuguclient/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from iuguclient.iuguapi import IuguApi, config
+from iuguclient.token import Token
+from iuguclient.customer import Customer, PaymentMethod
+from iuguclient.plan import Plan
+from iuguclient.subscription import Subscription
+from iuguclient.transfer import Transfer
+from iuguclient.marketplace import MarketPlace
+from iuguclient.invoice import Invoice
+from iuguclient.version import __version__
