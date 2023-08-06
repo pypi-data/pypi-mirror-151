7f633ad68e974ba760740945f3f743f4ad5cd261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:55 +0000

    Add package xmlApiParse/1.2.5/xmlApiParse-1.2.5.tar.gz

---
+++ packages/xmlApiParse/xmlApiParse-1.2.5.tar.gz/xmlApiParse-1.2.5/xmlApiParse/enums.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class SpecificCollectionsEnum(Enum):
+    order_types = 'UNCHANGEABLEORDERTYPES'
+    scheme_details = 'modischemedetails'
+    currencies = 'CURRENCIES'
+    menu_items = 'MENUITEMS'
+    employees = 'Employees'
+    modidiers = 'MODIFIERS'
+    tables = 'Tables'
+    cashes = 'Cashes'
+    classificator = 'CLASSIFICATORGROUPS'
+
+
+class AllCollectionsEnum(Enum):
+    system_info = 'GetSystemInfo'
+    order_list = 'GetOrderList'
+    reference = 'GetRefList'
