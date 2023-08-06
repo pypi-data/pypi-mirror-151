7160e924f277ec077abb1106943343cca21211cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:14 +0000

    Add package netkeiber/1.0.8/netkeiber-1.0.8.tar.gz

---
+++ packages/netkeiber/netkeiber-1.0.8.tar.gz/netkeiber-1.0.8/netkeiber/Library/netkeiberConfiger.py
@@ -0,0 +1,25 @@
+from LibHanger.Library.uwGlobals import configer
+from netkeiber.Library.netkeiberGlobals import *
+
+class netkeiberConfiger(configer):
+    
+    """
+    netkeiber共通設定クラス
+    """
+    
+    def __init__(self, tgv:netkeiberGlobal, file, configFolderName):
+        
+        """
+        コンストラクタ
+        """
+        
+        # 基底側コンストラクタ
+        super().__init__(tgv, file, configFolderName)
+        
+        # netkeibar.ini
+        da = netkeiberConfig()
+        da.getConfig(file, configFolderName)
+
+        # gvセット
+        tgv.netkeiberConfig = da
+        
