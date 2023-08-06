c11306ebdaada5d082b84198072efd31e8f18a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:17 +0000

    Add package persuader-technology-automata-trade-transform-repository/0.0.3/persuader-technology-automata-trade-transform-repository-0.0.3.tar.gz

---
+++ packages/persuader-technology-automata-trade-transform-repository/persuader-technology-automata-trade-transform-repository-0.0.3.tar.gz/persuader-technology-automata-trade-transform-repository-0.0.3/tradetransformrepo/repository/serialize/trade_transform_deserializer.py
@@ -0,0 +1,10 @@
+from coreutility.collection.dictionary_utility import as_data
+
+from tradetransformrepo.TradeTransform import TradeTransform
+
+
+def deserialize_trade_transform(trade_transform) -> TradeTransform:
+    trade = as_data(trade_transform, 'trade')
+    transform = as_data(trade_transform, 'transform')
+    deserialized = TradeTransform(trade, transform)
+    return deserialized
