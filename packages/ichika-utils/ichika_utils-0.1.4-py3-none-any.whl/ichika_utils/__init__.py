ac336445244fb7f2d8ab37e3658333a61830474c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:56 +0000

    Add package ichika-utils/0.1.4/ichika_utils-0.1.4-py3-none-any.whl

---
+++ packages/ichika-utils/ichika_utils-0.1.4-py3-none-any.whl/ichika_utils/__init__.py
@@ -0,0 +1,5 @@
+from .main import IchikaUtils
+from .std import StandardDeviationCalc
+from .lin_regression import LinRegression
+
+__all__ = ["IchikaUtils", "StandardDeviationCalc", "LinRegression"]
