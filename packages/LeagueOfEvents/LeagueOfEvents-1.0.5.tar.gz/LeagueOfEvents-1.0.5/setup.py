3e71a37c6745421fb99f7b30cedd7bbc8b006944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:39 +0000

    Add package LeagueOfEvents/1.0.5/LeagueOfEvents-1.0.5.tar.gz

---
+++ packages/LeagueOfEvents/LeagueOfEvents-1.0.5.tar.gz/LeagueOfEvents-1.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+NAME = "LeagueOfEvents"
+DESCRIPTION = "Create an event system for the League of Legends live game API"
+URL = "https://github.com/agroth01/LeagueOfEvents"
+EMAIL = "software.agroth@gmail.com"
+AUTHOR = "Alexander Groth"
+VERSION = "1.0.5"
+REQUIRED = [
+    "requests",
+    "urllib3",
+    "collections"
+]
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    author = AUTHOR,
+    author_email = EMAIL,
+    install_requires = REQUIRED,
+    url = URL,
+    packages = ["leagueofevents"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires = ">=3.8"
+)
