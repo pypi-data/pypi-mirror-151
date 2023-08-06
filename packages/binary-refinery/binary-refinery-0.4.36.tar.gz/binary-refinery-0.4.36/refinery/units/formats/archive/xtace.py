0e4080f29b8f7dbbdab2fbdf784765403f5ef350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:45 +0000

    Add package binary-refinery/0.4.36/binary-refinery-0.4.36.tar.gz

---
+++ packages/binary-refinery/binary-refinery-0.4.36.tar.gz/binary-refinery-0.4.36/refinery/units/formats/archive/xtace.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from refinery.units.formats.archive import ArchiveUnit
+from refinery.lib.thirdparty import acefile
+from refinery.lib.structures import MemoryFile
+
+
+class xtace(ArchiveUnit):
+    """
+    Extract files from an ACE archive.
+    """
+    def unpack(self, data):
+        ace = acefile.open(MemoryFile(data, read_as_bytes=True))
+        for member in ace.getmembers():
+            member: acefile.AceMember
+            comment = {} if not member.comment else {'comment': member.comment}
+            yield self._pack(
+                member.filename,
+                member.datetime,
+                lambda a=ace, m=member: a.read(m, pwd=self.args.pwd),
+                **comment
+            )
+
+    @classmethod
+    def handles(cls, data: bytearray) -> bool:
+        view = memoryview(data)
+        return b'**ACE**' in view[:0x1000]
