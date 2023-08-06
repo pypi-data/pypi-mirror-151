02fc3186ca6dd36b21f4b61fac5b243f236eae39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:01 +0000

    Add package hicham/0.3/hicham-0.3.tar.gz

---
+++ packages/hicham/hicham-0.3.tar.gz/hicham-0.3/hicham/hicham.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+class Hicham(): 
+  
+    def age(self):
+        
+        current_year = date.today().year
+        
+        print("age = " + str(current_year-1997))
+        
+    def nom(self): 
+        
+        print("nom : ZEKRI")
+        
+    def prenom(self): 
+        
+        print("prénom : Sidi Mohamed Hicham")
+        
+    def fonction(self): 
+        
+        print( "informaticien")
+        
