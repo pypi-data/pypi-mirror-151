f41ce3bceb207bb084079e2661299b5d250027d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:53 +0000

    Add package netproxy/0.1.0/netproxy-0.1.0.tar.gz

---
+++ packages/netproxy/netproxy-0.1.0.tar.gz/netproxy-0.1.0/src/netproxy/components/listeners.py
@@ -0,0 +1,29 @@
+from typing import TYPE_CHECKING, Literal
+
+from ..config import Endpoint, Listener
+from .http_proxy import HttpProxyFactory
+
+if TYPE_CHECKING:
+    from ..context import NetProxy
+
+
+class HttpProxyListener(Listener):
+    type:Literal['http_proxy'] = "http_proxy"
+    def _create_factory(self, context: "NetProxy"):
+        return HttpProxyFactory(context)
+
+Listener.register(HttpProxyListener)
+
+class WsgiListener(Listener):
+    type:Literal['wsgi'] = 'wsgi'
+    app:str
+    def _create_factory(self, context: "NetProxy"):
+        return context.wsgi[self.app]
+
+Listener.register(WsgiListener)
+
+class ReverseProxy(Listener):
+    type:Literal['reverse_proxy'] = "reverse_proxy"
+    def _create_factory(self, context: "NetProxy"):
+        return context.sites.use(self)
+
