5932430e459991d4361721cbe4e240d872b85194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:33 +0000

    Add package pawnstore/0.1.1/pawnstore-0.1.1-py3-none-any.whl

---
+++ packages/pawnstore/pawnstore-0.1.1-py3-none-any.whl/pawnstore/__init__.py
@@ -0,0 +1,28 @@
+import logging
+from .models import Game
+
+from pawnstore.chesscom import CHESSCOM
+from pawnstore.lichess import LICHESS
+
+__app__ = "pawnstore"
+__version__ = "0.1.1"
+
+logger = logging.getLogger(__name__)
+
+
+def pawnstore(chesscom=None, lichess=None, full=False):
+
+    if chesscom:
+        CHESSCOM.sync(chesscom, full)
+
+    if lichess:
+        LICHESS.sync(lichess, full)
+
+    return (
+        Game.select()
+        .where(
+            ((Game.user == chesscom) & (Game.website == CHESSCOM.name))
+            | ((Game.user == lichess) & (Game.website == LICHESS.name))
+        )
+        .order_by(Game.timestamp)
+    )
