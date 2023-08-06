71fbaffedfd23d3b3563e58c8d35e44d79a343a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:50 +0000

    Add package SemiSupervisedLearningDNX/3.3/SemiSupervisedLearningDNX-3.3.tar.gz

---
+++ packages/SemiSupervisedLearningDNX/SemiSupervisedLearningDNX-3.3.tar.gz/SemiSupervisedLearningDNX-3.3/README.md
@@ -0,0 +1,19 @@
+![](https://raw.githubusercontent.com/dpr1005/Semisupervised-learning-and-instance-selection-methods/main/branding/is-ssl-2-flatten.png)
+
+---
+
+# Semi-Supervised Algorithms
+
+This project has the following algorithms:
+- Co-Training
+- Tri-Training
+- Democratic Co-Learning
+- Density Peaks
+  - STDPNF
+
+- Ensemble
+  - RUSSEL
+
+# Release Notes
+
+See the commit logs at https://github.com/dpr1005/Semisupervised-learning-and-instance-selection-methods.
---
+++ packages/SemiSupervisedLearningDNX/SemiSupervisedLearningDNX-3.3.tar.gz/SemiSupervisedLearningDNX-3.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SemiSupervisedLearningDNX',
+    version='3.3',
+    license='MIT',
+    description='Semi Supervised Learning Algorithms.',
+    author='Daniel Puente Ramírez',
+    author_email='dani.pr21@gmail.com',
+    url='https://github.com/dpr1005/Semisupervised-learning-and-instance-selection-methods',
+    download_url='https://github.com/dpr1005/Semisupervised-learning-and-instance-selection-methods',
+    keywords=['Semi Supervised Learning', 'ML', 'IA']
+)
