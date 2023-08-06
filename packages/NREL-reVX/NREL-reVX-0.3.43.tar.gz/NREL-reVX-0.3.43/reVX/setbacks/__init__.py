749a5e096bb031f3ec3d0a568eea1a78b3677aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:18 +0000

    Add package NREL-reVX/0.3.43/NREL-reVX-0.3.43.tar.gz

---
+++ packages/NREL-reVX/NREL-reVX-0.3.43.tar.gz/NREL-reVX-0.3.43/reVX/setbacks/__init__.py
@@ -0,0 +1,8 @@
+
+# -*- coding: utf-8 -*-
+"""
+Compute setbacks
+"""
+from .wind_setbacks import (StructureWindSetbacks, RoadWindSetbacks,
+                            RailWindSetbacks, TransmissionWindSetbacks)
+from .solar_setbacks import ParcelSetbacks
