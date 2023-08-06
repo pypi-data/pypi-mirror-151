dbc471643385456955a18378c458ca2c4e5d4d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:27 +0000

    Add package quickstats/0.6.3.2/quickstats-0.6.3.2.tar.gz

---
+++ packages/quickstats/quickstats-0.6.3.2.tar.gz/quickstats-0.6.3.2/quickstats/components/workspaces/__init__.py
@@ -0,0 +1,7 @@
+from quickstats.components.workspaces.xml_ws_base import XMLWSBase, WSObjectType, XMLAttribType
+from quickstats.components.workspaces.sample import Sample
+from quickstats.components.workspaces.systematic import Systematic
+from quickstats.components.workspaces.asimov_handler import AsimovHandler
+from quickstats.components.workspaces.xml_ws_builder import XMLWSBuilder
+from quickstats.components.workspaces.xml_ws_modifier import XMLWSModifier
+from quickstats.components.workspaces.ws_comparer import WSComparer
