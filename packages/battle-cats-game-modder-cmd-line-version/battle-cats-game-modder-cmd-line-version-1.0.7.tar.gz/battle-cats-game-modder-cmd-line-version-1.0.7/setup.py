604b2e02a723ffd5acdfbbd255ef8990033ac2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:34 +0000

    Add package battle-cats-game-modder-cmd-line-version/1.0.7/battle-cats-game-modder-cmd-line-version-1.0.7.tar.gz

---
+++ packages/battle-cats-game-modder-cmd-line-version/battle-cats-game-modder-cmd-line-version-1.0.7.tar.gz/battle-cats-game-modder-cmd-line-version-1.0.7/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open(r"I:\coding\python\BCGM-python-cmd-line-version\src\BCGM_PY_CMD\files\version.txt", "r", encoding="utf-8") as fh:
+    version = fh.read()
+
+setuptools.setup(
+    name="battle-cats-game-modder-cmd-line-version",
+    version=version,
+    author="jo912345",
+    description="A battle cats tool for modifying, encrypting, and decrypting game files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/j0912345/BCGM-Python-cmd-line-version",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"" : "src"},
+    packages=setuptools.find_packages(where="src"),
+    data_files=[('files', ['src/BCGM_PY_CMD/files/version.txt'])],
+    python_requires=">=3.6",
+    install_requires=[
+        "colored", "tk", "alive-progress", "pycryptodomex", "requests"
+    ],
+    include_package_data=True
+)
