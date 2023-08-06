e706c95ec6aad8818dfb67e6ff91c04e181a9a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:10 +0000

    Add package prefect/2.0b5/prefect-2.0b5.tar.gz

---
+++ packages/prefect/prefect-2.0b5.tar.gz/prefect-2.0b5/src/prefect/orion/api/__init__.py
@@ -0,0 +1,21 @@
+from . import (
+    admin,
+    flows,
+    run_history,
+    flow_runs,
+    task_runs,
+    flow_run_states,
+    task_run_states,
+    deployments,
+    saved_searches,
+    dependencies,
+    logs,
+    concurrency_limits,
+    blocks,
+    work_queues,
+    block_specs,
+    ui,
+    root,
+    # Server relies on all of the above routes
+    server,
+)
---
+++ packages/prefect/prefect-2.0b5.tar.gz/prefect-2.0b5/src/prefect/orion/models/__init__.py
@@ -0,0 +1,16 @@
+from . import (
+    block_specs,
+    blocks,
+    configuration,
+    deployments,
+    concurrency_limits,
+    flow_run_states,
+    flow_runs,
+    flows,
+    task_run_states,
+    task_runs,
+    saved_searches,
+    logs,
+    work_queues,
+    agents,
+)
