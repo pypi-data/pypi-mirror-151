4e9518c9976a0d6086f42a85d7628638ca942cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:31 +0000

    Add package ray-for-mars/1.12.0/ray_for_mars-1.12.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/ray-for-mars/ray_for_mars-1.12.0-cp39-cp39-manylinux2014_x86_64.whl/ray_for_mars-1.12.0.data/purelib/ray/dashboard/client/build/precache-manifest.2f5ec4653a2174f5d35effb0b5726593.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76cb465f283b46447fcbc168774a357",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7bcde1b4ecfe92cafae",
+    "url": "./static/css/10.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "f8b05554e7df3c2f5dca",
+    "url": "./static/css/11.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "90cc599a65c0b7636e59",
+    "url": "./static/css/12.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "7537fc37486b71f6bd02",
+    "url": "./static/css/8.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "653585053941175fb3e5",
+    "url": "./static/js/0.03c9b48c.chunk.js"
+  },
+  {
+    "revision": "6aecd547c67c02937fa2",
+    "url": "./static/js/1.51f3a734.chunk.js"
+  },
+  {
+    "revision": "a7bcde1b4ecfe92cafae",
+    "url": "./static/js/10.fbc8be71.chunk.js"
+  },
+  {
+    "revision": "f8b05554e7df3c2f5dca",
+    "url": "./static/js/11.2f9be1e1.chunk.js"
+  },
+  {
+    "revision": "90cc599a65c0b7636e59",
+    "url": "./static/js/12.e34738f9.chunk.js"
+  },
+  {
+    "revision": "e29ce2ebcf6f54450fcd",
+    "url": "./static/js/13.099678e7.chunk.js"
+  },
+  {
+    "revision": "305ad15162b78112922b",
+    "url": "./static/js/14.8af99807.chunk.js"
+  },
+  {
+    "revision": "05da7e66a3ae09e4884b",
+    "url": "./static/js/15.4820511a.chunk.js"
+  },
+  {
+    "revision": "34186b522208ceb82bae",
+    "url": "./static/js/16.e0e4d862.chunk.js"
+  },
+  {
+    "revision": "59c3243ac18a180f44ac",
+    "url": "./static/js/17.54c685e8.chunk.js"
+  },
+  {
+    "revision": "b6698cbe8e90035dc8ff",
+    "url": "./static/js/2.91748bc4.chunk.js"
+  },
+  {
+    "revision": "393f39c5ad92eff1ce19",
+    "url": "./static/js/3.34779e0e.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "./static/js/3.34779e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c197410a412687188ba2",
+    "url": "./static/js/4.99eb0746.chunk.js"
+  },
+  {
+    "revision": "b52e1665624e7d011a4b",
+    "url": "./static/js/7.c5827967.chunk.js"
+  },
+  {
+    "revision": "b9d59c7bf327b5c961fd3d7120589581",
+    "url": "./static/js/7.c5827967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7537fc37486b71f6bd02",
+    "url": "./static/js/8.e2145db5.chunk.js"
+  },
+  {
+    "revision": "1a9cf335b97c02c3be7b",
+    "url": "./static/js/9.ccc0448a.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "./static/js/9.ccc0448a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c6211a45e6381201a2",
+    "url": "./static/js/main.c9764377.chunk.js"
+  },
+  {
+    "revision": "35224dac2a50e61da22e",
+    "url": "./static/js/runtime-main.ea705087.js"
+  },
+  {
+    "revision": "409917799d72263e1774be450d65fe16",
+    "url": "./static/media/logo.40991779.svg"
+  }
+]);
