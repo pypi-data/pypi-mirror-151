e9ad8f2c9651c3bdeb680bb85c6ddaeaa8b16832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:39 +0000

    Add package mathician/1.3.3/Mathician-1.3.3.tar.gz

---
+++ packages/mathician/Mathician-1.3.3.tar.gz/Mathician-1.3.3/mathician/__init__.py
@@ -0,0 +1,33 @@
+def square(num1):
+  return num1*num1
+
+def add(num1, num2):
+  return num1+num2
+
+def subtract(num1, num2):
+  return num1-num2
+
+def multiply(num1, num2):
+  return num1*num2
+
+def divide(num1, num2):
+  return num1/num2
+
+def squareroot(num1):
+  return num1/num1
+
+def randomnumber(num1, num2):
+  finalanswer = range(num1, num2)
+  return finalanswer
+
+def stringlettercount(String):
+  stringletters = len(String)
+  return stringletters
+
+def fahrenheit_to_celsius(num1):
+  answer = num1 - 32 * 0.5556
+  return answer
+
+def celsius_to_fahrenheit(num1):
+  answer = num1 * 1.8 + 32
+  return answer
