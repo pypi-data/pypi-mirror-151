235ce145983ddff5600204ee99c70cc448d56e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:07 +0000

    Add package dhlab/2.2.1/dhlab-2.2.1-py3-none-any.whl

---
+++ packages/dhlab/dhlab-2.2.1-py3-none-any.whl/dhlab/__init__.py
@@ -0,0 +1,16 @@
+# legacy code
+from dhlab.text import nbtokenizer
+from dhlab.legacy import (
+    nbtext,
+    graph_networkx_louvain,
+    token_map,
+    nbpictures,
+    nb_external_files,
+    module_update
+)
+
+# code from further down in the code tree
+from dhlab.text.corpus import Corpus
+from dhlab.text.conc_coll import Collocations, Concordance
+from dhlab.text.geo_data import GeoData
+from dhlab.ngram.ngram import Ngram, NgramBook, NgramNews
