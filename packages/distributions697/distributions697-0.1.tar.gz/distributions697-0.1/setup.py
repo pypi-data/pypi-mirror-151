798a8c022f1ffa8daf7be20c6cb6bf7b2c46872f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:11 +0000

    Add package distributions697/0.1/distributions697-0.1.tar.gz

---
+++ packages/distributions697/distributions697-0.1.tar.gz/distributions697-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions697',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions697'],
+      author = 'Alexander Hepel',
+      zip_safe=False)
