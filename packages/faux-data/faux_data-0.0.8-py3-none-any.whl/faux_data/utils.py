aee4e1f10f79da8a89b5f904c849dca0d2254171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:53 +0000

    Add package faux-data/0.0.8/faux_data-0.0.8-py3-none-any.whl

---
+++ packages/faux-data/faux_data-0.0.8-py3-none-any.whl/faux_data/utils.py
@@ -0,0 +1,22 @@
+import re
+
+
+def get_parts(val: str):
+    """Splits a string into parts respecting double and single quotes
+
+    Examples:
+        >>> get_parts("mycol Random Timestamp \"2023-03-03 00:00:00\" '2026-12-12 23:59:59'")
+        ["mycol", "Random", "Timestamp", "2023-03-03 00:00:00", "2026-12-12 23:59:59"]
+
+    """
+    groups = re.findall(r"[ ]?(?:(?!\"|')(\S+)|(?:\"|')(.+?)(?:\"|'))[ ]?",
+                        val)
+
+    # there are two matching groups for the two cases so get the first non empty val
+    def first_non_empty(g):
+        if g[0]:
+            return g[0]
+        else:
+            return g[1]
+
+    return [first_non_empty(group) for group in groups]
