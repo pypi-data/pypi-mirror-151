99683fa080f1d8e7498f00073f5ef6a63edde829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:10 +0000

    Add package pyqt-windows-buttons-widget/0.0.14/pyqt_windows_buttons_widget-0.0.14-py3-none-any.whl

---
+++ packages/pyqt-windows-buttons-widget/pyqt_windows_buttons_widget-0.0.14-py3-none-any.whl/pyqt_windows_buttons_widget/windowsButtonsWidget.py
@@ -0,0 +1,52 @@
+from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import QWidget
+from pyqt_titlebar_buttons_widget import TitlebarButtonsWidget
+
+
+class WindowsButtonsWidget(TitlebarButtonsWidget):
+    def __init__(self, base_widget: QWidget, hint: list = ['min', 'max', 'close']):
+        super().__init__(base_widget, hint)
+        self.__initUi()
+
+    def __initUi(self):
+        self._minimizeBtn.setText('🗕')
+        self._maximizeBtn.setText('🗖')
+        self._closeBtn.setText('🗙')
+
+        # fill the button's background with color
+        background_color = self._base_widget.palette().color(QPalette.Base).name()
+        for btn in self._btn_dict.values():
+            btn.setBackground(background_color)
+
+        self.__styleInit()
+
+        self.__window = self._base_widget.window()
+        self.__window.installEventFilter(self)
+
+    def __styleInit(self):
+        close_button_style = self._closeBtn.styleSheet() + f'''
+                             QPushButton:hover 
+                             {{ 
+                             background-color: #EE0000; 
+                             color: #ffffff;
+                             }}
+                             QPushButton:pressed
+                             {{
+                             background-color: #CC0000;
+                             }}
+                             '''
+
+        self._closeBtn.setStyleSheet(close_button_style)
+
+    def event(self, e):
+        if e.type() == 100:
+            self.__styleInit()
+        return super().event(e)
+
+    def eventFilter(self, obj, e):
+        if e.type() == 105:
+            if self.__window.isMaximized():
+                self._maximizeBtn.setText('🗗')
+            else:
+                self._maximizeBtn.setText('🗖')
+        return super().eventFilter(obj, e)
