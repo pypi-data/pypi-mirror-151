8ac429b1b9e16c22e34305e44b30a6807288f750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:12 +0000

    Add package frame-semantic-transformer/0.3.1/frame-semantic-transformer-0.3.1.tar.gz

---
+++ packages/frame-semantic-transformer/frame-semantic-transformer-0.3.1.tar.gz/frame-semantic-transformer-0.3.1/frame_semantic_transformer/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.3.1"
+
+from .FrameSemanticTransformer import (
+    FrameSemanticTransformer,
+    DetectFramesResult,
+    FrameElementResult,
+    FrameResult,
+)
+
+__all__ = (
+    "FrameSemanticTransformer",
+    "DetectFramesResult",
+    "FrameElementResult",
+    "FrameResult",
+)
