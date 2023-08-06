be8a6c358cd07958a4ad5790c53c4e4cc756afcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:18 +0000

    Add package fast-stats/0.0.3/fast_stats-0.0.3-cp37-none-win_amd64.whl

---
+++ packages/fast-stats/fast_stats-0.0.3-cp37-none-win_amd64.whl/fast_stats/__init__.py
@@ -0,0 +1,7 @@
+from .fast_stats import (
+    _binary_f1_score_reqs,
+    _binary_precision_reqs,
+    _binary_recall_reqs,
+    _tp_fp_fn_tn,
+)
+from .stats import binary_f1_score, binary_precision, binary_recall
