f0c6fa0d53ec207b735da2fd535de66285d4dbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:02 +0000

    Add package magniv/0.1.24/magniv-0.1.24.tar.gz

---
+++ packages/magniv/magniv-0.1.24.tar.gz/magniv-0.1.24/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+
+with open(str(this_directory) + "/proj_description.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="magniv",
+    description="Magniv Core Library",
+    author_email="hello@magniv.io",
+    url="https://www.magniv.io",
+    project_urls= {
+        "Documentation":"https://docs.magniv.io",
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.24",
+    py_modules=["magniv"],
+    packages=find_packages(),
+    install_requires=["Click", "docker"],
+    entry_points={"console_scripts": ["magniv-cli = magniv.scripts.magniv:cli",],},
+    include_package_data=True,
+)
