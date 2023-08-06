32981849810b9f50a7d1652562904391a9bd7661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:40 +0000

    Add package pdm-self/1.1.0/pdm-self-1.1.0.tar.gz

---
+++ packages/pdm-self/pdm-self-1.1.0.tar.gz/pdm-self-1.1.0/pdm_self/plugin.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+from pdm.cli.commands.base import BaseCommand
+
+
+class SelfCommand(BaseCommand):
+    """ """
+
+    def add_arguments(self, parser):
+        parser.add_argument("update", default=None, help="")
+
+    def handle(self, project, options):
+        if options.update:
+            home_dir = os.getenv("HOME")
+            update_cmd = (
+                f"{home_dir}/.local/share/pdm/venv/bin/python -m pip install -U pdm"
+            )
+            subprocess.run(
+                update_cmd, capture_output=True, text=True, shell=True
+            ).stdout
