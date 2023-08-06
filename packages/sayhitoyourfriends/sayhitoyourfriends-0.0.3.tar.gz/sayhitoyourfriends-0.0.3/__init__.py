f9e40251ebe54abcf9beca0c2e6a9495910d72bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:12 +0000

    Add package sayhitoyourfriends/0.0.3/sayhitoyourfriends-0.0.3.tar.gz

---
+++ packages/sayhitoyourfriends/sayhitoyourfriends-0.0.3.tar.gz/sayhitoyourfriends-0.0.3/__init__.py
@@ -0,0 +1,10 @@
+def hi(name):
+    print("hi" + name)
+def add(num1, num2):
+    print(num1 + num2)
+def subtract(num1, num2):
+    print(num1 - num2)
+def divide(num1, num2):
+    print(num1 / num2)
+def times(num1, num2):
+    print(num1 * num2)
