78e6ca0ad220978783b3140a232d5b3b50feb2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:13 +0000

    Add package mcHelper/6.0/mcHelper-6.0.tar.gz

---
+++ packages/mcHelper/mcHelper-6.0.tar.gz/mcHelper-6.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2022/5/17 10:57 AM
+@Author  : oujiewen2@shoplineapp.com
+@File    : setup.py
+'''
+import setuptools
+
+requirements = []  # 自定义工具中需要的依赖包
+
+setuptools.setup(
+    name="mcHelper",  # 自定义工具包的名字
+    version="6.0",  # 版本号
+    author="oujiewen",  # 作者名字
+    author_email="oujiewen2@shoplineapp.com",  # 作者邮箱
+    description="description",  # 自定义工具包的简介
+    license='MIT-0',  # 许可协议
+    url="",  # 项目开源地址
+    packages=setuptools.find_packages(),  # 自动发现自定义工具包中的所有包和子包
+    install_requires=requirements,  # 安装自定义工具包需要依赖的包
+    python_requires='>=3.5'  # 自定义工具包对于python版本的要求
+)
---
+++ packages/mcHelper/mcHelper-6.0.tar.gz/mcHelper-6.0/test/111.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2022/5/20 11:46 AM
+@Author  : oujiewen2@shoplineapp.com
+@File    : 111.py
+'''
+
+from mcHelpers import mcHelper
+
+mcHelper.mcHelper({}).login_all()
