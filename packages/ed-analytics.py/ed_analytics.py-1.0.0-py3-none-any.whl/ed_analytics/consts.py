f85c62bdfdea622a69219a7b84ff9dc8b24d2dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:39 +0000

    Add package ed-analytics.py/1.0.0/ed_analytics.py-1.0.0-py3-none-any.whl

---
+++ packages/ed-analytics.py/ed_analytics.py-1.0.0-py3-none-any.whl/ed_analytics/consts.py
@@ -0,0 +1,14 @@
+
+NAME = "ed-analytics.py"
+VERSION = "1.0.0"
+
+DESCRIPTION = "Educational Analytics toolsets for programming classrooms."
+
+AUTHOR = "SkylarTown"
+AUTHOR_EMAIL = "skylartwn@gmail.com"
+
+MAINTAINER = "Shreyas Ranganatha"
+MAINTAINER_EMAIL = "shreyass.ranganatha@gmail.com"
+
+if __name__ == '__main__':
+    quit(print(f"{NAME} [{VERSION}]"))
