51546400db85698fcd649955df3b063ff466f1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:03 +0000

    Add package QPT/1.0b3.dev8/QPT-1.0b3.dev8-py3-none-any.whl

---
+++ packages/QPT/QPT-1.0b3.dev8-py3-none-any.whl/qpt/ext/launcher/compatibility_mode.cmd
@@ -0,0 +1,11 @@
+echo on
+cd /d %~dp0
+set QPT_COLOR=False
+set QPT_MODE=Debug
+set PYTHONUNBUFFERED=1
+set PYTHONIOENCODING=utf-8
+set PYTHONPATH=Python/Lib/site-packages;Python/Lib;Python
+set PATH=Python/Lib/site-package;Python/Lib;Python/Scripts;Python;%PATH%
+cls
+"./Python/python.exe" -c "import sys;sys.path.append('./Python');sys.path.append('./Python/Lib/site-packages');sys.path.append('./Python/Scripts');import qpt.run as run"
+pause
