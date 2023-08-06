99ba1cecf90ebff979a6fcde640b41e0e1d9ae6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:16 +0000

    Add package testit-api-client/1.1.1/testit-api-client-1.1.1.tar.gz

---
+++ packages/testit-api-client/testit-api-client-1.1.1.tar.gz/testit-api-client-1.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='testit-api-client',
+    version='1.1.1',
+    description='API-client for Test IT',
+    long_description=open('README.md', "r").read(),
+    long_description_content_type="text/markdown",
+    url='https://pypi.org/project/testit-api-client/',
+    author='Pavel Butuzov',
+    author_email='pavel.butuzov@testit.software',
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    py_modules=['testit', 'testit_api_client'],
+    packages=['testit_api_client'],
+    package_dir={'testit_api_client': 'src'},
+    install_requires=['requests']
+)
