42e1656f18bc999207d837bcb6ceba1a177f0370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:09 +0000

    Add package streamlit-card/0.0.3/streamlit-card-0.0.3.tar.gz

---
+++ packages/streamlit-card/streamlit-card-0.0.3.tar.gz/streamlit-card-0.0.3/streamlit_card/frontend/build/precache-manifest.713b6f348085b3c7c12fd589d8e51ade.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b972f26f7d17a596711",
+    "url": "./static/js/main.c3f1ea43.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9a363c873f777b5725e0",
+    "url": "./static/js/2.921ed9e7.chunk.js"
+  },
+  {
+    "revision": "cc3aab9f5df00072c57346c85b0d9497",
+    "url": "./index.html"
+  }
+];
