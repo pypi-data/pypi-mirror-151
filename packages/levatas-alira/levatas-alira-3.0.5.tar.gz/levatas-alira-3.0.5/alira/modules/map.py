29abf2919c1e69c86df38f5048a076c6bd44a5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:55 +0000

    Add package levatas-alira/3.0.5/levatas-alira-3.0.5.tar.gz

---
+++ packages/levatas-alira/levatas-alira-3.0.5.tar.gz/levatas-alira-3.0.5/alira/modules/map.py
@@ -0,0 +1,29 @@
+from alira.instance import Instance
+from alira.modules.module import Module
+
+
+class Map(Module):
+    def __init__(
+        self,
+        module_id: str = None,
+        function: str = None,
+        **kwargs,
+    ):
+        super().__init__(
+            module_id=module_id or function.split(".")[-1],
+            **kwargs,
+        )
+
+        self.function = function
+
+    def run(self, instance: Instance, **kwargs):
+        fn = self._load_function(self.function)
+        if fn is None:
+            raise RuntimeError(f"Unable to load function {self.function}")
+
+        result = fn(instance=instance)
+
+        if not isinstance(result, dict):
+            raise RuntimeError("The result of the map operation must be a dictionary")
+
+        return result
