340b79e2b01f936f9b804014122a7b431ce87eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:17 +0000

    Add package pyaes256-encrypter/0.0.0/pyaes256_encrypter-0.0.0.tar.gz

---
+++ packages/pyaes256-encrypter/pyaes256_encrypter-0.0.0.tar.gz/pyaes256_encrypter-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='pyaes256_encrypter',
+    version='0.0.0',
+    url='https://github.com/MaiconRenildo/pyaes256_encrypter',
+    license='MIT License',
+    author='Maicon Renildo',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='maicon.renildo1@gmail.com',
+    keywords='aes256',
+    description=u'A package to simplify the use of AES-256 encryption with random initialization vector.',
+)
