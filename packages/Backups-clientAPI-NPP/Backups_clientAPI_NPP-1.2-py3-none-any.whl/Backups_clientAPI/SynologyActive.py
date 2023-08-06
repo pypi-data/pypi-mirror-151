151dc106202a6e215a27765de61cfea13ae546f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:04 +0000

    Add package Backups-clientAPI-NPP/1.2/Backups_clientAPI_NPP-1.2-py3-none-any.whl

---
+++ packages/Backups-clientAPI-NPP/Backups_clientAPI_NPP-1.2-py3-none-any.whl/Backups_clientAPI/SynologyActive.py
@@ -0,0 +1,217 @@
+import datetime
+import requests
+import utils
+from LlocDeCopies import LlocDeCopies
+from Copia import Copia
+
+
+class SynologyActive(LlocDeCopies):
+    def __init__(self, name:str, url:str, user:str, password:str, cookie: str):
+        """Constructor of the SynologyActive class 
+
+        Parameters
+        ----------
+        name : String
+            The object name
+
+        url : String
+            The url to the Synology NAS.
+
+        user : String
+            The user to use for Synology NAS. 
+
+        password : String
+            The password to use for the NAS user 
+
+        apipassw : cookie
+            The cookie for the api. how to get it in readme.
+
+        Returns
+        -------
+        SynologyActive
+            The newly instantiated SynologyActive object
+
+        """
+        self._cookie = cookie
+        super().__init__(name, url, user, password)
+        
+    def retrieve_copies(self, ruta:str, args) -> None:
+        """Saves copies to the array of this object
+        
+        Parameters
+        ----------
+        ruta : String
+            Route to the folder the program is in.
+        args:Namespace
+            arguments of argsparse.
+        """
+        query_parameters = {"api":"SYNO.API.Info", "version":"1", "method":"query", "query":"all"}
+        queryUrl = super().get_url() + "webapi/query.cgi"
+        try:
+            query = requests.get(queryUrl, params=query_parameters, headers={"cookie": self._cookie})
+            query = query.json()
+            path= str(query['data']['SYNO.API.Auth']['path'])
+        except Exception as e:
+            now = datetime.datetime.now()
+            date_string = now.strftime('%Y-%m-%d--%H-%M-%S-Query')
+            a = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+            a.write(str(e))
+            a.close()
+        user = super().get_user()
+        password = super().get_password()
+        try:
+            url = super().get_url()+"webapi/"+ path
+        except:
+            super().add_copies(Copia(super().get_name(), "Error en connectar amb la maquina", datetime.datetime.now(), self))
+            super().set_tempsUltimCheck()
+            return
+        url2 = super().get_url()+"webapi/entry.cgi"
+        nom = super().get_name()
+        try:
+            sid = self.login(user, password, url, args, ruta)
+            self.InfoCopies(url2, sid, args, ruta)
+            self.logout(url, args, ruta)
+        except:
+            now = datetime.datetime.now()
+            date_string = now.strftime('%Y-%m-%d--%H-%M-%S-Conexio')
+            f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+            f.write("Error en connectar amb la maquina "+nom)
+            f.close()
+            super().add_copies(Copia(super().get_name(), "Error en connectar amb la maquina", datetime.datetime.now(), self))
+        super().set_tempsUltimCheck()
+        
+    #Es logueja en la webapi de synology 
+    #Els parametres son les credencials, la url per fer el logeig i la cookie identificacio enlloc de la sid.
+    #Retorna la sid que servira per identificar-nos en les operacions seguents
+    def login(self, user:str, password:str, url:str, args, ruta:str) -> str:
+        """Logs in the api of any v7 synology NAS
+
+
+        Parameters
+        ----------
+        user: String
+            The user of the Synology nas
+
+        passwort: String
+            The password of the user mentioned avobe.
+
+        url: String
+            The url of the Synology NAS.
+
+        args:Namespace
+            arguments of argsparse.
+
+        ruta : String
+            Route to the folder the program is in.
+
+
+        Returns
+        -------
+        String
+            The SID of the newly created session.
+        """
+        login_parameters = {"api":"SYNO.API.Auth", "version":"3", "method":"login", "account": user, "passwd": password, "session":"ActiveBackup", "format":"cookie"}
+        my_headers = {"cookie": self._cookie}
+        
+        try:
+            response = requests.get(url, params=login_parameters, headers=my_headers).json()
+            sid = response['data']['sid']
+        except Exception as e:
+            now = datetime.datetime.now()
+            date_string = now.strftime('%Y-%m-%d--%H-%M-%S-login')
+            f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+            f.write(str(e))
+            f.close()
+        if	response['success'] != True:
+            now = datetime.datetime.now()
+            date_string = now.strftime('%Y-%m-%d--%H-%M-%S-login')
+            f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+            f.write(str(response))
+            f.close()
+        try:
+            return sid
+        except:
+            return None
+
+    #Tanca la sessió anteriorment oberta per la funcio login
+    #Els paramatres es la url del lloc de logout i la sid i la cookie per idenficació
+    #Retorna la resposta de la webapi tan si es error com si es correcte
+    def logout(self, url:str, args, ruta:str)->bool:
+        """Closes all the sessions previously opened with the login method of a given NAS.
+
+        Parameters
+        ----------
+        url:String
+            The url of the Synology NAS to close the sessions with.
+
+        args:Namespace
+            arguments of argsparse.
+
+        ruta : String
+            Route to the folder the program is in.
+        
+        Returns
+        -------
+        bool
+            If it logged out successfully True, otherwise False.
+        """
+        logout_parameters = {"api":"SYNO.API.Auth", "version":"2", "method":"logout", "session":"ActiveBackup"}
+        my_headers={"cookie": self._cookie}
+        response = requests.get(url, params=logout_parameters, headers=my_headers).json()
+        if	response['success'] != True:
+            now = datetime.datetime.now()
+            date_string = now.strftime('%Y-%m-%d--%H-%M-%S-Logout')
+            f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+            f.write(str(response))
+            f.close()
+            return False
+        return True
+
+    #Aconsegueix la informacio de les copies de seguretat de un NAS
+    #Els parametres son la sid i la cookie per identificació i la url del NAS al cual recolectar les dades
+    def InfoCopies(self, url:str, sid:str, args, ruta:str)->None:
+        """Retrieve the copies from the given Synology NAS url.
+
+        Parameters
+        ----------
+        url:String
+            The url of the Synology NAS.
+
+        sid: String
+            The sid of the opened session.
+
+        args:Namespace
+            arguments of argsparse.
+
+        ruta : String
+            Route to the folder the program is in.
+        
+        
+        """
+        copies_parameters = {"api":"SYNO.ActiveBackup.Overview", "version":"1", "method":"list_device_transfer_size", "time_start": int(super().get_tempsUltimCheck())-args.date, "time_end": utils.temps(), "_sid": sid}
+        response = requests.get(url, params=copies_parameters, headers={"cookie": self._cookie}).json()
+        if	response['success'] == True:
+            x = 0
+            while x < int(response['data']['total']):
+                numCopies = len(response['data']['device_list'][x]['transfer_list']) -1
+                if not(numCopies < 0):
+                    try:
+                        nomCopia = response['data']['device_list'][x]['transfer_list'][numCopies]['device_name']
+                        status = response['data']['device_list'][x]['transfer_list'][numCopies]['status']
+                        try:
+                            temps = utils.utcToTime(response['data']['device_list'][x]['transfer_list'][numCopies]['time_end'])
+                        except Exception as e:
+                            temps = utils.utcToTime(super().get_tempsUltimCheck())
+                    except:
+                        pass
+                    else:
+                        super().add_copies(Copia(nomCopia, utils.statusConvertor(status), temps, self))
+                x+=1
+            
+        else:
+            now = datetime.datetime.now()
+            date_string = now.strftime('%Y-%m-%d--%H-%M-%S-Backups')
+            f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+            f.write(str(response))
+            f.close()
+            super().add_copies(Copia(id, "Fallo en extraccio de dades", datetime.datetime.now(), self))
---
+++ packages/Backups-clientAPI-NPP/Backups_clientAPI_NPP-1.2-py3-none-any.whl/Backups_clientAPI/__main__.py
@@ -0,0 +1,222 @@
+import argparse
+import datetime
+import json
+import os
+from importlib_metadata import List
+import mysql.connector
+import yaml
+from os.path import exists
+
+####Local imports###
+from LlocDeCopies import LlocDeCopies
+
+from SynologyActive import SynologyActive
+from mspbackup import mspbackup
+from SynologyHyper import SynologyHyper
+from Pandora import Pandora
+
+
+
+__version__ = "1.2"
+
+def main(args:argparse.Namespace=None):
+	"""basic body of the program; retrieves the data from the APIs and it saves it in the given database
+
+	Parameters
+	----------
+	args:Namespace
+		Arguments entered through the command line
+
+	"""
+
+	global ruta
+	ruta = os.path.dirname(os.path.abspath(__file__))
+	parser = argparse.ArgumentParser(description='Una API per a recullir invormacio de varis NAS Synology que tinguin la versio 6 o mes.', epilog="Per configuracio adicional anar a config/config.yaml")
+	parser.add_argument('-q', '--quiet', help='Nomes mostra els errors i el missatge de acabada per pantalla.', action="store_false")
+	parser.add_argument('--portable-chrome-path', help="La ruta del executable de chrome", default=None, metavar="RUTA")
+	parser.add_argument('-tr','--tesseractpath', help='La ruta fins al fitxer tesseract.exe', default=ruta+'/tesseract/tesseract.exe', metavar='RUTA')
+	parser.add_argument('-g', '--graphicUI', help='Mostra el navegador graficament.', action="store_false")
+	parser.add_argument('--json-file', help='La ruta(fitxer no inclos) a on es guardara el fitxer de dades json. Per defecte es:'+ruta, default=ruta, metavar='RUTA')
+	parser.add_argument('-d', '--date', type=int, help='La cantitat de temps (en segons) enrere que agafara les dades de copies. Per defecte es 2592000(un mes)', default=2592000, metavar='SEC')
+	parser.add_argument('-v', '--versio', help='Mostra la versio', action='version', version='Backups_clientAPI-NPP v' + __version__)
+	args = parser.parse_args(args)
+	global conf
+	conf = ruta+"/config/config.yaml"
+	global current_transaction
+	current_transaction = 2
+
+	mydb = initialize()
+
+	nasos = retrieveData(mydb, args)
+
+
+
+	if not(saveDataDB(nasos)):
+		saveJSON(nasos, ruta, args)
+
+
+#Aquesta funció cre les carpetes i fitxers necesarris, segidament inicia la connexio a la DB i retorna aquesta
+# si no consegueix conectar tanca el programa
+def initialize():
+	"""Create the folders necessary for the program and iniciates the database connection taking the credentials from the config file.
+	If it doesn't manage to connect to the database it quits the program.
+
+	Returns
+	-------
+	MySQLConnection
+		Connection to hte the database.
+
+	"""
+	if not(os.path.exists(ruta+"/errorLogs")):
+		os.mkdir(ruta+"/errorLogs")
+	if not(os.path.exists(ruta+"/config")):
+		os.mkdir(ruta+"/config")
+	if not(exists(conf)):
+		print("Emplena el fitxer de configuracio de Base de Dades a config/config.yaml")
+		article_info = [
+			{
+				'BD': {
+				'host' : 'localhost',
+				'user': 'root',
+				'passwd': 'patata',
+				'database':'backups'
+				}
+			}
+		]
+		if not(os.path.exists(ruta+"/config")):
+			os.mkdir(ruta+"/config")
+		with open(conf, 'w') as yamlfile:
+			data = yaml.dump(article_info, yamlfile)
+
+	with open(conf, "r") as yamlfile:
+		data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+	servidorBD = data[0]['BD']['host']
+	usuariBD = data[0]['BD']['user']
+	contrassenyaBD = data[0]['BD']['passwd']
+	database = data[0]['BD']['database']
+	try:
+		mydb =mysql.connector.connect(
+    	    host=servidorBD,
+    	    user=usuariBD,
+    	    password=contrassenyaBD,
+    	    database=database
+    	    )
+		mycursor = mydb.cursor(buffered=True)
+	except:
+		try:        
+			mydb =mysql.connector.connect(
+	            host=servidorBD,
+	            user=usuariBD,
+	            password=contrassenyaBD
+	            )
+			print("Base de dades no existeix, creant-la ...")
+			mycursor = mydb.cursor(buffered=True)
+			mycursor.execute("CREATE DATABASE backups")
+			mydb =mysql.connector.connect(
+	            host=servidorBD,
+	            user=usuariBD,
+	            password=contrassenyaBD,
+	            database=database
+	            )
+			mycursor = mydb.cursor(buffered=True)
+			mycursor.execute("CREATE TABLE credencials (nom name(25), url VARCHAR(100), TipusCopies VARCHAR(25), user VARCHAR(45), password VARCHAR(45));")
+		except:
+			print("Login BDD incorrecte")
+			quit()
+	return mydb
+
+
+#Aquesta funció recull les dades de la base de dades i les transforma en objectes de la classe pertinent 
+#tambe executa la funcio retrieve_Copies de cada LlocDeCopies la cual afegeix totes les copies a l'array de copies de cada LlocDeCopies
+#S'he l'hi ha de donar la connexio a la base de dades junt amb els arguments i retorna una llista de llocDeCopies
+def retrieveData(mydb, args:argparse.Namespace)-> List[LlocDeCopies]:
+	"""Retrieves all the LlocDeCopies from a given database.
+	It also gets the copies from each LlocDeCopies and saves them in the corresponding LlocDeCopies object.
+	
+	Parameters
+	----------
+	mydb:MySQLConnection
+		Connection to the MySQL database.
+	args:Namespace
+		arguments of argsparse.
+
+	Returns
+	-------
+	List[LlocDeCopies]
+		List of all the LlocDeCopies in the database.
+
+	"""
+	mycursor = mydb.cursor(buffered=True)
+	nasos:List[LlocDeCopies] = []
+	mycursor.execute("SELECT * FROM credencials")
+	resultatbd = mycursor.fetchall()
+	for x in resultatbd:
+		if x[2] == "ActiveBackupBusiness":
+			nasos.append(SynologyActive(x[0], x[1], x[3], x[4], x[5]))
+		elif x[2] == "mspbackup":
+			nasos.append(mspbackup(x[0], x[1], x[3], x[4], x[5]))
+		elif x[2] == "HyperBackup":
+			nasos.append(SynologyHyper(x[0], x[1], x[3], x[4]))
+		elif x[2] == "Pandora":
+			nasos.append(Pandora(x[0], x[1], x[3], x[4], x[5]))
+
+	for nas in nasos:
+		if(nas.checkConnection()):
+			try:
+				nas.retrieve_copies(ruta, args)
+			except Exception as e:
+				now = datetime.datetime.now()
+				date_string = now.strftime('%Y-%m-%d--%H-%M-%S-dataRetrieval')
+				f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+				f.write(str(e))
+				f.close()
+		else:
+			now = datetime.datetime.now()
+			date_string = now.strftime('%Y-%m-%d--%H-%M-%S-Conexió')
+			f = open(ruta+"/errorLogs/"+date_string+".txt",'w')
+			f.write(str("Error de conexió amb el dispositiu. Comprova si esta ences o connectat a la xarxa"))
+			f.close()
+	return nasos
+
+
+#======In progress=========, guarda les dades en un JSON
+#S'he li ha de passar la llista de copies retorna boolea depenet si ho ha conseguit o no
+def saveJSON(nasos:List[LlocDeCopies], ruta:str, args)->bool:
+	pass
+
+
+def saveDataDB(nasos:List[LlocDeCopies])->bool:
+	"""Saves copies to the array of this object
+	
+	Parameters
+	----------
+	nasos : List[LlocDeCopies]
+		List of all the LlocDeCopies wanted to be saved in the DB
+
+	Returns
+	-------
+	bool
+		If it manages to save all the copies True otherwise False.
+
+	"""
+	try:
+		mydb = initialize()
+		mycursor = mydb.cursor(buffered=True)
+		try:
+			mycursor.execute("DROP TABLE copies;")
+		except:
+			pass
+		mycursor.execute("CREATE TABLE copies (ID varchar(50), Status varchar(50), NomLlocDeCopies varchar(50));")
+		for nas in nasos:
+			for copia in nas.get_copies():
+				
+				mycursor.execute("INSERT INTO copies (ID, Status, NomLlocDeCopies) VALUES ('" + copia.get_id() + "', '"+ copia.get_status()+"', '"+ (copia.get_LlocDeCopies()).get_name()+"');")
+		mydb.commit()
+		return True
+	except:
+		return False
+
+#El que fa aixo es que totes les execucions d'aquest fitxer iniciin la funcio main
+if __name__ == "__main__":
+    main()
