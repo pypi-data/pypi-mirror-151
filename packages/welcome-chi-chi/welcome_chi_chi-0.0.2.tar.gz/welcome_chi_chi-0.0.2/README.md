33b1dce6e7e439fcc415213896330f0476dac630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:09 +0000

    Add package welcome-chi-chi/0.0.2/welcome_chi_chi-0.0.2.tar.gz

---
+++ packages/welcome-chi-chi/welcome_chi_chi-0.0.2.tar.gz/welcome_chi_chi-0.0.2/README.md
@@ -0,0 +1,18 @@
+A Very Easy Recommendation Engine for Django
+===
+
+Installation
+---
+
+```
+pip install welcome_chi_chi
+```
+
+Usage
+---
+
+```
+import welcome_chi_chi.about_me as Chi
+
+Chi.introduces()
+```
---
+++ packages/welcome-chi-chi/welcome_chi_chi-0.0.2.tar.gz/welcome_chi_chi-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="welcome_chi_chi",
+    version="0.0.2",
+    author="autn",
+    description="An introduction from Chi Chi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["welcome_chi_chi"],
+    package_dir={'':'welcome_chi_chi'},
+    install_requires=[]
+)
