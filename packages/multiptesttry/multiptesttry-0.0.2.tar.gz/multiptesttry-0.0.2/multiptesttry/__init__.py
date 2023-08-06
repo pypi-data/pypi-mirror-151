2548afdd7b724487728a38ea3804851225afde15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:30 +0000

    Add package multiptesttry/0.0.2/multiptesttry-0.0.2.tar.gz

---
+++ packages/multiptesttry/multiptesttry-0.0.2.tar.gz/multiptesttry-0.0.2/multiptesttry/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from multiptesttry.multiple import product
+
+
+# In[ ]:
+
+
+
+
---
+++ packages/multiptesttry/multiptesttry-0.0.2.tar.gz/multiptesttry-0.0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+from setuptools import setup,find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Array simple product'
+
+# Setting up
+setup(
+    name="multiptesttry",
+    version=VERSION,
+    author="Aimane",
+    author_email="<aelmaimouni@leyton.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['python', 'product', 'tensors', 'arrays'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
+
+
