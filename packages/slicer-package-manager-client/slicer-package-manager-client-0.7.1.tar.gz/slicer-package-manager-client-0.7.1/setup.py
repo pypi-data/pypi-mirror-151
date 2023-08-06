a23b2f3ebf88babdc4d832e2f424c39971616baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:15 +0000

    Add package slicer-package-manager-client/0.7.1/slicer-package-manager-client-0.7.1.tar.gz

---
+++ packages/slicer-package-manager-client/slicer-package-manager-client-0.7.1.tar.gz/slicer-package-manager-client-0.7.1/setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+# This file is required to support editable install
+# See https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+
+setup()
