d86466a967f031861996b5edcef14d59d25fc3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:43 +0000

    Add package amniotic/0.1.0/amniotic-0.1.0.tar.gz

---
+++ packages/amniotic/amniotic-0.1.0.tar.gz/amniotic-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+packages = find_packages()
+name = next(iter(packages))
+path = Path(__file__).absolute().parent / name / 'version'
+__version__ = path.read_text().strip()
+
+setup(
+    name=name,
+    version=__version__,
+    url=f'https://github.com/fmtr/{name}',
+    license='Copyright © 2022 Frontmatter. All rights reserved.',
+    author='Frontmatter',
+    description='A multi-output, multi-theme ambient sound mixer for Home Assistant',
+    packages=packages,
+    package_data={
+        name: [f'version'],
+    },
+    install_requires=[
+        'paho-mqtt',
+        'python-vlc',
+        'getmac',
+        'pyyaml',
+        'appdirs'
+    ],
+    extras_require={},
+    entry_points={
+        'console_scripts': [
+            'amniotic = amniotic.mqtt:start',
+        ],
+    }
+)
