036a0c74d59744e957645e0f2d77f654828f1a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:10 +0000

    Add package s7smaths/1.0.1/s7smaths-1.0.1-py3-none-any.whl

---
+++ packages/s7smaths/s7smaths-1.0.1-py3-none-any.whl/s7smaths/advanced_maths.py
@@ -0,0 +1,2 @@
+def power(a: int, b: int) -> int:
+    return pow(a,b)
---
+++ packages/s7smaths/s7smaths-1.0.1-py3-none-any.whl/s7smaths/basic_maths.py
@@ -0,0 +1,20 @@
+def add(a: int, b: int) -> int:
+    """
+    A method to add two numbers
+
+    Parameters:
+     - a (int): the first number
+     - b (int): the second number
+
+    Returns: sum of a and b
+    """
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+def divide(a: int, b: int) -> float:
+    return a / b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
