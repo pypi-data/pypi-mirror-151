578d3d2d1fc67f49faefb0a55363ab28473c2c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package pyqt-notifier/0.0.12/pyqt-notifier-0.0.12.tar.gz

---
+++ packages/pyqt-notifier/pyqt-notifier-0.0.12.tar.gz/pyqt-notifier-0.0.12/setup.py
@@ -0,0 +1,27 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='pyqt-notifier',
+    version='0.0.12',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pyqt_notifier.ico': ['close.svg']},
+    description='PyQt notifier which shows at bottom right of the desktop screen',
+    url='https://github.com/yjg30737/pyqt-notifier.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PyQt5>=5.8',
+        'pyqt-svg-button>=0.0.1'
+    ]
+)
