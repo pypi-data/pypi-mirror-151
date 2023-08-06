7ca02cb9d9ddfdf7f7ad4bdc82805fbd463f29b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:16 +0000

    Add package muse-pytorch/0.0.3/muse-pytorch-0.0.3.tar.gz

---
+++ packages/muse-pytorch/muse-pytorch-0.0.3.tar.gz/muse-pytorch-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="muse-pytorch",
+    version="0.0.3",
+    author="Nicola Occelli",
+    author_email="nicola.occelli@ulb.be",
+    description="Pytorch version of MUSE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nico995/MUSE-pytorch",
+    project_urls={
+        "Bug Tracker": "https://github.com/Nico995/MUSE-pytorch/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: GPU :: NVIDIA CUDA :: 10.2",
+        "Natural Language :: English",
+    ],
+    package_dir={"": "muse_pytorch"},
+    packages=setuptools.find_packages(where="muse_pytorch"),
+    python_requires=">=3.8.5",
+)
