df29309d8e685d3418a1b67158e7b4b0fca5e7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:16 +0000

    Add package explainaboard/0.9.5/explainaboard-0.9.5-py2.py3-none-any.whl

---
+++ packages/explainaboard/explainaboard-0.9.5-py2.py3-none-any.whl/explainaboard/processors/word_segmentation.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from explainaboard import TaskType
+from explainaboard.metric import BMESF1ScoreConfig, MetricConfig
+from explainaboard.processors.processor_registry import register_processor
+from explainaboard.processors.sequence_labeling import SeqLabProcessor
+from explainaboard.utils.span_utils import BMESSpanOps
+
+
+@register_processor(TaskType.word_segmentation)
+class CWSProcessor(SeqLabProcessor):
+    @classmethod
+    def task_type(cls) -> TaskType:
+        return TaskType.word_segmentation
+
+    @classmethod
+    def default_metrics(
+        cls, source_language=None, target_language=None
+    ) -> list[MetricConfig]:
+        return [BMESF1ScoreConfig(name='F1')]
+
+    @classmethod
+    def default_span_ops(cls) -> BMESSpanOps:
+        return BMESSpanOps()
