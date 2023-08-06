13dc63b1db9e4d6a905b295f9e2686c136d81218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:57 +0000

    Add package dcicutils/3.13.0.2b27/dcicutils-3.13.0.2b27.tar.gz

---
+++ packages/dcicutils/dcicutils-3.13.0.2b27.tar.gz/dcicutils-3.13.0.2b27/dcicutils/common.py
@@ -0,0 +1,28 @@
+from typing_extensions import Literal
+
+
+# ===== Useful constants =====
+
+REGION = 'us-east-1'
+
+APP_CGAP = 'cgap'
+APP_FOURFRONT = 'fourfront'
+
+LEGACY_GLOBAL_ENV_BUCKET = 'foursight-envs'
+
+ORCHESTRATED_APPS = [APP_CGAP, APP_FOURFRONT]
+
+CHALICE_STAGE_DEV = 'dev'
+CHALICE_STAGE_PROD = 'prod'
+
+CHALICE_STAGES = [CHALICE_STAGE_DEV, CHALICE_STAGE_PROD]
+
+# ===== Type hinting names =====
+
+EnvName = str
+
+# Nicknames for enumerated sets of symbols. Note that these values must be syntactic literals,
+# so they can't use the variables defined above.
+
+ChaliceStage = Literal['dev', 'prod']
+OrchestratedApp = Literal['cgap', 'fourfront']
