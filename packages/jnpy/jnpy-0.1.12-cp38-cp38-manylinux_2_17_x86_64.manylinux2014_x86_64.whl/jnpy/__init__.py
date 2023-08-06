e58aa29339a87d76262eafc6cf18a025c150bc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:03 +0000

    Add package jnpy/0.1.12/jnpy-0.1.12-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/jnpy/jnpy-0.1.12-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/jnpy/__init__.py
@@ -0,0 +1,7 @@
+from _jnpy import *
+
+from pkg_resources import resource_string
+
+# def medusa():
+#     medusa_parameter_dir = resource_string(__name__, 'medusa_parameter')
+#     return medusa_init(medusa_parameter_dir)
