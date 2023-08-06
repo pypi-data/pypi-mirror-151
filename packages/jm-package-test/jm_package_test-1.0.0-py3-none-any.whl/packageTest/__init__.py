e7d4d946cc0f653ecd1ccb60ca662d8bb2f1d65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:22:04 +0000

    Add package jm-package-test/1.0.0/jm_package_test-1.0.0-py3-none-any.whl

---
+++ packages/jm-package-test/jm_package_test-1.0.0-py3-none-any.whl/packageTest/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['packageTest']
+
+print('jimin_')
---
+++ packages/jm-package-test/jm_package_test-1.0.0-py3-none-any.whl/packageTest/packageTest.py
@@ -0,0 +1,8 @@
+def test_package():
+    print("This is package test")
+
+def add_test(a, b):
+    return a+b
+
+def mul_test(a, b):
+    return a*b
