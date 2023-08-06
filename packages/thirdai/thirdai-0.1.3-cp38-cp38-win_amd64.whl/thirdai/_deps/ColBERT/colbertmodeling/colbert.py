a6163998bbf51e8014d03b243b9cde0dd5f04381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:15 +0000

    Add package thirdai/0.1.3/thirdai-0.1.3-cp38-cp38-win_amd64.whl

---
+++ packages/thirdai/thirdai-0.1.3-cp38-cp38-win_amd64.whl/thirdai/_deps/ColBERT/colbertmodeling/colbert.py
@@ -0,0 +1,79 @@
+from thirdai._deps.ColBERT.colbertconfig import ColBERTConfig
+from thirdai._deps.ColBERT.colbertmodeling.base_colbert import BaseColBERT
+
+import torch
+import string
+
+
+class ColBERT(BaseColBERT):
+    """
+    This class handles the basic encoding and scoring operations in ColBERT. It is used for training.
+    """
+
+    def __init__(self, name="bert-base-uncased"):
+        super().__init__(name)
+        self.use_gpu = self.colbert_config.total_visible_gpus > 0
+
+        if self.colbert_config.mask_punctuation:
+            self.skiplist = {
+                w: True
+                for symbol in string.punctuation
+                for w in [
+                    symbol,
+                    self.raw_tokenizer.encode(symbol, add_special_tokens=False)[0],
+                ]
+            }
+
+    def query(self, input_ids, attention_mask):
+        input_ids, attention_mask = input_ids.to(self.device), attention_mask.to(
+            self.device
+        )
+        Q = self.bert(input_ids, attention_mask=attention_mask)[0]
+        Q = self.linear(Q)
+
+        mask = (
+            torch.tensor(self.mask(input_ids, skiplist=[]), device=self.device)
+            .unsqueeze(2)
+            .float()
+        )
+        Q = Q * mask
+
+        return torch.nn.functional.normalize(Q, p=2, dim=2)
+
+    def doc(self, input_ids, attention_mask, keep_dims=True):
+        assert keep_dims in [True, False, "return_mask"]
+
+        input_ids, attention_mask = input_ids.to(self.device), attention_mask.to(
+            self.device
+        )
+        D = self.bert(input_ids, attention_mask=attention_mask)[0]
+        D = self.linear(D)
+
+        mask = (
+            torch.tensor(
+                self.mask(input_ids, skiplist=self.skiplist), device=self.device
+            )
+            .unsqueeze(2)
+            .float()
+        )
+        D = D * mask
+
+        D = torch.nn.functional.normalize(D, p=2, dim=2)
+        if self.use_gpu:
+            D = D.half()
+
+        if keep_dims is False:
+            D, mask = D.cpu(), mask.bool().cpu().squeeze(-1)
+            D = [d[mask[idx]] for idx, d in enumerate(D)]
+
+        elif keep_dims == "return_mask":
+            return D, mask.bool()
+
+        return D
+
+    def mask(self, input_ids, skiplist):
+        mask = [
+            [(x not in skiplist) and (x != 0) for x in d]
+            for d in input_ids.cpu().tolist()
+        ]
+        return mask
