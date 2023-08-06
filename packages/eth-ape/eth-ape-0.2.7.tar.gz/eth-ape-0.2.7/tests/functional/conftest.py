db14b76831e2c01cb2bc0b42a2e40d8a1eee3bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:44 +0000

    Add package eth-ape/0.2.7/eth-ape-0.2.7.tar.gz

---
+++ packages/eth-ape/eth-ape-0.2.7.tar.gz/eth-ape-0.2.7/tests/functional/conftest.py
@@ -0,0 +1,217 @@
+import json
+import tempfile
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Dict
+
+import pytest
+import yaml
+from eth.exceptions import HeaderNotFound
+from ethpm_types import ContractType
+
+import ape
+from ape.api import (
+    AccountContainerAPI,
+    EcosystemAPI,
+    NetworkAPI,
+    PluginConfig,
+    ProviderAPI,
+    ReceiptAPI,
+    TransactionAPI,
+)
+from ape.contracts import ContractContainer, ContractInstance
+from ape.exceptions import ChainError, ContractLogicError, ProviderNotConnectedError
+from ape.managers.config import CONFIG_FILE_NAME
+from ape_ethereum.transactions import TransactionStatusEnum
+
+
+def _get_raw_contract(compiler: str) -> Dict:
+    here = Path(__file__).parent
+    contracts_dir = here / "data" / "contracts"
+    return json.loads((contracts_dir / f"{compiler}_contract.json").read_text())
+
+
+RAW_SOLIDITY_CONTRACT_TYPE = _get_raw_contract("solidity")
+RAW_VYPER_CONTRACT_TYPE = _get_raw_contract("vyper")
+TEST_ADDRESS = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+
+
+@pytest.fixture
+def mock_account_container_api(mocker):
+    return mocker.MagicMock(spec=AccountContainerAPI)
+
+
+@pytest.fixture
+def mock_provider_api(mocker, mock_network_api):
+    mock = mocker.MagicMock(spec=ProviderAPI)
+    mock.network = mock_network_api
+    return mock
+
+
+class _ContractLogicError(ContractLogicError):
+    pass
+
+
+@pytest.fixture
+def mock_network_api(mocker):
+    mock = mocker.MagicMock(spec=NetworkAPI)
+    mock_ecosystem = mocker.MagicMock(spec=EcosystemAPI)
+    mock_ecosystem.virtual_machine_error_class = _ContractLogicError
+    mock.ecosystem = mock_ecosystem
+    return mock
+
+
+@pytest.fixture
+def mock_failing_transaction_receipt(mocker):
+    mock = mocker.MagicMock(spec=ReceiptAPI)
+    mock.status = TransactionStatusEnum.FAILING
+    mock.gas_used = 0
+    return mock
+
+
+@pytest.fixture
+def mock_web3(mocker):
+    return mocker.MagicMock()
+
+
+@pytest.fixture
+def mock_config_item(mocker):
+    return mocker.MagicMock(spec=PluginConfig)
+
+
+@pytest.fixture
+def mock_transaction(mocker):
+    return mocker.MagicMock(spec=TransactionAPI)
+
+
+@pytest.hookimpl(trylast=True, hookwrapper=True)
+def pytest_collection_finish(session):
+    with ape.networks.parse_network_choice("::test"):
+        # Sets the active provider
+        yield
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_protocol(item, nextitem):
+    module_name = item.module.__name__
+    prefix = "tests.functional"
+
+    if module_name.startswith(prefix):
+        snapshot_id = ape.chain.snapshot()
+        yield
+
+        try:
+            ape.chain.restore(snapshot_id)
+        except (HeaderNotFound, ChainError, ProviderNotConnectedError):
+            pass
+    else:
+        yield
+
+
+@pytest.fixture(scope="session")
+def networks_connected_to_tester():
+    with ape.networks.parse_network_choice("::test"):
+        yield ape.networks
+
+
+@pytest.fixture(scope="session")
+def ethereum(networks_connected_to_tester):
+    return networks_connected_to_tester.ethereum
+
+
+@pytest.fixture(scope="session")
+def eth_tester_provider(networks_connected_to_tester):
+    yield networks_connected_to_tester.active_provider
+
+
+@pytest.fixture
+def test_accounts(accounts):
+    return accounts.test_accounts
+
+
+@pytest.fixture
+def sender(test_accounts):
+    return test_accounts[0]
+
+
+@pytest.fixture
+def receiver(test_accounts):
+    return test_accounts[1]
+
+
+@pytest.fixture
+def owner(test_accounts):
+    return test_accounts[2]
+
+
+@pytest.fixture
+def solidity_contract_type() -> ContractType:
+    return ContractType.parse_obj(RAW_SOLIDITY_CONTRACT_TYPE)
+
+
+@pytest.fixture
+def solidity_contract_container(solidity_contract_type) -> ContractContainer:
+    return ContractContainer(contract_type=solidity_contract_type)
+
+
+@pytest.fixture
+def solidity_contract_instance(
+    owner, solidity_contract_container, networks_connected_to_tester
+) -> ContractInstance:
+    return owner.deploy(solidity_contract_container)
+
+
+@pytest.fixture
+def vyper_contract_type() -> ContractType:
+    return ContractType.parse_obj(RAW_VYPER_CONTRACT_TYPE)
+
+
+@pytest.fixture
+def vyper_contract_container(vyper_contract_type) -> ContractContainer:
+    return ContractContainer(contract_type=vyper_contract_type)
+
+
+@pytest.fixture
+def vyper_contract_instance(
+    owner, vyper_contract_container, networks_connected_to_tester
+) -> ContractInstance:
+    return owner.deploy(vyper_contract_container)
+
+
+@pytest.fixture(params=("solidity", "vyper"))
+def contract_container(
+    request, solidity_contract_container, vyper_contract_container, networks_connected_to_tester
+):
+    return solidity_contract_container if request.param == "solidity" else vyper_contract_container
+
+
+@pytest.fixture(params=("solidity", "vyper"))
+def contract_instance(request, solidity_contract_instance, vyper_contract_instance):
+    return solidity_contract_instance if request.param == "solidity" else vyper_contract_instance
+
+
+@pytest.fixture
+def temp_config():
+    @contextmanager
+    def func(data: Dict, config):
+        with tempfile.TemporaryDirectory() as temp_dir_str:
+            temp_dir = Path(temp_dir_str)
+            with config.using_project(temp_dir):
+                config._cached_configs = {}
+                config_file = temp_dir / CONFIG_FILE_NAME
+                config_file.touch()
+                config_file.write_text(yaml.dump(data))
+                config.load(force_reload=True)
+                yield
+                config_file.unlink()
+                config._cached_configs = {}
+
+    return func
+
+
+@pytest.fixture
+def clean_contracts_cache(chain):
+    original_cached_contracts = chain.contracts._local_contracts
+    chain.contracts._local_contracts = {}
+    yield
+    chain.contracts._local_contracts = original_cached_contracts
---
+++ packages/eth-ape/eth-ape-0.2.7.tar.gz/eth-ape-0.2.7/tests/integration/cli/test_compile.py
@@ -0,0 +1,154 @@
+import shutil
+
+import pytest
+
+from ape.contracts import ContractContainer
+
+from .utils import skip_projects, skip_projects_except
+
+
+@skip_projects(
+    ["unregistered-contracts", "one-interface", "geth", "only-dependency", "with-dependency"]
+)
+def test_compile_missing_contracts_dir(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile"])
+    assert result.exit_code == 0, result.output
+    assert "WARNING" in result.output, result.output
+    assert "Nothing to compile" in result.output
+
+
+@skip_projects_except(["unregistered-contracts"])
+def test_missing_extensions(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "--force"])
+    assert result.exit_code == 0, result.output
+    assert "WARNING: No compilers detected for the following extensions:" in result.output
+    assert ".test" in result.output
+    assert ".foobar" in result.output
+
+
+@skip_projects_except(["unregistered-contracts"])
+def test_no_compiler_for_extension(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "contracts/Contract.test"])
+    assert result.exit_code == 0, result.output
+    assert "WARNING: No compilers detected for the following extensions: .test" in result.output
+
+
+@skip_projects(
+    [
+        "empty-config",
+        "no-config",
+        "script",
+        "only-dependency",
+        "unregistered-contracts",
+        "test",
+        "geth",
+    ]
+)
+def test_compile(ape_cli, runner, project, clean_cache):
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+    # First time it compiles, it compiles the files with registered compilers successfully.
+    # Files with multiple extensions are currently not supported.
+    all_files = [f for f in project.path.glob("contracts/**/*")]
+    expected_files = [f for f in all_files if f.name.count(".") == 1]
+    unexpected_files = [f for f in all_files if f not in expected_files]
+
+    manifest = project.extract_manifest()
+    for file in expected_files:
+        assert file.name in manifest.sources
+
+    assert all([f.stem in result.output for f in expected_files])
+    assert not any([f.stem in result.output for f in unexpected_files])
+
+    result = runner.invoke(ape_cli, ["compile"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    # First time it compiles, it caches
+    for file in project.path.glob("contracts/**/*"):
+        assert file.stem not in result.output
+
+
+@skip_projects_except(["one-interface"])
+def test_can_access_contracts(project, clean_cache):
+    # This test does not use the CLI but still requires a project or run off of.
+    assert project.Interface, "Unable to access contract when needing to compile"
+    assert project.Interface, "Unable to access contract when not needing to compile"
+
+
+@skip_projects_except(["one-interface"])
+@pytest.mark.parametrize(
+    "contract_path",
+    ("Interface", "Interface.json", "contracts/Interface", "contracts/Interface.json"),
+)
+def test_compile_specified_contracts(ape_cli, runner, project, contract_path, clean_cache):
+    result = runner.invoke(ape_cli, ["compile", contract_path], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'Interface.json'" in result.output
+
+
+@skip_projects_except(["one-interface"])
+def test_compile_unknown_extension_does_not_compile(ape_cli, runner, project, clean_cache):
+    result = runner.invoke(
+        ape_cli, ["compile", "Interface.js"], catch_exceptions=False
+    )  # Suffix to existing extension
+    assert result.exit_code == 2, result.output
+    assert "Error: Contract 'Interface.js' not found." in result.output
+
+
+@skip_projects_except([])
+def test_compile_contracts(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "--size"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    # Still caches but displays bytecode size
+    for file in project.path.glob("contracts/**/*"):
+        assert file.stem in result.output
+
+
+@skip_projects_except(["with-dependency"])
+@pytest.mark.parametrize(
+    "contract_path",
+    (None, "contracts/", "Project", "contracts/Project.json"),
+)
+def test_compile_with_dependency(ape_cli, runner, project, contract_path):
+    cmd = ["compile", "--force"]
+
+    if contract_path:
+        cmd.append(contract_path)
+
+    result = runner.invoke(ape_cli, cmd, catch_exceptions=False)
+
+    dep_name_a = "__test_dependency_a__"
+    dep_name_b = "__test_dependency_b__"
+    dep_name_c = "__test_dependency_c__"
+    assert result.exit_code == 0, result.output
+    assert dep_name_a in project.dependencies
+    assert dep_name_b in project.dependencies
+    assert dep_name_c in project.dependencies
+    assert type(project.dependencies[dep_name_a]["local"].DependencyA) == ContractContainer
+    assert type(project.dependencies[dep_name_b]["local"].DependencyB) == ContractContainer
+    assert type(project.dependencies[dep_name_c]["local"].DependencyC) == ContractContainer
+
+
+@skip_projects_except(["with-dependency"])
+def test_compile_individual_contract_excludes_other_contract(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "Project", "--force"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Other" not in result.output
+
+
+@skip_projects_except(["with-dependency"])
+def test_compile_non_ape_project_deletes_ape_config_file(ape_cli, runner, project):
+    result = runner.invoke(ape_cli, ["compile", "Project", "--force"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "ape-config.yaml" not in [f.name for f in (project.path / "dependency_c").iterdir()]
+
+
+@skip_projects_except(["only-dependency"])
+def test_compile_only_dependency(ape_cli, runner, project, clean_cache):
+    dependency_cache = project.path / "dependency_a" / ".build"
+    if dependency_cache.is_dir():
+        shutil.rmtree(str(dependency_cache))
+
+    result = runner.invoke(ape_cli, ["compile", "--force"], catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "Compiling 'DependencyA.json'" in result.output
