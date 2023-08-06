a3f9f91d0e2f846ef53c5c5a3299d6b6f39869d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:03 +0000

    Add package statute-utils/0.0.2/statute-utils-0.0.2.tar.gz

---
+++ packages/statute-utils/statute-utils-0.0.2.tar.gz/statute-utils-0.0.2/statute_utils/formula/__init__.py
@@ -0,0 +1,15 @@
+from .named import NamedStatute, NamedStatuteIndeterminate
+from .provisions import Provision, ProvisionLabel
+from .serials import (
+    AdministrativeMatter,
+    BarMatter,
+    BatasPambansa,
+    CircularOCA,
+    CommonwealthAct,
+    ExecutiveOrder,
+    LegacyAct,
+    PresidentialDecree,
+    RepublicAct,
+    ResolutionEnBanc,
+    VetoMessage,
+)
