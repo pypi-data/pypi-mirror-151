860c7c7a9f763d2a9c041a738529bf4833853bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:18 +0000

    Add package mono-diff/0.1.5/mono-diff-0.1.5.tar.gz

---
+++ packages/mono-diff/mono-diff-0.1.5.tar.gz/mono-diff-0.1.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mono-diff',
+    version='0.1.5',
+    install_requires=['fire == 0.4.0','tenacity == 7.0.0']
+)
---
+++ packages/mono-diff/mono-diff-0.1.5.tar.gz/mono-diff-0.1.5/src/mono-diff/cli.py
@@ -0,0 +1,32 @@
+from typing import Optional
+import fire
+
+from dotnet_provider import DotnetProvider
+from mono import Mono
+import logging
+
+class MonoCli:
+    def __init__(self):
+        self.logger = logging.getLogger("mono")
+        self.mono = Mono(DotnetProvider())
+
+    def affected(self, branch: Optional[str] = None):
+        affected = self.mono.get_affected_projects(branch)
+        print(f"*************************************************")
+        print(f"*************************************************")
+        print(f"Files Changed: {affected.changed_files}")
+        print(f"Projects affected: {affected.affected_projects}")
+        print(f"******* Please build the following apps")
+        return affected.affected_apps
+
+    def build(self):
+        return self.mono.build_projects_to_apps_mapping()
+
+    def apps(self):
+        return self.mono.get_app_projects()
+
+def main():
+    fire.Fire(MonoCli)
+
+if __name__ == '__main__':
+    fire.Fire(MonoCli)
