5cd9202581a533c10e74ed1746a9618820abfcea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:02 +0000

    Add package streamlit-message/0.0.1/streamlit-message-0.0.1.tar.gz

---
+++ packages/streamlit-message/streamlit-message-0.0.1.tar.gz/streamlit-message-0.0.1/README.md
@@ -0,0 +1,22 @@
+# st-message
+
+Streamlit Component, for a UI Message, Chat component
+
+authors - [@gamcoh](https://github.com/gamcoh) 
+
+## Installation
+
+Install `streamlit-message` with pip
+```bash
+pip install streamlit-message 
+```
+
+usage, import the `message` function from `streamlit_message`
+```py
+import streamlit as st
+from streamlit_message import message
+
+message("My message") 
+message("Hello World!", is_user=True)  # align's the message to the right
+```
+   
---
+++ packages/streamlit-message/streamlit-message-0.0.1.tar.gz/streamlit-message-0.0.1/streamlit_message/frontend/build/precache-manifest.78fa46ced5c94fc12f30a1513e18588e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7cdcff860cd3cecbf3b5c734788367",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb0915ea27ae0b85e20b",
+    "url": "./static/js/2.f0499c68.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.f0499c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784c2c03884e118a51c1",
+    "url": "./static/js/main.bd83c092.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
