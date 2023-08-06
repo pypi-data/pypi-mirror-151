3e9183c3b3bcacf9d0bfb22345f8a8cb17d81d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:13 +0000

    Add package mcHelper/6.0/mcHelper-6.0-py2-none-any.whl

---
+++ packages/mcHelper/mcHelper-6.0-py2-none-any.whl/mcHelpers/test.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2022/5/19 5:45 PM
+@Author  : oujiewen2@shoplineapp.com
+@File    : test.py
+'''
+
+
+def intToBin(number, index, feature=True):
+    """index为该数据位宽,number为待转换数据,
+    feature为True则进行十进制转二进制，为False则进行二进制转十进制。"""
+
+    if (feature == True):  # 十进制转换为二进制
+        if (number >= 0):
+            b = bin(number)
+            b = '0' * (index + 2 - len(b)) + b
+        else:
+            b = 2 ** (index) + number
+            b = bin(b)
+            b = '1' * (index + 2 - len(b)) + b  # 注意这里算出来的结果是补码
+        b = b.replace("0b", '')
+        b = b.replace('-', '')
+        return b
+    elif (feature == False):  # 二进制转换为十进制
+        i = int(str(number), 2)
+        if (i >= 2 ** (index - 1)):  # 如果是负数
+            i = -(2 ** index - i)
+            return i
+        else:
+            return i
+
+def tableId(storeId,maxId):
+    a = intToBin(storeId, 64)
+    b = '0' * 32 +a[0:32]
+    c = ''
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            c += '0'
+        else:
+            c += '1'
+    return(int('0b' + c[33:], base=2))%int(maxId)
+
+def dbId(id):
+    pass
+
+
+if __name__ == '__main__':
+
+    store_id=324234234234
+    a=intToBin(324234234234, 64)
+    b='0'*32+intToBin(324234234234,64)[0:32]
+    c=''
+    for i in range(len(a)):
+        if a[i]==b[i]:
+            c+='0'
+        else:
+            c+='1'
+    print(int('0b'+c[33:],base=2))
+    print(tableId(store_id))
+
