ea20d0c61ac86202c70610da5816c173b5acc9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:04 +0000

    Add package skunkworks-repo/1.0.2/skunkworks-repo-1.0.2.tar.gz

---
+++ packages/skunkworks-repo/skunkworks-repo-1.0.2.tar.gz/skunkworks-repo-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='skunkworks-repo',
+    version='1.0.2',    
+    description='The Skunk Works Code Repository',
+    url='https://github.com/skunk-ink/skunkworks',
+    download_url='https://github.com/skunk-ink/skunkworks/archive/refs/tags/v1.0.2.tar.gz',
+    keywords=['Handshake', 'API', 'HNS'],
+    author='skunk-ink',
+    license='MIT',
+    packages=['handshake'],
+    install_requires=['requests',
+                      'json'                    
+                      ],
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
