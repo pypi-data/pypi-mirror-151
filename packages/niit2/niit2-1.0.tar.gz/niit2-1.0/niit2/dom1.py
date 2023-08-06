a3e106f9e2cefb421a8164cd5d31cb4f93d62e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:11 +0000

    Add package niit2/1.0/niit2-1.0.tar.gz

---
+++ packages/niit2/niit2-1.0.tar.gz/niit2-1.0/niit2/dom1.py
@@ -0,0 +1,6 @@
+from . import dom2
+#.同级
+from .. import niit
+#.. 上一级
+def add():
+    print('dom1......')
---
+++ packages/niit2/niit2-1.0.tar.gz/niit2-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='niit2',
+    version='1.0',
+    description='这是一个测试的模块',
+    author='myx',
+    author_email='1522038992@qq.com',
+    py_modules=['niit2.dom1','niit2.dom2']
+
+)
+
+# python setup.py   sdist
+# python setup.py   install
