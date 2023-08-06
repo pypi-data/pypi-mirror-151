2e7f87ef2685280069e3002b8c333d8a2b37a4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:49 +0000

    Add package arcane-ga4/0.1.0/arcane-ga4-0.1.0.tar.gz

---
+++ packages/arcane-ga4/arcane-ga4-0.1.0.tar.gz/arcane-ga4-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arcane', 'arcane.ga4']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-analytics-admin==0.8.1',
+ 'google-api-core==2.8.0',
+ 'google-auth==2.6.6']
+
+setup_kwargs = {
+    'name': 'arcane-ga4',
+    'version': '0.1.0',
+    'description': 'Utility functions for ga4 api call',
+    'long_description': '# Arcane ga4 README\n\nThis package helps us to interact with Google Analytics V4 API:\n\n    + [Analytics Data API](https://developers.google.com/analytics/devguides/reporting/data/v1)\n    + [Google Analytics Admin API](https://developers.google.com/analytics/devguides/config/admin/v1)\n\n\n## Release history\nTo see changes, please see CHANGELOG.md\n',
+    'author': 'Arcane',
+    'author_email': 'product@arcane.run',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
