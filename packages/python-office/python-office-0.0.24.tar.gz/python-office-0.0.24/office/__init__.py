44b2ad48dc49c1190f50e85868d058733eaeaa48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:14 +0000

    Add package python-office/0.0.24/python-office-0.0.24.tar.gz

---
+++ packages/python-office/python-office-0.0.24.tar.gz/python-office-0.0.24/office/__init__.py
@@ -0,0 +1,12 @@
+from . import word
+from . import pdf
+from . import image
+from . import file
+from . import video
+from . import tools
+from . import ppt
+from . import excel
+
+print('=' * 10)
+print( '【python-office库】，全部功能文档：http://www.python4office.cn/python-office/profile/')
+print('=' * 10)
