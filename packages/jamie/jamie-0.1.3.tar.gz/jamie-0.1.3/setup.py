6b401e590ac1d48fc4496567c3ed8e28ffa4efa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:15 +0000

    Add package jamie/0.1.3/jamie-0.1.3.tar.gz

---
+++ packages/jamie/jamie-0.1.3.tar.gz/jamie-0.1.3/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+@Author : Jamie
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="jamie",
+    version="0.1.3",
+    author="Jamie",
+    author_email="jmzhao29@gmail.com",
+    description="A Python package developed for jamie's code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD",
+    url="https://github.com/PotatoXi/Jamie",
+    project_urls={
+        "Bug Tracker": "https://github.com/PotatoXi/Jamie/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={'Jamie': 'src'},
+    packages=['src'],
+    #packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+
+)
+
---
+++ packages/jamie/jamie-0.1.3.tar.gz/jamie-0.1.3/src/jamie_hello.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@Author : Jamie
+"""
+import sys
+
+def say(m):
+    print('hello', m)
+
+if __name__ == '__main__':
+    say(sys.argv[1])
+
+def add_old(number):
+    return number + 1
+
+def add_2(number):
+    return number + 2
