625c65577b99ad7684060bc8825a6398e48c7b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:42 +0000

    Add package hyssop/1.1.7/hyssop-1.1.7-py3-none-any.whl

---
+++ packages/hyssop/hyssop-1.1.7-py3-none-any.whl/hyssop/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2020-Present the hyssop authors and contributors.
+#
+# This module is part of hyssop and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+'''
+File created: August 21st 2020
+
+Modified By: hsky77
+Last Updated: May 21st 2022 10:09:41 am
+'''
+
+
+Module_Path = __path__[0]
+Version = '1.1.7'
