b24031d1093e71eefca8431e8de6d05942b61fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:54 +0000

    Add package eltch/0.9.3/eltch-0.9.3.tar.gz

---
+++ packages/eltch/eltch-0.9.3.tar.gz/eltch-0.9.3/setup.py
@@ -0,0 +1,42 @@
+from codecs import open
+
+from setuptools import setup
+
+'''
+:authors: dymshnc.
+:license: Apache License, Version 2.0, see LICENSE file.
+
+:copyright: (c) 2022 by dymshnc.
+'''
+
+with open("README.md", "r", "utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='eltch',
+    version='0.9.3',
+    description='Electrotechnical assistant.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='dymshnc',
+    author_email='officialdyomshin@gmail.com',
+    url='https://github.com/dymshnc/eltch',
+    packages=["eltch"],
+    python_requires=">=3.6",
+    license='Apache License, Version 2.0, see LICENSE file',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+    ],
+    project_urls={
+        "Documentation": "https://github.com/dymshnc/eltch#readme",
+        "Source": "https://github.com/dymshnc/eltch",
+    },
+)
