f284b27cb6b9f577314f071983a8032214d589fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:33 +0000

    Add package pysolardb/0.1.4/pysolardb-0.1.4.tar.gz

---
+++ packages/pysolardb/pysolardb-0.1.4.tar.gz/pysolardb-0.1.4/pysolardb/__init__.py
@@ -0,0 +1,9 @@
+"""
+PySolarDB
+---------
+    A package created to easily extract meteordlogical and solar data from the SolarDB API
+"""
+
+__version__ = "0.1.4"
+__author__ = "Emmanuel Parfait"
+__credits__ = "LE2P Energy Lab"
