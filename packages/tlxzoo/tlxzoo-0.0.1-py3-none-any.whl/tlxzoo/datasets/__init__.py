30f0d5d81b725903a3e09b9852d2bc6bf4ede49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:30 +0000

    Add package tlxzoo/0.0.1/tlxzoo-0.0.1-py3-none-any.whl

---
+++ packages/tlxzoo/tlxzoo-0.0.1-py3-none-any.whl/tlxzoo/datasets/__init__.py
@@ -0,0 +1,10 @@
+from .cifar10 import *
+from .data_loader import *
+from .circles import *
+from .coco import *
+from .wider import *
+from .libri_speech import *
+from .sst2 import *
+from .conll2003 import *
+from .synth90k import *
+
