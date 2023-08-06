e4aa0f592f499cc592196f4022171de4fb27e6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:25:39 +0000

    Add package im-botposter/0.0.2/im_botposter-0.0.2.tar.gz

---
+++ packages/im-botposter/im_botposter-0.0.2.tar.gz/im_botposter-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from hashlib import md5
+from setuptools import setup, find_packages
+
+setup(
+  name='im_botposter',
+  version = "0.0.2",
+  url='https://github.com/Adiziel/Imposter',
+  description='Im_poster is a Telegram Posts Management Tool',
+  long_description= 'Imposter is a telegram bot which creates a post with title and message with and index to track that post, in telegram channel or group in which this bot has been added.',
+  author='Adiziel',
+  author_email='aditya.pathak.plus@gmail.com',
+  packages=find_packages(),
+  keywords=['python', 'telegram', 'telegram bot'],
+  install_requires=['requests', 'json']
+)
