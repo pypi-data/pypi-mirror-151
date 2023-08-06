06c13c0dea2a7925af69445cf5dfe7d7c30793bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:50 +0000

    Add package pyqt-svg-button/0.0.25/pyqt_svg_button-0.0.25-py3-none-any.whl

---
+++ packages/pyqt-svg-button/pyqt_svg_button-0.0.25-py3-none-any.whl/pyqt_svg_button/svgButton.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QPushButton, QWidget
+from pyqt_svg_abstractbutton import SvgAbstractButton
+
+
+class SvgButton(QPushButton, SvgAbstractButton):
+    def __init__(self, base_widget: QWidget = None, *args, **kwargs):
+        super().__init__(base_widget, *args, **kwargs)
