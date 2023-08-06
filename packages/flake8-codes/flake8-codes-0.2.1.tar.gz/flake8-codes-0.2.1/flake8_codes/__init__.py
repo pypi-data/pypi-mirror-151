8ad8e0b841d825e9c0d204fa499c7d90372e8afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:39 +0000

    Add package flake8-codes/0.2.1/flake8-codes-0.2.1.tar.gz

---
+++ packages/flake8-codes/flake8-codes-0.2.1.tar.gz/flake8-codes-0.2.1/flake8_codes/__init__.py
@@ -0,0 +1,10 @@
+"""
+CLI tool to introspect flake8 plugins and their codes.
+"""
+
+from ._codes import extract
+from ._plugins import Plugin, get_installed
+
+
+__version__ = '0.2.1'
+__all__ = ['extract', 'Plugin', 'get_installed', '__version__']
