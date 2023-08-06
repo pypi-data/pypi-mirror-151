13b12f0715a49e8e2f86a2f9838c7804f1f758fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:22 +0000

    Add package regobslib/0.2.5/regobslib-0.2.5.tar.gz

---
+++ packages/regobslib/regobslib-0.2.5.tar.gz/regobslib-0.2.5/src/regobslib/__init__.py
@@ -0,0 +1,9 @@
+from .region import SnowRegion
+from .connection import Connection
+from .submit import SnowRegistration, DangerSign, AvalancheObs, AvalancheActivity, Weather, SnowCover, \
+    CompressionTest, SnowProfile, AvalancheProblem, DangerAssessment, Incident, Note, Image, Url, \
+    DestructiveSize, Sensitivity, Distribution, WeakLayer, Position, Elevation, Expositions, Direction, \
+    LocalImage
+from .varsom import SnowVarsom
+from .aps import Aps
+from .misc import TZ as REGOBS_TZ
