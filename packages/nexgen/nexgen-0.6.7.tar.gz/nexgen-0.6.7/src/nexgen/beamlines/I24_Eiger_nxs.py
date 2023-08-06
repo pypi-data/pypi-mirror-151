50abf4ebb9d6d715ccda0ed97567d33974fb1491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:40 +0000

    Add package nexgen/0.6.7/nexgen-0.6.7.tar.gz

---
+++ packages/nexgen/nexgen-0.6.7.tar.gz/nexgen-0.6.7/src/nexgen/beamlines/I24_Eiger_nxs.py
@@ -0,0 +1,374 @@
+"""
+Create a NeXus file for serial crystallography datasets collected on I24 Eiger 2X 9M detector.
+"""
+import glob
+import logging
+import sys
+from collections import namedtuple
+from pathlib import Path
+from typing import List
+
+import h5py
+
+from .. import get_iso_timestamp, get_nexus_filename
+from ..nxs_write.NexusWriter import ScanReader, call_writers
+from ..nxs_write.NXclassWriters import write_NXdatetime, write_NXentry, write_NXnote
+from ..tools.VDS_tools import image_vds_writer
+from .I24_Eiger_params import dset_links, eiger9M_params, goniometer_axes, source
+
+# import numpy as np
+
+
+# Define a logger object and a formatter
+logger = logging.getLogger("NeXusGenerator.I24")
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
+# Define a stream handler
+CH = logging.StreamHandler(sys.stdout)
+CH.setLevel(logging.DEBUG)
+CH.setFormatter(formatter)
+logger.addHandler(CH)
+
+ssx_collect = namedtuple(
+    "ssx_collect",
+    [
+        "visitpath",
+        "filename",
+        "exp_type",
+        "num_imgs",
+        "beam_center",
+        "detector_distance",
+        "start_time",
+        "stop_time",
+        "exposure_time",
+        "transmission",
+        "wavelength",
+        "flux",
+        "pump_status",
+        "pump_exp",
+        "pump_delay",
+    ],
+)
+
+coordinate_frame = "mcstas"
+
+# Initialize dictionaries
+goniometer = goniometer_axes
+detector = eiger9M_params
+module = {}
+beam = {}
+attenuator = {}
+
+
+def extruder(
+    master_file: Path,
+    filename: List[Path],
+    SSX: namedtuple,
+    metafile: Path = None,  # Just in case meta is not generated for some reason
+):
+    """
+    Write the NeXus file for extruder collections, pump-probe and not.
+
+    Args:
+        master_file (Path):     Path to the NeXus file to be written.
+        filename (list):        List of paths to file.
+        SSX (namedtuple):       Parameters passed from the beamline.
+        metafile (Path):        Path to the _meta.h5 file. Deafults to None.
+    """
+    logger.info(f"Write NeXus file for {SSX.exp_type}")
+
+    goniometer["starts"] = goniometer["ends"] = goniometer["increments"] = [
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+    # Get timestamps in the correct format
+    timestamps = (
+        get_iso_timestamp(SSX.start_time),
+        get_iso_timestamp(SSX.stop_time),
+    )
+    logger.info(f"Timestamps recorded: {timestamps}")
+
+    # Get scan range array and rotation axis
+    OSC, TRANSL = ScanReader(goniometer, n_images=SSX.num_imgs)
+    del TRANSL
+
+    logger.info("Goniometer information")
+    for j in range(len(goniometer["axes"])):
+        logger.info(
+            f"Goniometer axis: {goniometer['axes'][j]} => {goniometer['starts'][j]}, {goniometer['types'][j]} on {goniometer['depends'][j]}"
+        )
+
+    try:
+        with h5py.File(master_file, "x") as nxsfile:
+            write_NXentry(nxsfile)
+
+            if timestamps[0]:
+                write_NXdatetime(nxsfile, (timestamps[0], None))
+
+            call_writers(
+                nxsfile,
+                filename,
+                coordinate_frame,
+                (detector["mode"], SSX.num_imgs),
+                goniometer,
+                detector,
+                module,
+                source,
+                beam,
+                attenuator,
+                OSC,
+                transl_scan=None,
+                metafile=metafile,
+                link_list=dset_links,
+            )
+
+            # Write pump-probe information if requested
+            if SSX.pump_status == "true":
+                logger.info("Pump status is True, write pump information to file.")
+                pump_info = {}
+                if SSX.pump_exp:
+                    pump_info["pump_exposure_time"] = SSX.pump_exp
+                    logger.info(f"Recorded pump exposure time: {SSX.pump_exp}")
+                else:
+                    pump_info["pump_exposure_time"] = None
+                    logger.warning(
+                        "Pump exposure time has not been recorded and won't be written to file."
+                    )
+                if SSX.pump_delay:
+                    pump_info["pump_delay"] = SSX.pump_delay
+                    logger.info(f"Recorded pump delay time: {SSX.pump_delay}")
+                else:
+                    pump_info["pump_delay"] = None
+                    logger.warning(
+                        "Pump delay has not been recorded and won't be written to file."
+                    )
+                loc = "/entry/source/notes"
+                write_NXnote(nxsfile, loc, pump_info)
+
+            # Write VDS
+            image_vds_writer(nxsfile, (SSX.num_imgs, *detector["image_size"]))
+
+            if timestamps[1]:
+                write_NXdatetime(nxsfile, (None, timestamps[1]))
+            logger.info(f"{master_file} correctly written.")
+    except Exception as err:
+        logger.exception(err)
+        logger.info(
+            f"An error occurred and {master_file} couldn't be written correctly."
+        )
+
+
+def fixed_target(
+    master_file: Path,
+    filename: List[Path],
+    SSX: namedtuple,
+    metafile: Path = None,
+):
+    """
+    Write the NeXus file for fixed target collections, pump probe and not.
+
+    Args:
+        master_file (Path):         Path to the NeXus file to be written.
+        filename (List[Path]):      List of paths to file.
+        SSX (namedtuple):           Parameters passed from the beamline.
+        metafile (Path, optional):  Path to the _meta.h5 file. Defaults to None.
+    """
+    logger.info(f"Write NeXus file for {SSX.exp_type}")
+
+    # Get timestamps in the correct format
+    timestamps = (
+        get_iso_timestamp(SSX.start_time),
+        get_iso_timestamp(SSX.stop_time),
+    )
+    logger.info(f"Timestamps recorded: {timestamps}")
+
+    # Goniometer
+    # Set start and end values from input
+    # omega sam_z sam_y sam_x
+    goniometer["starts"] = []
+    goniometer["ends"] = []
+    goniometer["increments"] = []
+
+    # Identify rotation and grid scan axes, calculate ranges
+    OSC, TRANSL = ScanReader(goniometer, n_images=SSX.num_imgs)
+
+    # Log data
+    logger.info("Goniometer information")
+    for j in range(len(goniometer["axes"])):
+        logger.info(
+            f"Goniometer axis: {goniometer['axes'][j]} => {goniometer['starts'][j]}, {goniometer['types'][j]} on {goniometer['depends'][j]}"
+        )
+    logger.info(f"Oscillation axis: {OSC.keys()[0]}.")
+    logger.info(f"Fixed target axes: {list(TRANSL.keys())}.")
+
+    try:
+        with h5py.File(master_file, "x") as nxsfile:
+            write_NXentry(nxsfile)
+
+            if timestamps[0]:
+                write_NXdatetime(nxsfile, (timestamps[0], None))
+
+            call_writers(
+                nxsfile,
+                filename,
+                coordinate_frame,
+                (detector["mode"], SSX.num_imgs),
+                goniometer,
+                detector,
+                module,
+                source,
+                beam,
+                attenuator,
+                OSC,
+                transl_scan=TRANSL,
+                metafile=metafile,
+                link_list=dset_links,
+            )
+
+            # Write pump-probe information if requested
+            if SSX.pump_status == "true":
+                logger.info("Pump status is True, write pump information to file.")
+                pump_info = {}
+                if SSX.pump_exp:
+                    pump_info["pump_exposure_time"] = SSX.pump_exp
+                    logger.info(f"Recorded pump exposure time: {SSX.pump_exp}")
+                else:
+                    pump_info["pump_exposure_time"] = None
+                    logger.warning(
+                        "Pump exposure time has not been recorded and won't be written to file."
+                    )
+                if SSX.pump_delay:
+                    pump_info["pump_delay"] = SSX.pump_delay
+                    logger.info(f"Recorded pump delay time: {SSX.pump_delay}")
+                else:
+                    pump_info["pump_delay"] = None
+                    logger.warning(
+                        "Pump delay has not been recorded and won't be written to file."
+                    )
+                loc = "/entry/source/notes"
+                write_NXnote(nxsfile, loc, pump_info)
+
+            # Write VDS
+            image_vds_writer(nxsfile, (SSX.num_imgs, *detector["image_size"]))
+
+            if timestamps[1]:
+                write_NXdatetime(nxsfile, (None, timestamps[1]))
+            logger.info(f"{master_file} correctly written.")
+    except Exception as err:
+        logger.exception(err)
+        logger.info(
+            f"An error occurred and {master_file} couldn't be written correctly."
+        )
+
+
+def grid_scan_3D():
+    pass
+
+
+def write_nxs(**ssx_params):
+    """
+    Gather all parameters from the beamline and call appropriate writer function for serial crystallography.
+    """
+    # Get info from the beamline
+    SSX = ssx_collect(
+        visitpath=Path(ssx_params["visitpath"]).expanduser().resolve(),
+        filename=ssx_params["filename"],  # Template: test_##
+        exp_type=ssx_params["exp_type"],
+        num_imgs=ssx_params["num_imgs"],
+        beam_center=ssx_params["beam_center"],
+        detector_distance=ssx_params["det_dist"],
+        start_time=ssx_params["start_time"].strftime("%Y-%m-%dT%H:%M:%S")
+        if ssx_params["start_time"]
+        else None,  # This should be datetiem type
+        stop_time=ssx_params["stop_time"].strftime("%Y-%m-%dT%H:%M:%S")
+        if ssx_params["stop_time"]
+        else None,  # idem.
+        exposure_time=ssx_params["exp_time"],
+        transmission=ssx_params["transmission"],
+        wavelength=ssx_params["wavelength"],
+        flux=ssx_params["flux"],
+        pump_status=ssx_params["pump_status"],
+        pump_exp=ssx_params["pump_exp"],
+        pump_delay=ssx_params["pump_delay"],
+    )
+
+    # Add to dictionaries
+    detector["starts"] = [SSX.detector_distance]
+    detector["exposure_time"] = SSX.exposure_time
+    detector["beam_center"] = SSX.beam_center
+
+    attenuator["transmission"] = SSX.transmission
+
+    beam["wavelength"] = SSX.wavelength
+    beam["flux"] = SSX.flux
+
+    logger.info(f"Current collection directory: {SSX.visitpath}")
+    # Find metafile in directory and get info from it
+    try:
+        metafile = [
+            f for f in SSX.visitpath.iterdir() if SSX.filename + "_meta" in f.as_posix()
+        ][0]
+        logger.info(f"Found {metafile} in directory.")
+    except IndexError:
+        logger.warning(
+            "No _meta.h5 file found in directory. External links in the NeXus file will be broken."
+        )
+        logger.error(
+            "Missing metadata, unable to write NeXus file. Please use command line tool."
+        )
+        # TODO add instructions for using command line tool
+
+    module["fast_axis"] = detector.pop("fast_axis")
+    module["slow_axis"] = detector.pop("slow_axis")
+    # Set value for module_offset calculation.
+    module["module_offset"] = "1"
+
+    # Find datafiles
+    filename_template = (
+        metafile.parent / metafile.name.replace("meta", f"{6*'[0-9]'}")
+    ).as_posix()
+    filename = [
+        Path(f).expanduser().resolve() for f in sorted(glob.glob(filename_template))
+    ]
+
+    # Add some information to logger
+    logger.info("Creating a NeXus file for %s ..." % metafile.name)
+    # Get NeXus filename
+    master_file = get_nexus_filename(filename[0])
+    logger.info("NeXus file will be saved as %s" % master_file)
+
+    # Call correct function for the current experiment
+    if SSX.exp_type == "extruder":
+        extruder(master_file, filename, SSX, metafile)
+    elif SSX.exp_type == "fixed_target":
+        fixed_target(master_file, filename, SSX, metafile)
+    elif SSX.exp_type == "3Dgridscan":
+        grid_scan_3D()
+
+
+# # Example usage
+# if __name__ == "__main__":
+#     from datetime import datetime
+
+#     write_nxs(
+#         visitpath=sys.argv[1],
+#         filename=sys.argv[2],
+#         exp_type="extruder",
+#         num_imgs=2450,
+#         beam_center=[1590.7, 1643.7],
+#         det_dist=0.5,
+#         # start_time=None,
+#         # stop_time=None,
+#         start_time=datetime.now(),
+#         stop_time=datetime.now(),
+#         exp_time=0.002,
+#         transmission=1.0,
+#         wavelength=0.649,
+#         flux=None,
+#         pump_status="true",  # this is a string on the beamline
+#         pump_exp=None,
+#         pump_delay=None,
+#     )
---
+++ packages/nexgen/nexgen-0.6.7.tar.gz/nexgen-0.6.7/src/nexgen/nxs_copy/CopyTristanNexus.py
@@ -0,0 +1,170 @@
+"""
+Tools for copying the metadata from Tristan NeXus files.
+"""
+
+import logging
+from pathlib import Path
+from typing import Optional, Union
+
+import h5py
+import numpy as np
+
+from ..nxs_write import create_attributes
+from . import convert_scan_axis, get_nexus_tree, identify_tristan_scan_axis
+
+tristan_logger = logging.getLogger("CopyNeXus.tristan")
+
+
+def single_image_nexus(
+    data_file: Optional[Union[Path, str]],
+    tristan_nexus: Optional[Union[Path, str]],
+    write_mode: str = "x",
+) -> str:
+    """
+    Create a NeXus file for a single-image or a stationary pump-probe dataset.
+
+    Copy the nexus tree from the original NeXus file for a collection on Tristan
+    detector. In the case of a single image, the input scan_axis is a (start, stop) tuple where start and
+    stop have the same value, for a pump-probe experiment the values might differ for some older datasets.
+    The scan_axis in the new file will therefore be one single number, equal to the "start".
+
+    Args:
+        data_file:      String or Path pointing to the HDF5 file containing the newly binned images.
+        tristan_nexus:  String or Path pointing to the input NeXus file with experiment metadata to be copied.
+        write_mode:     String indicating writing mode for the output NeXus file.  Accepts any valid
+                        h5py file opening mode.
+
+    Returns:
+        nxs_filename:   The name of the output NeXus file.
+    """
+    data_file = Path(data_file).expanduser().resolve()
+    tristan_nexus = Path(tristan_nexus).expanduser().resolve()
+    nxs_filename = data_file.parent / f"{data_file.stem}.nxs"
+    with h5py.File(tristan_nexus, "r") as nxs_in, h5py.File(
+        nxs_filename, write_mode
+    ) as nxs_out:
+        # Copy the whole tree except for nxdata
+        nxentry = get_nexus_tree(nxs_in, nxs_out)
+        # Create nxdata group
+        nxdata = nxentry.create_group("data")
+        # Add link to data
+        nxdata["data"] = h5py.ExternalLink(data_file.name, "data")
+        # Compute and write axis information
+        ax, ax_attr = identify_tristan_scan_axis(nxs_in)
+        if ax:
+            create_attributes(
+                nxdata,
+                ("NX_class", "axes", "signal", ax + "_indices"),
+                (
+                    "NXdata",
+                    ax,
+                    "data",
+                    [
+                        0,
+                    ],
+                ),
+            )
+            try:
+                ax_range = nxs_in["entry/data"][ax][0]
+            except ValueError:
+                # Some early Tristan data from before March 2021, where the goniometer
+                # was not moved during the data collection, record the rotation axis
+                # position as a scalar.
+                ax_range = nxs_in["entry/data"][ax][()]
+            nxdata.create_dataset(ax, data=ax_range)
+            # Write the attributes
+            for key, value in ax_attr.items():
+                nxdata[ax].attrs.create(key, value)
+            # Now fix all other instances of scan_axis in the tree
+            nxsample = nxentry["sample"]
+            convert_scan_axis(nxsample, nxdata, ax)
+
+    return nxs_filename.as_posix()
+
+
+def multiple_images_nexus(
+    data_file: Optional[Union[Path, str]],
+    tristan_nexus: Optional[Union[Path, str]],
+    write_mode: str = "x",
+    osc: float = None,
+    nbins: int = None,
+) -> str:
+    """
+    Create a NeXus file for a multiple-image dataset or multiple image sequences from a pump-probe collection.
+
+    Copy the nexus tree from the original NeXus file for a collection on Tristan
+    detector. In this case multiple images from a rotation collection have been
+    binned and the scan_axis to be found in the input file is a (start, stop) tuple.
+    The scan_axis in the new file will therefore be a list of angles.
+    Osc and num_bins are mutually exclusive arguments to work out the scan_axis list.
+
+    Args:
+        data_file:      String or Path pointing to the HDF5 file containing the newly binned images.
+        tristan_nexus:  String or Path pointing to the input NeXus file with experiment metadata to be copied.
+        write_mode:     String indicating writing mode for the output NeXus file.  Accepts any valid
+                        h5py file opening mode.
+        osc:            Oscillation angle (degrees).
+        nbins:          Number of binned images.
+
+    Returns:
+        nxs_filename:   The name of the output NeXus file.
+    """
+    data_file = Path(data_file).expanduser().resolve()
+    tristan_nexus = Path(tristan_nexus).expanduser().resolve()
+    nxs_filename = data_file.parent / f"{data_file.stem}.nxs"
+    with h5py.File(tristan_nexus, "r") as nxs_in, h5py.File(
+        nxs_filename, write_mode
+    ) as nxs_out:
+        # Copy the whole tree except for nxdata
+        nxentry = get_nexus_tree(nxs_in, nxs_out)
+        # Create nxdata group
+        nxdata = nxentry.create_group("data")
+        # Add link to data
+        nxdata["data"] = h5py.ExternalLink(data_file.name, "data")
+        # Compute and write axis information
+        ax, ax_attr = identify_tristan_scan_axis(nxs_in)
+        if ax:
+            create_attributes(
+                nxdata,
+                ("NX_class", "axes", "signal", ax + "_indices"),
+                (
+                    "NXdata",
+                    ax,
+                    "data",
+                    [
+                        0,
+                    ],
+                ),
+            )
+            try:
+                (start, stop) = nxs_in["entry/data"][ax][()]
+            except (TypeError, ValueError):
+                # Some early Tristan data from before March 2021, where the goniometer
+                # was not moved during the data collection, record the rotation axis
+                # position as a scalar.
+                start = stop = nxs_in["entry/data"][ax][()]
+
+            if osc and nbins:
+                raise ValueError(
+                    "osc and nbins are mutually exclusive, "
+                    "please pass only one of them."
+                )
+            elif osc:
+                ax_range = np.arange(start, stop, osc)
+            elif nbins:
+                ax_range = np.linspace(start, stop, nbins + 1)[:-1]
+            else:
+                raise ValueError(
+                    "Impossible to calculate scan_axis, "
+                    "please pass either osc or nbins."
+                )
+
+            nxdata.create_dataset(ax, data=ax_range)
+            # Write the attributes
+            for key, value in ax_attr.items():
+                nxdata[ax].attrs.create(key, value)
+            # Now fix all other instances of scan_axis in the tree
+            nxsample = nxentry["sample"]
+            convert_scan_axis(nxsample, nxdata, ax)
+
+    return nxs_filename.as_posix()
