56d1a78ccb863740fc0c8ac42b7c2292eca8a601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:42 +0000

    Add package hiss-repl/4.0.0/hiss-repl-4.0.0.tar.gz

---
+++ packages/hiss-repl/hiss-repl-4.0.0.tar.gz/hiss-repl-4.0.0/src/hiss/cli/__init__.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import warnings as _warnings
+
+from pathlib import Path
+
+import IPython
+
+from .config import build_config, load_rc
+from .magic import *  # noqa
+from .virtualenv import load_venv
+
+_PYTHON_VERSION = "python{0}.{1}".format(*sys.version_info[0:2])
+_HISS_CONFIG = Path(os.environ.get("HOME", "~"), ".hiss").expanduser()
+_BANNER = """
+hiss - {python_version}
+
+"""
+
+
+def main():
+    # type: (str, bool) -> None
+    """Console script for hiss"""
+    banner = _BANNER.format(python_version=_PYTHON_VERSION)
+
+    for warning in (UserWarning, DeprecationWarning, RuntimeWarning):
+        _warnings.filterwarnings("ignore", category=warning)
+
+    # check for and (optionally) enter virtualenv
+    load_venv(_PYTHON_VERSION)
+
+    rc = {}
+    config = Path("~/.hiss").expanduser()
+    if config.exists():  # type: ignore
+        rc.update(load_rc(config))
+
+    # build configuration
+    hc = build_config(rc, banner)
+
+    # start customized ipython!
+    IPython.start_ipython(argv=extra_args, config=hc, quick=True, auto_create=False)
+
+
+if __name__ == "__main__":
+    main()
