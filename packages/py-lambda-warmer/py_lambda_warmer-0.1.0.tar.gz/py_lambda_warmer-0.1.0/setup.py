ff00c0d7b950d22646a61f909b06adf5cc6e00ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:59 +0000

    Add package py-lambda-warmer/0.1.0/py_lambda_warmer-0.1.0.tar.gz

---
+++ packages/py-lambda-warmer/py_lambda_warmer-0.1.0.tar.gz/py_lambda_warmer-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="py_lambda_warmer",
+    version="0.1.0",
+    description="Warmer Utility for Lambda Function",
+    author="satyam soni",
+    author_email="satyamsoni@hotmail.co.uk",
+    py_modules=["warmer", ],
+    url="https://github.com/satyamsoni2211/LambdaWarmerPy.git",
+    keywords="lambda cold start warming performance",
+    license="mit",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ]
+)
