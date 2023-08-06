71ef3817a2de08ceaf219aaf5703b079075e82a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:06 +0000

    Add package qrutils/0.0.1/qrutils-0.0.1.tar.gz

---
+++ packages/qrutils/qrutils-0.0.1.tar.gz/qrutils-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from ensurepip import version
+from setuptools import find_packages, setup
+
+setup(
+    name="qrutils",
+    version="0.0.1",
+    author='VictorT',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm',
+        'pandas',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ]
+)
