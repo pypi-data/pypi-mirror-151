949e71002bb073b35a0888f53dc68a8105dc7160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:38 +0000

    Add package persuader-technology-automata-exchange-transform-repository/0.0.6/persuader-technology-automata-exchange-transform-repository-0.0.6.tar.gz

---
+++ packages/persuader-technology-automata-exchange-transform-repository/persuader-technology-automata-exchange-transform-repository-0.0.6.tar.gz/persuader-technology-automata-exchange-transform-repository-0.0.6/exchangetransformrepo/repository/serialize/exchange_transform_deserializer.py
@@ -0,0 +1,13 @@
+from coreutility.collection.dictionary_utility import as_data
+
+from exchangetransformrepo.ExchangeTransform import ExchangeTransform
+
+
+def deserialize_exchange_transform(exchange_transform) -> ExchangeTransform:
+    instrument = as_data(exchange_transform, 'instrument')
+    transform = as_data(exchange_transform, 'transform')
+    ignore = as_data(exchange_transform, 'ignore', False)
+    deserialized = ExchangeTransform(instrument, transform)
+    if ignore:
+        deserialized.ignore = ignore
+    return deserialized
