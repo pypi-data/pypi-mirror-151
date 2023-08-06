d634f1b4a3b0ddfc7e9f88beadb7421432a06c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:38 +0000

    Add package TG-AutoPoster/2.4.4/TG_AutoPoster-2.4.4.tar.gz

---
+++ packages/TG-AutoPoster/TG_AutoPoster-2.4.4.tar.gz/TG_AutoPoster-2.4.4/TG_AutoPoster/__main__.py
@@ -0,0 +1,40 @@
+from loguru import logger as log
+from .TG_AutoPoster import AutoPoster, create_parser
+import os
+
+
+if __name__ == "__main__":
+    args = create_parser().parse_args()
+    if args.debug:
+        log.add("./logs/bot_log_DEBUG.log", level="DEBUG", rotation="5 MB")
+    else:
+        log.remove()
+        log.add("./logs/bot_log_{time}.log", level="INFO")
+    log.info("Начало работы.")
+    autoposter = AutoPoster(
+        config_path=args.config, cache_dir=args.cache_dir, ipv6=args.ipv6
+    )
+    if args.loop or args.sleep:
+        sleep_time = args.sleep if args.sleep else 3600
+        log.info(
+            "Программе был передан аргумент --loop (-l). "
+            "Запуск бота в бесконечном цикле."
+        )
+        autoposter.infinity_run(interval=sleep_time)
+    elif os.getenv("TG_SLEEP") or os.getenv("TG_LOOP"):
+        try:
+            sleep_time = int(os.getenv("TG_SLEEP"))
+        except ValueError:
+            log.warning(
+                "Переменная окружения TG_SLEEP задана неверно, "
+                "используется значение 3600"
+            )
+            sleep_time = 3600
+        log.info(
+            "При запуске программы была найдена переменная окружения TG_LOOP. "
+            "Запуск бота в бесконечном цикле."
+        )
+        autoposter.infinity_run(interval=sleep_time)
+    else:
+        autoposter.run()
+    log.info("Работа завершена. Выход.")
---
+++ packages/TG-AutoPoster/TG_AutoPoster-2.4.4.tar.gz/TG_AutoPoster-2.4.4/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = "2.4.4"
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="TG_AutoPoster",
+    version=version,
+
+    author="qwertyadrian",
+    author_email="me@qwertyadrian.ru",
+
+    description="Telegram Bot for AutoPosting from VK",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url="https://github.com/qwertyadrian/TG_AutoPoster",
+    download_url="https://github.com/qwertyadrian/TG_AutoPoster/archive/v{}.zip".format(
+        version
+    ),
+
+    license="MIT License, see LICENCE.md",
+    platforms=["OS Independent"],
+
+    packages=find_packages(),
+    install_requires=[
+        "pyrogram==2.0.24",
+        "tgcrypto",
+        "loguru==0.6.0",
+        "wget==3.2",
+        "mutagen==1.45.1",
+        "beautifulsoup4==4.11.1",
+        "vk_api==11.9.8",
+        "streamlink==4.0.1",
+    ],
+
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Communications :: Chat",
+    ]
+)
