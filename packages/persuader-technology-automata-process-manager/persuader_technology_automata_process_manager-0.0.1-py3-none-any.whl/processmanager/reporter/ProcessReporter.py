032165f5899964a1d35296dd2bba1e1d810acb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:34 +0000

    Add package persuader-technology-automata-process-manager/0.0.1/persuader_technology_automata_process_manager-0.0.1-py3-none-any.whl

---
+++ packages/persuader-technology-automata-process-manager/persuader_technology_automata_process_manager-0.0.1-py3-none-any.whl/processmanager/reporter/ProcessReporter.py
@@ -0,0 +1,14 @@
+from coreutility.date_utility import get_utc_timestamp
+from processrepo.Process import ProcessStatus, Process
+from processrepo.repository.ProcessRepository import ProcessRepository
+
+
+class ProcessReporter:
+
+    def __init__(self, repository: ProcessRepository):
+        self.repository = repository
+
+    def report(self, process_name, status: ProcessStatus):
+        instant = get_utc_timestamp()
+        process = Process(process_name, instant, status)
+        self.repository.store(process)
