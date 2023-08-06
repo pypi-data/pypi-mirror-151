334cb8d529e313b236762bc7c68a8f1d23557ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:47 +0000

    Add package data-manipulation/0.30/data_manipulation-0.30-py3-none-any.whl

---
+++ packages/data-manipulation/data_manipulation-0.30-py3-none-any.whl/data_manipulation/__init__.py
@@ -0,0 +1,18 @@
+from data_manipulation import (
+    base,
+    beautifulsoup_,
+    django_,
+    geopandas_,
+    pandas_,
+    psycopg2_,
+    pyspark_,
+)
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
