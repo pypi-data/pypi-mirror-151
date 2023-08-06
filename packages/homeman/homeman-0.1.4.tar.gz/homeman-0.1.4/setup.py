88b7c565198b7051b9b7b70dca31265104ce24a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:48:05 +0000

    Add package homeman/0.1.4/homeman-0.1.4.tar.gz

---
+++ packages/homeman/homeman-0.1.4.tar.gz/homeman-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'homeman',
+    version = '0.1.4',
+    description = 'keep files synchronized between user home and your special home directory',
+    author = 'sunn4room',
+    author_email = 'sunn4room@163.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages = find_packages(),
+    install_requires = ['colorama'],
+    entry_points = {
+        'console_scripts': [
+            'homeman = homeman:main'
+        ]
+    }
+)
