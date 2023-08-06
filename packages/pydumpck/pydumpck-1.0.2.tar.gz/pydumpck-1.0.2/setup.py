3e6c5e5f6b3276df0f28e7f6a9cdf9e6831a884f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:23 +0000

    Add package pydumpck/1.0.2/pydumpck-1.0.2.tar.gz

---
+++ packages/pydumpck/pydumpck-1.0.2.tar.gz/pydumpck-1.0.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+import requests
+
+
+long_description = 'a tool for decomplier exe,elf,pyz,pyc packed by python which is base on pycdc.sometimes its result not exactly right ,maybe could use uncompyle6 etc.'
+if os.path.exists("requirements.txt"):
+    install_requires = open("requirements.txt").read().split("\n")
+else:
+    install_requires = []
+
+setup(
+    name='pydumpck',
+    version='1.0.2',
+    keywords=('pydumpck', 'decomplier', 'pe', 'elf', 'pyc', 'pyz'),
+    description='a pyfile decomplier base on pycdc for windows',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    license='MIT Licence',
+    url='https://github.com/serfend/pydumpck',
+    author='serfend',
+    author_email='serfend@foxmail.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'pydump = main.__main__:main'
+        ]
+    }
+)
