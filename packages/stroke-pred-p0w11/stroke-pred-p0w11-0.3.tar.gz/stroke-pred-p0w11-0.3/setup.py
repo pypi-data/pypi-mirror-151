f5be587270ebac7b34775473fc0a4ee8875aab20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:21 +0000

    Add package stroke-pred-p0w11/0.3/stroke-pred-p0w11-0.3.tar.gz

---
+++ packages/stroke-pred-p0w11/stroke-pred-p0w11-0.3.tar.gz/stroke-pred-p0w11-0.3/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='stroke-pred-p0w11',
+    version='0.3',
+    license='MIT',
+    author="Patrick Saade",
+    author_email='patrick_saade@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Isaacgv/stroke_heart_prediction',
+    keywords='stroke prediction project',
+    install_requires=[
+          'scikit-learn',
+          'pandas',
+          'numpy',
+      ],
+
+)
