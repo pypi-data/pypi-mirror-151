2e5d4e5a938240845fea934572691b84be295df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:31 +0000

    Add package maxhonchar/0.0.1/maxhonchar-0.0.1.tar.gz

---
+++ packages/maxhonchar/maxhonchar-0.0.1.tar.gz/maxhonchar-0.0.1/README.md
@@ -0,0 +1,15 @@
+"My first PyPi package" project
+===============
+Hello world!
+
+Installing
+============
+
+    pip install example-publish-pypi-medium
+
+Usage
+=====
+
+    >>> from src.logic import run
+    >>> run.print_hello()
+    'hello!'
---
+++ packages/maxhonchar/maxhonchar-0.0.1.tar.gz/maxhonchar-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='maxhonchar',
+    version='0.0.1',
+    license='MIT',
+    author="Max Honchar",
+    author_email='maxgonchar9@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/honchardev/Fun',
+    keywords='maxhonchar pypi test project',
+    install_requires=[]
+)
