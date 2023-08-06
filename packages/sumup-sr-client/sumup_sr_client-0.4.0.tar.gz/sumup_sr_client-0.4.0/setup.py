b3822ef253c4537f60498d9d952940f635836f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:20 +0000

    Add package sumup-sr-client/0.4.0/sumup_sr_client-0.4.0.tar.gz

---
+++ packages/sumup-sr-client/sumup_sr_client-0.4.0.tar.gz/sumup_sr_client-0.4.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sumup_sr_client',
+    version='0.4.0',
+    description='A Python Package for the sumup schema registry client',
+    author='Ajay Muppuri',
+    author_email='ajay.muppuri@sumup.com',
+    packages=['schema_registry', 'schema_registry.client', 'schema_registry.registry', 'schema_registry.utils'],
+    install_requires=['acryl-datahub[kafka]'
+                      ]
+)
+download_url="https://github.com/sumup/data-ingestion-infrastructure/archive/refs/tags/0.1.0.tar.gz"
