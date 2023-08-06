8034b7b8ef93512ac9ffd3926269d209222f68bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:02 +0000

    Add package python-ramda/0.1.2/python_ramda-0.1.2-py3-none-any.whl

---
+++ packages/python-ramda/python_ramda-0.1.2-py3-none-any.whl/test/private/test__curry2.py
@@ -0,0 +1,30 @@
+import unittest
+
+import ramda as R
+from ramda.private._curry2 import _curry2
+
+"""
+https://github.com/ramda/ramda/blob/master/test/internal/_curry2.js
+"""
+
+
+class Test_Curry2(unittest.TestCase):
+  def test_supports_placeholder(self):
+    def f(a, b): return [a, b]
+    g = _curry2(f)
+    _ = R.__
+
+    self.assertEqual([1, 2], g(1)(2))
+    self.assertEqual([1, 2], g(1, 2))
+
+    self.assertEqual([1, 2], g(_, 2)(1))
+    self.assertEqual([1, 2], g(1, _)(2))
+
+    self.assertEqual([1, 2], g(_, _)(1)(2))
+    self.assertEqual([1, 2], g(_, _)(1, 2))
+    self.assertEqual([1, 2], g(_, _)(_)(1, 2))
+    self.assertEqual([1, 2], g(_, _)(_, 2)(1))
+
+
+if __name__ == '__main__':
+  unittest.main()
