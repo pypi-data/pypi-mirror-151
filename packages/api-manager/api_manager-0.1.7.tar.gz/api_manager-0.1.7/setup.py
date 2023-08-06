0f7e9ecca92558070b6022886b76de8c80dda84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:04 +0000

    Add package api-manager/0.1.7/api_manager-0.1.7.tar.gz

---
+++ packages/api-manager/api_manager-0.1.7.tar.gz/api_manager-0.1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ['requests', 'allure-python-commons', 'waiting']
+
+setup(
+    name="api_manager",
+    version="0.1.7",
+    author="Nikita Filonov",
+    author_email="filonov.nikitkaa@gmail.com",
+    description="Api Manager",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nikita-Filonov/models_manager",
+    packages=find_packages(),
+    install_requires=requirements,
+)
