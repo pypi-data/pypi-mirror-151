b362dabd67cb48a1f2431b099a354c8ad6fda077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:10 +0000

    Add package front/0.1.33/front-0.1.33.tar.gz

---
+++ packages/front/front-0.1.33.tar.gz/front-0.1.33/front/elements/__init__.py
@@ -0,0 +1,15 @@
+from .elements import (
+    ContainerFlag,
+    InputComponentFlag,
+    FrontElementBase,
+    FrontContainerBase,
+    FrontComponentBase,
+    InputBase,
+    TextInputBase,
+    IntInputBase,
+    FloatInputBase,
+    AppBase,
+    FuncViewBase,
+)
+from .tree_maker_base import ElementTreeMakerBase
+from .implementation import implement_component
---
+++ packages/front/front-0.1.33.tar.gz/front-0.1.33/front/elements/elements.py
@@ -0,0 +1,116 @@
+from abc import ABC, abstractmethod
+from inspect import Parameter, _empty
+from typing import Any, Callable, Iterable
+from enum import IntFlag
+
+
+class ContainerFlag(IntFlag):
+    APP = (0,)
+    VIEW = (1,)
+
+
+class InputComponentFlag(IntFlag):
+    TEXT = (1,)
+    INT = (2,)
+    FLOAT = (4,)
+    FLOAT_SLIDER = (8,)
+
+
+# class OutputComponentFlag(IntFlag):
+#     TEXT = 1,
+#     NUMBER = 2,
+
+
+# ComponentFlag = Union[InputComponentFlag, OutputComponentFlag]
+# ElementFlag = Union[ContainerFlag, ComponentFlag]
+
+
+class FrontElementBase(ABC):
+    @abstractmethod
+    def render(self):
+        # raise NotImplementedError('This method needs to be implemented in subclasses.')
+        pass
+
+
+class FrontContainerBase(FrontElementBase):
+    children: Iterable[FrontElementBase]
+
+    def __init__(self, children: Iterable[FrontElementBase] = None):
+        self.children = children or []
+
+
+class FrontComponentBase(FrontElementBase):
+    pass
+
+
+class AppBase(FrontContainerBase):
+    title: str = None
+
+
+class InputBase(FrontComponentBase):
+    def __init__(self, label: str, input_key: str = None, init_value: Any = None):
+        super().__init__()
+        self.label = label
+        self.input_key = input_key
+        self.init_value = init_value
+
+
+class FuncViewBase(FrontContainerBase):
+    def __init__(self, func: Callable, children: Iterable[InputBase] = None):
+        super().__init__(children)
+        self.func = func
+        self.name = func.__name__ or 'Front Func View'
+
+
+class TextInputBase(InputBase):
+    def __init__(
+        self, label: str, input_key: str = None, init_value: Any = None
+    ) -> None:
+        super().__init__(label, input_key, init_value)
+        self.init_value = str(self.init_value) if self.init_value is not None else ''
+
+
+class NumberInputBase(InputBase):
+    def __init__(
+        self,
+        label: str,
+        input_key: str = None,
+        init_value: Any = None,
+        min_value=None,
+        max_value=None,
+        format: str = None,
+    ):
+        super().__init__(label, input_key, init_value)
+        self.min_value = min_value
+        self.max_value = max_value
+        self.format = format
+
+
+class IntInputBase(NumberInputBase):
+    def __init__(
+        self,
+        label: str,
+        input_key: str = None,
+        init_value: Any = None,
+        min_value: int = None,
+        max_value: int = None,
+        format: str = None,
+    ):
+        super().__init__(label, input_key, init_value, min_value, max_value, format)
+        self.init_value = int(self.init_value) if self.init_value is not None else 0
+
+
+class FloatInputBase(NumberInputBase):
+    def __init__(
+        self,
+        label: str,
+        input_key: str = None,
+        init_value: Any = None,
+        min_value: float = None,
+        max_value: float = None,
+        format: str = None,
+        step: float = None,
+    ):
+        super().__init__(label, input_key, init_value, min_value, max_value, format)
+        self.init_value = float(self.init_value) if self.init_value is not None else 0.0
+        self.step = step
