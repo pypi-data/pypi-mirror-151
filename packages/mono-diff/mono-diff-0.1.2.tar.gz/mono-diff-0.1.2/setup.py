0535d604685c9c36d350538e8b84c856cef699d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:16 +0000

    Add package mono-diff/0.1.2/mono-diff-0.1.2.tar.gz

---
+++ packages/mono-diff/mono-diff-0.1.2.tar.gz/mono-diff-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+# requirements_file = 'requirements.txt'
+# requirements =  [x.strip() for x in open(requirements_file).readlines() if not x.startswith('#')]
+
+setup(
+    name='mono-diff',
+    version='0.1.2',
+    license='MIT',
+    author="Ran Sasportas",
+    author_email='ran729@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ran729/mono',
+    keywords='mono',
+    install_requires=['fire', 'tenacity']
+)
