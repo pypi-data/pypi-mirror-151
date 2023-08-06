eb8808be818f05d3e7bca3ca49a2838af42025b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:38 +0000

    Add package Museparation/0.0.1a17/Museparation-0.0.1a17.tar.gz

---
+++ packages/Museparation/Museparation-0.0.1a17.tar.gz/Museparation-0.0.1a17/museparation/util/random_amplify.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def random_amplify(stem_dicc, minimum, maximum):
+	for key in stem_dicc:
+		stem_dicc[key] = stem_dicc[key] * np.random.uniform(minimum, maximum)
+	return stem_dicc
