0024b99bd0353b549d8a8f2a2cd6ad753d8adfd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:51 +0000

    Add package data-manipulation/0.32/data_manipulation-0.32-py3-none-any.whl

---
+++ packages/data-manipulation/data_manipulation-0.32-py3-none-any.whl/data_manipulation/beautifulsoup_.py
@@ -0,0 +1,34 @@
+def build_soup(url):
+    """
+    Return Beautifulsoup object from given url
+
+    Parameters
+    ----------
+    url: str
+        URL
+
+    Examples
+    --------
+    >>> a = build_soup("https://google.com")
+    >>> type(a)
+    <class 'bs4.BeautifulSoup'>
+
+    Returns
+    -------
+    BS4 or None
+    """
+    import requests
+    from bs4 import BeautifulSoup
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, features="lxml")
+        return soup
+    else:
+        print(f"response status code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
