a79167d3e6dfc1270bb99695f0108fc1949ad981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:16 +0000

    Add package all-prac-codes/0.8/all_prac_codes-0.8.tar.gz

---
+++ packages/all-prac-codes/all_prac_codes-0.8.tar.gz/all_prac_codes-0.8/README.rst
@@ -0,0 +1,19 @@
+
+
+Example Project
+===============
+This contains all BE prac codes
+
+Installing
+============
+
+.. code-block:: bash
+
+    pip install all-prac-codes
+
+Usage
+=====
+
+.. code-block:: bash
+
+    get_dh()
---
+++ packages/all-prac-codes/all_prac_codes-0.8.tar.gz/all_prac_codes-0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='all_prac_codes',
+    version='0.8',
+    license='MIT',
+    author="AH Hacker",
+    author_email='email@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='practicle',
+    install_requires=[
+      ],
+
+)
