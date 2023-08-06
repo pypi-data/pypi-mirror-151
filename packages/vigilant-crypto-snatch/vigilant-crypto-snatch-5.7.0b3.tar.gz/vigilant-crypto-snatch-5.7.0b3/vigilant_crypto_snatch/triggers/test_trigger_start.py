6e1abf812e7fb0c59fde0a56ac0414fd45a16376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:18 +0000

    Add package vigilant-crypto-snatch/5.7.0b3/vigilant-crypto-snatch-5.7.0b3.tar.gz

---
+++ packages/vigilant-crypto-snatch/vigilant-crypto-snatch-5.7.0b3.tar.gz/vigilant-crypto-snatch-5.7.0b3/vigilant_crypto_snatch/triggers/test_trigger_start.py
@@ -0,0 +1,33 @@
+import datetime
+
+import pytest
+
+from ..core import AssetPair
+from ..datastorage import ListDatastore
+from ..historical import MockHistorical
+from ..marketplace import MockMarketplace
+from .concrete import BuyTrigger
+from .factory import make_buy_trigger
+from .interface import TriggerSpec
+
+
+@pytest.fixture
+def drop_trigger_with_start() -> BuyTrigger:
+    datastore = ListDatastore()
+    source = MockHistorical()
+    market = MockMarketplace()
+    trigger_spec = TriggerSpec(
+        asset_pair=AssetPair("BTC", "EUR"),
+        volume_fiat=10.0,
+        cooldown_minutes=10,
+        start=datetime.datetime(2021, 7, 16),
+    )
+    result = make_buy_trigger(datastore, source, market, trigger_spec)
+    return result
+
+
+def test_trigger_with_start(drop_trigger_with_start: BuyTrigger) -> None:
+    before = datetime.datetime(2021, 7, 15)
+    after = datetime.datetime(2021, 7, 17)
+    assert not drop_trigger_with_start.has_cooled_off(before)
+    assert drop_trigger_with_start.has_cooled_off(after)
