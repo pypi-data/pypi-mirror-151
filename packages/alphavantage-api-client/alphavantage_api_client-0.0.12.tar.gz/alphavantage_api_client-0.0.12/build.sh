def31cbd9c4994883b1bbe0978c45b4baeecf849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:22 +0000

    Add package alphavantage-api-client/0.0.12/alphavantage_api_client-0.0.12.tar.gz

---
+++ packages/alphavantage-api-client/alphavantage_api_client-0.0.12.tar.gz/alphavantage_api_client-0.0.12/build.sh
@@ -0,0 +1,7 @@
+rm -rf ./alphavantage_api_client.egg-info
+rm -rf ./build
+rm -rf ./dist
+#pip wheel . -w dist
+python3 setup.py bdist_wheel
+#python3 setup.py sdist
+#python3 -m build
---
+++ packages/alphavantage-api-client/alphavantage_api_client-0.0.12.tar.gz/alphavantage_api_client-0.0.12/setup.py
@@ -0,0 +1,43 @@
+import os.path
+import pathlib
+import subprocess
+
+import setuptools
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# get module version from git tag
+client_version = subprocess.run(['git', 'describe', '--tags'],
+                                stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# assert "." in alphavantage_api_client_version
+# assert os.path.isfile("alphavantage_api_client/version.py")
+# with open("alphavantage_api_client/VERSION","w", encoding="utf-8") as fh:
+#    fh.write(f'{alphavantage_api_client_version}\n')
+
+# This call to setup() does all the work
+setup(
+    name="alphavantage_api_client",
+    version=client_version,
+    description="Interact with Alphavantage REST API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xrgarcia/alphavantage-api-client",
+    author="Slashbin, LLC",
+    author_email="support@slashbin.us",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    packages=setuptools.find_packages(),
+    py_modules=["alphavantage_api_client"],
+    include_package_data=True,
+    install_requires=["requests"],
+    python_requires=">=3.6"
+)
