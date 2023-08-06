0a1f2555503a631e0ae7cc290b168e23a97290ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:00 +0000

    Add package RelevanceAI-dev/2.2.0.2022.5.23.6.16.22.111043/RelevanceAI-dev-2.2.0.2022.5.23.6.16.22.111043.tar.gz

---
+++ packages/RelevanceAI-dev/RelevanceAI-dev-2.2.0.2022.5.23.6.16.22.111043.tar.gz/RelevanceAI-dev-2.2.0.2022.5.23.6.16.22.111043/relevanceai/operations_new/processing/text/sentence_splitting/ops.py
@@ -0,0 +1,12 @@
+"""
+Sentence Splitting Operator
+Split Text
+"""
+from relevanceai.operations_new.apibase import OperationsAPIBase
+from relevanceai.operations_new.processing.text.sentence_splitting.base import (
+    SentenceSplittingBase,
+)
+
+
+class SentenceSplitterOps(SentenceSplittingBase, OperationsAPIBase):
+    pass
