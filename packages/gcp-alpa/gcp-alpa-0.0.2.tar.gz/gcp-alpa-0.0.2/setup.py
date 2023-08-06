218aec24c6c3da103340c6eaa6325fba784e33f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:52 +0000

    Add package gcp-alpa/0.0.2/gcp-alpa-0.0.2.tar.gz

---
+++ packages/gcp-alpa/gcp-alpa-0.0.2.tar.gz/gcp-alpa-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gcp-alpa", # Replace with your own username
+    version="0.0.2",
+    author="Albert Pang",
+    author_email="alpaaccount@mac.com",
+    description="GCP API Wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alpaalpa/gcp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "munch",
+        "requests"
+    ]
+)
