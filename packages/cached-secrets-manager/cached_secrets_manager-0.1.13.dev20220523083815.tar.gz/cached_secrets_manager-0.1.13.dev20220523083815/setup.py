419803573d26ce53e49221b43ed5c1d6b4f2c450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:46 +0000

    Add package cached-secrets-manager/0.1.13.dev20220523083815/cached_secrets_manager-0.1.13.dev20220523083815.tar.gz

---
+++ packages/cached-secrets-manager/cached_secrets_manager-0.1.13.dev20220523083815.tar.gz/cached_secrets_manager-0.1.13.dev20220523083815/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cached_secrets_manager']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.21.20,<2.0.0', 'botocore>=1.24.20,<2.0.0']
+
+setup_kwargs = {
+    'name': 'cached-secrets-manager',
+    'version': '0.1.13.dev20220523083815',
+    'description': '',
+    'long_description': None,
+    'author': 'Maximilian Walther',
+    'author_email': 'max@spryfox.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.12,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
