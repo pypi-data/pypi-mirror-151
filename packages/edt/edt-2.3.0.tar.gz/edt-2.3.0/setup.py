3dc7f1656ed0d3fca91d7cebd4cef4d5dc74effc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:26 +0000

    Add package edt/2.3.0/edt-2.3.0.tar.gz

---
+++ packages/edt/edt-2.3.0.tar.gz/edt-2.3.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+import sys
+
+import numpy as np
+
+# NOTE: If edt.cpp does not exist:
+# cython -3 --fast-fail -v --cplus edt.pyx
+
+extra_compile_args = []
+if sys.platform == 'win32':
+  extra_compile_args += [
+    '/std:c++11', '/O2'
+  ]
+else:
+  extra_compile_args += [
+    '-std=c++11', '-O3', '-ffast-math', '-pthread'
+  ]
+
+if sys.platform == 'darwin':
+  extra_compile_args += [ '-stdlib=libc++', '-mmacosx-version-min=10.9' ]
+
+
+setuptools.setup(
+  setup_requires=['pbr'],
+  python_requires="~=3.6", # >= 3.6 < 4.0
+  ext_modules=[
+    setuptools.Extension(
+      'edt',
+      sources=[ 'edt.cpp' ],
+      language='c++',
+      include_dirs=[ np.get_include() ],
+      extra_compile_args=extra_compile_args,
+    ),
+  ],
+  long_description_content_type='text/markdown',
+  pbr=True
+)
