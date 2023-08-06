21f9bc70458bcd27819d66604ff1f1b5f9d96d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:42 +0000

    Add package statistical-methods/0.0.1/statistical_methods-0.0.1-py3-none-any.whl

---
+++ packages/statistical-methods/statistical_methods-0.0.1-py3-none-any.whl/test/finance_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import random as rd
+import unittest 
+
+import sys
+
+sys.path.insert(0, 'C:/Users/natji/statistical_methods/statistical_methods/')
+
+from finance import *
+
+class TestFinance(unittest.TestCase):
+
+    def test_semivariance(self):
+        assert True
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/statistical-methods/statistical_methods-0.0.1-py3-none-any.whl/test/timeseries_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random as rd
+import unittest
+
+import sys
+
+sys.path.insert(0, 'C:/Users/natji/statistical_methods/statistical_methods/')
+
+from timeseries import *
+
+class TestTimeseries(unittest.TestCase):
+
+    def test_difference(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
