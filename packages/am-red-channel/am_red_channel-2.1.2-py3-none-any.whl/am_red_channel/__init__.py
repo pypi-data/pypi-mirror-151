561a45abe26627ba691e0aa6b34c5192fd657612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:14 +0000

    Add package am-red-channel/2.1.2/am_red_channel-2.1.2-py3-none-any.whl

---
+++ packages/am-red-channel/am_red_channel-2.1.2-py3-none-any.whl/am_red_channel/__init__.py
@@ -0,0 +1,4 @@
+from am_red_channel.am import AM_SendDataSameDevice
+from am_red_channel.am import AM_GetDataSameDevice
+from am_red_channel.am import AM_SendData
+from am_red_channel.am import AM_GetData
