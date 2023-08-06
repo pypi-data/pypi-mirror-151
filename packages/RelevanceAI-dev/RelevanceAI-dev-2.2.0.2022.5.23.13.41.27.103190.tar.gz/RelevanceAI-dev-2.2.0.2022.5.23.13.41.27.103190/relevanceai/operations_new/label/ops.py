0126a4f1afb1406f89265f51e106b36621fcd9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:27 +0000

    Add package RelevanceAI-dev/2.2.0.2022.5.23.13.41.27.103190/RelevanceAI-dev-2.2.0.2022.5.23.13.41.27.103190.tar.gz

---
+++ packages/RelevanceAI-dev/RelevanceAI-dev-2.2.0.2022.5.23.13.41.27.103190.tar.gz/RelevanceAI-dev-2.2.0.2022.5.23.13.41.27.103190/relevanceai/operations_new/label/ops.py
@@ -0,0 +1,13 @@
+"""
+Labelling with API-related functions
+"""
+from relevanceai.operations_new.label.base import LabelBase
+from relevanceai.operations_new.apibase import OperationsAPIBase
+
+
+class LabelOps(LabelBase, OperationsAPIBase):  # type: ignore
+    """
+    Label Operations
+    """
+
+    pass
