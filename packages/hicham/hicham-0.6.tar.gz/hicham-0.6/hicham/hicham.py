d9d350f956ce88ecadd1dd1fd40f054e545cb207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:18 +0000

    Add package hicham/0.6/hicham-0.6.tar.gz

---
+++ packages/hicham/hicham-0.6.tar.gz/hicham-0.6/hicham/hicham.py
@@ -0,0 +1,171 @@
+from datetime import date
+
+class Hicham(): 
+  
+    def age(self):
+        
+        current_year = date.today().year
+        
+        print("age = " + str(current_year-1997))
+        
+    def nom(self): 
+        
+        print("nom : ZEKRI")
+        
+    def prenom(self): 
+        
+        print("prenom : Sidi Mohamed Hicham")
+        
+    def fonction(self): 
+        
+        print( "Ingenieur et Master 2 en informatique ")
+
+    def cv(self): 
+        
+        print('''
+
+
+
+
+        Formations
+        ------------
+
+        Master 02 Datascale
+        Universite de Paris saclay UVSQ
+        Sept 2021- present
+
+        Specialite : Data Management In Large-Scale Distributed
+        Systems
+        Modules : Integration de donnees, Architectures Big Data,
+        Machine Learning, Data Mining, Web Semantique, Cloud
+        computing, Data Driven Services.
+        -------------------------------------------------------
+        Ingenieur et Master 02 en informatique
+        Ecole superieur d informatique ESI
+        Sept 2015- Sept 2020
+
+        Specialite : Systeme d Information et Web
+        Modules: Data analysis, Oracle, optimization BDD, Big Data,
+        web dev, middleware, Gestion de projet, Machine Learning,
+        Cloud, Probabilite ...
+        Realisation : Major de promotion
+        --------------------------------------------------------------
+        --------------------------------------------------------------
+        Experiences
+        ------------
+        Manager IT Responsable de migration de donnees chez
+        Farimer
+        Nov 2020- Juin 2020
+
+        . Responsable de la migration des donnees.
+        . Developpement d'une application de gestion des
+        stocks utilisant Flask (python avec postgress)
+        . Gestion de l'ERP Odoo (ajout de modules, mises a
+        jour ....)
+        ------------------------------------------------------------------
+        Stage : Ingenieur de recherche en analyse de donnees chez
+        Laboratoire Paris Nord LIPN
+        Fev 2020- Sept 2020
+
+        Stage de fin d'etudes intitule "Visualisation et analyse de
+        series temporelles multi-variees". Utilisation de toutes les
+        nouvelles technologies d'analyse et de visualisation de
+        donnees ainsi que de la chaine de developpement logiciel.
+        ---------------------------------------------------------------------
+        Stage : Data Engineer chez Credit Agricole
+        Avril 2022- Sept 2022
+
+        Stage de fin d'etudes intitule, mise en place d'une plateforme
+        de correction des IA d'extraction d'information Le DataLab
+        Groupe a developpe et a mis en production un socle d'IA
+        documentaire permettant d'extraire des informations
+        specifiques de divers documents utilises dans les processus
+        metiers du groupe Credit Agricole. 
+
+        ------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------
+
+
+        Projets
+        ---------
+
+        VisIO
+        Sept 2020
+
+        . Conception et mise en oeuvre d'un outil d'analyse et
+        de visualisation de series temporelles multi variees.
+        Outils: Scala, Python, Spark, Docker, javascript, Plotly,
+        Angular7, Flask, Nosql et Sql db.
+        ------------------------------------------------------------------------------
+        Breast Cancer Detection
+        Fev 2019
+
+        . Developpement d'un modele de reseau neuronal
+        convolutif (CNN) pour detecter le cancer du sein a
+        partir des images de mammographies.
+        Outils: Python, Django , Pytorch, sk-learn et Docker.
+        ------------------------------------------------------------------------------
+        BeHere
+        Feb 2016
+
+        . Une application Android qui donne aux enseignants
+        la possibilite d'envoyer des courriels, de prendre
+        des notes et de verifier la presence des etudiants
+        dans une universite.
+        Outils: Android studio, Firebase, Java, Mysql. 
+
+        ------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------
+
+
+        Extra-scolaires
+        -----------------
+        Co-Fondateur
+        Ingeniums Club
+        Nov 2017- Spet 2020 en Algerie
+        Participant
+        ------------------------------------------------------------------------------
+        Le plus grand evenement au monde : le Hajj
+        Hackathon
+        Juin 2018 en KSA
+        Participant
+        ------------------------------------------------------------------------------
+        evenement de visualisation de donnees HackaViz
+        Avril 2020 en France/ Toulouse
+
+        ------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------
+        Competences
+        ------------
+
+        . Langages de programmation : Python, Javascript, Java, Scala,
+        node, R
+        . Base de donnees et Big data : MySQL, ORACLE, Postgres,
+        MongoDB, Casandra, Redis, Neo4j, Spark
+        . Framework web: Django, Flask, Spring Boot, Laravel,
+        AngularJs, Node JS, Express JS, STRAPI.
+        . Devops: Docker, kubernates, CI/CD
+        . Machine learning: Pytorch, ScikitLearn, Kiras, Pandas,
+        Numpay.
+        . Framework Mobile: React Native.
+
+        ------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------
+
+        Langues
+        --------
+
+        Anglais : Competences professionnelles de travail
+        Francais : Competences professionnelles de travail
+        Arabe : Competence native 
+
+
+
+
+
+        ''')
+
+    def contact(self): 
+        print("telephone: +33 766406495")      
+        print("mail: hsmz.zekri@gmail.com")   
+        print("linkedIn : https://www.linkedin.com/in/sidi-mohamed-hicham-zekri-617a2b164/")   
