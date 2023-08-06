d7e14fc3976939e6cd803a63472ffdf68b8595c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:39 +0000

    Add package deepdog/0.6.1/deepdog-0.6.1.tar.gz

---
+++ packages/deepdog/deepdog-0.6.1.tar.gz/deepdog-0.6.1/deepdog/__init__.py
@@ -0,0 +1,18 @@
+import logging
+from deepdog.meta import __version__
+from deepdog.bayes_run import BayesRun
+from deepdog.bayes_run_simulpairs import BayesRunSimulPairs
+
+
+def get_version():
+	return __version__
+
+
+__all__ = [
+	"get_version",
+	"BayesRun",
+	"BayesRunSimulPairs",
+]
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
