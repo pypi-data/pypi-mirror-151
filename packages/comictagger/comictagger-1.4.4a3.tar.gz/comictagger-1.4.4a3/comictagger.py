36d7af3e1c1a32160c69db8b3cb5c2dfd2d14163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:58 +0000

    Add package comictagger/1.4.4a3/comictagger-1.4.4a3.tar.gz

---
+++ packages/comictagger/comictagger-1.4.4a3.tar.gz/comictagger-1.4.4a3/comictagger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import localefix
+from comictaggerlib.main import ctmain
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    localefix.configure_locale()
+    ctmain()
