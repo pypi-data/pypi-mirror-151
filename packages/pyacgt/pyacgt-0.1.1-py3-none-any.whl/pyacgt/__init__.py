cb8fd4a67faf5a5c64f7620f8838ec9c1919cef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:37 +0000

    Add package pyacgt/0.1.1/pyacgt-0.1.1-py3-none-any.whl

---
+++ packages/pyacgt/pyacgt-0.1.1-py3-none-any.whl/pyacgt/__init__.py
@@ -0,0 +1,5 @@
+from .util import cmd_exists
+from .examples import squareKnotPres
+from .acme import makeACMEinput, runACMEinput
+# import typing as T
+# import typing_extensions as TE
---
+++ packages/pyacgt/pyacgt-0.1.1-py3-none-any.whl/pyacgt/examples.py
@@ -0,0 +1,30 @@
+
+# import typing_extensions as TE
+# from typing_extensions import typing as T
+from typing import List, Tuple, Any
+
+
+def squareKnotPres(p: int, q: int) -> Tuple[List[str], List[str]]:
+    """
+    Runs acme on `template`
+
+    Parameters
+    ----------
+    p,q: int
+        meridional slope parameters for $$\mu_{p/q}$$.
+
+    Return
+    ------
+    Tuple[List[str],Words]
+        returns the generators and relators for $$G_{p/q}$$.
+    """
+    if q == 2 * p + 1:
+        return (["a", "b"], ["abaBAB", "a" * (p + 1) + "B" * p])
+    elif p == 2 and q % 4 == 1:
+        n = int((q - 1) / 4)
+        return (["a", "b"], ["abaBAB", "aaBB" * n + "a"])
+    elif p == 2 and q % 4 == 3:
+        n = int((q - 3) / 4)
+        return (["a", "b"], ["abaBAB", "aaBB" * (n + 1) + "b"])
+    else:
+        raise NotImplementedError(f"The case {p}/{q} is not yet implemented")
