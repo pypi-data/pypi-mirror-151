96e6a64132b2b0df7a02311d806214f2be70113b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:29 +0000

    Add package glacier-lengths/0.1.2/glacier_lengths-0.1.2-py3-none-any.whl

---
+++ packages/glacier-lengths/glacier_lengths-0.1.2-py3-none-any.whl/glacier_lengths/__init__.py
@@ -0,0 +1,5 @@
+"""Tools to statistically measure glacier lengths."""
+from glacier_lengths.core import (buffer_centerline, cut_centerlines,
+                                  measure_lengths)
+
+__version__ = "0.1.2"
