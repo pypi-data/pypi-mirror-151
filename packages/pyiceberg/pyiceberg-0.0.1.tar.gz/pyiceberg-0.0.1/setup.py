1501a91136e0427f11eaa634066ec2972536fe4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:00 +0000

    Add package pyiceberg/0.0.1/pyiceberg-0.0.1.tar.gz

---
+++ packages/pyiceberg/pyiceberg-0.0.1.tar.gz/pyiceberg-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+import os
+
+setup(
+    name='pyiceberg',
+    version='0.0.1',
+    url='https://github.com/apache/iceberg',
+    author='Apache Software Foundation',
+    author_email='dev@iceberg.apache.org',
+    description='Apache Iceberg is a new table format for storing large, slow-moving tabular data.',
+    packages=find_packages(),    
+    install_requires=[],
+)
+
+if not os.getenv("OVERRIDE"):
+    raise RuntimeError('This is a placeholder for the Apache Iceberg Python library')
+
