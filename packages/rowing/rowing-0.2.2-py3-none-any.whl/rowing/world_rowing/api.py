b640ba7cc18b9c1f7cfcef85f33682f8ba675ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:39 +0000

    Add package rowing/0.2.2/rowing-0.2.2-py3-none-any.whl

---
+++ packages/rowing/rowing-0.2.2-py3-none-any.whl/rowing/world_rowing/api.py
@@ -0,0 +1,491 @@
+import datetime
+from collections.abc import Mapping
+import logging
+
+import pandas as pd
+
+from .utils import (
+    getnesteditem,
+    extract_fields,
+    read_times,
+    map_concurrent,
+    CURRENT_TIMEZONE,
+    format_timedelta_hours,
+    format_totalseconds,
+    merge,
+    cache,
+    _pyodide
+)
+
+if _pyodide:
+    use_requests = False
+    import pyodide
+    from js import XMLHttpRequest
+    import json
+    from urllib import parse
+else:
+    use_requests = True
+    import requests
+
+logger = logging.getLogger("world_rowing.api")
+
+OLYMPIC_BOATCLASS = [
+    "M1x",
+    "LM2x",
+    "W1x",
+    "M2x",
+    "M2-",
+    "LW2x",
+    "M4-",
+    "W1x",
+    "W2x",
+    "W2-",
+    "M4x",
+    "M8+",
+    "W4x",
+    "W8+",
+    "W4-",
+]
+
+RACE_PHASES = {
+    "Repechage": "0959f5e8-f85a-40fb-93ab-b6c477f6aade",
+    "Test Race": "92b34c4e-af58-4e91-8f4a-22c09984a006",
+    "Heat": "cd3d5ca1-5aed-4146-b39b-a192ae6533f1",
+    "Final": "e0fc3320-cd66-43af-a5b5-97afd55b2971",
+    "Semifinal": "e6693585-d2cf-464c-9f8e-b2e531b26400",
+}
+
+
+def stringify_value(value):
+    if isinstance(value, (str, int, float)):
+        return str(value)
+    else:
+        return "||".join(map(str, value))
+
+
+def prepare_options(key, options):
+    if isinstance(options, str):
+        return ((key, options),)
+
+    _options = options.items() if isinstance(options, Mapping) else options
+    return ((f"{key}[{field}]", stringify_value(value)) for field, value in _options)
+
+
+def prepare_params(**kwargs):
+    return {
+        field: value
+        for key, options in kwargs.items()
+        for field, value in prepare_options(key, options)
+    }
+
+
+if use_requests:
+    def load_json_url(url, params=None, **kwargs):
+        r = requests.get(url, params=params)
+        r.raise_for_status()
+        if r.text:
+            return r.json()
+        else:
+            return {}
+
+
+else:
+
+    def load_json_url(url, params=None, **kwargs):
+        if params:
+            query = parse.urlencode(params)
+            url = parse.urlparse(url)._replace(query=query).geturl()
+
+        # Some requests only work asyncronously
+        req = XMLHttpRequest.new()
+        req.open("GET", url, False)
+        req.send(None)
+        if req.response:
+            return json.loads(req.response)
+        else:
+            return {}
+
+
+def prepare_request(*endpoints, **kwargs):
+    endpoint = "/".join(endpoints)
+    url = f"https://world-rowing-api.soticcloud.net/stats/api/{endpoint}"
+    params = prepare_params(**kwargs) or None
+    return url, params
+
+
+def request_worldrowing_data(*endpoints, **kwargs):
+    return load_json_url(*prepare_request(*endpoints, **kwargs))
+
+
+cached_request_worldrowing_data = cache(request_worldrowing_data)
+
+
+@cache
+def load_competition_best_times(json_url):
+    return pd.DataFrame.from_records(
+        extract_fields(record, WBT_RECORDS)
+        for record in load_json_url(json_url)["BestTimes"]
+    )
+
+
+def get_worldrowing_data(*endpoints, cached=True, **kwargs):
+    if cached:
+        hashable_kws = {
+            k: vals
+            if isinstance(vals, (str, int, float))
+            else tuple(dict(vals).items())
+            for k, vals in kwargs.items()
+        }
+        data = cached_request_worldrowing_data(*endpoints, **hashable_kws)
+    else:
+        data = request_worldrowing_data(*endpoints, **kwargs)
+
+    return data.get("data", [])
+
+
+def get_worldrowing_record(*endpoints, **kwargs):
+    data = get_worldrowing_data(*endpoints, **kwargs)
+    return pd.Series(data, name=data.pop("id", data.get("DisplayName", "")))
+
+
+def get_worldrowing_records(*endpoints, **kwargs):
+    records = pd.DataFrame.from_records(
+        get_worldrowing_data(*endpoints, **kwargs))
+    if "id" in records.columns:
+        records.set_index("id", inplace=True)
+
+    for col in records.columns:
+        if "Date" in col:
+            dates = pd.to_datetime(records[col])
+            if dates.notna().all():
+                records[col] = dates
+
+    return records
+
+
+def get_competition_events(competition_id=None, cached=True):
+    competition_id = competition_id or get_most_recent_competition().name
+    return get_worldrowing_records(
+        "event",
+        cached=True,
+        filter=(("competitionId", competition_id),),
+        # sort=(("Date", "asc"),),
+    )
+
+
+def get_competition_races(competition_id=None, cached=True):
+    competition_id = competition_id or get_most_recent_competition().name
+    return get_worldrowing_records(
+        "race",
+        cached=cached,
+        filter=(("event.competitionId", competition_id),),
+        sort=(("eventId", "asc"), ("Date", "asc")),
+    )
+
+
+def get_competitions(year=None, fisa=True, has_results=True, cached=True, **kwargs):
+    year = year or datetime.date.today().year
+    kwargs['filter'] = tuple(dict(kwargs.get('filter', ())).items())
+    kwargs['filter'] += ('Year', year),
+    kwargs['filter'] += ('IsFisa', (1 if fisa else "")),
+    kwargs['filter'] += ('HasResults', (1 if has_results else '')),
+    kwargs.setdefault("sort", {"StartDate": "asc"})
+    kwargs.setdefault("sort", {"StartDate": "asc"})
+    return get_worldrowing_records("competition", cached=cached, **kwargs)
+
+
+def get_this_years_competitions(fisa=True, has_results=True):
+    year = datetime.date.today().year
+    return get_competitions(year=year, fisa=fisa, has_results=has_results)
+
+
+def get_last_years_competitions(fisa=True, has_results=True):
+    year = datetime.date.today().year
+    return get_competitions(year=year - 1, fisa=fisa, has_results=has_results)
+
+
+def get_most_recent_competition(fisa=True):
+    competitions = get_this_years_competitions(fisa=fisa)
+    started = competitions.StartDate < datetime.datetime.now()
+    if not started.any():
+        competitions = get_last_years_competitions(fisa=fisa)
+        started = competitions.StartDate < datetime.datetime.now()
+
+    competition = competitions.loc[started].iloc[-1]
+    logger.info(f"loaded most recent competition: {competition.DisplayName}")
+    return competition
+
+
+def get_last_race_started(fisa=True, competition=None):
+    races = get_last_races(n=1, fisa=fisa, competition=competition)
+    if races is not None:    
+        race = races.iloc[0]
+        logger.info(f"loaded last race started: {race.DisplayName}")
+        return race
+
+get_most_recent_race = get_last_race_started
+
+def get_last_races(n=1, fisa=True, competition=None):
+    if competition is None:
+        competition = get_most_recent_competition(fisa)
+    races = get_competition_races(competition.name)
+    print(races)
+    if not races.empty:
+        started = races.DateString < datetime.datetime.now().astimezone()
+        return races.loc[started].sort_values("DateString").iloc[-n:]
+
+
+def get_next_races(n=1, fisa=True, competition=None):
+    if competition is None:
+        competition = get_most_recent_competition(fisa)
+    races = get_competition_races(competition.name)
+    to_race = races.DateString > datetime.datetime.now().astimezone()
+    return races.loc[to_race].sort_values("DateString").iloc[:n]
+
+
+def show_next_races(n=10, fisa=True, competition=None):
+    next_races = get_next_races(n, fisa=fisa, competition=competition)[
+        ["DisplayName", "DateString"]
+    ].reset_index(drop=True)
+    next_races.DateString = next_races.DateString.dt.tz_convert(
+        CURRENT_TIMEZONE)
+    next_races.columns = ["Race", "Time"]
+    now = datetime.datetime.now().astimezone(CURRENT_TIMEZONE)
+    next_races["Time to race"] = (
+        next_races.Time - now).apply(format_timedelta_hours)
+    return next_races
+
+
+def get_boat_types():
+    return get_worldrowing_records("boatClass", cached=True)
+
+
+def get_competition_types():
+    return get_worldrowing_records("competitionType", cached=True)
+
+
+def get_statistics():
+    return get_worldrowing_records("statistic", cached=True)
+
+
+def get_venues():
+    return get_worldrowing_records("venue", cached=True)
+
+
+RACEBOAT_FIELDS = {
+    "id": ("id",),
+    "raceId": ("raceId",),
+    "boatId": ("boatId",),
+    "Country": ("DisplayName",),
+    "Rank": ("Rank",),
+    "Lane": ("Lane",),
+    "ResultTime": ("ResultTime",),
+}
+
+
+def get_race_results(
+    race_id=None, event_id=None, competition_id=None, cached=True, **kwargs
+):
+    filters = tuple(dict(kwargs.pop("filter", ())).items())
+    if race_id:
+        filters += (("id", race_id),)
+    if event_id:
+        filters += (("eventId", event_id),)
+    if competition_id:
+        filters += (("event.competitionId", competition_id),)
+
+    race_data = get_worldrowing_data(
+        "race",
+        cached=cached,
+        filter=filters,
+        include="raceBoats.raceBoatIntermediates",
+        **kwargs,
+    )
+    results = pd.DataFrame.from_records(
+        [
+            extract_fields(boat, RACEBOAT_FIELDS)
+            for race in race_data
+            for boat in race["raceBoats"]
+            # if boat['ResultTime']
+        ]
+    )
+    if len(results):
+        results.set_index(["raceId", "id"], inplace=True)
+        if "ResultTime" in results.columns:
+            results.ResultTime = read_times(results.ResultTime)
+    return results
+
+
+INTERMEDIATE_FIELDS = {
+    "id": ("id",),
+    # 'raceId': 'raceId',
+    "raceBoatId": ("raceBoatId",),
+    "Rank": ("Rank",),
+    "ResultTime": ("ResultTime",),
+    "distanceId": ("distanceId",),
+    "distance": ("distance", "DisplayName"),
+}
+
+
+def get_intermediate_results(
+    race_id=None, event_id=None, competition_id=None, cached=True, **kwargs
+):
+    filters = tuple(dict(kwargs.pop("filter", ())).items())
+    if race_id:
+        filters += (("id", race_id),)
+    if event_id:
+        filters += (("eventId", event_id),)
+    if competition_id:
+        filters += (("event.competitionId", competition_id),)
+
+    race_data = get_worldrowing_data(
+        "race",
+        cached=cached,
+        filter=filters,
+        include="raceBoats.raceBoatIntermediates.distance",
+        **kwargs,
+    )
+    # race_fields = RACEBOAT_FIELDS.copy()
+    # race_fields['raceBoatId'] = race_fields.pop('id')
+    results = pd.DataFrame.from_records(
+        [
+            {
+                **extract_fields(boat, RACEBOAT_FIELDS),
+                **extract_fields(inter, INTERMEDIATE_FIELDS),
+            }
+            for race in race_data
+            for boat in race["raceBoats"]
+            if boat["ResultTime"]
+            for inter in sorted(
+                boat["raceBoatIntermediates"], key=lambda x: x["ResultTime"]
+            )
+        ]
+    )
+
+    if len(results):
+        results.set_index(["raceId", "raceBoatId", "id"], inplace=True)
+        results.ResultTime = pd.to_timedelta(results.ResultTime)
+    return results
+
+
+WBT_RECORDS = {
+    "BoatClass": ("BoatClass",),
+    "ResultTime": ("Competitor", "ResultTime"),
+    "Competition": ("Competition", "Name"),
+    "Venue": ("Venue", "Name"),
+    "Event": ("Event", "DisplayName"),
+    "EventId": ("Event", "Id"),
+    "Race": ("Race", "Name"),
+    "RaceId": ("Race", "Id"),
+    "Country": ("Competitor", "Nationality", "Name"),
+    "Date": ("DateOfBT",),
+}
+
+
+def _extract_wbt_record(record):
+    return {key: getnesteditem(record, *items) for key, items in WBT_RECORDS.items()}
+
+
+@cache
+def get_competition_best_times():
+    wbt_stats = get_worldrowing_records(
+        "statistic", cached=True, filter=(("Category", "WBT"),)
+    )
+    wbts, _ = map_concurrent(
+        load_competition_best_times,
+        dict(zip(wbt_stats.description, zip(wbt_stats.url))),
+        show_progress=False,
+    )
+    wbts = (
+        pd.concat(wbts, names=["CompetitionType"]).reset_index(
+            0).reset_index(drop=True)
+    )
+    wbts.CompetitionType = wbts.CompetitionType.str.extract(r": ([a-zA-Z]+)")
+    wbts.ResultTime = pd.to_timedelta(wbts.ResultTime)
+    return wbts
+
+
+def get_world_best_times():
+    return (
+        get_competition_best_times()
+        .sort_values("ResultTime")
+        .groupby("BoatClass")
+        .first()
+    )
+
+
+def find_world_best_time(
+    boat_class=None,
+    event=None,
+    race=None,
+    race_id=None,
+):
+    if boat_class is None:
+        if event is None:
+            if race is None:
+                if race_id is None:
+                    raise ValueError(
+                        "must pass at least one of "
+                        "boat_class, event, race or race_id"
+                    )
+                race = get_worldrowing_data("race", race_id)
+            event = get_worldrowing_data("event", race["eventId"])
+        boat_class = get_worldrowing_data("boatClass", event["boatClassId"])[
+            "DisplayName"
+        ]
+
+    return get_world_best_times().loc[boat_class]
+
+
+def get_competition_pgmts(competition_id=None, finals_only=False):
+    competition_id = competition_id or get_most_recent_competition().name
+    competition_races = get_competition_races(competition_id)
+    competition_events = get_competition_events(competition_id)
+    competition_results = get_race_results(competition_id=competition_id)
+    boat_classes = get_boat_types()
+    wbts = get_world_best_times().ResultTime
+    wbts.index.name, wbts.name = "id", "worldBestTime"
+
+    if competition_results.empty:
+        return competition_results
+
+    if finals_only:
+        competition_races = competition_races.loc[
+            competition_races.racePhaseId == RACE_PHASES["Final"]
+        ]
+
+    competition_results = merge(
+        (
+            competition_results.reset_index(),
+            competition_races[["eventId", "Date"]],
+            competition_events["boatClassId"],
+            boat_classes,
+            wbts,
+        ),
+        left_on=("raceId", "eventId", "boatClassId", "DisplayName"),
+        right_on="id",
+    )
+    competition_results["PGMT"] = (
+        competition_results.worldBestTime / competition_results.ResultTime
+    )
+
+    results = competition_results.loc[
+        competition_results.ResultTime > pd.to_timedelta(0),
+        [
+            "DisplayName",
+            "PGMT",
+            "ResultTime",
+            "worldBestTime",
+            "Country",
+            "Rank",
+            "Lane",
+            "Date",
+        ],
+    ]
+    results.columns = ["Boat", "PGMT", "Time",
+                       "WBT", "Country", "Rank", "Lane", "Date"]
+    # results['PGMT'] = results.worldBestTime / results.Time
+    results["Rank"] = results.Rank.astype(int)
+    results.Time = results.Time.dt.total_seconds().apply(format_totalseconds)
+    results.WBT = results.WBT.dt.total_seconds().apply(format_totalseconds)
+    return results.sort_values("PGMT", ascending=False).reset_index(drop=True)
---
+++ packages/rowing/rowing-0.2.2-py3-none-any.whl/rowing/world_rowing/utils.py
@@ -0,0 +1,404 @@
+
+import os
+import sys
+from pathlib import Path
+from functools import lru_cache, wraps
+from datetime import timedelta, datetime
+import logging
+from typing import Callable, Dict, TypeVar, Tuple, Any
+from contextlib import nullcontext
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+try:
+    from concurrent.futures import ProcessPoolExecutor
+except ModuleNotFoundError:
+    ProcessPoolExecutor = None
+
+import numpy as np
+import pandas as pd
+from scipy.special import erf
+
+_pyodide = "pyodide" in sys.modules
+
+
+_file_path = Path(os.path.abspath(__file__))
+_module_path = _file_path.parent
+_data_path = _module_path / 'data'
+_flag_path = _data_path / 'flags'
+
+# Make lru_cache 3.7 compatible
+cache = lru_cache(maxsize=128)
+
+
+def Phi(z):
+    sq2 = 1.4142135623730951
+    return 1/2 + erf(z/sq2)/2
+
+
+CURRENT_TIMEZONE = datetime.now().astimezone().tzinfo
+
+
+def ignore_nans(func):
+    @wraps(func)
+    def nan_func(arg, *args, **kwargs):
+        if np.isnan(arg):
+            return arg
+        else:
+            return func(arg, *args, **kwargs)
+
+    return nan_func
+
+
+def first(it, *args):
+    return next(iter(it), *args)
+
+
+def read_times(times):
+    minfmt = ~ times.fillna('').str.match(r"[0-9]+:[0-9][0-9]?:[0-9][0-9]?")
+    new_times = times.copy()
+    new_times[minfmt] = "0:" + times
+    return pd.to_timedelta(new_times)
+
+
+@cache
+def get_iso_country_data(data_path=_data_path / 'iso_country.json'):
+    return pd.read_json(data_path)
+
+
+@cache
+def get_iso2_names(data_path=_data_path / 'iso_country.json'):
+    iso_country = get_iso_country_data(data_path)
+    return pd.concat([
+        pd.Series(
+            iso_country.iso2[col.notna()].values,
+            index=col[col.notna()],
+        )
+        for _, col in iso_country.iteritems()
+    ])
+
+
+@cache
+def get_iso3_names(data_path=_data_path / 'iso_country.json'):
+    iso_country = get_iso_country_data(data_path)
+    return pd.concat([
+        pd.Series(
+            iso_country.iso3[col.notna()].values,
+            index=col[col.notna()].str.upper(),
+        )
+        for _, col in iso_country.iteritems()
+    ])
+
+
+@cache
+def find_country_iso2(
+    country,
+    data_path=_data_path / 'iso_country.json'
+):
+    return get_iso2_names(data_path)[country]
+
+
+@cache
+def get_flag_im(
+        country,
+        data_path=_data_path / 'iso_country.json',
+        flag_path=_flag_path
+):
+    import matplotlib.pyplot as plt
+    iso2cnt = find_country_iso2(
+        country, data_path
+    )
+    im = plt.imread(
+        flag_path / f"{iso2cnt}.png"
+    )
+    return im
+
+
+def update_table(table, index, columns, update):
+    index, columns = map(np.asarray, (index, columns))
+    for row, rowvals in update.iterrows():
+        i, = np.where(index == row)[0]
+        for col, val in rowvals.items():
+            j, = np.where(columns == col)[0]
+            table[i + 1, j].get_text().set_text(val)
+
+
+def make_flag_box(
+        country,
+        xy,
+        base_width=900,
+        zoom=0.04,
+        resample=True,
+        frameon=False,
+        xycoords='data',
+        box_alignment=(0.5, 0.),
+        pad=0,
+        data_path=_data_path / 'iso_country.json',
+        flag_path=_flag_path,
+        offset_kws=None,
+        **kwargs
+):
+    from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+    im = get_flag_im(
+        country,
+        data_path=data_path,
+        flag_path=flag_path,
+    )
+    offset_im = OffsetImage(
+        im,
+        zoom=zoom * base_width / im.shape[1],
+        resample=resample,
+        **(offset_kws or {})
+    )
+    return AnnotationBbox(
+        offset_im, xy,
+        frameon=frameon,
+        xycoords=xycoords,
+        box_alignment=box_alignment,
+        pad=0,
+        **kwargs
+    )
+
+
+def update_fill_between(poly, x, y0, y1):
+    x, y0, y1 = map(np.asarray, (x, y0, y1))
+    vertices = poly.get_paths()[0].vertices
+    vertices[1:len(x)+1, 0] = x
+    vertices[1:len(x)+1, 1] = y0
+    vertices[len(x) + 1:-2, 0] = x[::-1]
+    vertices[len(x) + 1:-2, 1] = y1[::-1]
+    vertices[0, 0] = vertices[-1, 0] = x[0]
+    vertices[0, 1] = vertices[-1, 1] = y1[0]
+
+
+def update_fill_betweenx(poly, y, x0, x1):
+    y, x0, x1 = map(np.asarray, (y, x0, x1))
+    vertices = poly.get_paths()[0].vertices
+    n = len(y)
+    vertices[1:n+1, 0] = x0
+    vertices[1:n+1, 1] = y
+    vertices[n + 1:-2, 0] = x1[::-1]
+    vertices[n + 1:-2, 1] = y[::-1]
+    vertices[0, 0] = vertices[-1, 0] = x1[0]
+    vertices[0, 1] = vertices[-1, 1] = y[0]
+
+
+def format_totalseconds(seconds, hundreths=True):
+    return format_timedelta(timedelta(seconds=seconds), hundreths=hundreths)
+
+
+def format_timedelta(td, hours=False, hundreths=True):
+    mins, secs = divmod(td.seconds, 60)
+    if hours:
+        hs, mins = divmod(mins, 60)
+        return f"{hs:02d}:{mins:02d}:{secs:02d}"
+    else:
+        end = f".{(td.microseconds // 10_000):02d}" if hundreths else ''
+        return f"{mins}:{secs:02d}{end}"
+
+
+def format_timedelta_hours(td):
+    return format_timedelta(td, hours=True)
+
+
+def format_yaxis_splits(ax=None, ticks=True, hundreths=False):
+    format_axis_splits(ax=ax, yticks=ticks, hundreths=hundreths)
+
+
+def format_xaxis_splits(ax=None, ticks=True, hundreths=False):
+    format_axis_splits(ax=ax, yticks=False, xticks=ticks, hundreths=hundreths)
+
+
+def format_axis_splits(ax=None, yticks=True, xticks=False, hundreths=False):
+    import matplotlib.pyplot as plt
+    ax = ax or plt.gca()
+    if yticks:
+        if yticks is True:
+            yticks = ax.get_yticks()
+        ax.set_yticks(yticks)
+        ax.set_yticklabels(
+            [format_totalseconds(s, hundreths) for s in yticks]
+        )
+    if xticks:
+        if xticks is True:
+            xticks = ax.get_xticks()
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(
+            [format_totalseconds(s, hundreths) for s in xticks]
+        )
+
+
+K = TypeVar("K")
+A = TypeVar('A')
+V = TypeVar('V')
+
+
+def map_concurrent(
+    func: Callable[..., V],
+    inputs: Dict[K, Tuple],
+    threaded: bool = True,
+    max_workers: int = 10,
+    show_progress: bool = True,
+    raise_on_err: bool = False,
+    **kwargs,
+) -> Tuple[Dict[K, V], Dict[K, Exception]]:
+    """
+    This function is equalivant to calling,
+
+    >>> output = {k: func(*args, **kwargs) for k, args in inputs.items()}
+
+    except that the function is called using either `ThreadPoolExecutor` 
+    if `threaded=True` or a `ProcessPoolExecutor` otherwise.
+
+    The function returns a tuple of `(output, errors)` where errors returns
+    the errors that happened during the calling of any of the functions. So
+    the function will run all the other work before 
+
+    The function also generates a status bar indicating the progress of the
+    computation.
+
+    Alternatively if `raise_on_err=True` then the function will reraise the
+    same error.
+
+    Examples
+    --------
+    >>> import time
+    >>> def do_work(arg):
+    ...     time.sleep(0.5)
+    ...     return arg
+    >>> inputs = {i: (i,) for i in range(20)}
+    >>> output, errors = map_concurrent(do_work, inputs)
+    100%|███████████████████| 20/20 [00:01<00:00, 19.85it/s, completed=18]
+    >>> len(output), len(errors)
+    (20, 0)
+
+    >>> def do_work2(arg):
+    ...     time.sleep(0.5)
+    ...     if arg == 5:
+    ...         raise(ValueError('something went wrong'))
+    ...     return arg
+    >>> output, errors = map_concurrent(do_work2, inputs)
+    100%|████████| 20/20 [00:01<00:00, 19.86it/s, completed=18, nerrors=1]
+    >>> len(output), len(errors)
+    (19, 1)
+    >>> errors
+{5: ValueError('something went wrong')}
+
+    >>> try:
+    ...     output, errors = map_concurrent(
+    ...         do_work2, inputs, raise_on_err=True)
+    ... except ValueError:
+    ...     print("task failed successfully!")
+    ...
+    45%|█████████▍           | 9/20 [00:00<00:00, 17.71it/s, completed=5]
+    task failed!
+    """
+    output = {}
+    errors = {}
+
+    Executor = ThreadPoolExecutor if threaded else ProcessPoolExecutor
+
+    if show_progress:
+        from tqdm.auto import tqdm
+        pbar = tqdm(total=len(inputs))
+    else:
+        pbar = nullcontext()
+    with Executor(max_workers=max_workers) as executor, pbar:
+        work = {
+            executor.submit(func, *args, **kwargs): k
+            for k, args in inputs.items()
+        }
+        status: Dict[str, Any] = {}
+        for future in as_completed(work):
+            status['completed'] = key = work[future]
+            if show_progress:
+                pbar.update(1)
+                pbar.set_postfix(**status)
+            try:
+                output[key] = future.result()
+            except Exception as exc:
+                if raise_on_err:
+                    raise exc
+                else:
+                    logging.warning(f"{key} experienced error {exc}")
+                    errors[key] = exc
+                    status['nerrors'] = len(errors)
+
+    return output, errors
+
+
+def _map_singlethreaded(
+    func: Callable[..., V],
+    inputs: Dict[K, Tuple],
+    threaded: bool = True,
+    max_workers: int = 10,
+    show_progress: bool = True,
+    raise_on_err: bool = False,
+    **kwargs,
+) -> Tuple[Dict[K, V], Dict[K, Exception]]:
+    output = {}
+    errors = {}
+
+    status: Dict[str, Any] = {}
+    if show_progress:
+        from tqdm.auto import tqdm
+        pbar = tqdm(total=len(inputs))
+    else:
+        pbar = nullcontext()
+
+    with pbar:
+        for key, args in inputs.items():
+            try:
+                output[key] = func(*args, **kwargs)
+            except Exception as exc:
+                if raise_on_err:
+                    raise exc
+                else:
+                    logging.warning(f"{key} experienced error {exc}")
+                    errors[key] = exc
+                    status['nerrors'] = len(errors)
+
+            if show_progress:
+                pbar.update(1)
+                pbar.set_postfix(**status)
+
+    return output, errors
+
+
+if _pyodide:
+    map_concurrent = _map_singlethreaded
+
+
+def getnesteditem(container, *items):
+    value = container
+    for item in items:
+        value = value[item]
+
+    return value
+
+
+def extract_fields(record, fields):
+    return {
+        k: getnesteditem(record, *items) for k, items in fields.items()
+    }
+
+
+def merge(dfs, **kwargs):
+    import pandas as pd
+    left = dfs[0]
+    n = len(dfs[1:])
+
+    def iterate(val):
+        if isinstance(val, tuple):
+            return iter(val)
+        else:
+            return (val for _ in range(n))
+
+    iter_kwargs = {k: iterate(val) for k, val in kwargs.items()}
+
+    for right in dfs[1:]:
+        kws = {
+            k: next(val) for k, val in iter_kwargs.items()
+        }
+        left = pd.merge(left, right, **kws)
+
+    return left
