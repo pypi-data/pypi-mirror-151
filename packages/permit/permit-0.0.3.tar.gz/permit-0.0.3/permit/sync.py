446240966f66814a6cfb012362340fde9e8a56c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:50 +0000

    Add package permit/0.0.3/permit-0.0.3.tar.gz

---
+++ packages/permit/permit-0.0.3.tar.gz/permit-0.0.3/permit/sync.py
@@ -0,0 +1,43 @@
+import asyncio
+from distutils.log import debug
+from typing import List
+
+from permit.constants import DEFAULT_PDP_URL
+from permit.enforcement.enforcer import Action, Resource, User
+from permit.resources.interfaces import ResourceConfig, ResourceTypes
+from permit.resources.reporter import ResourceStub
+from permit.utils.context import Context
+
+from .client import Permit as AsyncPermit
+from .mutations.sync import PermitApiClient
+from .utils.sync import run_sync
+
+
+class Permit(AsyncPermit):
+    def __init__(
+        self,
+        token: str,
+        pdp: str = DEFAULT_PDP_URL,
+        debug_mode: bool = False,
+        **options,
+    ):
+        super().__init__(token=token, pdp=pdp, debug_mode=debug_mode, **options)
+        # use sync mutations client instead of async version
+        self._mutations_client = PermitApiClient(self._config)
+
+    def check(
+        self,
+        user: User,
+        action: Action,
+        resource: Resource,
+        context: Context = {},
+    ) -> bool:
+        return run_sync(
+            super().check(user=user, action=action, resource=resource, context=context)
+        )
+
+    def resource(self, config: ResourceConfig) -> ResourceStub:
+        return run_sync(super().resource(config=config))
+
+    def sync_resources(self, config: ResourceTypes) -> List[ResourceStub]:
+        return run_sync(super().sync_resources(config=config))
