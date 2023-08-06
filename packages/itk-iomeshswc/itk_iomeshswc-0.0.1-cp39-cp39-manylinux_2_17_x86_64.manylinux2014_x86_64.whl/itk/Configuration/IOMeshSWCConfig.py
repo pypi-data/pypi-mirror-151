204e301208e0535fbd155fa603486e250ed144ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:20 +0000

    Add package itk-iomeshswc/0.0.1/itk_iomeshswc-0.0.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/itk-iomeshswc/itk_iomeshswc-0.0.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/itk/Configuration/IOMeshSWCConfig.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKIOMeshBase', 'ITKCommon', )
+templates = (  ('SWCMeshIO', 'itk::SWCMeshIO', 'itkSWCMeshIO', True),
+  ('SWCMeshIO', 'itk::SWCMeshIO', 'itkSWCMeshIO', True),
+  ('SWCMeshIOFactory', 'itk::SWCMeshIOFactory', 'itkSWCMeshIOFactory', True),
+)
+factories = ()
