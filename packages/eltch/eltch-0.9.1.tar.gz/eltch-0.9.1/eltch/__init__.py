153295909dd2c9f2de3437f4a3ccc08adfb6b5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:31 +0000

    Add package eltch/0.9.1/eltch-0.9.1.tar.gz

---
+++ packages/eltch/eltch-0.9.1.tar.gz/eltch-0.9.1/eltch/__init__.py
@@ -0,0 +1,12 @@
+'''
+:authors: dymshnc.
+:license: Apache License, Version 2.0, see LICENSE file.
+
+:copyright: (c) 2022 by dymshnc.
+'''
+
+from .eltch import EWheel
+
+__author__ = 'dymshnc'
+__version__ = '0.9'
+__email__ = 'officialdyomshin@gmail.com'
---
+++ packages/eltch/eltch-0.9.1.tar.gz/eltch-0.9.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='eltch',
+      version='0.9.1',
+      description='Electrotechnical assistant.',
+      packages=['eltch'],
+      author_email='officialdyomshin@gmail.ru',
+      zip_safe=False)
