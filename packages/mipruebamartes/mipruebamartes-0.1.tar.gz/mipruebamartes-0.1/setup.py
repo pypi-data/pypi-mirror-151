717487d94aec04ba4a5299677fb01764d758de89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:21 +0000

    Add package mipruebamartes/0.1/mipruebamartes-0.1.tar.gz

---
+++ packages/mipruebamartes/mipruebamartes-0.1.tar.gz/mipruebamartes-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name='mipruebamartes',
+    version='0.1',
+    packages=find_namespace_packages(),
+    py_modules=['mipruebamartes']
+)
