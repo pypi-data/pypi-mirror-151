8f3e25d82e15f88c6d2f8e24f545a3a4929bd3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:24:18 +0000

    Add package windmill-api/1.8.3/windmill_api-1.8.3-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.8.3-py3-none-any.whl/windmill_api/api/workspace/validate_id.py
@@ -0,0 +1,89 @@
+from typing import Any, Dict
+
+import httpx
+
+from ...client import Client
+from ...models.validate_id_json_body import ValidateIdJsonBody
+from ...types import Response
+
+
+def _get_kwargs(
+    *,
+    client: Client,
+    json_body: ValidateIdJsonBody,
+) -> Dict[str, Any]:
+    url = "{}/workspaces/validate_id".format(client.base_url)
+
+    headers: Dict[str, Any] = client.get_headers()
+    cookies: Dict[str, Any] = client.get_cookies()
+
+    json_json_body = json_body.to_dict()
+
+    return {
+        "url": url,
+        "headers": headers,
+        "cookies": cookies,
+        "timeout": client.get_timeout(),
+        "json": json_json_body,
+    }
+
+
+def _build_response(*, response: httpx.Response) -> Response[Any]:
+    return Response(
+        status_code=response.status_code,
+        content=response.content,
+        headers=response.headers,
+        parsed=None,
+    )
+
+
+def sync_detailed(
+    *,
+    client: Client,
+    json_body: ValidateIdJsonBody,
+) -> Response[Any]:
+    """validate id
+
+    Args:
+        json_body (ValidateIdJsonBody):
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        client=client,
+        json_body=json_body,
+    )
+
+    response = httpx.post(
+        verify=client.verify_ssl,
+        **kwargs,
+    )
+
+    return _build_response(response=response)
+
+
+async def asyncio_detailed(
+    *,
+    client: Client,
+    json_body: ValidateIdJsonBody,
+) -> Response[Any]:
+    """validate id
+
+    Args:
+        json_body (ValidateIdJsonBody):
+
+    Returns:
+        Response[Any]
+    """
+
+    kwargs = _get_kwargs(
+        client=client,
+        json_body=json_body,
+    )
+
+    async with httpx.AsyncClient(verify=client.verify_ssl) as _client:
+        response = await _client.post(**kwargs)
+
+    return _build_response(response=response)
