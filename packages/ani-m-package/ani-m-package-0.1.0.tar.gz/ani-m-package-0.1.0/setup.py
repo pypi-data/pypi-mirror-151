6bd19f2da1c81d14fe43f7eb2a4d9b4494949f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:35 +0000

    Add package ani-m-package/0.1.0/ani-m-package-0.1.0.tar.gz

---
+++ packages/ani-m-package/ani-m-package-0.1.0.tar.gz/ani-m-package-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ani-m-package',
+    version='0.1.0',
+    description='Python package to print donut.',
+    url='',
+    author='Ani Marutyan',
+    author_email='ani_marutyan@edu.aua.am',
+    license='BSD 2-clause',
+    packages=['ani-m-package'],
+    install_requires=['pandas',
+                      'scipy',
+                      'matplotlib'
+                      ],
+)
