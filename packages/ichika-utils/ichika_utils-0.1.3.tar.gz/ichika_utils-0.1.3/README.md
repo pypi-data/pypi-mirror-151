c4b7bb167f2610d7b52b61896005bf40f0e594a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:18 +0000

    Add package ichika-utils/0.1.3/ichika_utils-0.1.3.tar.gz

---
+++ packages/ichika-utils/ichika_utils-0.1.3.tar.gz/ichika_utils-0.1.3/README.md
@@ -0,0 +1,3 @@
+# Ichika Utils
+
+A set of utils used by Ichika. This is not production ready, and not even ready for anything. this is highly experimental.
---
+++ packages/ichika-utils/ichika_utils-0.1.3.tar.gz/ichika_utils-0.1.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['ichika_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.5.2,<4.0.0',
+ 'numba>=0.55.1,<0.56.0',
+ 'pandas>=1.4.2,<2.0.0',
+ 'scikit-learn>=1.1.1,<2.0.0',
+ 'scipy>=1.8.0,<2.0.0',
+ 'uvloop>=0.16.0,<0.17.0']
+
+setup_kwargs = {
+    'name': 'ichika-utils',
+    'version': '0.1.3',
+    'description': 'Asynchronous Statistics Utils for Ichika',
+    'long_description': '# Ichika Utils\n\nA set of utils used by Ichika. This is not production ready, and not even ready for anything. this is highly experimental.',
+    'author': 'No767',
+    'author_email': '73260931+No767@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/No767/Ichika-Utils',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
