f9b14b12d967ff8d39a56a4d0f4200363919105f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:48 +0000

    Add package yk-utils/1.1.4/yk_utils-1.1.4.tar.gz

---
+++ packages/yk-utils/yk_utils-1.1.4.tar.gz/yk_utils-1.1.4/yk_utils/models/__init__.py
@@ -0,0 +1,5 @@
+""" Models Module Init """
+from .deserialization import deserialize_model, deserialize_dbmodel
+from .base_model import Model
+from .error import ErrorSchema, Error
+from .base_enum import BaseEnum
