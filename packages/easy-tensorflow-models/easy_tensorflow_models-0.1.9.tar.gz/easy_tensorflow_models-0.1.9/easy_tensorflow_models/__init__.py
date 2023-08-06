1b33c26639d414a078a9fb6ca670a8894886eba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package easy-tensorflow-models/0.1.9/easy_tensorflow_models-0.1.9.tar.gz

---
+++ packages/easy-tensorflow-models/easy_tensorflow_models-0.1.9.tar.gz/easy_tensorflow_models-0.1.9/easy_tensorflow_models/__init__.py
@@ -0,0 +1,3 @@
+from .datamodel import DataBatch, Metrics
+from .internal import L2Regularizer, FeatureEmbeddingBlock, TaggerBase, TensorflowModelBase, TensorflowModelManager, TensorflowBlockBase
+from .store import BatchRetriever, BatchGenerator, BatchProcessorBase
