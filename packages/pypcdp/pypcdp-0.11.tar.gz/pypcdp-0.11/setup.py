ab41fd660b1b1ceb869a79df0366cca5ccbdf090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:37 +0000

    Add package pypcdp/0.11/pypcdp-0.11.tar.gz

---
+++ packages/pypcdp/pypcdp-0.11.tar.gz/pypcdp-0.11/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name='pypcdp',
+    version='0.11',
+    author='xgzh',
+    author_email="",
+    description="PCDP api",
+    long_description="PCDP Api",
+    # platforms='win32',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.8',
+)
