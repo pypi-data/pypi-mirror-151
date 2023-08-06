da70456e0be2f30a10a35165fed51cebdd52b82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:15 +0000

    Add package scProjection/0.21/scProjection-0.21.tar.gz

---
+++ packages/scProjection/scProjection-0.21.tar.gz/scProjection-0.21/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+setuptools.setup(name='scProjection',
+    version='0.21',
+    description='Projection and Deconvolution using deep heirarchical and generative neural network.',
+    url='https://github.com/ucdavis/quonlab/tree/master/development/deconvAllen',
+    author='Nelson Johansen, Gerald Quon',
+    author_email='njjohansen@ucdavis.edu, gquon@ucdavis.edu',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'tensorflow',
+        'tensorflow_probability',
+        'sklearn',
+        'numpy',
+        'sys',
+        'os',
+        'glob',
+        'gzip',
+        'time',
+        'traceback',
+        'logging'
+      ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6")
