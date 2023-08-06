d9b934b4edb59dcd988b42a29e43b11cefb737bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:40 +0000

    Add package rasgotransforms/1.5.2/rasgotransforms-1.5.2-py3-none-any.whl

---
+++ packages/rasgotransforms/rasgotransforms-1.5.2-py3-none-any.whl/rasgotransforms/transforms/levenshtein/levenshtein.sql
@@ -0,0 +1,8 @@
+SELECT *, 
+    {% for col in columns1 %}
+        {%- for col2 in columns2 -%}
+            EDITDISTANCE({{col}}, {{col2}}) as {{col}}_{{col2}}_Distance {{ ", " if not loop.last else "" }}
+        {%- endfor -%}
+    {%- endfor -%}
+
+FROM {{source_table}}
