f4bc85f6282864a30e97468ddd03094bd6fe3a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:30 +0000

    Add package icon-getinfo/0.1.2/icon-getinfo-0.1.2.tar.gz

---
+++ packages/icon-getinfo/icon-getinfo-0.1.2.tar.gz/icon-getinfo-0.1.2/icon_getinfo/__version__.py
@@ -0,0 +1,9 @@
+####
+__title__ = 'icon-getinfo'
+__description__ = 'Icon Blockchain Network node get information.'
+__url__ = 'https://github.com/HonamSong/icon_getinfo'
+__version__ = '0.1.2'
+__author__ = 'Honam Song'
+__author_email__ = 'hnsong@iconloop.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 hnsong'
