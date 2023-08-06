5f02bd3bd9e0c19958e527110dd02c57d0be8914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:32 +0000

    Add package sapply/0.1.12/sapply-0.1.12.tar.gz

---
+++ packages/sapply/sapply-0.1.12.tar.gz/sapply-0.1.12/src/sapply/flip.py
@@ -0,0 +1,155 @@
+HTMLTable = {
+	u'\u0022': '&quot;'       , u'\u0026': '&amp;'      , u'\u003C': '&lt;'       , u'\u003E': '&gt;',
+	u'\u00C2': '&iexcl;'      , u'\u00C2': '&cent;'     , u'\u00C2': '&pound;'    , u'\u00C2': '&curren;',
+	u'\u00C2': '&yen;'        , u'\u00C2': '&brvbar;'   , u'\u00C2': '&sect;'     , u'\u00C2': '&uml;',
+	u'\u00C2': '&copy;'       , u'\u00C2': '&ordf;'     , u'\u00C2': '&laquo;'    , u'\u00C2': '&not;',
+	u'\u00C2': '&shy;'        , u'\u00C2': '&reg;'      , u'\u00C2': '&macr;'     , u'\u00C2': '&deg;',
+	u'\u00C2': '&plusmn;'     , u'\u00C2': '&sup2;'     , u'\u00C2': '&sup3;'     , u'\u00C2': '&acute;',
+	u'\u00C2': '&micro;'      , u'\u00C2': '&para;'     , u'\u00C2': '&middot;'   , u'\u00C2': '&cedil;',
+	u'\u00C2': '&sup1;'       , u'\u00C2': '&ordm;'     , u'\u00C2': '&raquo;'    , u'\u00C2': '&frac14;',
+	u'\u00C2': '&frac12;'     , u'\u00C2': '&frac34;'   , u'\u00C2': '&iquest;'   , u'\u00C3': '&Agrave;',
+	u'\u00C3': '&Aacute;'     , u'\u00C3': '&Acirc;'    , u'\u00C3': '&Atilde;'   , u'\u00C3': '&Auml;',
+	u'\u00C3': '&Aring;'      , u'\u00C3': '&AElig;'    , u'\u00C3': '&Ccedil;'   , u'\u00C3': '&Egrave;',
+	u'\u00C3': '&Eacute;'     , u'\u00C3': '&Ecirc;'    , u'\u00C3': '&Euml;'     , u'\u00C3': '&Igrave;',
+	u'\u00C3': '&Iacute;'     , u'\u00C3': '&Icirc;'    , u'\u00C3': '&Iuml;'     , u'\u00C3': '&ETH;',
+	u'\u00C3': '&Ntilde;'     , u'\u00C3': '&Ograve;'   , u'\u00C3': '&Oacute;'   , u'\u00C3': '&Ocirc;',
+	u'\u00C3': '&Otilde;'     , u'\u00C3': '&Ouml;'     , u'\u00C3': '&times;'    , u'\u00C3': '&Oslash;',
+	u'\u00C3': '&Ugrave;'     , u'\u00C3': '&Uacute;'   , u'\u00C3': '&Ucirc;'    , u'\u00C3': '&Uuml;',
+	u'\u00C3': '&Yacute;'     , u'\u00C3': '&THORN;'    , u'\u00C3': '&szlig;'    , u'\u00C3': '&agrave;',
+	u'\u00C3': '&aacute;'     , u'\u00C3': '&acirc;'    , u'\u00C3': '&atilde;'   , u'\u00C3': '&auml;',
+	u'\u00C3': '&aring;'      , u'\u00C3': '&aelig;'    , u'\u00C3': '&ccedil;'   , u'\u00C3': '&egrave;',
+	u'\u00C3': '&eacute;'     , u'\u00C3': '&ecirc;'    , u'\u00C3': '&euml;'     , u'\u00C3': '&igrave;',
+	u'\u00C3': '&iacute;'     , u'\u00C3': '&icirc;'    , u'\u00C3': '&iuml;'     , u'\u00C3': '&eth;',
+	u'\u00C3': '&ntilde;'     , u'\u00C3': '&ograve;'   , u'\u00C3': '&oacute;'   , u'\u00C3': '&ocirc;',
+	u'\u00C3': '&otilde;'     , u'\u00C3': '&ouml;'     , u'\u00C3': '&divide;'   , u'\u00C3': '&oslash;',
+	u'\u00C3': '&ugrave;'     , u'\u00C3': '&uacute;'   , u'\u00C3': '&ucirc;'    , u'\u00C3': '&uuml;',
+	u'\u00C3': '&yacute;'     , u'\u00C3': '&thorn;'    , u'\u00C3': '&yuml;'     , u'\u00C5': '&OElig;',
+	u'\u00C5': '&oelig;'      , u'\u00C5': '&Scaron;'   , u'\u00C5': '&scaron;'   , u'\u00C5': '&Yuml;',
+	u'\u00C6': '&fnof;'       , u'\u00CB': '&circ;'     , u'\u00CB': '&tilde;'    , u'\u00CE': '&Alpha;',
+	u'\u00CE': '&Beta;'       , u'\u00CE': '&Gamma;'    , u'\u00CE': '&Delta;'    , u'\u00CE': '&Epsilon;',
+	u'\u00CE': '&Zeta;'       , u'\u00CE': '&Eta;'      , u'\u00CE': '&Theta;'    , u'\u00CE': '&Iota;',
+	u'\u00CE': '&Kappa;'      , u'\u00CE': '&Lambda;'   , u'\u00CE': '&Mu;'       , u'\u00CE': '&Nu;',
+	u'\u00CE': '&Xi;'         , u'\u00CE': '&Omicron;'  , u'\u00CE': '&Pi;'       , u'\u00CE': '&Rho;',
+	u'\u00CE': '&Sigma;'      , u'\u00CE': '&Tau;'      , u'\u00CE': '&Upsilon;'  , u'\u00CE': '&Phi;',
+	u'\u00CE': '&Chi;'        , u'\u00CE': '&Psi;'      , u'\u00CE': '&Omega;'    , u'\u00CE': '&alpha;',
+	u'\u00CE': '&beta;'       , u'\u00CE': '&gamma;'    , u'\u00CE': '&delta;'    , u'\u00CE': '&epsilon;',
+	u'\u00CE': '&zeta;'       , u'\u00CE': '&eta;'      , u'\u00CE': '&theta;'    , u'\u00CE': '&iota;',
+	u'\u00CE': '&kappa;'      , u'\u00CE': '&lambda;'   , u'\u00CE': '&mu;'       , u'\u00CE': '&nu;',
+	u'\u00CE': '&xi;'         , u'\u00CE': '&omicron;'  , u'\u00CF': '&pi;'       , u'\u00CF': '&rho;',
+	u'\u00CF': '&sigmaf;'     , u'\u00CF': '&sigma;'    , u'\u00CF': '&tau;'      , u'\u00CF': '&upsilon;',
+	u'\u00CF': '&phi;'        , u'\u00CF': '&chi;'      , u'\u00CF': '&psi;'      , u'\u00CF': '&omega;',
+	u'\u00CF': '&thetasym;'   , u'\u00CF': '&upsih;'    , u'\u00CF': '&piv;'      , u'\u00E2': '&ensp;',
+	u'\u00E2': '&emsp;'       , u'\u00E2': '&thinsp;'   , u'\u00E2': '&zwnj;'     , u'\u00E2': '&zwj;',
+	u'\u00E2': '&lrm;'        , u'\u00E2': '&rlm;'      , u'\u00E2': '&ndash;'    , u'\u00E2': '&mdash;',
+	u'\u00E2': '&lsquo;'      , u'\u00E2': '&rsquo;'    , u'\u00E2': '&sbquo;'    , u'\u00E2': '&ldquo;',
+	u'\u00E2': '&rdquo;'      , u'\u00E2': '&bdquo;'    , u'\u00E2': '&dagger;'   , u'\u00E2': '&Dagger;',
+	u'\u00E2': '&bull;'       , u'\u00E2': '&hellip;'   , u'\u00E2': '&permil;'   , u'\u00E2': '&prime;',
+	u'\u00E2': '&Prime;'      , u'\u00E2': '&lsaquo;'   , u'\u00E2': '&rsaquo;'   , u'\u00E2': '&oline;',
+	u'\u00E2': '&frasl;'      , u'\u00E2': '&euro;'     , u'\u00E2': '&image;'    , u'\u00E2': '&weierp;',
+	u'\u00E2': '&real;'       , u'\u00E2': '&trade;'    , u'\u00E2': '&alefsym;'  , u'\u00E2': '&larr;',
+	u'\u00E2': '&uarr;'       , u'\u00E2': '&rarr;'     , u'\u00E2': '&darr;'     , u'\u00E2': '&harr;',
+	u'\u00E2': '&crarr;'      , u'\u00E2': '&lArr;'     , u'\u00E2': '&uArr;'     , u'\u00E2': '&rArr;',
+	u'\u00E2': '&dArr;'       , u'\u00E2': '&hArr;'     , u'\u00E2': '&forall;'   , u'\u00E2': '&part;',
+	u'\u00E2': '&exist;'      , u'\u00E2': '&empty;'    , u'\u00E2': '&nabla;'    , u'\u00E2': '&isin;',
+	u'\u00E2': '&notin;'      , u'\u00E2': '&ni;'       , u'\u00E2': '&prod;'     , u'\u00E2': '&sum;',
+	u'\u00E2': '&minus;'      , u'\u00E2': '&lowast;'   , u'\u00E2': '&radic;'    , u'\u00E2': '&prop;',
+	u'\u00E2': '&infin;'      , u'\u00E2': '&ang;'      , u'\u00E2': '&and;'      , u'\u00E2': '&or;',
+	u'\u00E2': '&cap;'        , u'\u00E2': '&cup;'      , u'\u00E2': '&int;'      , u'\u00E2': '&there4;',
+	u'\u00E2': '&sim;'        , u'\u00E2': '&cong;'     , u'\u00E2': '&asymp;'    , u'\u00E2': '&ne;',
+	u'\u00E2': '&equiv;'      , u'\u00E2': '&le;'       , u'\u00E2': '&ge;'       , u'\u00E2': '&sub;',
+	u'\u00E2': '&sup;'        , u'\u00E2': '&nsub;'     , u'\u00E2': '&sube;'     , u'\u00E2': '&supe;',
+	u'\u00E2': '&oplus;'      , u'\u00E2': '&otimes;'   , u'\u00E2': '&perp;'     , u'\u00E2': '&sdot;',
+	u'\u00E2': '&lceil;'      , u'\u00E2': '&rceil;'    , u'\u00E2': '&lfloor;'   , u'\u00E2': '&rfloor;',
+	u'\u00E2': '&lang;'       , u'\u00E2': '&rang;'     , u'\u00E2': '&loz;'      , u'\u00E2': '&spades;',
+	u'\u00E2': '&clubs;'      , u'\u00E2': '&hearts;'   , u'\u00E2': '&diams;'
+}
+
+flipTable = {
+	'a': u'\u0250'      , 'b': 'q'              , 'c': u'\u0254'    , 'd': 'p',
+    'e': u'\u01DD'      , 'f': u'\u025F'        , 'g': u'\u0183'    , 'h': u'\u0265',
+    'i': u'\u1D09'      , 'j': u'\u027E'        , 'k': u'\u029E'    , 'm': u'\u026F',
+    'n': 'u'            , 'r': u'\u0279'        , 't': u'\u0287'    , 'v': u'\u028C',
+    'w': u'\u028D'      , 'y': u'\u028E'        , 'A': u'\u2200'    , 'C': u'\u0186',
+    'E': u'\u018E'      , 'F': u'\u2132'        , 'G': u'\u05E4'    , 'H': 'H',
+    'I': 'I'            , 'J': u'\u017F'        , 'L': u'\u02E5'    , 'M': 'W',
+    'N': 'N'            , 'P': u'\u0500'        , 'T': u'\u2534'    , 'U': u'\u2229',
+    'V': u'\u039B'      , 'Y': u'\u2144'        , '1': u'\u0196'    , '2': u'\u1105',
+    '3': u'\u0190'      , '4': u'\u3123'        , '5': u'\u03DB'    , '6': '9',
+    '7': u'\u3125'      , '8': '8'              , '9': '6'          , '0': '0',
+    '.': u'\u02D9'      , ',': '\''             , '\'': ','         , '\'': ',,',
+    '`': ','            , '?': u'\u00BF'        , '!': u'\u00A1'    , '[': ']',
+    ']': '['            , '(': ')'              , ')': '('          , '{': '}',
+    '}': '{'            , '<': '>'              , '>': '<'          , '&': u'\u214B',
+    '_': u'\u203E'      , u'\u2234': u'\u2235'  , u'\u2045': u'\u2046'
+}
+
+flipTableFlipped = {
+	u'\u0250': 'a'      , 'q': 'b'              , u'\u0254': 'c'    , 'p': 'd',
+	u'\u01DD': 'e'      , u'\u025F': 'f'        , u'\u0183': 'g'    , u'\u0265': 'h',
+	u'\u1D09': 'i'      , u'\u027E': 'j'        , u'\u029E': 'k'    , u'\u026F': 'm',
+	'u': 'n'            , u'\u0279': 'r'        , u'\u0287': 't'    , u'\u028C': 'v',
+	u'\u028D': 'w'      , u'\u028E': 'y'        , u'\u2200': 'A'    , u'\u0186': 'C',
+	u'\u018E': 'E'      , u'\u2132': 'F'        , u'\u05E4': 'G'    , 'H': 'H',
+	'I': 'I'            , u'\u017F': 'J'        , u'\u02E5': 'L'    , 'W': 'M',
+	'N': 'N'            , u'\u0500': 'P'        , u'\u2534': 'T'    , u'\u2229': 'U',
+	u'\u039B': 'V'      , u'\u2144': 'Y'        , u'\u0196': '1'    , u'\u1105': '2',
+	u'\u0190': '3'      , u'\u3123': '4'        , u'\u03DB': '5'    , '9': '6',
+	u'\u3125': '7'      , '8': '8'              , '6': '9'          , '0': '0',
+	u'\u02D9': '.'      , '\'': ','             , ',': '\''         , ',,': '\'',
+	',': '`'            , u'\u00BF': '?'        , u'\u00A1': '!'    , ']': '[',
+	'[': ']'            , ')': '('              , '(': ')'          , '}': '{',
+	'{': '}'            , '>': '<'              , '<': '>'          , u'\u214B': '&',
+	u'\u203E': '_'      , u'\u2235': u'\u2234'  , u'\u2046': u'\u2045'
+}
+
+flipTableHTML = {
+	u'\u0250': '&#592;'   , u'\u0254': '&#596;'     , u'\u01DD': '&#477;'     , u'\u025F': '&#607;',
+	u'\u0183': '&#387;'   , u'\u0265': '&#613;'     , u'\u1D09': '&#7433;'    , u'\u027E': '&#638;',
+	u'\u029E': '&#670;'   , u'\u026F': '&#623;'     , u'\u0279': '&#633;'     , u'\u0287': '&#647;',
+	u'\u028C': '&#652;'   , u'\u028D': '&#653;'     , u'\u028E': '&#654;'     , u'\u2200': '&#8704;',
+	u'\u0186': '&#390;'   , u'\u018E': '&#398;'     , u'\u2132': '&#8498;'    , u'\u05E4': '&#1508;',
+	u'\u017F': '&#383;'   , u'\u02E5': '&#741;'     , u'\u0500': '&#1280;'    , u'\u2534': '&#9524;',
+	u'\u2229': '&#8745;'  , u'\u039B': '&#923;'     , u'\u2144': '&#8516;'    , u'\u0196': '&#406;',
+	u'\u1105': '&#4357;'  , u'\u0190': '&#400;'     , u'\u3123': '&#12579;'   , u'\u03DB': '&#987;',
+	u'\u3125': '&#12581;' , u'\u02D9': '&#729;'     , u'\u00BF': '&#191;'     , u'\u00A1': '&#161;',
+	u'\u214B': '&#8523;'  , u'\u203E': '&#8254;'    , u'\u2235': '&#8757;'    , u'\u2234': '&#8756;',
+	u'\u2046': '&#8262;'  , u'\u2045': '&#8261;'
+}
+
+
+def flip(text, backwards=True, upside_down=True, convert_html=False):
+    txt = ''
+    html = ''
+
+    for char in range(0, len(text)):
+        reverse = ''
+        flipped = ''
+        did_convert_html = False
+
+        if backwards:
+            reverse = text[(-1 * (char - len(text) + 1))]
+        else: # Do not reverse
+            reverse = text[char]
+
+        s = reverse
+
+        if convert_html:
+            len_html = len(html)
+            html_last_char = ''
+            if (len_html >= 1):
+                html_last_char = html[len_html - 1]
+            if (s == ' ' and (html_last_char == ' ' or html.substring(len_html - 6) == '&nbsp;')):
+                html += '&nbsp;'
+            elif (s == '\n'):
+                did_convert_html = True
+
+        if upside_down:
+             flipped = (flipTable.get(s) or flipTableFlipped.get(s) or flipTable.get(s.lower()) or s)
+        else:
+             flipped = s
+        txt += flipped
+        if (convert_html and not did_convert_html):
+            html += (flipTableHTML.get(flipped) or HTMLTable.get(flipped) or flipped);
+    print(txt)
+    if html != '':
+        print(html)
