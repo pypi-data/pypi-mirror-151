d95a64fe84ef7eb5a80238abb4b0dcb34e981631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:09 +0000

    Add package humlab-penelope/0.7.15/humlab_penelope-0.7.15-py3-none-any.whl

---
+++ packages/humlab-penelope/humlab_penelope-0.7.15-py3-none-any.whl/penelope/pipeline/co_occurrence/legacy/pipelines.py
@@ -0,0 +1,36 @@
+# from penelope.co_occurrence import ContextOpts
+# from penelope.corpus import ExtractTaggedTokensOpts, TokensTransformOpts
+# from penelope.utility import deprecated
+
+# from ... import pipelines
+
+
+# @deprecated
+# def wildcard_to_partitioned_by_key_co_occurrence_pipeline(
+#     *,
+#     transform_opts: TokensTransformOpts = None,
+#     extract_opts: ExtractTaggedTokensOpts = None,
+#     context_opts: ContextOpts = None,
+#     global_threshold_count: int = None,
+# ) -> pipelines.CorpusPipeline:
+#     """Computes generic partitioned co-occurrence"""
+#     try:
+#         pipeline: pipelines.CorpusPipeline = (
+#             pipelines.wildcard()
+#             .tagged_frame_to_tokens(
+#                 extract_opts=extract_opts,
+#                 transform_opts=transform_opts,
+#             )
+#             .vocabulary(lemmatize=extract_opts.lemmatize, progress=True)
+#             .tqdm()
+#             .to_corpus_co_occurrence(
+#                 context_opts=context_opts,
+#                 transform_opts=transform_opts,
+#                 global_threshold_count=global_threshold_count,
+#             )
+#         )
+
+#         return pipeline
+
+#     except Exception as ex:
+#         raise ex
