e68a8e5532f31bbb8cee05c75cdcbb7b964e2ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:46 +0000

    Add package EventHubPublisher/0.0.25/EventHubPublisher-0.0.25.tar.gz

---
+++ packages/EventHubPublisher/EventHubPublisher-0.0.25.tar.gz/EventHubPublisher-0.0.25/EventPublisher/Settings/Settings.py
@@ -0,0 +1,10 @@
+
+class Settings(object):
+    def __init__(self, connectionString, name):
+        self.connectionString = connectionString
+        self.name = name
+
+    def object_decoder(obj):
+        if '__type__' in obj and obj['__type__'] == 'Settings':
+            return Settings(obj['connectionString'], obj['name'])
+        return obj
