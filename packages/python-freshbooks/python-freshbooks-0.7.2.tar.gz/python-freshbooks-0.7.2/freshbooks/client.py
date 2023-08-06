16ee3895fdcd99558eb67eeba6cc83a8c473a832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:53 +0000

    Add package python-freshbooks/0.7.2/python-freshbooks-0.7.2.tar.gz

---
+++ packages/python-freshbooks/python-freshbooks-0.7.2.tar.gz/python-freshbooks-0.7.2/freshbooks/client.py
@@ -0,0 +1,47 @@
+import json
+
+from authlib.integrations.requests_client import OAuth2Session
+
+from freshbooks import BaseClient
+from freshbooks.exceptions import FreshBooksUnauthenticatedError, FreshBooksPaymentRequiredError
+
+
+class Client(BaseClient):
+    @property
+    def authlib_integration_client(self):
+        return OAuth2Session
+
+    def fetch_access_token(self, authorization_response):
+        return self.session.fetch_token(
+            self.token_endpoint, authorization_response=authorization_response
+        )
+
+    def refresh_access_token(self):
+        return self.session.refresh_token(
+            self.token_endpoint, refresh_token=self.refresh_token
+        )
+
+    def get(self, url, params={}):
+        response = self.session.get(f"{self.base_url}{url}", params=params)
+        self.handle_errors(response)
+        return response.json()
+
+    def post(self, url, payload):
+        response = self.session.post(url, data=json.dumps(payload))
+        self.handle_errors(response)
+        return response.json()
+
+    def load_current_user(self):
+        result = self.get("/auth/api/v1/users/me?exclude_groups=1")["response"]
+        self.current_user = self.build_identity(result)
+        return self.current_user
+
+    def set_active_business(self, business_uuid):
+        if not self.current_user:
+            self.load_current_user()
+        self.set_active_business_and_roles_id(self.current_user, business_uuid)
+
+    def get_currencies(self):
+        return self.get(
+            f"/accounting/account/{self.active_account_id}/systems/currencies"
+        )["response"]["result"]["currencies"]["currency_codes"]
---
+++ packages/python-freshbooks/python-freshbooks-0.7.2.tar.gz/python-freshbooks-0.7.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="python-freshbooks",
+    author_email="alexandre.baizeau@gmail.com",
+    license="MIT License",
+    version="0.7.2",
+    packages=find_packages(),
+    install_requires=[
+        "authlib",
+    ],
+    description="Python wrapper for the FreshBooks API",
+    url="https://github.com/alexbaizeau/python-freshbooks",
+)
