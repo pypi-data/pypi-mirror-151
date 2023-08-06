bcb4e1735c25f4a6b11222b9780f92ae9348de30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:46 +0000

    Add package dnasim/0.0.1/dnasim-0.0.1.tar.gz

---
+++ packages/dnasim/dnasim-0.0.1.tar.gz/dnasim-0.0.1/README.md
@@ -0,0 +1,4 @@
+# DNAsim
+
+**DNAsim** is an open-source framework for DNA Computing simulations in Python.
+
---
+++ packages/dnasim/dnasim-0.0.1.tar.gz/dnasim-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='dnasim',
+      version='0.0.1',
+      description='DNA Computing simulations in Python',
+      url='http://github.com/hritog/dnasim',
+      author='Hristo Georgiev',
+      author_email='hristo.a.georgiev@gmail.com',
+      license='Apache 2.0',
+      packages=['dnasim'],
+      zip_safe=False,
+)
+
