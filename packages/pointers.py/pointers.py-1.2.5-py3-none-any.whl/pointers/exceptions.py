7c831931f2e7de80aca77acf2cb4230d7325124c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:07 +0000

    Add package pointers.py/1.2.5/pointers.py-1.2.5-py3-none-any.whl

---
+++ packages/pointers.py/pointers.py-1.2.5-py3-none-any.whl/pointers/exceptions.py
@@ -0,0 +1,31 @@
+__all__ = ("IsMallocPointerError", "AllocationError", "NotEnoughChunks")
+
+
+class IsMallocPointerError(Exception):
+    """Raised when trying perform an operation on a malloc pointer that isn't supported."""  # noqa
+
+    pass
+
+
+class AllocationError(Exception):
+    """Raised when a memory allocation fails."""
+
+    pass
+
+
+class NotEnoughChunks(Exception):
+    """Raised when there aren't enough chunks in a CallocPointer."""
+
+    pass
+
+
+class IsFrozenError(Exception):
+    """Raised when trying to move the address of a frozen pointer."""
+
+    pass
+
+
+class DereferenceError(Exception):
+    """Raised when trying to dereference an object address that doesn't exist."""  # noqa
+
+    pass
