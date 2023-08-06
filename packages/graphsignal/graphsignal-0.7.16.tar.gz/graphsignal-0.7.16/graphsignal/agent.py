fc2a147f28e0e09955d7d75726e15f565f893f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:50 +0000

    Add package graphsignal/0.7.16/graphsignal-0.7.16.tar.gz

---
+++ packages/graphsignal/graphsignal-0.7.16.tar.gz/graphsignal-0.7.16/graphsignal/agent.py
@@ -0,0 +1,17 @@
+
+class Agent(object):
+    def __init__(self):
+        self.worker_id = None
+        self.start_ms = None
+        self.api_key = None
+        self.workload_name = None
+        self.run_id = None
+        self.node_rank = None
+        self.local_rank = None
+        self.world_rank = None
+        self.debug_mode = None
+        self.uploader = None
+        self.process_reader = None
+        self.nvml_reader = None
+        self.params = None
+        self.metrics = None
