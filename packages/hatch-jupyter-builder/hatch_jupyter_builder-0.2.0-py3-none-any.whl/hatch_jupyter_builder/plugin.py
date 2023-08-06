fbdfea821dedc53fca73073f99e705c482dc421c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:52 +0000

    Add package hatch-jupyter-builder/0.2.0/hatch_jupyter_builder-0.2.0-py3-none-any.whl

---
+++ packages/hatch-jupyter-builder/hatch_jupyter_builder-0.2.0-py3-none-any.whl/hatch_jupyter_builder/plugin.py
@@ -0,0 +1,38 @@
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+from .utils import (
+    ensure_targets,
+    get_build_func,
+    install_pre_commit_hook,
+    normalize_kwargs,
+)
+
+
+class JupyterBuildHook(BuildHookInterface):
+    PLUGIN_NAME = "jupyter-builder"
+
+    def initialize(self, version, build_data):
+        if self.target_name not in ["wheel", "sdist"]:
+            return
+
+        should_install_hook = self.config.get("install-pre-commit-hook")
+
+        if version == "editable" and should_install_hook:
+            install_pre_commit_hook()
+
+        # Get the configuration options.
+        build_function = self.config.get("build-function")
+        build_kwargs = self.config.get("build-kwargs", {})
+        ensured_targets = self.config.get("ensured-targets", [])
+
+        if not build_function:
+            return
+
+        # Get build function and call it with normalized parameter names.
+        build_func = get_build_func(build_function)
+        build_kwargs = normalize_kwargs(build_kwargs)
+        build_func(self.target_name, version, **build_kwargs)
+
+        # Ensure targets in distributable dists.
+        if version == "standard":
+            ensure_targets(ensured_targets)
