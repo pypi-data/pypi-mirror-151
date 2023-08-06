0b009f7266f5336e1678c085b21de5b4c4378357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:16 +0000

    Add package streamlit-fb-post/0.0.1/streamlit-fb-post-0.0.1.tar.gz

---
+++ packages/streamlit-fb-post/streamlit-fb-post-0.0.1.tar.gz/streamlit-fb-post-0.0.1/README.md
@@ -0,0 +1,21 @@
+# st-fb-post
+
+Streamlit Component, for Facebook Post UI
+
+authors - [@gamcoh](https://github.com/gamcoh) 
+
+## Installation
+
+Install `streamlit-fb-post` with pip
+```bash
+pip install streamlit-fb-post
+```
+
+usage, import the `message` function from `streamlit_fb_post`
+```py
+import streamlit as st
+from streamlit_fb_post import post
+
+post("Happy birthday to the main character in our family!", legend="25 feb", link="https://www.facebook.com/")
+```
+   
---
+++ packages/streamlit-fb-post/streamlit-fb-post-0.0.1.tar.gz/streamlit-fb-post-0.0.1/streamlit_fb_post/frontend/build/precache-manifest.8963ad6c8e71f12e3d880b756aa9d1ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712dc7ae9f58f661a7c1e4ea806a908e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7854bcc9f25c7e197aa7",
+    "url": "./static/js/2.28c7124d.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.28c7124d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0261f7ecbcbd598a18",
+    "url": "./static/js/main.ce2e5b28.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
