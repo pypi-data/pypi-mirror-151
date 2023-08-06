5d0548cf241ccae501acff089aea57222d1faab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:18 +0000

    Add package serverless-builder/2.7.7/serverless-builder-2.7.7.tar.gz

---
+++ packages/serverless-builder/serverless-builder-2.7.7.tar.gz/serverless-builder-2.7.7/serverless/aws/features/encryption.py
@@ -0,0 +1,78 @@
+from troposphere.kms import Alias, Key
+
+from serverless.service.plugins.kms import KMSGrant
+from serverless.service.types import Feature
+
+
+class Encryption(Feature):
+    def __init__(self):
+        super().__init__()
+        self.key = Key(
+            title="ServiceEncryptionKey",
+            Description="Encryption Key for ${self:service}",
+            Enabled=True,
+            PendingWindowInDays=14,
+            EnableKeyRotation=True,
+            KeyPolicy={
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "Enable IAM User Permissions",
+                        "Effect": "Allow",
+                        "Principal": {"AWS": "arn:aws:iam::${aws:accountId}:root"},
+                        "Action": "kms:*",
+                        "Resource": "*",
+                    },
+                    {
+                        "Effect": "Allow",
+                        "Principal": {"Service": "logs.${aws:region}.amazonaws.com"},
+                        "Action": [
+                            "kms:Encrypt*",
+                            "kms:Decrypt*",
+                            "kms:ReEncrypt*",
+                            "kms:GenerateDataKey*",
+                            "kms:Describe*",
+                        ],
+                        "Resource": "*",
+                        "Condition": {
+                            "ArnLike": {
+                                "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:${aws:region}:${aws:accountId}:log-group:/services/${self:service}/*"
+                            }
+                        },
+                    },
+                ],
+            },
+        )
+
+        self.alias = Alias(
+            "ServiceEncryptionKeyAlias", AliasName="alias/${self:service}-${sls:stage}", TargetKeyId=self.key.Ref()
+        )
+
+    def pre_render(self, service):
+        super().pre_render(service)
+        for fn in service.functions.all():
+            self.key.KeyPolicy["Statement"].append(
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "logs.${aws:region}.amazonaws.com"},
+                    "Action": [
+                        "kms:Encrypt*",
+                        "kms:Decrypt*",
+                        "kms:ReEncrypt*",
+                        "kms:GenerateDataKey*",
+                        "kms:Describe*",
+                    ],
+                    "Resource": "*",
+                    "Condition": {
+                        "ArnLike": {
+                            "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:${aws:region}:${aws:accountId}:log-group:/aws/lambda/"
+                            + fn.name.spinal
+                        }
+                    },
+                }
+            )
+
+    def enable(self, service):
+        service.resources.add(self.key)
+        service.resources.add(self.alias)
+        service.plugins.add(KMSGrant())
