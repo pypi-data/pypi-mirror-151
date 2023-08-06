e026592455c3e51e159925eb6bfc7180f63e565e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:00 +0000

    Add package jupyterlab-mutableai/0.1.12/jupyterlab_mutableai-0.1.12-py3-none-any.whl

---
+++ packages/jupyterlab-mutableai/jupyterlab_mutableai-0.1.12-py3-none-any.whl/jupyterlab_mutableai/utils.py
@@ -0,0 +1,24 @@
+import requests
+from urllib.parse import urljoin
+import json
+
+ANALYTICS = "ANALYTICS"
+
+
+def update_analytics(base_url, api_key, action):
+    payload = json.dumps({
+        "action": action
+    })
+
+    url = urljoin("https://" + base_url, ANALYTICS)
+
+    response = requests.post(
+        url=url,
+        data=payload,
+        headers={
+            "Content-Type": "application/json",
+            "x-api-key": api_key,
+        },
+    )
+
+    print("Analytics: ", response)
