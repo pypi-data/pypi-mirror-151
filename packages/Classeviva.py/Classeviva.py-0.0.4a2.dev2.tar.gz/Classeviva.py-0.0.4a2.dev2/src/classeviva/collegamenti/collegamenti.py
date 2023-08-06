6692ac5a626e4e5d0e2b9b7a950ce42f2a190cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:52 +0000

    Add package Classeviva.py/0.0.4a2.dev2/Classeviva.py-0.0.4a2.dev2.tar.gz

---
+++ packages/Classeviva.py/Classeviva.py-0.0.4a2.dev2.tar.gz/Classeviva.py-0.0.4a2.dev2/src/classeviva/collegamenti/collegamenti.py
@@ -0,0 +1,5 @@
+class Collegamenti:
+    base: str = "https://web.spaggiari.eu/rest"
+    accesso: str = f"{base}/v1/auth/login"
+    stato: str = f"{base}/v1/auth/status"
+    biglietto: str = f"{base}/v1/auth/ticket"
