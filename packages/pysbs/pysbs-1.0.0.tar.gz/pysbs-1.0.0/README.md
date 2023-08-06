f7e761e40b7a3e6cfcfef0347ca5dafe89c94504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:20 +0000

    Add package pysbs/1.0.0/pysbs-1.0.0.tar.gz

---
+++ packages/pysbs/pysbs-1.0.0.tar.gz/pysbs-1.0.0/README.md
@@ -0,0 +1,9 @@
+```
+from pysbs import SBS
+sbs = SBS(open("main.sbs", "rb"))
+print(sbs["test"])
+sbs["test"] = "hello"
+print(sbs["test"])
+sbs.dump(open("main.sbs", "wb"))
+```
+
---
+++ packages/pysbs/pysbs-1.0.0.tar.gz/pysbs-1.0.0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import *
+setup(name="pysbs", version="1.0.0", author="wer310Libs", author_email="oficalkapudovo@gmail.com", description="Library for ooen and write .sbs files", packages=find_packages())
+
