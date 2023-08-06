24b603c6d666d2eae5a60746b40db6ca7954a190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:06 +0000

    Add package phytest/0.1.7/phytest-0.1.7.tar.gz

---
+++ packages/phytest/phytest-0.1.7.tar.gz/phytest-0.1.7/phytest/main.py
@@ -0,0 +1,38 @@
+import inspect
+import os
+from pathlib import Path
+from typing import Optional
+
+import pytest
+
+
+def main(
+    testfile: Optional[Path] = None,
+    alignment: Optional[Path] = None,
+    alignment_format: Optional[str] = 'fasta',
+    tree: Optional[Path] = None,
+    tree_format: Optional[str] = 'newick',
+    data: Optional[Path] = None,
+    verbose: bool = False,
+    report: bool = False,
+    expression: str = None,
+):
+    if not testfile:
+        testfile = Path(os.path.abspath((inspect.stack()[1])[1]))
+    args = [testfile]
+    if not verbose:
+        args.extend(["-ra", "--tb=no", "--no-header"])
+    if alignment is not None:
+        args.extend(["--alignment", alignment])
+        args.extend(["--alignment-format", alignment_format])
+    if tree is not None:
+        args.extend(["--tree", tree])
+        args.extend(["--tree-format", tree_format])
+    if data is not None:
+        args.extend(["--data", data])
+    if report:
+        args.extend(["--html=report.html", "--self-contained-html"])
+    if expression:
+        args.extend(["-k", expression])
+    exit_code = pytest.main(args, plugins=['phytest'])
+    return exit_code
