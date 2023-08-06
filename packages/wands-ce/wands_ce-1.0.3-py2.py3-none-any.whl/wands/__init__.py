9c18b0c610343bfe6e50a04f4a1dbe993582d09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:14 +0000

    Add package wands-ce/1.0.3/wands_ce-1.0.3-py2.py3-none-any.whl

---
+++ packages/wands-ce/wands_ce-1.0.3-py2.py3-none-any.whl/wands/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: onefeng
+@time: 2022/5/19 15:49
+"""
+
+__version__ = '1.0.0'
+
+from wands.parse.address_parse import AddressParse
+
+parse_address = AddressParse()
