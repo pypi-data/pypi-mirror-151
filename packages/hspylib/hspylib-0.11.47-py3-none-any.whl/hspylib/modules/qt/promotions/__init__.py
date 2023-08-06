4e8ee99b8cd0b9944f2235a387611b7e6eb05aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:21 +0000

    Add package hspylib/0.11.47/hspylib-0.11.47-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.47-py3-none-any.whl/hspylib/modules/qt/promotions/__init__.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.47
+#
+# Package: main.hspylib.modules.qt.promotions
+"""Package initialization."""
+
+__all__ = [
+  'hcombobox', 
+  'hconsole', 
+  'hframe', 
+  'hlabel', 
+  'hlistwidget', 
+  'htablemodel', 
+  'htableview'
+]
+__version__ = '0.11.47'
