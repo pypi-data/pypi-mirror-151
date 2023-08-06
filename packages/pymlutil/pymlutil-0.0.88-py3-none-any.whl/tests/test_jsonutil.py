aeb2db882be806be89d5127c72d440affaa9c68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:51 +0000

    Add package pymlutil/0.0.88/pymlutil-0.0.88-py3-none-any.whl

---
+++ packages/pymlutil/pymlutil-0.0.88-py3-none-any.whl/tests/test_jsonutil.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import numpy as np
+import cv2
+import json
+from collections import defaultdict
+import unittest
+import torch
+
+sys.path.insert(0, os.path.abspath('')) # Test files from current path rather than installed module
+from pymlutil.jsonutil import *
+
+test_config = 'test.yaml'
+
+class Test(unittest.TestCase):
+    def test_cmd(self):
+        self.assertEqual(cmd('ls -la', check=True, timeout=5), 0)
+
+    def test_yaml(self):
+        test = ReadDict(test_config)
+        assert test is not None
+        assert 'test_yaml' in test
+        self.assertEqual(test['test_yaml'][0]['zero'], 0)
+        self.assertEqual(test['test_yaml'][1]['one'], 1)
+        self.assertEqual(test['test_yaml'][2]['two'], 2)
+
+if __name__ == '__main__':
+    unittest.main()
