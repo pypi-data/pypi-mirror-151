da97b3baf4c84c69e7ac387cf5599b327f7e3ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package Validandolosdatosdelprofile/0.0.1/Validandolosdatosdelprofile-0.0.1.tar.gz

---
+++ packages/Validandolosdatosdelprofile/Validandolosdatosdelprofile-0.0.1.tar.gz/Validandolosdatosdelprofile-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.1' 
+PACKAGE_NAME = 'Validandolosdatosdelprofile' 
+AUTHOR = 'XXXX XXXXX'
+AUTHOR_EMAIL = 'XXX@gmail.com'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Aquí debes incluir una descripción corta de la librería' 
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
+LONG_DESC_TYPE = "text/markdown"
+
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
