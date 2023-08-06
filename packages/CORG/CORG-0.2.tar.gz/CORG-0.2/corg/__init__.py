8a5e3207b930dfe4244949adb8b14c298a3e4d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:40 +0000

    Add package CORG/0.2/CORG-0.2.tar.gz

---
+++ packages/CORG/CORG-0.2.tar.gz/CORG-0.2/corg/__init__.py
@@ -0,0 +1,7 @@
+from .benchmark_dimension import BenchmarkDimension
+
+from .discover_dimension import DiscoverDimension
+
+from .__version__ import __version__
+
+__all__ = ['__version__', 'BenchmarkDimension', 'DiscoverDimension']
