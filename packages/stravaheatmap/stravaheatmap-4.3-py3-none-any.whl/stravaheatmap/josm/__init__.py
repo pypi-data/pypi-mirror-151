5a019af0d727ca93bcfd3c0be3331c4fbcb4b9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:00 +0000

    Add package stravaheatmap/4.3/stravaheatmap-4.3-py3-none-any.whl

---
+++ packages/stravaheatmap/stravaheatmap-4.3-py3-none-any.whl/stravaheatmap/josm/__init__.py
@@ -0,0 +1,44 @@
+import platform
+import sys
+
+from colorama import Fore, Style
+from getpass import getpass
+
+from stravacookies import ( StravaCFetchCookieError,
+                            StravaCFetchOsError,
+                            StravaCFetchJosmprefsError
+                          )
+from .imgupdater import    ( ImgUpdater,
+                            MacOsImgUpdater,
+                            LinuxImgUpdater,
+                            WindowsImgUpdater
+                          )
+
+try:
+    if (platform.system() == "Darwin"):
+        stravaImgUpdater = MacOsImgUpdater()
+    elif (platform.system() == "Linux"):
+        stravaImgUpdater = LinuxImgUpdater()
+    elif (platform.system() == "Windows"):
+        stravaImgUpdater = WindowsImgUpdater()
+    else:
+        raise StravaCFetchOsError(platform.system())
+
+    stravaEmail = input(f"{Fore.CYAN}**** Enter your Strava account email:{Style.RESET_ALL} ")
+    stravaPassword = getpass(f"{Fore.CYAN}**** Enter your Strava account password:{Style.RESET_ALL} ")
+
+    print("Backing up preferences.xml...")
+    stravaImgUpdater.bakPrefs()
+    print("Updating preferences.xml...")
+    stravaImgUpdater.updPrefs(stravaEmail, stravaPassword)
+    print("Done.")
+
+except StravaCFetchOsError as e:
+    print("Only macOS/linux/Windows are supported.", file=sys.stderr)
+    print("Detected OS: " + e.message, file=sys.stderr)
+except StravaCFetchCookieError as e:
+    print("Strava cookies unavailable.", file=sys.stderr)
+    print(e.message, file=sys.stderr)
+except StravaCFetchJosmprefsError as e:
+    print("Couldn't locate JOSM preferences file.", file=sys.stderr)
+    print( e.message, file=sys.stderr)
---
+++ packages/stravaheatmap/stravaheatmap-4.3-py3-none-any.whl/stravaheatmap/josm/imgupdater.py
@@ -0,0 +1,76 @@
+import os
+import datetime
+from shutil import copy
+import xml.etree.ElementTree as ET
+
+from stravacookies.fetcher import StravaCookieFetcher
+
+class ImgUpdater(object):
+    def __init__(self):
+        self.josmPreferences = "./fakepath.xml" # has to be set in OS-specific constructor
+        self.cookieFetcher=StravaCookieFetcher() # may be overridden in OS-specific constructor
+
+    def bakPrefs(self):
+        bakfile = self.josmPreferences + '.' + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '.bak'
+        copy(self.josmPreferences, bakfile)
+
+    def updPrefs(self, stravaEmail, stravaPassword):
+        stravaCookieFetcher = self.cookieFetcher
+        stravaCookieFetcher.fetchCookies(stravaEmail, stravaPassword)
+        cookieString = stravaCookieFetcher.getCookieString()
+        ET.register_namespace('', "http://josm.openstreetmap.de/preferences-1.0")
+        doc = ET.parse(self.josmPreferences)
+        root = doc.getroot()
+        for maptag in doc.findall("./{http://josm.openstreetmap.de/preferences-1.0}maps/{http://josm.openstreetmap.de/preferences-1.0}map/{http://josm.openstreetmap.de/preferences-1.0}tag[@key='url']"):
+            if "strava.com" in maptag.attrib["value"]:
+                print("Updating Strava Imagery URL...")
+                fullUrl = maptag.attrib["value"]
+                splittedUrl = fullUrl.split('?')
+                baseUrl = splittedUrl[0]
+                print("--> [" + baseUrl + "]")
+                fullUrl = baseUrl + "?" + cookieString
+                maptag.attrib["value"] = fullUrl
+        print("Writing out preferences.xml...")
+        doc.write(self.josmPreferences, encoding="UTF-8")
+
+
+class MacOsImgUpdater(ImgUpdater):
+    def __init__( self ):
+        super().__init__()
+        self.josmPreferences = os.path.expanduser('~/Library/Preferences/JOSM/preferences.xml')
+
+
+class LinuxImgUpdater(ImgUpdater):
+    def __init__( self ):
+        super().__init__()
+        # On Linux, three possible locations for JOSM preference file
+        # according to https://josm.openstreetmap.de/wiki/Help/Preferences
+        # (plus, users may specify a custom directory within JOSM;
+        # this script doesn't support that)
+        pref = os.path.expanduser('~/.josm/preferences.xml')
+        if not os.path.isfile( pref ):
+            try:
+                pref = os.path.join(
+                    os.environ['XDG_CONFIG_HOME'],
+                    'JOSM', 'preferences.xml')
+            except KeyError:
+                pref = os.path.expanduser('~/.config/JOSM/preferences.xml')
+        if not os.path.isfile( pref ):
+            message = "Supported locations of preferences.xml:\n"
+            message+= "~/.josm/, $XDG_CONFIG_HOME/JOSM, or ~/.config/JOSM\n"
+            message+= "(custom preference locations are not supported)"
+            raise StravaCFetchJosmprefsError(message)
+        self.josmPreferences = pref
+
+
+class WindowsImgUpdater(ImgUpdater):
+    def __init__( self ):
+        super().__init__()
+        self.josmPreferences = os.path.join(
+            os.path.expandvars('%APPDATA%\JOSM'),
+            'preferences.xml')
+        # location of JOSM preference file for Windows
+        # according to https://josm.openstreetmap.de/wiki/Help/Preferences
+        if not os.path.isfile( self.josmPreferences ):
+            message = "Expect preferences.xml to sit in %APPDATA%\JOSM"
+            raise StravaCFetchJosmprefsError(message)
