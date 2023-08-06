9a2cafca533f9b08e0a3907ec892ec8666b36ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:09 +0000

    Add package name-matching/0.8.1/name_matching-0.8.1.tar.gz

---
+++ packages/name-matching/name_matching-0.8.1.tar.gz/name_matching-0.8.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+   name='name_matching',
+   version='0.8.1',
+   description='A package for the matching of company names',
+   author='Michiel Nijhuis',
+   author_email='m.nijhuis@ndb.nl',
+   packages=['name_matching'],
+   python_requires='<3.9',
+   install_requires = ['abydos==0.5.0',
+			'cleanco',
+			'numba',
+			'scikit-learn', 
+         'pandas',
+         'numpy',
+			'tqdm'],
+)
