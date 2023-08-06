73f19b23617ccbde0718296184ea7fdd453bb388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:14 +0000

    Add package openlineage-airflow/0.8.2/openlineage-airflow-0.8.2.tar.gz

---
+++ packages/openlineage-airflow/openlineage-airflow-0.8.2.tar.gz/openlineage-airflow-0.8.2/openlineage/airflow/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0.
+#
+# -*- coding: utf-8 -*-
+from pkg_resources import parse_version
+from airflow.version import version as AIRFLOW_VERSION
+
+__author__ = """OpenLineage"""
+__version__ = "0.8.2"
+
+if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
+    from openlineage.airflow.dag import DAG
+    __all__ = ["DAG"]
