d090b85c40c53fba91130e464400410f1ee832d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:49 +0000

    Add package solara/0.1.1/solara-0.1.1-py2.py3-none-any.whl

---
+++ packages/solara/solara-0.1.1-py2.py3-none-any.whl/solara/__init__.py
@@ -0,0 +1,15 @@
+"""Build webapps using IPywidgets"""
+__version__ = "0.1.1"
+# isort: skip_file
+
+from . import util
+
+# flake8: noqa: F402
+from .datatypes import *
+from .hooks import *
+from .memoize import memoize
+from .components import *
+from .widgets import watch as auto_reload_vue
+
+github_url = "https://github.com/widgetti/solara"
+git_branch = "master"
