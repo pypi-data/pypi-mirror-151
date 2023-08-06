e00a4cf900f1e45a74a6e68ad0614f112d161b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:08 +0000

    Add package nbproject/0.0.6/nbproject-0.0.6-py2.py3-none-any.whl

---
+++ packages/nbproject/nbproject-0.0.6-py2.py3-none-any.whl/nbproject/__init__.py
@@ -0,0 +1,31 @@
+"""nbproject: Manage your notebooks."""
+import traceback
+import sys
+from types import ModuleType
+
+from ._header import Header  # noqa
+
+
+_module = sys.modules[__name__]
+
+
+class LazyVersion(ModuleType):
+    @property
+    def __version__(self):
+        from ._version import get_versions
+
+        return "0.0.6"  # get_versions()["version"]
+
+
+_module.__class__ = LazyVersion
+
+
+def within_flit():
+    for frame in traceback.extract_stack():
+        if frame.name == "get_docstring_and_version_via_import":
+            return True
+    return False
+
+
+if within_flit():
+    _module.__dict__["__version__"] = _module.__version__
