f15190cdaee59b0e049a378ea7edfe143fde260d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:37 +0000

    Add package lstmcpipe/0.8.1/lstmcpipe-0.8.1.tar.gz

---
+++ packages/lstmcpipe/lstmcpipe-0.8.1.tar.gz/lstmcpipe-0.8.1/lstmcpipe/scripts/script_batch_filelist_cta.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import argparse
+from os.path import join, basename
+from os import environ
+from lstmcpipe.utils import rerun_cmd
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Batches the ctapipe-stage1 for all the files " "within a text file.")
+    parser.add_argument(
+        "--file_list",
+        "-f",
+        type=str,
+        dest="file_list",
+        help="Path to file containing list of DL0 simtel files to be processed.",
+        required=True,
+        nargs="+",
+    )
+    parser.add_argument(
+        "--output_dir",
+        "-o",
+        type=str,
+        dest="output_dir",
+        help="ctapipe-stage1 output directory argument.",
+        required=True,
+    )
+    parser.add_argument(
+        "--config_file",
+        "-c",
+        type=str,
+        dest="config_file",
+        help="ctapipe-stage1 configuration file argument.",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    task_id = int(environ["SLURM_ARRAY_TASK_ID"])
+    file_for_this_job = args.file_list[task_id]
+    print("Using file: ", file_for_this_job)
+
+    with open(file_for_this_job, "r") as filelist:
+        for file in filelist:
+            file = file.strip("\n")
+
+            # ctapipe takes the output filename
+            # so we need to construct it first
+            output = join(args.output_dir, basename(file.replace(".simtel.gz", ".dl1.h5")))
+
+            cmd = ["ctapipe-stage1", f"--input={file}", f"--output={output}"]
+            if args.config_file:
+                cmd.append("--config={}".format(args.config_file))
+
+            rerun_cmd(cmd, output, max_ntry=2)
