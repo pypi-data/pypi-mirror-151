e2322260712e74fca17c32da888aa2d5281f2d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:55 +0000

    Add package nils-first-package/0.1/nils_first_package-0.1.tar.gz

---
+++ packages/nils-first-package/nils_first_package-0.1.tar.gz/nils_first_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nils_first_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nils_first_package'],
+      zip_safe=False)
