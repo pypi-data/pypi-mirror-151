030a3ea0c721c1edca30976e75111ce164714259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:19 +0000

    Add package fast-stats/0.0.3/fast_stats-0.0.3-cp39-cp39-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/fast-stats/fast_stats-0.0.3-cp39-cp39-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/fast_stats/stats.py
@@ -0,0 +1,76 @@
+from typing import Union
+
+import numpy as np
+
+from .fast_stats import (
+    _binary_f1_score_reqs,
+    _binary_precision_reqs,
+    _binary_recall_reqs,
+)
+
+# from math import isnan  # for Rust returning float nan
+
+
+Result = Union[None, float]
+
+
+def _precision(tp: int, tp_fp: int, zero_division: str = "none") -> Result:
+    if tp_fp == 0:
+        if zero_division == "none":
+            return None
+        elif zero_division == "zero":
+            return 0.0
+    return tp / tp_fp
+
+
+def _recall(tp: int, tp_fn: int, zero_division: str = "none") -> Result:
+    if tp_fn == 0:
+        if zero_division == "none":
+            return None
+        elif zero_division == "zero":
+            return 0.0
+    return tp / tp_fn
+
+
+def binary_precision(
+    y_true: np.ndarray, y_pred: np.ndarray, zero_division: str = "none"
+) -> Result:
+    assert y_true.shape == y_pred.shape, "y_true and y_pred must be same shape"
+    assert isinstance(y_pred, np.ndarray) and isinstance(
+        y_true, np.ndarray
+    ), "y_true and y_pred must be numpy arrays"
+
+    tp, tp_fp = _binary_precision_reqs(y_true, y_pred)
+    return _precision(tp, tp_fp, zero_division)
+
+
+def binary_recall(
+    y_true: np.ndarray, y_pred: np.ndarray, zero_division: str = "none"
+) -> Result:
+    assert y_true.shape == y_pred.shape, "y_true and y_pred must be same shape"
+    assert isinstance(y_pred, np.ndarray) and isinstance(
+        y_true, np.ndarray
+    ), "y_true and y_pred must be numpy arrays"
+
+    tp, tp_fn = _binary_recall_reqs(y_true, y_pred)
+    return _recall(tp, tp_fn, zero_division)
+
+
+def binary_f1_score(
+    y_true: np.ndarray, y_pred: np.ndarray, zero_division: str = "none"
+):
+    assert y_true.shape == y_pred.shape, "y_true and y_pred must be same shape"
+    assert isinstance(y_pred, np.ndarray) and isinstance(
+        y_true, np.ndarray
+    ), "y_true and y_pred must be numpy arrays"
+
+    tp, tp_fp, tp_fn = _binary_f1_score_reqs(y_true, y_pred)
+    p, r = _precision(tp, tp_fp, "zero"), _recall(tp, tp_fn, "zero")
+
+    if p + r == 0:
+        if zero_division == "none":
+            return None
+        elif zero_division == "zero":
+            return 0.0
+
+    return 2 * p * r / (p + r)
