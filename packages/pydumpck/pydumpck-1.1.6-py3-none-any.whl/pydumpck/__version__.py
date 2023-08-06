6e9cf31bf0ad078886c856e2ec7b8efeb75570aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:20 +0000

    Add package pydumpck/1.1.6/pydumpck-1.1.6-py3-none-any.whl

---
+++ packages/pydumpck/pydumpck-1.1.6-py3-none-any.whl/pydumpck/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "pydumpck"
+__description__ = "a pyfile decomplier base on pycdc for windows."
+__keywords__ = ('pydumpck', 'decomplier', 'pe', 'elf', 'pyc', 'pyz')
+__url__ = "https://github.com/serfend/pydumpck"
+__version__ = "1.1.6"
+__author__ = "serfend"
+__author_email__ = "serfend@foxmail.com"
+__license__ = "MIT Licence"
