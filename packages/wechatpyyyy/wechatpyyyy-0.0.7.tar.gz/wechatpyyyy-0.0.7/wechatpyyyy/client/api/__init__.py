960ed90dc17265006d61a9c8c521d5b3002c72f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:04 +0000

    Add package wechatpyyyy/0.0.7/wechatpyyyy-0.0.7.tar.gz

---
+++ packages/wechatpyyyy/wechatpyyyy-0.0.7.tar.gz/wechatpyyyy-0.0.7/wechatpyyyy/client/api/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from wechatpyyyy.client.api.card import WeChatCard  # NOQA
+from wechatpyyyy.client.api.customservice import WeChatCustomService  # NOQA
+from wechatpyyyy.client.api.datacube import WeChatDataCube  # NOQA
+from wechatpyyyy.client.api.device import WeChatDevice  # NOQA
+from wechatpyyyy.client.api.group import WeChatGroup  # NOQA
+from wechatpyyyy.client.api.invoice import WeChatInvoice  # NOQA
+from wechatpyyyy.client.api.jsapi import WeChatJSAPI  # NOQA
+from wechatpyyyy.client.api.material import WeChatMaterial  # NOQA
+from wechatpyyyy.client.api.media import WeChatMedia  # NOQA
+from wechatpyyyy.client.api.menu import WeChatMenu  # NOQA
+from wechatpyyyy.client.api.merchant import WeChatMerchant  # NOQA
+from wechatpyyyy.client.api.message import WeChatMessage  # NOQA
+from wechatpyyyy.client.api.misc import WeChatMisc  # NOQA
+from wechatpyyyy.client.api.poi import WeChatPoi  # NOQA
+from wechatpyyyy.client.api.qrcode import WeChatQRCode  # NOQA
+from wechatpyyyy.client.api.scan import WeChatScan  # NOQA
+from wechatpyyyy.client.api.semantic import WeChatSemantic  # NOQA
+from wechatpyyyy.client.api.shakearound import WeChatShakeAround  # NOQA
+from wechatpyyyy.client.api.tag import WeChatTag  # NOQA
+from wechatpyyyy.client.api.template import WeChatTemplate  # NOQA
+from wechatpyyyy.client.api.user import WeChatUser  # NOQA
+from wechatpyyyy.client.api.wifi import WeChatWiFi  # NOQA
+from wechatpyyyy.client.api.wxa import WeChatWxa  # NOQA
+from wechatpyyyy.client.api.marketing import WeChatMarketing  # NOQA
+from wechatpyyyy.client.api.cloud import WeChatCloud  # NOQA
