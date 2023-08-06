8f5e050c10e6fad12800bb0c2f46e17a91cf95b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:46 +0000

    Add package cellworld-experiment-service/0.0.25/cellworld_experiment_service-0.0.25.tar.gz

---
+++ packages/cellworld-experiment-service/cellworld_experiment_service-0.0.25.tar.gz/cellworld_experiment_service-0.0.25/cellworld_experiment_service/experiment_messages.py
@@ -0,0 +1,67 @@
+from json_cpp import JsonObject
+from cellworld import *
+from datetime import datetime
+from uuid import uuid1
+
+
+class StartExperimentRequest(JsonObject):
+    def __init__(self, prefix: str = "", suffix: str = "", world: World_info = None, subject_name: str = "", duration: int = 0):
+        self.prefix = prefix
+        self.suffix = suffix
+        if not world:
+            world = World_info()
+        self.world = world
+        self.subject_name = subject_name
+        self.duration = duration
+
+
+class StartExperimentResponse(JsonObject):
+    def __init__(self, experiment_name: str = "", start_date: datetime = None, world: World_info = None, subject_name: str = "", duration: int =0):
+        self.experiment_name = experiment_name
+        if not start_date:
+            start_date = datetime.now()
+        self.start_date = start_date
+        if not world:
+            world = World_info()
+        self.world = world
+        self.subject_name = subject_name
+        self.duration = duration
+
+
+class StartEpisodeRequest(JsonObject):
+    def __init__(self, experiment_name: str = ""):
+        self.experiment_name = experiment_name
+
+
+class SetBehaviorRequest(JsonObject):
+    def __init__(self, behavior: int = 0):
+        self.behavior = behavior
+
+
+class FinishExperimentRequest(JsonObject):
+    def __init__(self, experiment_name: str = ""):
+        self.experiment_name = experiment_name
+
+
+class GetExperimentRequest(JsonObject):
+    def __init__(self, experiment_name: str = ""):
+        self.experiment_name = experiment_name
+
+
+class GetExperimentResponse(JsonObject):
+    def __init__(self, experiment_name: str = "", world_info: World_info = None, start_date: datetime = None, subject_name: str = "", duration: int = 0, remaining_time: float =0.0, episode_count: int=0):
+        self.experiment_name = experiment_name
+        if not world_info:
+            world_info = World_info()
+        self.world_info = world_info
+        if not start_date:
+            start_date = datetime.now()
+        self.start_date = start_date
+        self.subject_name = subject_name
+        self.duration = duration
+        self.remaining_time = remaining_time
+        self.episode_count = episode_count
+
+class CaptureRequest(JsonObject):
+    def __init__(self, frame: int = 0):
+        self.frame = frame
