df223db5928b9bcee06f2533314bf9e81b9dab26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:08 +0000

    Add package hicham/0.4/hicham-0.4.tar.gz

---
+++ packages/hicham/hicham-0.4.tar.gz/hicham-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+requires = []
+
+
+setup(
+    name='hicham',
+    version='0.4',
+    description='Awesome library',
+    url='',
+    author='zekri sidi mohamed hicham',
+    author_email='h.zekri@esi-sba.dz',
+    license='MIT',
+    keywords='this is me',
+    packages=[
+        "hicham"
+    ],
+    install_requires=requires,
+
+)
