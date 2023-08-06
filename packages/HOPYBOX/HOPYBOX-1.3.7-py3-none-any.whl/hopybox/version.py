faa57b3351ab57b89d358f9c9e41dcc6b217df9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:15 +0000

    Add package HOPYBOX/1.3.7/HOPYBOX-1.3.7-py3-none-any.whl

---
+++ packages/HOPYBOX/HOPYBOX-1.3.7-py3-none-any.whl/hopybox/version.py
@@ -0,0 +1,5 @@
+from platform import system,python_version
+version_number = 137
+version_type = 'default'
+#version_type = 'Beta'
+version = 'HOPYBOX 1.3.7 ({}, May 22 2022, 14:54:42)\n[Python {}] on {}\nType "help" or "license" for more information'.format(version_type,python_version(),system())
