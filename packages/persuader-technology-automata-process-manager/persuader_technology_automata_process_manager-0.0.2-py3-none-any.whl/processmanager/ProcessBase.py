6ca0de98cdde27feec510e7678f0049ea5149942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:05 +0000

    Add package persuader-technology-automata-process-manager/0.0.2/persuader_technology_automata_process_manager-0.0.2-py3-none-any.whl

---
+++ packages/persuader-technology-automata-process-manager/persuader_technology_automata_process_manager-0.0.2-py3-none-any.whl/processmanager/ProcessBase.py
@@ -0,0 +1,33 @@
+from processrepo.Process import ProcessStatus
+from processrepo.repository.ProcessRepository import ProcessRepository
+
+from processmanager.reporter.ProcessReporter import ProcessReporter
+
+
+class ProcessBase:
+
+    def __init__(self, options, market, process_name):
+        self.options = options
+        self.market = market
+        self.process_name = process_name
+        self.process_state = ProcessStatus.STOPPED
+        self.process_reporter = self.__init_process_reporter()
+
+    def __init_process_reporter(self):
+        process_repository = ProcessRepository(self.options)
+        return ProcessReporter(process_repository)
+
+    def running(self):
+        self.process_state = ProcessStatus.RUNNING
+        self.report_process_status()
+
+    def error(self):
+        self.process_state = ProcessStatus.ERROR
+        self.report_process_status()
+
+    def stopped(self):
+        self.process_state = ProcessStatus.STOPPED
+        self.report_process_status()
+
+    def report_process_status(self):
+        self.process_reporter.report(self.process_name, self.market, self.process_state)
