f7a8755cd3675fe964fe70af41e4d75bce4b1440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:48 +0000

    Add package ocean-lib/0.8.6/ocean_lib-0.8.6-py2.py3-none-any.whl

---
+++ packages/ocean-lib/ocean_lib-0.8.6-py2.py3-none-any.whl/ocean_lib/common/aquarius/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright 2022 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Ocean Aquarius module."""
+
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+from .aquarius_provider import AquariusProvider  # noqa
