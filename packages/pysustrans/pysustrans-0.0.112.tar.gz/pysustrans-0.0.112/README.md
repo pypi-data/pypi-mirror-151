a8d3677c85c605370a6bc95b2d09435b672dfdb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:05 +0000

    Add package pysustrans/0.0.112/pysustrans-0.0.112.tar.gz

---
+++ packages/pysustrans/pysustrans-0.0.112.tar.gz/pysustrans-0.0.112/README.md
@@ -0,0 +1,20 @@
+# pysustrans
+
+A set of **D**ata **A**nalytics **T**ools in **p**ython
+
+A package that allows performing common statitical analysis on transportation related data.
+
+## Documentation:
+Modules documentation can be found on [pysustrans github](https://github.com/rahul-raoniar/pysustrans/blob/main/README.md)
+
+## Installation:
+pysustrans can be installed directly using `pip`:
+```
+pip install pysustrans
+```
+
+**Python Require** `Python>=3.8`
+**Dependencies:** `numpy`, `pandas`, `scipy`, `matplotlib`, `seaborn`
+
+## Contributions:
+Contributions, bugfixes and suggestions are welcomed.
---
+++ packages/pysustrans/pysustrans-0.0.112.tar.gz/pysustrans-0.0.112/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent.resolve()
+long_description = (this_directory / "README.md").read_text(encoding="utf8")
+
+# Details
+PACKAGE_NAME = 'pysustrans'
+AUTHOR = 'Rahul Raoniar'
+EMAIL = "rahul.raoniar@outlook.com"
+URL = 'https://github.com/rahul-raoniar/pysustrans'
+DOWNLOAD_URL = 'https://pypi.org/project/pysustrans/'
+VERSION = "0.0.112"
+DESCRIPTION = 'pysustran is a statistical Python package for Transportation data analysis.'
+LONG_DESCRIPTION = long_description
+LICENSE = 'MIT'
+CLASSIFIERS = [
+        'Programming Language :: Python :: 3'
+]
+
+LONG_DESC_TYPE = "text/markdown"
+
+
+# Setting up
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn'],
+    classifiers= CLASSIFIERS,
+    python_requires=">=3.8"
+)
+
