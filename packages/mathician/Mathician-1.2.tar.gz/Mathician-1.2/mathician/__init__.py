15e1cc36e6c013f1baf6c853efdb95c1d7bc1ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:20 +0000

    Add package mathician/1.2/Mathician-1.2.tar.gz

---
+++ packages/mathician/Mathician-1.2.tar.gz/Mathician-1.2/mathician/__init__.py
@@ -0,0 +1,17 @@
+def square(num1):
+  return num1*num1
+
+def add(num1, num2):
+  return num1+num2
+
+def subtract(num1, num2):
+  return num1-num2
+
+def multiply(num1, num2):
+  return num1*num2
+
+def divide(num1, num2):
+  return num1/num2
+
+def squarerot(num1):
+  return num1/num1
