4d516ae581e4ac2a350c105c160e576dfd990525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:24 +0000

    Add package birdcall/1.0.5/birdcall-1.0.5.tar.gz

---
+++ packages/birdcall/birdcall-1.0.5.tar.gz/birdcall-1.0.5/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='birdcall',
+    version='1.0.5',
+    packages=setuptools.find_packages(),
+    url='https://github.com/lukehollenback/birdcall',
+    license='Apache License 2.0',
+    author='Luke Hollenback',
+    author_email='luke.hollenback+birdcall@gmail.com',
+    description='Module for serverless Twitter bots.',
+    install_requires=['python-dotenv', 'tweepy'],
+    python_requires='>=3.8'
+)
