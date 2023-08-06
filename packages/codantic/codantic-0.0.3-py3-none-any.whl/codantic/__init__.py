5cea5fc5a7e20dbde38aa32d0881eb1a781f3f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:22:12 +0000

    Add package codantic/0.0.3/codantic-0.0.3-py3-none-any.whl

---
+++ packages/codantic/codantic-0.0.3-py3-none-any.whl/codantic/__init__.py
@@ -0,0 +1,4 @@
+from .evaluator import IOUType, ODCocoEvaluator
+from .models import (Annotation, BBox, CaptionAnnotation, CaptionCocoDataset,
+                     Category, CocoDataset, FaceAnnotation, FaceCocoDataset,
+                     Image, ODAnnotation, ODCocoDataset)
