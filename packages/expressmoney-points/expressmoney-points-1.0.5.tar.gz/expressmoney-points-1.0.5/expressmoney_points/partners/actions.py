03cd19bd1ed85d2ac2af8f4690ceef2763a92542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:48:02 +0000

    Add package expressmoney-points/1.0.5/expressmoney-points-1.0.5.tar.gz

---
+++ packages/expressmoney-points/expressmoney-points-1.0.5.tar.gz/expressmoney-points-1.0.5/expressmoney_points/partners/actions.py
@@ -0,0 +1,29 @@
+__all__ = ('NewUserActionPoint',)
+
+from expressmoney.api import *
+
+SERVICE = 'partners'
+APP = 'actions'
+
+
+class NewUserActionCreateContract(Contract):
+    user_id = serializers.IntegerField(min_value=1)
+    http_referer = serializers.URLField()
+
+
+class NewUserActionReadContract(Contract):
+    created = serializers.DateTimeField()
+    referral = serializers.IntegerField(min_value=1)
+
+
+class NewUserActionID(ID):
+    _service = SERVICE
+    _app = APP
+    _view_set = 'profile'
+
+
+class NewUserActionPoint(ListPointMixin, CreatePointMixin, ContractPoint):
+    _point_id = NewUserActionID()
+    _create_contract = NewUserActionCreateContract
+    _read_contract = NewUserActionReadContract
+    _sort_by = 'created'
