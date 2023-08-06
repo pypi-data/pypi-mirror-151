10b30c41d866129f3a02c3fb2437dd068087eb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:14 +0000

    Add package tsetmc/0.43.2/tsetmc-0.43.2-py3-none-any.whl

---
+++ packages/tsetmc/tsetmc-0.43.2-py3-none-any.whl/tsetmc/database/__init__.py
@@ -0,0 +1,54 @@
+from json import dump as _dump
+
+from tsetmc.instruments import _DB_PATH, Instrument as _Instrument, _L18S
+from tsetmc.market_watch import market_watch_init as _market_watch_init
+
+
+# todo: add tests for this module
+
+# cs == 69: اوراق تامين مالي
+# cs == 59: اوراق حق تقدم استفاده از تسهيلات مسكن
+_CS_EXCLUSIONS = {59, 69}
+
+# see dev/tsetmc_source_files/market_watch.html
+_YVAL_EXCLUSIONS = {
+    # OraghMosharekat
+    306, 301, 706, 208, 701,
+}
+
+
+def _dump_l18s():
+    with open(_DB_PATH, 'w', encoding='utf8') as f:
+        _dump(_L18S, f, check_circular=False, ensure_ascii=False, indent='\t', sort_keys=True)
+
+
+async def add_instrument_to_db(inst: _Instrument) -> None:
+    # usually used in conjunction with Instrument.from_search
+    ins_code = inst.code
+    d = await inst.page_data()
+    # isin = df.at['کد 12 رقمی نماد', 1]
+    l18 = d['l18']
+    _L18S[l18] = ins_code, l18, d['l30']
+    _dump_l18s()
+
+
+async def update_db_using_market_watch() -> None:
+    global _L18S
+    mwi = await _market_watch_init(market_state=False, best_limits=False)
+    df = mwi['prices']
+    # flow == 3: futures market
+    df = df.query('flow != 3 and cs not in @_CS_EXCLUSIONS and yval not in @_YVAL_EXCLUSIONS')
+    glv = df.index.get_level_values
+    ins_codes = glv('ins_code')
+    l18s = glv('l18')
+    l30s = glv('l30')
+    # zip create an iterator which will be consumed on the first run
+    values = *zip(ins_codes, l18s, l30s),
+    old_len = len(_L18S)
+    _L18S |= zip(l18s, values)
+    diff = len(_L18S) - old_len
+    if diff:
+        _dump_l18s()
+        print(f'{diff} new entries were added.')
+    else:
+        print('No new entries were found.')
