5d9ae90651730c7f8a55c643aa1da60457fdab15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:38 +0000

    Add package twodarr/0.2/twodarr-0.2.tar.gz

---
+++ packages/twodarr/twodarr-0.2.tar.gz/twodarr-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='twodarr',
+    packages=['twodarr'],
+    version='0.2',
+    license='MIT',
+    author='Justin Riegerix',
+    author_email='1305jjr@gmail.com',
+    url='https://github.com/ConjunctionJustin/twodarr/tree/main',
+    download_url='https://github.com/ConjunctionJustin/twodarr/archive/refs/tags/v0.2.tar.gz',
+    description=open('README.txt').read()
+)
---
+++ packages/twodarr/twodarr-0.2.tar.gz/twodarr-0.2/twodarr/__init__.py
@@ -0,0 +1,3 @@
+from check_bound import check_bound
+from all_adj import all_adj
+from some_adj import some_adj
