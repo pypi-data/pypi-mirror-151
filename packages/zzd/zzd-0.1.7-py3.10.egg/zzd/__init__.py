39d6d6e4356fb9eab3a37981d4ee434bcdbb0643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:28 +0000

    Add package zzd/0.1.7/zzd-0.1.7-py3.10.egg

---
+++ packages/zzd/zzd-0.1.7-py3.10.egg/zzd/__init__.py
@@ -0,0 +1,10 @@
+name = "zzdlab"
+version = "0.1.3"
+
+#method to predict ppi interaction
+from zzd.methods.interolog import interolog
+
+#encode function
+from zzd.utils.scores import scores
+#from zzd.utils.encode import encode
+
