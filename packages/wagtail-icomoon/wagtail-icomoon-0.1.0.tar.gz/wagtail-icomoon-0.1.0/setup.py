385015eadf4684e44fa9395fabfa0d3599186252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:25 +0000

    Add package wagtail-icomoon/0.1.0/wagtail-icomoon-0.1.0.tar.gz

---
+++ packages/wagtail-icomoon/wagtail-icomoon-0.1.0.tar.gz/wagtail-icomoon-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wagtail_icomoon']
+
+package_data = \
+{'': ['*'], 'wagtail_icomoon': ['templates/icons/*']}
+
+install_requires = \
+['wagtail>=2.16']
+
+setup_kwargs = {
+    'name': 'wagtail-icomoon',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Seb',
+    'author_email': 'seb@neonjungle.studio',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
