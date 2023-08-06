1299513586bafade4a8c9af3f22b37b9f5700636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:00 +0000

    Add package ccxtools/0.0.14/ccxtools-0.0.14.tar.gz

---
+++ packages/ccxtools/ccxtools-0.0.14.tar.gz/ccxtools-0.0.14/src/ccxtools/exchange.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Exchange(metaclass=ABCMeta):
+
+    def __init__(self):
+        self.ccxt_inst = None
+
+    def get_balance(self, ticker):
+        return self.ccxt_inst.fetch_balance()[ticker]['total']
