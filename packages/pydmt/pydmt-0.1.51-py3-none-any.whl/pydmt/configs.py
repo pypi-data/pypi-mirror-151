ab17de8a34822fe83ecfeac19757157ed5cdfd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:09 +0000

    Add package pydmt/0.1.51/pydmt-0.1.51-py3-none-any.whl

---
+++ packages/pydmt/pydmt-0.1.51-py3-none-any.whl/pydmt/configs.py
@@ -0,0 +1,50 @@
+"""
+All configurations for pydmt
+"""
+
+
+from pytconf import Config, ParamCreator
+
+
+class ConfigSudo(Config):
+    """
+    Parameters to control whether we use 'sudo' or not
+    """
+    sudo = ParamCreator.create_bool(
+        help_string="use sudo?",
+        default=True,
+    )
+
+
+class ConfigFlow(Config):
+    """
+    Parameters to configure the flow of pydmt
+    """
+    stop_after_error = ParamCreator.create_bool(
+        help_string="Should pydmt stop after first error?",
+        default=True,
+    )
+
+
+class ConfigTarget(Config):
+    """
+    Parameters to configure what is the target of the build
+    """
+    dev = ParamCreator.create_bool(
+        help_string="Is the target to build a dev environment?",
+        default=True,
+    )
+
+
+class ConfigOutput(Config):
+    """
+    Parameters to configure the output of pydmt
+    """
+    verbose = ParamCreator.create_bool(
+        help_string="Should output be verbose?",
+        default=True,
+    )
+    print_not = ParamCreator.create_bool(
+        help_string="print out what we are not doing",
+        default=False,
+    )
