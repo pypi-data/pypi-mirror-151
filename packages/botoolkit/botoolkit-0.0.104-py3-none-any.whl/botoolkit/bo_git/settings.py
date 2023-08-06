8f0453450e16a661de114eb9148930263ee5f7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:13 +0000

    Add package botoolkit/0.0.104/botoolkit-0.0.104-py3-none-any.whl

---
+++ packages/botoolkit/botoolkit-0.0.104-py3-none-any.whl/botoolkit/bo_git/settings.py
@@ -0,0 +1,21 @@
+from pathlib import (
+    Path,
+)
+
+
+TOOL_NAME = NAMESPACE = 'bogit'
+
+TEMPLATE_CONF_FILE_PATH = (
+    Path(__file__).parent.absolute() / 'templates' / f'{TOOL_NAME}.conf'
+)
+
+CONSOLE_SCRIPTS = [
+    f'{TOOL_NAME} = botoolkit.bo_git.main:main',
+]
+
+NAMESPACES = {
+    f'{NAMESPACE}': [
+        'configure = botoolkit.bo_git.commands:ConfigureBOWebBBCommand',
+        'check closed issues remote branches = botoolkit.bo_git.commands:CheckClosedIssueRemoteBranchCommand',
+    ],
+}
