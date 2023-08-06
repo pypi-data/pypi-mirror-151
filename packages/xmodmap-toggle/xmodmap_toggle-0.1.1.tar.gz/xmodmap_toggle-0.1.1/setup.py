6cf58c1c5e1e6dc9666d2b9093d4aafff302f236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package xmodmap-toggle/0.1.1/xmodmap_toggle-0.1.1.tar.gz

---
+++ packages/xmodmap-toggle/xmodmap_toggle-0.1.1.tar.gz/xmodmap_toggle-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['xmodmap_toggle']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['xmodmap_toggle = xmodmap_toggle.main:main']}
+
+setup_kwargs = {
+    'name': 'xmodmap-toggle',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Christoffer Aakre',
+    'author_email': 'christoffer.corfield@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
