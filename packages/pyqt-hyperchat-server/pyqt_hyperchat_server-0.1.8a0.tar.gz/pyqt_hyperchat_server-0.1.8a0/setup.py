deacc13c42cabcc70ce29d473d13b1b713652a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:14 +0000

    Add package pyqt-hyperchat-server/0.1.8a0/pyqt_hyperchat_server-0.1.8a0.tar.gz

---
+++ packages/pyqt-hyperchat-server/pyqt_hyperchat_server-0.1.8a0.tar.gz/pyqt_hyperchat_server-0.1.8a0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="pyqt_hyperchat_server",
+      version="0.1.8a",
+      description="pyqt_hyperchat_server",
+      author="Wrexan",
+      author_email="fwrexan@gmail.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=['server/server_run']
+      )
