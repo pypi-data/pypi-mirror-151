33e4326819044d4b5fa5cf222fcfe341f4865789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:00 +0000

    Add package opteryx/0.0.2b6/opteryx-0.0.2b6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/opteryx/opteryx-0.0.2b6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/storage/schemes/mabel_partitions.py
@@ -0,0 +1,79 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from opteryx.storage import BasePartitionScheme
+
+
+class MabelPartitionScheme(BasePartitionScheme):
+    """
+    Handle reading data using the Mabel partition scheme.
+    """
+
+    def partition_format(self):
+        return "year_{yyyy}/month_{mm}/day_{dd}"
+
+    def filter_blobs(self, list_of_blobs):
+        def _extract_by(path):
+            parts = path.split("/")
+            for part in parts:
+                if part.startswith("by_"):
+                    return part
+
+        def _extract_as_at(path):
+            parts = path.split("/")
+            for part in parts:
+                if part.startswith("as_at_"):
+                    return part
+            return ""
+
+        # The segments are stored in folders with the prefix 'by_', as in,
+        # segments **by** field name
+        list_of_segments = {
+            _extract_by(blob) for blob in list_of_blobs if "/by_" in blob
+        }
+        chosen_segment = ""
+
+        # If we have multiple 'by_' segments, pick one - pick the first one until
+        # we start making cost-based decisions
+        if list_of_segments:
+            list_of_segments = sorted(list_of_segments)
+            chosen_segment = list_of_segments.pop()
+            # Do the pruning
+            list_of_blobs = [
+                blob for blob in list_of_blobs if f"/{chosen_segment}/" in blob
+            ]
+
+        # work out if there's an as_at part
+        as_ats = {_extract_as_at(blob) for blob in list_of_blobs if "as_at_" in blob}
+        if as_ats:
+            as_ats = sorted(as_ats)
+            as_at = as_ats.pop()
+
+            is_complete = lambda blobs, as_at: any(
+                [blob for blob in blobs if as_at + "/frame.complete" in blob]
+            )
+            is_invalid = lambda blobs, as_at: any(
+                [blob for blob in blobs if (as_at + "/frame.ignore" in blob)]
+            )
+
+            while not is_complete(list_of_blobs, as_at) or is_invalid(
+                list_of_blobs, as_at
+            ):
+                if len(as_ats) > 0:
+                    as_at = as_ats.pop()
+                else:
+                    return []
+
+            # get_logger().debug(f"Reading Frame `{as_at}`")
+            return [blob for blob in list_of_blobs if (as_at in blob)]
+
+        return list_of_blobs
