6af9076f70de4f9bf47473619e2ec86bcafab0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:00 +0000

    Add package awslambdabootstrap/0.0.2/awslambdabootstrap-0.0.2.tar.gz

---
+++ packages/awslambdabootstrap/awslambdabootstrap-0.0.2.tar.gz/awslambdabootstrap-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awslambdabootstrap',
+    version="0.0.2",
+    license='GPLv3+',
+    author="James Davies",
+    author_email='james.davies@made.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/malachantrio/aws-lambda-bootstrap',
+    keywords='example project',
+    install_requires=[
+        'boto3',
+        'structlog',
+        'python-json-logger'
+    ]
+)
