9ca6e9cc904385fda0c9fa826b788054cfa47146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:42 +0000

    Add package alembic-api/0.0.2/alembic_api-0.0.2.tar.gz

---
+++ packages/alembic-api/alembic_api-0.0.2.tar.gz/alembic_api-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alembic_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy>=1.4.0,<2.0.0', 'alembic>=1.7.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'alembic-api',
+    'version': '0.0.2',
+    'description': '',
+    'long_description': None,
+    'author': 'dotX12',
+    'author_email': 'hash@shitposting.team',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
