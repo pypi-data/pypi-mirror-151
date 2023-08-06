d6bdbe86590b6284af6914257e8b93fd132ec394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:00 +0000

    Add package qontract-reconcile/0.6.6rc69/qontract_reconcile-0.6.6rc69-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.6.6rc69-py3-none-any.whl/reconcile/openshift_saas_deploy_trigger_upstream_jobs.py
@@ -0,0 +1,26 @@
+import sys
+
+import reconcile.openshift_saas_deploy_trigger_base as osdt_base
+
+from reconcile.status import ExitCodes
+from reconcile.utils.saasherder import TriggerTypes
+from reconcile.utils.semver_helper import make_semver
+
+
+QONTRACT_INTEGRATION = "openshift-saas-deploy-trigger-upstream-jobs"
+QONTRACT_INTEGRATION_VERSION = make_semver(0, 2, 0)
+
+
+def run(dry_run, thread_pool_size=10, internal=None, use_jump_host=True):
+    error = osdt_base.run(
+        dry_run=dry_run,
+        trigger_type=TriggerTypes.UPSTREAM_JOBS,
+        integration=QONTRACT_INTEGRATION,
+        integration_version=QONTRACT_INTEGRATION_VERSION,
+        thread_pool_size=thread_pool_size,
+        internal=internal,
+        use_jump_host=use_jump_host,
+    )
+
+    if error:
+        sys.exit(ExitCodes.ERROR)
