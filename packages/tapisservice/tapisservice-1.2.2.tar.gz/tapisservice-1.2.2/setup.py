244ed44df778d7d58c88354858a3afdc9b4e42d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:36 +0000

    Add package tapisservice/1.2.2/tapisservice-1.2.2.tar.gz

---
+++ packages/tapisservice/tapisservice-1.2.2.tar.gz/tapisservice-1.2.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tapisservice',
+ 'tapisservice.tapisdjango',
+ 'tapisservice.tapisfastapi',
+ 'tapisservice.tapisflask']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pycryptodome>=3.6.0', 'tapipy>=1.1.1']
+
+setup_kwargs = {
+    'name': 'tapisservice',
+    'version': '1.2.2',
+    'description': "Python lib for interacting with an instance of the Tapis API Framework's tapisservice plugin.",
+    'long_description': 'Tapipy plugin granting Tapis service functionality using `import tapisservice`.',
+    'author': 'Joe Stubbs',
+    'author_email': 'jstubbs@tacc.utexas.edu',
+    'maintainer': 'Joe Stubbs',
+    'maintainer_email': 'jstubbs@tacc.utexas.edu',
+    'url': 'https://github.com/tapis-project/tapipy-tapisservice',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
