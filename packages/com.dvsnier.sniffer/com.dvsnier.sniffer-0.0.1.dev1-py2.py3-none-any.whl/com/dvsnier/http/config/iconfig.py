39667b9b932d2682fa22a4e3f670569b7ff6d733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:57 +0000

    Add package com.dvsnier.sniffer/0.0.1.dev1/com.dvsnier.sniffer-0.0.1.dev1-py2.py3-none-any.whl

---
+++ packages/com.dvsnier.sniffer/com.dvsnier.sniffer-0.0.1.dev1-py2.py3-none-any.whl/com/dvsnier/http/config/iconfig.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+import json
+import logging
+import time
+
+from com.dvsnier.config.cfg.configuration import Configuration
+
+
+class IConfig(object):
+    '''the iconfig class'''
+
+    cfg = None
+
+    def __init__(self):
+        super(IConfig, self).__init__()
+
+    def config(self, config):
+        'the resolve local configuration list'
+        parsing_configuration_start = time.time()
+        self.cfg = Configuration().obtain_config(config)
+        logging.debug('the current config is {}'.format(json.dumps(self.cfg, indent=4, ensure_ascii=False)))
+        parsing_configuration_end = time.time()
+        logging.info(
+            'the parsing configuration file complete, that total time consumed {:.3f} seconds '.format(parsing_configuration_end -
+                                                                                                       parsing_configuration_start))
+        return self.cfg
