ad0583b5db1ded600e892cec5e0de5cae7b7837e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:38 +0000

    Add package splink/3.0.0.dev12/splink-3.0.0.dev12.tar.gz

---
+++ packages/splink/splink-3.0.0.dev12.tar.gz/splink-3.0.0.dev12/splink/misc.py
@@ -0,0 +1,36 @@
+from math import log2
+
+
+def dedupe_preserving_order(list_of_items):
+    return list(dict.fromkeys(list_of_items))
+
+
+def escape_columns(cols):
+    return [escape_column(c) for c in cols]
+
+
+def escape_column(col):
+    return f'"{col}"'
+
+
+def prob_to_bayes_factor(prob):
+    return prob / (1 - prob)
+
+
+def prob_to_match_weight(prob):
+    return log2(prob_to_bayes_factor(prob))
+
+
+def bayes_factor_to_prob(bf):
+    return bf / (1 + bf)
+
+
+def interpolate(start, end, num_elements):
+    steps = num_elements - 1
+    step = (end - start) / steps
+    vals = [start + (i * step) for i in range(0, num_elements)]
+    return vals
+
+
+def normalise(vals):
+    return [v / sum(vals) for v in vals]
