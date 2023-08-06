4e028e844dd229467fc26490c695f0c9825d2561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:29:33 +0000

    Add package cognite-geospatial-sdk/0.10.4/cognite_geospatial_sdk-0.10.4-py3-none-any.whl

---
+++ packages/cognite-geospatial-sdk/cognite_geospatial_sdk-0.10.4-py3-none-any.whl/cognite/geospatial/shape/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2020 Cognite AS
+# flake8: noqa
+
+from .affine import AffineTransformation
+from .box import Box2
+from .geometry_definition import GeometryDefinition
+from .index import Index2, Index3, Position2, Position3
+from .surface import Surface
