398c1deeb07a83c593eeb3d1e8b49be28d1d416d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:43 +0000

    Add package notify-py/0.0.1/notify_py-0.0.1.tar.gz

---
+++ packages/notify-py/notify_py-0.0.1.tar.gz/notify_py-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='notify_py',
+    version='0.0.1',
+    description='This is a holding package for notify-py. This will soon be the main place to install.',
+    long_description='This is a holding package for notify-py. This will soon be the main place to install.',
+    url='https://github.com/ms7m/notify-py',
+    author='Mustafa Mohamed',
+    author_email='mustafa@ms7m.me',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
