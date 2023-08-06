1ce842d9ec76707b1a7c704c9fca3d9176b8c2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:27 +0000

    Add package mgbasiccalculator/0.0.2/mgbasiccalculator-0.0.2.tar.gz

---
+++ packages/mgbasiccalculator/mgbasiccalculator-0.0.2.tar.gz/mgbasiccalculator-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='mgbasiccalculator',
+  version='0.0.2',
+  description='A very python basic calculator. Created to check if we can use the vs code to upload some the files and use then words workds lot of words cant keep it short so adding more words',
+  #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Mukul Ganwal',
+  author_email='mukulganwal@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
