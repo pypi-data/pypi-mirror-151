ae2d024d3241963e3aef8c74a20c4b14a4ac8886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:05 +0000

    Add package vproces/0.0.5/vproces-0.0.5.tar.gz

---
+++ packages/vproces/vproces-0.0.5.tar.gz/vproces-0.0.5/setup.py
@@ -0,0 +1,24 @@
+# Standard library imports
+from setuptools import setup, find_packages
+
+setup(
+    name='vproces',
+    include_package_data=True,
+    packages = find_packages(),
+    version='0.0.5',
+    author='Wojciech A',
+    author_email='wa@gmail.com',
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6, <4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    install_requires=[
+        'opencv-python>4.5.5'
+    ]
+)
