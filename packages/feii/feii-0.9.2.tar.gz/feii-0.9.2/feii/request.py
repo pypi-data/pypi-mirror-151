5158da3a668857b59a9fda69e0bc52e12c15a8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:51 +0000

    Add package feii/0.9.2/feii-0.9.2.tar.gz

---
+++ packages/feii/feii-0.9.2.tar.gz/feii-0.9.2/feii/request.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# import requests
+# from feii.config import Config
+
+class Request:
+  def __init__(self,
+    request: str = '',
+  ):
+    super().__init__()
+    self.request = request
+
+  def status_request(self):
+    return self.request.status_code == 200 or self.request.status_code == 201
