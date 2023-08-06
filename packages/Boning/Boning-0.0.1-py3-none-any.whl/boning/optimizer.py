dcd29cdf5eb8fda48a85365debeb39947cf19743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:52 +0000

    Add package Boning/0.0.1/Boning-0.0.1-py3-none-any.whl

---
+++ packages/Boning/Boning-0.0.1-py3-none-any.whl/boning/optimizer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""
+@Time : 2021/7/8 11:05
+@Author : 詹荣瑞
+@File : optimizer.py
+@desc : 本代码未经授权禁止商用
+"""
+import torch
+torch.optim.SGD
+
+torch.nn.Conv2d
