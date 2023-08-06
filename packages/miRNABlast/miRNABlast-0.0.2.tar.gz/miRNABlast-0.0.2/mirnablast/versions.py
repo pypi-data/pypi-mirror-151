492dbc610248aa6872b0c7b2e640c6a67b6a9db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:02 +0000

    Add package miRNABlast/0.0.2/miRNABlast-0.0.2.tar.gz

---
+++ packages/miRNABlast/miRNABlast-0.0.2.tar.gz/miRNABlast-0.0.2/mirnablast/versions.py
@@ -0,0 +1,18 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.2",
+        "features": [
+            "Debug Setup.py error",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "Separate miRNABlast from the original Genome_work_tools",
+        ],
+    },
+]
