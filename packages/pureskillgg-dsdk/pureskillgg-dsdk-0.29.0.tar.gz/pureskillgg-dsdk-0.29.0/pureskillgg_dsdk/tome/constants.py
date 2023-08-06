05bc4926e7dd88d17aa3c0a188236cc3c7b42e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:29 +0000

    Add package pureskillgg-dsdk/0.29.0/pureskillgg-dsdk-0.29.0.tar.gz

---
+++ packages/pureskillgg-dsdk/pureskillgg-dsdk-0.29.0.tar.gz/pureskillgg-dsdk-0.29.0/pureskillgg_dsdk/tome/constants.py
@@ -0,0 +1,24 @@
+import os
+import structlog
+
+
+def get_tome_manifest_key_fs(path):
+    return os.path.join(path, "tome")
+
+
+def get_tome_path_fs(root_path, prefix, tome_name):
+    tome_parent_path = (
+        root_path if prefix is None else os.path.join(root_path, *prefix.split("/"))
+    )
+    return os.path.join(tome_parent_path, tome_name)
+
+
+def get_page_key_fs(path, subtype, page):
+    name = page[subtype]
+    return os.path.join(path, name)
+
+
+def filter_ds_reader_logs(_, __, event_dict):
+    if event_dict.get("client") == "ds_reader_fs":
+        raise structlog.DropEvent
+    return event_dict
