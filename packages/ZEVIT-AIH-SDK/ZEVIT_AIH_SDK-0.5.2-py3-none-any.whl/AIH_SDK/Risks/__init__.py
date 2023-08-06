849b2463cdd9127862b36d2dcfebb6ad5b2d50ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:27 +0000

    Add package ZEVIT-AIH-SDK/0.5.2/ZEVIT_AIH_SDK-0.5.2-py3-none-any.whl

---
+++ packages/ZEVIT-AIH-SDK/ZEVIT_AIH_SDK-0.5.2-py3-none-any.whl/AIH_SDK/Risks/__init__.py
@@ -0,0 +1,17 @@
+from .Category import Category
+from .Classification import Classification, ClassificationElement
+from .PropertyDefinition import PropertyDefinition
+from .Mitigation import Mitigation
+from .RiskAssessmentResult import RiskAssessmentResult
+from .Risk import Risk
+
+
+__all__ = [
+    'Category',
+    'Classification',
+    'ClassificationElement',
+    'PropertyDefinition',
+    'Mitigation',
+    'Risk',
+    'RiskAssessmentResult',
+]
