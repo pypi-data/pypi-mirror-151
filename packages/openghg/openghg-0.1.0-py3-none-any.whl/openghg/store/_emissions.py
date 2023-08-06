2d4a33f722b666c93ec52ebd3d4d17d62acacd16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:32:35 +0000

    Add package openghg/0.1.0/openghg-0.1.0-py3-none-any.whl

---
+++ packages/openghg/openghg-0.1.0-py3-none-any.whl/openghg/store/_emissions.py
@@ -0,0 +1,155 @@
+from pathlib import Path
+from typing import DefaultDict, Dict, Optional, Union
+from xarray import Dataset
+
+from openghg.store.base import BaseStore
+
+__all__ = ["Emissions"]
+
+
+class Emissions(BaseStore):
+    """This class is used to process emissions / flux data"""
+
+    _root = "Emissions"
+    _uuid = "c5c88168-0498-40ac-9ad3-949e91a30872"
+    _metakey = f"{_root}/uuid/{_uuid}/metastore"
+
+    @staticmethod
+    def read_file(
+        filepath: Union[str, Path],
+        species: str,
+        source: str,
+        domain: str,
+        date: Optional[str] = None,
+        high_time_resolution: Optional[bool] = False,
+        period: Optional[Union[str, tuple]] = None,
+        continuous: bool = True,
+        overwrite: bool = False,
+    ) -> Dict:
+        """Read emissions file
+
+        Args:
+            filepath: Path of emissions file
+            species: Species name
+            domain: Emissions domain
+            source: Emissions source
+            high_time_resolution: If this is a high resolution file
+            period: Period of measurements. Only needed if this can not be inferred from the time coords
+                    If specified, should be one of:
+                     - "yearly", "monthly"
+                     - suitable pandas Offset Alias
+                     - tuple of (value, unit) as would be passed to pandas.Timedelta function
+            continuous: Whether time stamps have to be continuous.
+            overwrite: Should this data overwrite currently stored data.
+        Returns:
+            dict: Dictionary of datasource UUIDs data assigned to
+        """
+        from collections import defaultdict
+        from xarray import open_dataset
+        from openghg.store import assign_data, load_metastore, datasource_lookup
+        from openghg.util import (
+            clean_string,
+            hash_file,
+            timestamp_now,
+        )
+        from openghg.store import infer_date_range
+
+        species = clean_string(species)
+        source = clean_string(source)
+        domain = clean_string(domain)
+        date = clean_string(date)
+
+        filepath = Path(filepath)
+
+        em_store = Emissions.load()
+
+        # Load in the metadata store
+        metastore = load_metastore(key=em_store._metakey)
+
+        file_hash = hash_file(filepath=filepath)
+        if file_hash in em_store._file_hashes and not overwrite:
+            print(
+                f"This file has been uploaded previously with the filename : {em_store._file_hashes[file_hash]} - skipping."
+            )
+
+        em_data = open_dataset(filepath)
+
+        # Some attributes are numpy types we can't serialise to JSON so convert them
+        # to their native types here
+        attrs = {}
+        for key, value in em_data.attrs.items():
+            try:
+                attrs[key] = value.item()
+            except AttributeError:
+                attrs[key] = value
+
+        author_name = "OpenGHG Cloud"
+        em_data.attrs["author"] = author_name
+
+        metadata = {}
+        metadata.update(attrs)
+
+        metadata["species"] = species
+        metadata["domain"] = domain
+        metadata["source"] = source
+        metadata["date"] = date
+        metadata["author"] = author_name
+        metadata["processed"] = str(timestamp_now())
+
+        # As emissions files handle things slightly differently we need to check the time values
+        # more carefully.
+        # e.g. a flux / emissions file could contain e.g. monthly data and be labelled as 2012 but
+        # contain 12 time points labelled as 2012-01-01, 2012-02-01, etc.
+
+        em_time = em_data.time
+
+        start_date, end_date, period_str = infer_date_range(
+            em_time, filepath=filepath, period=period, continuous=continuous
+        )
+
+        if date is None:
+            # Check for how granular we should make the date label
+            if "year" in period_str:
+                date = f"{start_date.year}"
+            elif "month" in period_str:
+                date = f"{start_date.year}{start_date.month:02}"
+            else:
+                date = start_date.astype("datetime64[s]").astype(str)
+
+        metadata["start_date"] = str(start_date)
+        metadata["end_date"] = str(end_date)
+
+        metadata["max_longitude"] = round(float(em_data["lon"].max()), 5)
+        metadata["min_longitude"] = round(float(em_data["lon"].min()), 5)
+        metadata["max_latitude"] = round(float(em_data["lat"].max()), 5)
+        metadata["min_latitude"] = round(float(em_data["lat"].min()), 5)
+
+        metadata["time_resolution"] = "high" if high_time_resolution else "standard"
+        metadata["time_period"] = period_str
+
+        key = "_".join((species, source, domain, date))
+
+        emissions_data: DefaultDict[str, Dict[str, Union[Dict, Dataset]]] = defaultdict(dict)
+        emissions_data[key]["data"] = em_data
+        emissions_data[key]["metadata"] = metadata
+
+        required = ("species", "source", "domain", "date")
+        lookup_results = datasource_lookup(metastore=metastore, data=emissions_data, required_keys=required)
+
+        data_type = "emissions"
+        datasource_uuids = assign_data(
+            data_dict=emissions_data,
+            lookup_results=lookup_results,
+            overwrite=overwrite,
+            data_type=data_type,
+        )
+
+        em_store.add_datasources(uuids=datasource_uuids, data=emissions_data, metastore=metastore)
+
+        # Record the file hash in case we see this file again
+        em_store._file_hashes[file_hash] = filepath.name
+
+        em_store.save()
+        metastore.close()
+
+        return datasource_uuids
---
+++ packages/openghg/openghg-0.1.0-py3-none-any.whl/openghg/store/_footprints.py
@@ -0,0 +1,213 @@
+from typing import DefaultDict, Dict, List, Optional, Union, NoReturn
+from pathlib import Path
+from pandas import Timestamp
+from xarray import Dataset
+
+from openghg.store.base import BaseStore
+
+__all__ = ["Footprints"]
+
+
+class Footprints(BaseStore):
+    """This class is used to process footprints model output"""
+
+    _root = "Footprints"
+    _uuid = "62db5bdf-c88d-4e56-97f4-40336d37f18c"
+    _metakey = f"{_root}/uuid/{_uuid}/metastore"
+
+    @staticmethod
+    def read_file(
+        filepath: Union[str, Path],
+        site: str,
+        height: str,
+        domain: str,
+        model: str,
+        metmodel: Optional[str] = None,
+        species: Optional[str] = None,
+        network: Optional[str] = None,
+        period: Optional[Union[str, tuple]] = None,
+        continuous: bool = True,
+        retrieve_met: bool = False,
+        high_spatial_res: bool = False,
+        high_time_res: bool = False,
+        overwrite: bool = False,
+        # model_params: Optional[Dict] = None,
+    ) -> Dict[str, Dict]:
+        """Reads footprints data files and returns the UUIDS of the Datasources
+        the processed data has been assigned to
+
+        Args:
+            filepath: Path of file to load
+            site: Site name
+            height: Height above ground level in metres
+            domain: Domain of footprints
+            model: Model used to create footprint (e.g. NAME or FLEXPART)
+            metmodel: Underlying meteorlogical model used (e.g. UKV)
+            species: Species name. Only needed if footprint is for a specific species e.g. co2 (and not inert)
+            network: Network name
+            period: Period of measurements. Only needed if this can not be inferred from the time coords
+            continuous: Whether time stamps have to be continuous.
+            retrieve_met: Whether to also download meterological data for this footprints area
+            high_spatial_res : Indicate footprints include both a low and high spatial resolution.
+            high_time_res: Indicate footprints are high time resolution (include H_back dimension)
+                           Note this will be set to True automatically for Carbon Dioxide data.
+            overwrite: Overwrite any currently stored data
+            high_res: ***
+
+        Returns:
+            dict: UUIDs of Datasources data has been assigned to
+        """
+        from collections import defaultdict
+        from xarray import open_dataset
+        from openghg.util import (
+            hash_file,
+            timestamp_now,
+            clean_string,
+        )
+        from openghg.store import assign_data, infer_date_range, load_metastore, datasource_lookup
+
+        filepath = Path(filepath)
+
+        site = clean_string(site)
+        network = clean_string(network)
+        height = clean_string(height)
+        domain = clean_string(domain)
+
+        fp = Footprints.load()
+
+        # Load in the metadata store
+        metastore = load_metastore(key=fp._metakey)
+
+        file_hash = hash_file(filepath=filepath)
+        if file_hash in fp._file_hashes and not overwrite:
+            print(
+                f"This file has been uploaded previously with the filename : {fp._file_hashes[file_hash]} - skipping."
+            )
+
+        fp_data = open_dataset(filepath)
+
+        # Need to read the metadata from the footprints and then store it
+        # Do we need to chunk the footprints / will a Datasource store it correctly?
+        metadata: Dict[str, Union[str, float, List[float]]] = {}
+
+        metadata["data_type"] = "footprints"
+        metadata["site"] = site
+        metadata["height"] = height
+        metadata["domain"] = domain
+        metadata["model"] = model
+
+        if species is not None:
+            metadata["species"] = clean_string(species)
+
+        if network is not None:
+            metadata["network"] = clean_string(network)
+
+        if metmodel is not None:
+            metadata["metmodel"] = clean_string(metmodel)
+
+        fp_time = fp_data.time
+
+        start_date, end_date, period_str = infer_date_range(
+            fp_time, filepath=filepath, period=period, continuous=continuous
+        )
+
+        metadata["start_date"] = str(start_date)
+        metadata["end_date"] = str(end_date)
+        metadata["time_period"] = period_str
+
+        metadata["max_longitude"] = round(float(fp_data["lon"].max()), 5)
+        metadata["min_longitude"] = round(float(fp_data["lon"].min()), 5)
+        metadata["max_latitude"] = round(float(fp_data["lat"].max()), 5)
+        metadata["min_latitude"] = round(float(fp_data["lat"].min()), 5)
+
+        # TODO: Pull out links to underlying data format into a separate format function
+        #  - high_spatial_res - data vars - "fp_low", "fp_high", coords - "lat_high", "lon_high"
+        #  - high_time_res - data vars - "fp_HiTRes", coords - "H_back"
+
+        metadata["spatial_resolution"] = "standard_spatial_resolution"
+
+        if high_spatial_res:
+            try:
+                metadata["max_longitude_high"] = round(float(fp_data["lon_high"].max()), 5)
+                metadata["min_longitude_high"] = round(float(fp_data["lon_high"].min()), 5)
+                metadata["max_latitude_high"] = round(float(fp_data["lat_high"].max()), 5)
+                metadata["min_latitude_high"] = round(float(fp_data["lat_high"].min()), 5)
+
+                metadata["spatial_resolution"] = "high_spatial_resolution"
+            except KeyError:
+                raise KeyError("Expected high spatial resolution. Unable to find lat_high or lon_high data.")
+
+        if species == "co2":
+            # Expect co2 data to have high time resolution
+            high_time_res = True
+
+        metadata["time_resolution"] = "standard_time_resolution"
+
+        if high_time_res:
+            if "fp_HiTRes" in fp_data:
+                metadata["time_resolution"] = "high_time_resolution"
+            else:
+                raise KeyError("Expected high time resolution. Unable to find fp_HiTRes data.")
+
+        metadata["heights"] = [float(h) for h in fp_data.height.values]
+        # Do we also need to save all the variables we have available in this footprints?
+        metadata["variables"] = list(fp_data.keys())
+
+        # if model_params is not None:
+        #     metadata["model_parameters"] = model_params
+
+        # Set the attributes of this Dataset
+        fp_data.attrs = {"author": "OpenGHG Cloud", "processed": str(timestamp_now())}
+
+        # This might seem longwinded now but will help when we want to read
+        # more than one footprints at a time
+        key = "_".join((site, domain, model, height))
+
+        footprint_data: DefaultDict[str, Dict[str, Union[Dict, Dataset]]] = defaultdict(dict)
+        footprint_data[key]["data"] = fp_data
+        footprint_data[key]["metadata"] = metadata
+
+        # These are the keys we will take from the metadata to search the
+        # metadata store for a Datasource, they should provide as much detail as possible
+        # to uniquely identify a Datasource
+        required = ("site", "model", "height", "domain")
+        lookup_results = datasource_lookup(metastore=metastore, data=footprint_data, required_keys=required)
+
+        data_type = "footprints"
+        datasource_uuids: Dict[str, Dict] = assign_data(
+            data_dict=footprint_data,
+            lookup_results=lookup_results,
+            overwrite=overwrite,
+            data_type=data_type,
+        )
+
+        fp.add_datasources(uuids=datasource_uuids, data=footprint_data, metastore=metastore)
+
+        # Record the file hash in case we see this file again
+        fp._file_hashes[file_hash] = filepath.name
+
+        fp.save()
+
+        metastore.close()
+
+        return datasource_uuids
+
+    def search(
+        self,
+        site: str,
+        network: str,
+        start_date: Optional[Union[str, Timestamp]],
+        end_date: Optional[Union[str, Timestamp]],
+    ) -> NoReturn:
+        """Search for a footprints from a specific site and network, return a dictionary of data
+        so the user can choose
+        """
+        raise NotImplementedError()
+
+    def retrieve(self, uuid: str, dates: str) -> NoReturn:
+        """"""
+        raise NotImplementedError()
+
+    def _get_metdata(self) -> NoReturn:
+        """This retrieves the metadata for this footprints"""
+        raise NotImplementedError()
