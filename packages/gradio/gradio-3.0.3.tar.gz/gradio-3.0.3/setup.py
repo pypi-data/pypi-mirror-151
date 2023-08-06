a46c7ae0fe964f8074acb0207768b75d7a407ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:03 +0000

    Add package gradio/3.0.3/gradio-3.0.3.tar.gz

---
+++ packages/gradio/gradio-3.0.3.tar.gz/gradio-3.0.3/setup.py
@@ -0,0 +1,42 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="gradio",
+    version="3.0.3",
+    include_package_data=True,
+    description="Python library for easily interacting with trained machine learning models",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Abubakar Abid, Ali Abid, Ali Abdalla, Dawood Khan, Ahsen Khaliq, Pete Allen, Ömer Faruk Özdemir",
+    author_email="team@gradio.app",
+    url="https://github.com/gradio-app/gradio-UI",
+    packages=["gradio"],
+    license="Apache License 2.0",
+    keywords=["machine learning", "visualization", "reproducibility"],
+    install_requires=[
+        "analytics-python",
+        "aiohttp",
+        "fastapi",
+        "ffmpy",
+        "markdown-it-py[linkify,plugins]",
+        "matplotlib",
+        "numpy",
+        "orjson",
+        "pandas",
+        "paramiko",
+        "pillow",
+        "pycryptodome",
+        "python-multipart",
+        "pydub",
+        "requests",
+        "uvicorn",
+        "Jinja2"
+    ],
+)
