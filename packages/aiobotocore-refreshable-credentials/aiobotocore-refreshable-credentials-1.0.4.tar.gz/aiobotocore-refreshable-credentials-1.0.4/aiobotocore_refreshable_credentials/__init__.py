4e4b418b2ea90e0135d6cb8178bc70cb8492de5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:15 +0000

    Add package aiobotocore-refreshable-credentials/1.0.4/aiobotocore-refreshable-credentials-1.0.4.tar.gz

---
+++ packages/aiobotocore-refreshable-credentials/aiobotocore-refreshable-credentials-1.0.4.tar.gz/aiobotocore-refreshable-credentials-1.0.4/aiobotocore_refreshable_credentials/__init__.py
@@ -0,0 +1,13 @@
+"""
+aiobotocore-refreshable-credentials
+===================================
+
+"""
+from aiobotocore_refreshable_credentials.session import get_session
+
+version = '1.0.4'
+
+__all__ = [
+    'get_session',
+    'version'
+]
