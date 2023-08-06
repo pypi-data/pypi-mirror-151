6a6c77442458023a66914a3b58afdc081268175e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:30 +0000

    Add package animegifs/0.2.0/animegifs-0.2.0.tar.gz

---
+++ packages/animegifs/animegifs-0.2.0.tar.gz/animegifs-0.2.0/animegifs/dis_utils/gifs.py
@@ -0,0 +1,8 @@
+import json
+from urllib.request import urlopen
+
+url = 'https://github.com/MarcoSa-2000/animegifs/raw/main/animegifs/dis_utils/gifs.json'
+response = urlopen(url)
+data = json.loads(response.read())
+
+attack = data['attack']
