7425336268d73e95c430ff2d76de927ade8c64f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package dislevel/2.0.3/dislevel-2.0.3-py3-none-any.whl

---
+++ packages/dislevel/dislevel-2.0.3-py3-none-any.whl/dislevel/discord/slash.py
@@ -0,0 +1,80 @@
+import os
+from typing import Optional, Union
+
+from discord import Embed, File, Interaction, Member, app_commands
+from discord.ext import commands
+from easy_pil.utils import run_in_executor
+
+from ..card import get_card
+from ..utils import (
+    get_leaderboard_data,
+    get_member_data,
+    get_member_position,
+    set_bg_image,
+)
+
+
+class LevelingSlash(commands.Cog):
+    """Leveling commands"""
+
+    def __init__(self, bot: Union[commands.Bot, commands.AutoShardedBot]):
+        self.bot = bot
+
+    @app_commands.command(description="Check rank of a user")
+    async def rank(self, interaction: Interaction, *, member: Optional[Member]):
+        """Check rank of a user"""
+        if not member:
+            member = interaction.user
+
+        user_data = await get_member_data(self.bot, member.id, interaction.guild.id)
+        user_data["position"] = await get_member_position(
+            self.bot, member.id, interaction.guild.id
+        )
+        user_data["profile_image"] = str(member.display_avatar.url)
+        user_data["name"] = str(member).split("#")[0]
+        user_data["descriminator"] = str(member).split("#")[1]
+
+        image = await run_in_executor(get_card, data=user_data)
+        file = File(fp=image, filename="card.png")
+
+        await interaction.response.send_message(file=file)
+
+    @app_commands.command(description="See the server leaderboard")
+    async def leaderboard(self, interaction: Interaction):
+        """See the server leaderboard"""
+        leaderboard_data = await get_leaderboard_data(self.bot, interaction.guild.id)
+
+        embed = Embed(title=f"Leaderboard", description="")
+        embed.set_thumbnail(url=os.environ.get("DISLEVEL_LEADERBOARD_ICON"))
+
+        position = 0
+        for data in leaderboard_data:
+            member = None
+            if self.bot.intents.members:
+                member = interaction.guild.get_member(data["member_id"])
+            else:
+                member = await interaction.guild.fetch_member(data["member_id"])
+
+            if member:
+                position += 1
+                embed.description += f"{position}. {member.mention} - {data['xp']}\n"
+
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(description="Set image of your card bg")
+    async def setbg(self, interaction: Interaction, *, url: str):
+        """Set image of your card bg"""
+        await set_bg_image(self.bot, interaction.user.id, interaction.guild.id, url)
+        await interaction.response.send_message("Background image has been updated")
+
+    @app_commands.command(description="Reset image of your card bg")
+    async def resetbg(self, interaction: Interaction):
+        """Reset image of your card bg"""
+        await set_bg_image(self.bot, interaction.user.id, interaction.guild.id, "")
+        await interaction.response.send_message(
+            "Background image has been set to default"
+        )
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(LevelingSlash(bot))
---
+++ packages/dislevel/dislevel-2.0.3-py3-none-any.whl/dislevel/utils.py
@@ -0,0 +1,211 @@
+import os
+from typing import List, Union
+
+from ._models import Field
+
+leveling_table: str = None
+
+
+async def prepare_db(database, additional_fields: List[Field] = list()) -> None:
+    """Prepares the database for leveling"""
+    leveling_table = os.environ.get("DISLEVEL_TABLE")
+
+    default_fields = [
+        Field(name="id", type="BIGSERIAL", primary=True),
+        Field(name="member_id", type="BIGINT", null=False),
+        Field(name="guild_id", type="BIGINT", null=False),
+        Field(name="xp", type="BIGINT", null=False, default=0),
+        Field(name="level", type="BIGINT", null=False, default=1),
+        Field(name="bg_image", type="TEXT"),
+    ]
+
+    all_fields = default_fields + additional_fields
+    field_schema = ""
+
+    for index, field in enumerate(all_fields):
+        statement = f"{field.name} {field.type}"
+
+        if field.primary:
+            statement += " PRIMARY KEY"
+
+        if not field.null:
+            statement += " NOT NULL"
+
+        if field.default != None:
+            statement = f"{statement} DEFAULT %r" % field.default
+
+        field_schema += statement + (", " if index < len(all_fields) - 1 else "")
+
+    schema = f"CREATE TABLE IF NOT EXISTS {leveling_table}({field_schema})"
+
+    try:
+        await database.execute(schema)
+    except Exception as e:
+        print(e)
+
+
+def get_percentage(data):
+    user_xp = data["xp"]
+    user_level = data["level"]
+    min_xp = user_level**5
+    next_level_xp = (user_level + 1) ** 5
+    xp_required = next_level_xp - min_xp
+    xp_have = user_xp - min_xp
+
+    data["percentage"] = (100 * xp_have) / xp_required
+    data["next_level_xp"] = next_level_xp
+
+    return data
+
+
+async def get_member_data(bot, member_id: int, guild_id: int) -> Union[dict, None]:
+    """Returns data of an member"""
+    database = bot.dislevel_database
+    leveling_table = os.environ.get("DISLEVEL_TABLE")
+
+    data = await database.fetch_one(
+        f"""
+        SELECT  * 
+        FROM    {leveling_table} 
+        WHERE   guild_id = :guild_id 
+        AND     member_id = :member_id
+        """,
+        {"guild_id": guild_id, "member_id": member_id},
+    )
+
+    if not data:
+        return None
+
+    return get_percentage(dict(data))
+
+
+async def get_leaderboard_data(bot, guild_id: int):
+    """Get a guild's leaderboard data"""
+    database = bot.dislevel_database
+    leveling_table = os.environ.get("DISLEVEL_TABLE")
+
+    data = await database.fetch_all(
+        f"""
+        SELECT   member_id, xp
+        FROM     {leveling_table}
+        WHERE    guild_id = :guild_id
+        ORDER BY xp
+        DESC
+        LIMIT 10
+        """,
+        {"guild_id": guild_id},
+    )
+
+    guild_data = [dict(row) for row in data]
+    return guild_data
+
+
+async def get_member_position(bot, member_id: int, guild_id: int):
+    """Get position of a member"""
+    database = bot.dislevel_database
+    leveling_table = os.environ.get("DISLEVEL_TABLE")
+
+    data = await database.fetch_all(
+        f"""SELECT  *
+             FROM   {leveling_table} 
+            WHERE   guild_id = :guild_id 
+         ORDER BY   xp 
+             DESC
+        """,
+        {"guild_id": guild_id},
+    )
+
+    position = 0
+    for row in data:
+        position += 1
+        if member_id == row["member_id"]:
+            break
+
+    return position
+
+
+async def update_xp(bot, member_id: int, guild_id: int, amount: int = 0) -> None:
+    """Increate xp of a member"""
+    database = bot.dislevel_database
+    leveling_table = os.environ.get("DISLEVEL_TABLE")
+
+    user_data = await get_member_data(bot, member_id, guild_id)
+
+    if user_data:
+        level = user_data["level"]
+        new_xp = user_data["xp"] + amount
+        new_level = int(new_xp ** (1 / 5))
+
+        await database.execute(
+            f"""
+            UPDATE  {leveling_table} 
+                SET  xp = :xp, 
+                    level = :level 
+                WHERE  member_id = :member_id 
+                AND  guild_id = :guild_id
+            """,
+            {
+                "xp": new_xp,
+                "level": new_level,
+                "guild_id": guild_id,
+                "member_id": member_id,
+            },
+        )
+
+        if new_level > level:
+            bot.dispatch(
+                "dislevel_levelup",
+                guild_id=guild_id,
+                member_id=member_id,
+                level=new_level,
+            )
+
+    else:
+        level = int(amount ** (1 / 5))
+        await database.execute(
+            f"""
+            INSERT  INTO {leveling_table}
+                    (member_id, guild_id, xp, level) 
+            VALUES  (:member_id, :guild_id, :xp, :level)
+            """,
+            {
+                "xp": amount,
+                "level": level,
+                "guild_id": guild_id,
+                "member_id": member_id,
+            },
+        )
+
+
+async def delete_member_data(bot, member_id: int, guild_id: int) -> None:
+    """Deletes a member's data. Usefull when you want to delete member's data if they leave server"""
+    database = bot.dislevel_database
+    leveling_table = os.environ.get("DISLEVEL_TABLE")
+
+    await database.executec(
+        f"""
+        DELETE  FROM {leveling_table}
+         WHERE  member_id = :member_id
+           AND  guild_id = :guild_id
+        """,
+        {
+            "guild_id": guild_id,
+            "member_id": member_id,
+        },
+    )
+
+
+async def set_bg_image(bot, member_id: int, guild_id: int, url) -> None:
+    """Set bg image"""
+    database = bot.dislevel_database
+    leveling_table = os.environ.get("DISLEVEL_TABLE")
+
+    await database.execute(
+        f"""
+        UPDATE  {leveling_table}
+        SET     bg_image = :bg_image
+        WHERE   guild_id = :guild_id
+        AND     member_id = :member_id 
+        """,
+        {"bg_image": url, "guild_id": guild_id, "member_id": member_id},
+    )
