965284dade03406465c7e05ae078d37511752296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:05 +0000

    Add package devcloudcli/1.2.12/devcloudcli-1.2.12.tar.gz

---
+++ packages/devcloudcli/devcloudcli-1.2.12.tar.gz/devcloudcli-1.2.12/dc_cli/src/scripts/edge-inference-app-services/industrial/textile-defect-classifier/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+echo "Installing Industrial-textile-defect-classifier ..."
+
+pip3 install --upgrade pip --user && pip3 install edgesoftware --user
+echo 8f4db493-0381-4391-8ec8-2af1b450ad45 | $HOME/.local/bin/edgesoftware install Industrial-textile-defect-classifier 61703650d8ecccee55019b27
