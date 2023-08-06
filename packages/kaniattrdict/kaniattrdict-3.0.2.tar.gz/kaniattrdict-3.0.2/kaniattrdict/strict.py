65dcc375920500ab7fb22c9713b5b2aaf11b5f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:46 +0000

    Add package kaniattrdict/3.0.2/kaniattrdict-3.0.2.tar.gz

---
+++ packages/kaniattrdict/kaniattrdict-3.0.2.tar.gz/kaniattrdict-3.0.2/kaniattrdict/strict.py
@@ -0,0 +1,24 @@
+"""
+A subclass of MutableAttr that has defaultdict support.
+"""
+from collections.abc import Mapping
+
+import six
+
+from .default import AttrDefault
+
+
+__all__ = ['AttrStrictKey']
+
+
+class AttrStrictKey(AttrDefault):
+    """
+    Restricting add new key.
+    """
+    def __init__(self, default_factory=None, items=None, sequence_type=tuple, pass_key=False):
+        super(AttrDefault, self).__init__(default_factory, items, sequence_type, pass_key)
+        
+    def __setitem__(self, key, value):
+        if key not in self:
+            raise KeyError("{} is not a legal key of this StricDict".format(repr(key)))
+        super(AttrStrictKey, self).__setitem__(key, value)
---
+++ packages/kaniattrdict/kaniattrdict-3.0.2.tar.gz/kaniattrdict-3.0.2/setup.py
@@ -0,0 +1,45 @@
+"""
+To install KaniAttrDict:
+
+    python setup.py install
+"""
+from setuptools import setup
+
+
+DESCRIPTION = "A dict with attribute-style access"
+
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    LONG_DESCRIPTION = DESCRIPTION
+
+
+setup(
+    name="kaniattrdict",
+    version="3.0.2",
+    author="Fx Kirin",
+    author_email="fx.kirin@gmail.com",
+    packages=("kaniattrdict",),
+    url="",
+    license="MIT License",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=(
+        "Development Status :: 7 - Inactive",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ),
+    install_requires=(
+        'six',
+    ),
+    tests_require=(
+        'nose>=1.0',
+        'coverage',
+    ),
+    zip_safe=True,
+)
