9862a5cc7ff9af42beedb0e6a04006aeb3e0f3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:34 +0000

    Add package cyberdrop-dl/2.21.8/cyberdrop-dl-2.21.8.tar.gz

---
+++ packages/cyberdrop-dl/cyberdrop-dl-2.21.8.tar.gz/cyberdrop-dl-2.21.8/cyberdrop_dl/utils/crawlers/Chibisafe_Spider.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+from colorama import Fore
+from yarl import URL
+
+from ..base_functions import bunkr_parse, log, logger, make_title_safe, ssl_context, check_direct
+from ..data_classes import DomainItem
+
+
+class ChibisafeCrawler():
+    def __init__(self, *, include_id=False):
+        self.include_id = include_id
+
+    async def fetch(self, session, url):
+        domain_obj = DomainItem(url.host, {})
+
+        if await check_direct(url):
+            if "bunkr" in url.host:
+                link = await bunkr_parse(url)
+            else:
+                link = URL(url)
+            await domain_obj.add_to_album(link=link, referral=url, title="Chibisafe Loose Files")
+            return domain_obj
+
+        await log("Starting scrape of " + str(url), Fore.WHITE)
+
+        try:
+            async with session.get(url, ssl=ssl_context) as response:
+                text = await response.text()
+                soup = BeautifulSoup(text, 'html.parser')
+
+                title = soup.select_one("h1[id=title]").get_text()
+                if title is None:
+                    title = url.name
+                elif self.include_id:
+                    titlep2 = url.name
+                    titlep2 = [s for s in titlep2 if "." in s][-1]
+                    title = title + " - " + titlep2
+                title = await make_title_safe(title.replace(r"\n", "").strip())
+
+                links = soup.select("a[class=image]")
+                for link in links:
+                    link = URL(link.get('href'))
+                    if 'bunkr' in link.host:
+                        link = await bunkr_parse(link)
+                    await domain_obj.add_to_album(title, link, url)
+
+        except Exception as e:
+            logger.debug("Error encountered while handling %s", str(url), exc_info=True)
+            await log("Error scraping " + str(url))
+            logger.debug(e)
+
+        await log("Finished scrape of " + str(url), Fore.WHITE)
+
+        return domain_obj
---
+++ packages/cyberdrop-dl/cyberdrop-dl-2.21.8.tar.gz/cyberdrop-dl-2.21.8/cyberdrop_dl/utils/scraper_helper.py
@@ -0,0 +1,174 @@
+import asyncio
+
+import aiofiles
+from yarl import URL
+
+from .crawlers.Anonfiles_Spider import AnonfilesCrawler
+from .crawlers.Chibisafe_Spider import ChibisafeCrawler
+from .crawlers.Coomer_Spider import CoomerCrawler
+from .crawlers.Cyberfile_Spider import CyberfileCrawler
+from .crawlers.Erome_Spider import EromeCrawler
+from .crawlers.Gfycat_Spider import GfycatCrawler
+from .crawlers.GoFile_Spider import GofileCrawler
+from .crawlers.Kemono_Spider import KemonoCrawler
+from .crawlers.Pixeldrain_Crawler import PixelDrainCrawler
+from .crawlers.Redgifs_Spider import RedGifsCrawler
+from .crawlers.ShareX_Spider import ShareXCrawler
+from .crawlers.Thotsbay_Spider import ThotsbayCrawler
+from .base_functions import log
+from .data_classes import CascadeItem, AsyncRateLimiter
+
+
+class ScrapeMapper():
+    def __init__(self, *, session, include_id=False, thotsbay_auth=None, separate_posts=False):
+        self.include_id = include_id
+        self.separate_posts = separate_posts
+        self.thotsbay_auth = thotsbay_auth
+        self.session = session
+        self.Cascade = CascadeItem({})
+
+        self.anonfiles_crawler = None
+        self.chibisafe_crawler = None
+        self.coomer_crawler = None
+        self.cyberfile_crawler = None
+        self.erome_crawler = None
+        self.gfycat_crawler = None
+        self.gofile_crawler = None
+        self.kemono_crawler = None
+        self.pixeldrain_crawler = None
+        self.redgifs_crawler = None
+        self.sharex_crawler = None
+        self.thotsbay_crawler = None
+
+        self.semaphore = asyncio.Semaphore(1)
+        self.mapping = {"anonfiles.com": self.Anonfiles, "bunkr.is": self.Chibisafe,
+                        "bunkr.to": self.Chibisafe, "coomer.party": self.coomer,
+                        "cyberdrop.cc": self.Chibisafe, "cyberdrop.me": self.Chibisafe,
+                        "cyberdrop.nl": self.Chibisafe, "cyberdrop.to": self.Chibisafe,
+                        "cyberfile.is": self.cyberfile, "erome.com": self.Erome,
+                        "gfycat.com": self.gfycat, "gofile.io": self.GoFile,
+                        "jpg.church": self.ShareX, "kemono.party": self.Kemono,
+                        "pixeldrain.com": self.Pixeldrain,
+                        "pixl.is": self.ShareX, "putme.ga": self.ShareX,
+                        "putmega.com": self.ShareX, "redgifs.com": self.redgifs,
+                        "socialmediagirls.com": self.ThotsBay, "thotsbay.com": self.ThotsBay}
+
+    async def Anonfiles(self, url: URL, title=None):
+        if not self.anonfiles_crawler:
+            self.anonfiles_crawler = AnonfilesCrawler(
+                include_id=self.include_id)
+        domain_obj = await self.anonfiles_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def Chibisafe(self, url: URL, title=None):
+        if not self.chibisafe_crawler:
+            self.chibisafe_crawler = ChibisafeCrawler(
+                include_id=self.include_id)
+        domain_obj = await self.chibisafe_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def coomer(self, url: URL, title=None):
+        if not self.coomer_crawler:
+            self.coomer_crawler = CoomerCrawler(include_id=self.include_id)
+        domain_obj = await self.coomer_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def cyberfile(self, url: URL, title=None):
+        if not self.cyberfile_crawler:
+            self.cyberfile_crawler = CyberfileCrawler()
+        async with self.semaphore:
+            domain_obj = await self.cyberfile_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def Erome(self, url: URL, title=None):
+        if not self.erome_crawler:
+            self.erome_crawler = EromeCrawler(include_id=self.include_id)
+        domain_obj = await self.erome_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def GoFile(self, url: URL, title=None):
+        if not self.gofile_crawler:
+            try:
+                self.gofile_crawler = GofileCrawler()
+            except:
+                await log("Couldn't start the GoFile crawler")
+                return
+        domain_obj = await self.gofile_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def Kemono(self, url: URL, title=None):
+        if not self.kemono_crawler:
+            self.kemono_crawler = KemonoCrawler(include_id=self.include_id)
+        domain_obj = await self.kemono_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def gfycat(self, url: URL, title=None):
+        if not self.gfycat_crawler:
+            self.gfycat_crawler = GfycatCrawler(
+                scraping_mapper=self, session=self.session)
+        content_url = await self.gfycat_crawler.fetch(self.session, url)
+        if content_url:
+            if title:
+                await self.Cascade.add_to_album("gfycat.com", f"{title}/gifs", content_url, url)
+            else:
+                await self.Cascade.add_to_album("gfycat.com", "gifs", content_url, url)
+
+    async def Pixeldrain(self, url: URL, title=None):
+        if not self.pixeldrain_crawler:
+            self.pixeldrain_crawler = PixelDrainCrawler()
+        domain_obj = await self.pixeldrain_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def redgifs(self, url: URL, title=None):
+        if not self.redgifs_crawler:
+            self.redgifs_crawler = RedGifsCrawler(scraping_mapper=self, session=self.session)
+        content_url = await self.redgifs_crawler.fetch(self.session, url)
+        if content_url:
+            if title:
+                await self.Cascade.add_to_album("redgifs.com", f"{title}/gifs", content_url, url)
+            else:
+                await self.Cascade.add_to_album("redgifs.com", "gifs", content_url, url)
+
+    async def ShareX(self, url: URL, title=None):
+        if not self.sharex_crawler:
+            self.sharex_crawler = ShareXCrawler(include_id=self.include_id)
+        if "jpg.church" in url.host:
+            async with AsyncRateLimiter(19):
+                domain_obj = await self.sharex_crawler.fetch(self.session, url)
+        else:
+            domain_obj = await self.sharex_crawler.fetch(self.session, url)
+        if title:
+            await domain_obj.append_title(title)
+        await self.Cascade.add_albums(domain_obj)
+
+    async def ThotsBay(self, url: URL, title=None):
+        if not self.thotsbay_crawler:
+            self.thotsbay_crawler = ThotsbayCrawler(
+                include_id=self.include_id, auth=self.thotsbay_auth,
+                scraping_mapper=self, session=self.session, separate_posts=self.separate_posts)
+        await self.Cascade.extend(await self.thotsbay_crawler.fetch(self.session, url))
+
+    async def map_url(self, url_to_map: URL, title=None):
+        for key, value in self.mapping.items():
+            if key in url_to_map.host:
+                await value(url=url_to_map, title=title)
+                return
+        await log(str(url_to_map) + " is not supported currently.")
+        async with aiofiles.open("./Unsupported_Urls.txt", mode='a') as f:
+            await f.write(str(url_to_map)+"\n")
