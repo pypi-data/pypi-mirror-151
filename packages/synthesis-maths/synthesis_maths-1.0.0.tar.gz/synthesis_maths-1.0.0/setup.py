6ad8173932d83c0fe2e3aadee12a54d84bb18eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:22 +0000

    Add package synthesis-maths/1.0.0/synthesis_maths-1.0.0.tar.gz

---
+++ packages/synthesis-maths/synthesis_maths-1.0.0.tar.gz/synthesis_maths-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0' 
+DESCRIPTION = 'Synthesis math'
+LONG_DESCRIPTION = 'Library with utterly non-unique math methods for use hopefully nowhere'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="synthesis_maths", 
+        version=VERSION,
+        author="Pieter Rossouw",
+        author_email="pieter@synthesis.co.za",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'math'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
