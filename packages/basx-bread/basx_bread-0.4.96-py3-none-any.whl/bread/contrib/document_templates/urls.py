54d59656df4e7ab88edbacf1b6a5295dd96c7739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:40 +0000

    Add package basx-bread/0.4.96/basx_bread-0.4.96-py3-none-any.whl

---
+++ packages/basx-bread/basx_bread-0.4.96-py3-none-any.whl/bread/contrib/document_templates/urls.py
@@ -0,0 +1,31 @@
+from bread import menu, views
+from bread.contrib.document_templates.models import DocumentTemplate
+from bread.contrib.document_templates.views import (
+    DocumentTemplateEditView,
+    generate_document,
+)
+from bread.utils import Link, autopath, default_model_paths, model_urlname, urls
+
+urlpatterns = [
+    *default_model_paths(
+        DocumentTemplate,
+        browseview=views.BrowseView._with(
+            rowclickaction=views.BrowseView.gen_rowclickaction("edit")
+        ),
+        editview=DocumentTemplateEditView,
+        readview=DocumentTemplateEditView,
+        addview=views.AddView._with(fields=["name", "model", "file"]),
+    ),
+    autopath(generate_document, model_urlname(DocumentTemplate, "generate_document")),
+]
+
+menu.registeritem(
+    menu.Item(
+        Link(
+            href=urls.reverse_model(DocumentTemplate, "browse"),
+            label="Document Templates",
+            iconname="document--blank",
+        ),
+        menu.Group("Document Templates", iconname="document--blank"),
+    )
+)
