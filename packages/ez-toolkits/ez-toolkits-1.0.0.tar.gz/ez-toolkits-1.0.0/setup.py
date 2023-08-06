1b0f6e4a0bdaddd95f2b03234fe827dfbaa34e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:25 +0000

    Add package ez-toolkits/1.0.0/ez-toolkits-1.0.0.tar.gz

---
+++ packages/ez-toolkits/ez-toolkits-1.0.0.tar.gz/ez-toolkits-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ez-toolkits',
+    version='1.0.0',
+    author='septvean',
+    author_email='septvean@gmail.com',
+    description='Easy Toolkits',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    python_requires='>=3.10',
+    install_requires=[
+        'matplotlib>=3.0.0',
+        'pandas>=1.0.0',
+        'requests>=2.0.0'
+    ]
+)
