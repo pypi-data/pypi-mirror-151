03945bcec5d9a8f22eff97726a6a947ec0b6d0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:56 +0000

    Add package arfs/0.3.8/arfs-0.3.8.tar.gz

---
+++ packages/arfs/arfs-0.3.8.tar.gz/arfs-0.3.8/docs/conf.py
@@ -0,0 +1,97 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../../arfs'))
+# -- Project information -----------------------------------------------------
+
+project = 'arfs'
+copyright = '2022, Thomas Bury'
+author = 'Thomas Bury'
+
+# The full version, including alpha/beta/rc tags
+release = '0.3.2'
+
+# If extensions (or modules to document with autodoc) are in another
+# directory, add these directories to sys.path here. If the directory is
+# relative to the documentation root, use os.path.abspath to make it
+# absolute, like shown here.
+sys.path.append(os.path.join(os.path.abspath(os.pardir)))
+autodoc_mock_imports = ["_tkinter"]
+
+# Get the project root dir, which is the parent dir of this
+cwd = os.getcwd()
+project_root = os.path.dirname(cwd)
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
+    'sphinx_copybutton',
+    'sphinx_rtd_theme',
+    'nbsphinx',
+    'sphinx_tabs.tabs',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+autosummary_generate = True
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_title = 'ARFS Documentation'
+html_show_sourcelink = True
+html_logo = "logo.png"
+
+# -- Napoleon settings (for numpydoc parsing) --------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#configuration
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = True
+napoleon_use_param = True
+napoleon_use_rtype = False
+napoleon_preprocess_types = True
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
---
+++ packages/arfs/arfs-0.3.8.tar.gz/arfs-0.3.8/docs/index.rst
@@ -0,0 +1,39 @@
+.. arfs documentation master file, created by
+   sphinx-quickstart on Mon Feb  7 15:05:24 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to arfs's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: User's guide
+
+   Introduction
+   Methods overview
+   modules
+
+
+.. toctree::
+   :maxdepth: 4
+   :glob:
+   :caption: Tutorials
+
+   notebooks/Classification.ipynb
+   notebooks/Regression.ipynb
+   notebooks/pre-filtering.ipynb
+   notebooks/Categoricals.ipynb
+   notebooks/Collinearity.ipynb
+   notebooks/large_data_sampling.ipynb
+   notebooks/Boruta_comparison.ipynb
+   notebooks/BorutaShap_comparison.ipynb
+   
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
