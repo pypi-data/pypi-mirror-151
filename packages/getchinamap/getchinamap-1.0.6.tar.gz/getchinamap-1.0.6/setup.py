059444ba2298fe04c802905322623d1a9bb8bffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:32:10 +0000

    Add package getchinamap/1.0.6/getchinamap-1.0.6.tar.gz

---
+++ packages/getchinamap/getchinamap-1.0.6.tar.gz/getchinamap-1.0.6/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='getchinamap',  # 包名
+      version='1.0.6',  # 版本号
+      description='get china map data from Amap',
+      long_description=long_description,
+      author='yuanzhoulvpi2017',
+      author_email='1582034172@qq.com',
+      url='https://github.com/yuanzhoulvpi2017/chinamap',
+      install_requires=['tqdm', 'pandas', 'geopandas'],
+      license='BSD License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
