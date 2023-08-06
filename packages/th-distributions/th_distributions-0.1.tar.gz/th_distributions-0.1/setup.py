b086e974659c831e44a7bd15da907f9b0837715b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:29 +0000

    Add package th-distributions/0.1/th_distributions-0.1.tar.gz

---
+++ packages/th-distributions/th_distributions-0.1.tar.gz/th_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='th_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['th_distributions'],
+      author='Travis Helm',
+      author_email='travhao@gmail.com',
+      zip_safe=False)
