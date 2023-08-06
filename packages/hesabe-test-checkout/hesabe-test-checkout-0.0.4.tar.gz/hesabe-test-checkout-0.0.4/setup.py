970a8abf311f39bd7def6b174105a14d1763f1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:40 +0000

    Add package hesabe-test-checkout/0.0.4/hesabe-test-checkout-0.0.4.tar.gz

---
+++ packages/hesabe-test-checkout/hesabe-test-checkout-0.0.4.tar.gz/hesabe-test-checkout-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+VERSION = '0.0.4'
+DESCRIPTION = 'checkout test package'
+LONG_DESCRIPTION = 'checkout test package'
+# Setting up
+setup(
+name="hesabe-test-checkout",
+version=VERSION,
+author="KL",
+author_email="sounak.karmaalab@gmail.com",
+description=DESCRIPTION,
+long_description_content_type="text/markdown",
+long_description=LONG_DESCRIPTION,
+packages=find_packages(),
+install_requires=['requests', 'pycryptodome'],
+keywords=['python', 'django', 'hesabe'],
+classifiers=[
+"Development Status :: 1 - Planning",
+"Intended Audience :: Developers",
+"Programming Language :: Python :: 3",
+"Operating System :: Unix",
+"Operating System :: MacOS :: MacOS X",
+"Operating System :: Microsoft :: Windows",
+]
+)
