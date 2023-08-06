d4c82abcd83c6a2127ebb6350479523cc5412d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:35 +0000

    Add package vprocesss/0.0.1/vprocesss-0.0.1.tar.gz

---
+++ packages/vprocesss/vprocesss-0.0.1.tar.gz/vprocesss-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vprocesss',
+    include_package_data=True,
+    packages = find_packages(),
+    version='0.0.1',
+    author='Wojciech A',
+    author_email='wa@gmail.com',
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    python_requires='>=3.6, <4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    install_requires=[
+        'opencv-python>4.5.5'
+    ]
+)
