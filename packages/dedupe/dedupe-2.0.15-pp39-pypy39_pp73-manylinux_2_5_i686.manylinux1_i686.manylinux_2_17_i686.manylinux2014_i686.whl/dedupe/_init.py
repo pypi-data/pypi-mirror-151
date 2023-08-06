e89b49fbe10e240405ef5183179941c28628d2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:52 +0000

    Add package dedupe/2.0.15/dedupe-2.0.15-pp39-pypy39_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/dedupe/dedupe-2.0.15-pp39-pypy39_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/dedupe/_init.py
@@ -0,0 +1,9 @@
+from dedupe.api import StaticDedupe, Dedupe  # noqa: F401
+from dedupe.api import StaticRecordLink, RecordLink  # noqa: F401
+from dedupe.api import StaticGazetteer, Gazetteer  # noqa: F401
+from dedupe.convenience import (  # noqa: F401
+    console_label,
+    training_data_dedupe,
+    training_data_link,
+    canonicalize,
+)
