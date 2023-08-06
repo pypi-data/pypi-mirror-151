1831799ced68c50cb5e368262ef6b11246ad6dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:26 +0000

    Add package pupyl/0.13.4/pupyl-0.13.4.tar.gz

---
+++ packages/pupyl/pupyl-0.13.4.tar.gz/pupyl-0.13.4/setup.py
@@ -0,0 +1,43 @@
+"""Pupyl pypi package setup. """
+import setuptools
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+
+setuptools.setup(
+    name="pupyl",
+    version="0.13.4",
+    author="Nelson Forte",
+    author_email="policratus@gmail.com",
+    description="🧿 Pupyl is a really fast image search "
+    "library which you can index your own (millions of) images "
+    "and find similar images in millisecond.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/policratus/pupyl",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        # Next line was added to fix an issue related to a
+        # faulty integration between keras 2.9.0 and tensorflow 2.9.0
+        # Hope to remove it as soon as possible.
+        'protobuf==3.20.1',
+        'tensorflow==2.9.0',
+        'keras==2.9.0',
+        'annoy==1.17.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        (
+            'License :: OSI Approved :: '
+            'GNU Lesser General Public License v3 (LGPLv3)'
+        ),
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Image Recognition'
+    ],
+    python_requires='>=3.8',
+    entry_points={'console_scripts': ['pupyl = pupyl.cli:pupyl']}
+)
