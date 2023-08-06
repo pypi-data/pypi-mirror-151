871cbc68c05d72726fd15b1dddcd56b745de92fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:50 +0000

    Add package wasmer-compiler-cranelift-py310/1.0.0/wasmer_compiler_cranelift_py310-1.0.0-cp310-cp310-macosx_10_7_x86_64.whl

---
+++ packages/wasmer-compiler-cranelift-py310/wasmer_compiler_cranelift_py310-1.0.0-cp310-cp310-macosx_10_7_x86_64.whl/wasmer_compiler_cranelift/__init__.py
@@ -0,0 +1,5 @@
+from .wasmer_compiler_cranelift import *
+
+__doc__ = wasmer_compiler_cranelift.__doc__
+if hasattr(wasmer_compiler_cranelift, "__all__"):
+    __all__ = wasmer_compiler_cranelift.__all__
