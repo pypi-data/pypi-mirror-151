089bb033caca7d1377b5bedd38dc2a6cd6daff5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:59 +0000

    Add package snake-and-camel/1.0.0/snake_and_camel-1.0.0.tar.gz

---
+++ packages/snake-and-camel/snake_and_camel-1.0.0.tar.gz/snake_and_camel-1.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='snake_and_camel',
+    version="1.0.0",
+    author="Thiago Lima de Oliveira",
+    description="Simple Python Functions to convert snake case to camel case and vice versa",
+    packages=["snake_and_camel"]
+)
