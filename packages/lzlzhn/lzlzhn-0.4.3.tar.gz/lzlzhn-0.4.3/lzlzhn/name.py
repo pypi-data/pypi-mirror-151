94c744509028f6395947ecfa708bae3f0be3a28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:45 +0000

    Add package lzlzhn/0.4.3/lzlzhn-0.4.3.tar.gz

---
+++ packages/lzlzhn/lzlzhn-0.4.3.tar.gz/lzlzhn-0.4.3/lzlzhn/name.py
@@ -0,0 +1,35 @@
+import time
+
+
+def name():
+    """
+    from lzlzhn import name
+    name.name()
+    please input your real name
+    """
+    name = input('请输入你的真实的名字/please input your real name' + '\n')
+    if name == '谷柏慧':
+        print('你来啦，给你准备的小惊喜还喜欢么')
+        time.sleep(1.5)
+        print('你是我最好的朋友')
+        time.sleep(1.5)
+        print('看到这个给我发微信，有惊喜哦')
+        time.sleep(1.5)
+    elif name == '卞觉':
+        print('哦吼，你来啦，卞觉')
+    elif name == '刘喜睿':
+        print('呵呵')
+    else:
+        sex = input('你的性别？是男的还是女的？（输入男/女）/Your sex？  Male or Female？')
+        if sex == 'Male' or sex == '男':
+            print('你好呀，兄弟，欢迎你使用我的第三方模块，使用guide看看我的模块都有什么功能吧！')
+            print('Hello, bro, welcome to use my third-party module, use the guide module to see what my module has!')
+        elif sex == '女' or sex == 'Female':
+            print('你好呀，美女，欢迎你使用我的第三方模块，使用guide看看我的模块都有什么功能吧！')
+            print('Hello, lady, welcome to use my third-party module, use the guide module to see what my module has!')
+        else:
+            print('你输入的不正确请检查')
+            print('Please check if you entered it incorrectly')
+            exit_1 = input('退出按任意键 /Press any key to exit')
+            if exit_1:
+                exit()
---
+++ packages/lzlzhn/lzlzhn-0.4.3.tar.gz/lzlzhn-0.4.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='lzlzhn',
+    version='0.4.3',
+    author='24K野生程序员/24K Wild Programmers',
+    author_email='liuniandexiaohuo@qq.com',
+    description='一个很会玩的第三方库,更新版本号与更新/修复个数有关',
+    long_description_content_type="""text/markdown""",
+    url='https://blog.csdn.net/l15668952150/article/details/124575394',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
