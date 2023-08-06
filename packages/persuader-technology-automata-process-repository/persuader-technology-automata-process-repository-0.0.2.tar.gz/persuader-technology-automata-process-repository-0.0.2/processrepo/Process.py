506a2aec249829454b6ecb3e737d66420e35d01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:36 +0000

    Add package persuader-technology-automata-process-repository/0.0.2/persuader-technology-automata-process-repository-0.0.2.tar.gz

---
+++ packages/persuader-technology-automata-process-repository/persuader-technology-automata-process-repository-0.0.2.tar.gz/persuader-technology-automata-process-repository-0.0.2/processrepo/Process.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+from enum import Enum
+
+
+class ProcessStatus(Enum):
+    UNKNOWN = 'unknown'
+    RUNNING = 'running'
+    ERROR = 'error'
+    STOPPED = 'stopped'
+
+    @staticmethod
+    def parse(value):
+        result = [member for name, member in ProcessStatus.__members__.items() if member.value.lower() == value.lower()]
+        return result[0]
+
+
+@dataclass
+class Process:
+    name: str
+    instant: int
+    status: ProcessStatus = field(default=ProcessStatus.UNKNOWN)
