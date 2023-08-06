86f79833c39cc2afbe02330fa3e397f0d1d04b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:49 +0000

    Add package llamirhosseinll/1.0.1/llamirhosseinll-1.0.1.tar.gz

---
+++ packages/llamirhosseinll/llamirhosseinll-1.0.1.tar.gz/llamirhosseinll-1.0.1/llamirhosseinll/pdf2text.py
@@ -0,0 +1,14 @@
+""" this module provides function to convert a pdf to text."""
+
+
+def convert(path):
+    """
+    convert the given pdf to text
+
+    parameters:
+    path(str): the path to  a pdf file.
+
+    returns:
+    str: the content of the pdf file as text.
+    """
+    print('pdf to text')
